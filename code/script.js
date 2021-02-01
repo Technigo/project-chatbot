46664a53ae07878c90c2fddba981fd8f4048f3f5
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-02-01 14:50:50 +0100

    Added User answer

--- code/script.js
+++ code/script.js
@@ -1,5 +1,6 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const flowerForm = document.getElementById('flower-form')
 
 // Global variables, if you need any, declared here
 
@@ -7,8 +8,8 @@ const chat = document.getElementById('chat')
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  if (sender === 'user') {
-    chat.innerHTML += `
+    if (sender === 'user') {
+        chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
@@ -16,8 +17,8 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-  } else if (sender === 'bot') {
-    chat.innerHTML += `
+    } else if (sender === 'bot') {
+        chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
@@ -25,18 +26,32 @@ const showMessage = (message, sender) => {
         </div>
       </section>
     `
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+    }
+    // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+    chat.scrollTop = chat.scrollHeight
 }
 
 // Starts here
-const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+const Greeting = () => {
+    showMessage(`Hello there, What's your name?`, 'bot')
+        // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
+
 // Set up your eventlisteners here
+
+
+
+flowerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.getElementById('name-input').value;
+
+    const userReply = () => {
+        showMessage(value, 'user')
+    }
+    userReply()
+});
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -44,4 +59,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(Greeting, 1000)
