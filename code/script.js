1f3df4580e29d2ae0fa0fad19bc0bf40ae1f00b4
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-02-01 14:49:12 +0100

    Added console.log to greeting-Iteration 1 done.

--- code/script.js
+++ code/script.js
@@ -8,6 +8,7 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log('I am a user')
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,6 +18,7 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
+    console.log('I am a robot')
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -33,7 +35,9 @@ const showMessage = (message, sender) => {
 // Starts here
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
+  console.log('Greeting from bot');
   // Just to check it out, change 'bot' to 'user' here 👆
+
 }
 
 // Set up your eventlisteners here
@@ -44,4 +48,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 500)
