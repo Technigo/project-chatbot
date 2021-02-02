9b4f68b807d60f12ba24bcabdad1946e14e94e20
sirihemsley <siri.hemsley@gmail.com>
2021-02-02 13:10:59 +0100

    Iteration 3, not managed to display next bot message but showing name

--- code/script.js
+++ code/script.js
@@ -1,9 +1,24 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const nameForm = document.getElementById('name-form')
+const nameInput = document.getElementById('name-input')
 
 // Global variables, if you need any, declared here
 
 // Functions declared here
+
+const handleNameInput = (event) => {
+  event.preventDefault()
+  // Store the value in a variable so we can access it after we 
+  // clear it from the input
+  const name = nameInput.value
+  showMessage(name, 'user')
+  nameInput.value = ''
+// After 1 second, show the next question by invoking the next function.
+// passing the name into it to have access to the user's name if we want
+// to use it in the next question from the bot.
+setTimeout(() => showMessage(name), 1000)
+}
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -22,22 +37,30 @@ const showMessage = (message, sender) => {
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
-          <p>message</p>
+          <p>${message}</p>
         </div>
       </section>
     `
   }
+
+
+
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'user')
+  showMessage(`Hello there, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
+} 
+
+const foodoption = () => {
+  showMessage(`Do you want pizza or pasta?`, 'bot')
 }
 
 // Set up your eventlisteners here
+nameForm.addEventListener('submit', handleNameInput)
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -45,4 +68,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 2000)
+setTimeout(greeting, 1000)
