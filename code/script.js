eea0232496077c3f1b16b04b609aef95dbb46b13
NoemiSalome <noemifrischknecht@noemis-macbook-pro.home>
2021-02-03 00:26:42 +0100

    deleteted unused code, added all the questions we want

--- code/script.js
+++ code/script.js
@@ -5,18 +5,6 @@ let value = document.getElementById('input-value'); /*is thsi correct*/
 const inputWrapper = document.getElementById("input-wrapper")
 
 let userName = "" /* when do we call this variable?*/
-/*
-
-/*
-const drink = 
-  <select id= "select">
-  <option value= "" selected disabled>Dont forget to make your choice</option>
-  <option value="nature">Nature</option>
-  <option value="mountain">Mountain</option>
-  <option value="city">City</option>
-  <option value="beach">Beach</option>
-  </select>
-*/
 
 //Question 1 -warm or cold 
 const askForWarmOrCold = () => {
@@ -42,29 +30,57 @@ document
 // the user choice to question 1 
 const askForLocation = locationChoice => {
   showMessage(`Is that so.. then let me know what view you prefer, `, "bot")
-  inputWrapper.innerHTML=`
+  inputWrapper.innerHTML= `
   <select id= "select">
   <option id= "" selected disabled> Chose from this list</option>
-  <option id="mountainOption">Mountain</option>
-  <option id="cityOption">City</option>
-  <option id="beachOption">Beach</option>
+  <option value="1" id="mountainOption">Mountain</option>
+  <option value ="2" id="cityOption">City</option>
+  <option value="3" id="beachOption">Beach</option>
   </select>
   `
-}
-/*
-   if (locationChoice === "mountainOption" || "Mountain") {
+
+ // this if statment does not yet work. locationChoice does not exist ? 
+   if (locationChoice === "1") {
     showMessage ("I wanna go all the way up", "user")
     setTimeout (() => askForDrink (), 1000) 
-   } else if (locationChoice === "cityOption") {
+   } else if (locationChoice === "2") {
     showMessage ("I wanna spend money", "user")
     setTimeout(()=> askForDrink(), 1000)
    } else {
     showMessage ("I don't wanna do anything", "user")
     setTimeout(() => askForDrink (), 1000)
-   } */
+   } 
+  } 
+ // question number 2 
+  const askForDrink = (drinkChoice) => {
+    showMessage(`What kind of drinker are you`, "bot")
+    inputWrapper.innerHTML=`
+    <button id="drinkButton1">umbrella</button>
+    <button id="drinkButton2">make it hot</button>
+    <button id="drinkButton3">sweet</button>
+    `
+  document
+    .getElementById("drinkButton1")
+    .addEventListener("click", () => {
+    showMessage ("I like those umbrella drinks", "user")
+    setTimeout (() => yourDestination(), 1000)
+    })
+  document
+    .getElementById("drinkButton2")
+    .addEventListener("click", () =>{
+    showMessage ("As long as its hot I am good", "user")
+    setTimeout (() => yourDestination (), 1000)
+  })
+  document
+    .getElementById("drinkButton3")
+    .addEventListener("click", () =>{
+    showMessage ("I like it sweet", "user")
+    setTimeout (() => yourDestination (), 1000)
+  })
+}
 
-   /*
-  if (document.getElementById("mountainOption").locationChoice ===True){
+  /*
+  if (document.getElementById('mountainOption').selected === True){
     showMessage ("I wanna go all the way up", "user")
     setTimeout (() => askForDrink (), 1000) 
   } else if (document.getElementById("cityOption").selected === True) {
@@ -74,38 +90,8 @@ const askForLocation = locationChoice => {
       showMessage ("I don't wanna do anything", "user")
     setTimeout(() => askForDrink (), 1000)
     }
-   
+  }// dont leave this bracket in the wrong place, should be after IF statement. 
   */
- 
-  /*document
-  .getElementById("mountainOption")
-  .addEventListener("click", () => {
-    showMessage ("I wanna go all the way up", "user")
-    setTimeout  
-  })
-  document
-  .getElementById("cityOption")
-  .addEventListener("click", () => {
-    showMessage ("I wanna spend money", "user")
-    setTimeout
-  })
-  document
-  .getElementById("beachOption")
-  .addEventListener("click", () => {
-    showMessage ("I don't wanna do anything", "user")
-    setTimeout
-  })
-
-*/
-
-/*
- const selectedLocation = document.getElementById("select")
-    selectedLocation.addEventListener("change", () => {
-    showMessage
-    })
- */   
-
-
 
 
 // Global variables, if you need any, declared here
@@ -166,23 +152,8 @@ chat.innerHTML += `
   `
   setTimeout (() => showMessage(`Nice to meet you ${value}! My name is Vacation Bot`, "bot"), 1000)
   setTimeout(()=> askForWarmOrCold (), 3000);
+
 });
-
-
-/*
-const handleNameInput = (event) => {
-  event.preventDefault()
-  const userName = nameInput.value
-  name.value = ""
-  showMessage(userName, "user")
-  setTimeout (() => showMessage(`Nice to meet you ${value}!`, "bot"), 1000)
-} */
-
-/*const firstResponse = () => {
-  showMessage (`My name is Vacation Bot`, 'bot')
-  inputWrapper.innerHTML=``
-  <button></Button> 
-}*/
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -191,4 +162,3 @@ const handleNameInput = (event) => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
-
