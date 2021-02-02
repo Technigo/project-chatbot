e83c80eeae4ed8c7b58c3cc8478bd7466940f27a
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-02 16:08:20 +0100

    changes

--- code/script.js
+++ code/script.js
@@ -1,10 +1,21 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat');
 const form = document.getElementById('name-form');
+const input = document.getElementById('name-input');
+const inputWrapper = document.getElementById('input-wrapper');
+
 
 // Global variables, if you need any, declared here
+//declare that dish "exists"
+var dish;
+var fill;
+
 
 // Functions declared here
+
+const botReply = (msg) =>{
+  showMessage (msg, "bot")
+} 
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -27,11 +38,28 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
+
+  
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
+// const showOptions = (message, sender) => {
+//   if (sender === 'bot') {
+//     chat.innerHTML += `
+//     <section class="bot-msg">
+//       <img src="assets/bot.png" alt="Bot" />
+//       <div class="bubble bot-bubble">
+//         <p>${message}</p>
+//       </div>
+//     </section>
+//   `
+//     chat.scrollTop = chat.scrollHeight
+
+//   }
+
 // Starts here
+
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
@@ -40,14 +68,70 @@ const greeting = () => {
 // Set up your eventlisteners here
 form.addEventListener('submit', (event)=>{
   event.preventDefault();
-  const value = document.getElementById('name-input').value;
+  const name = input.value
+  input.value = '';
 
-  showMessage(`${value}`, "user")
+//Set time, 2 second
+  const messageOne = () => {
+  showMessage(`${name}`, "user")}
+  setTimeout(messageOne, 1000)
+  
 //Set time, 3 second 
-  showMessage(`Hello ${value}`, "bot")
-  //Set time, 3 second 
-  showMessage (`What would you like to order`, "bot")
+  const messageTwo = () => {
+  showMessage(`Hello ${name}`, "bot")}
+  setTimeout(messageTwo, 2000)
+    
+ //Set time, 4 second 
+  const messageThree = () => {
+  showMessage(`What would you like to order`, "bot")}
+  setTimeout(messageThree, 3000)
+  setTimeout(() => showFoodOptions(), 4000)
+
+
 })
+
+
+const showFoodOptions = () => {
+  inputWrapper.innerHTML =`
+    <button id="hp-btn">Harry Potter</button>
+    <button id="hrg-btn">Harmione Granger</button>
+    <button id="rw-btn">Ron Weasley</button>
+   `
+   document
+    .getElementById('hp-btn')
+    .addEventListener('click', () => showMenu("Harry Potter"))
+
+    document
+    .getElementById('hrg-btn')
+    .addEventListener('click', () => showMenu("Harmione Granger"))
+
+    document
+    .getElementById('rw-btn')
+    .addEventListener('click', () => showMenu("Ron Weasley"))
+
+ }
+
+ 
+const showMenu = (dish) => {
+    botReply(`${dish} burger is a great choice, please select your favorite filling`)
+    window.dish=`${dish}` // this window method will tranform it from local variable to a global variable 
+    inputWrapper.innerHTML =`
+    <select id="select"> 
+      <option value=" " selected disabled> your filling </option>
+      <option value="meat"> Meat </option>
+      <option value="chicken"> Chicken </option>
+      <option value="fish"> Fish </option>
+      <option value="vegitarian"> Vegitarian </option>
+    </select>
+    `
+    const select = document.getElementById('select')
+    select.addEventListener('change', () => sideDish (select.value).fill) 
+  }
+    
+const sideDish = (fill) => {
+  botReply(`you have ordered ${dish} with ${fill} please chose your favorite side dish`)
+  window.fill=`${fill}`
+}
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
@@ -55,3 +139,4 @@ form.addEventListener('submit', (event)=>{
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
+
