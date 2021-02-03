53d47c581a3c2cd9fc6b055a6f79bf483de12b94
mmolliss <malin.vannesjo@gmail.com>
2021-02-03 20:44:00 +0100

    Added ;

--- code/script.js
+++ code/script.js
@@ -1,9 +1,10 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
-let userAnswer = document.getElementById('name-input')
-const nameForm = document.getElementById('name-form')
-let currentQuestion = 0;
+const chat = document.getElementById('chat');
+let userAnswer = document.getElementById('name-input');
+const nameForm = document.getElementById('name-form');
+
 // Global variables, if you need any, declared here
+let currentQuestion = 0;
 
 // Functions declared here
 
@@ -45,7 +46,7 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, what's your name?`, 'bot')
+  showMessage(`Hello there, what's your name?`, 'bot');
 }
 
 
@@ -57,23 +58,23 @@ nameForm.addEventListener("submit", (event)=> {
    const value = document.getElementById('name-input').value;
      if (currentQuestion === 0) {
      clearInput();
-     showMessage(value, 'user')
-     showMessage(`Hi ${value}, nice to meet you! I'm Bot and I'll be your waiter today. What do you feel like drinking?`, "bot")
+     showMessage(value, 'user');
+     showMessage(`Hi ${value}, nice to meet you! I'm Bot and I'll be your waiter today. What do you feel like drinking?`, "bot");
      increaseCounter();
    } else if (currentQuestion === 1) {
      clearInput();
-     showMessage(value, 'user')
-     showMessage(`Nice! ${value} is a great choice! What do you feel like eating with that?`, "bot")
+     showMessage(value, 'user');
+     showMessage(`Nice! ${value} is a great choice! What do you feel like eating with that?`, "bot");
      increaseCounter();
    } else if (currentQuestion === 2) {
      clearInput();
-     showMessage(value, 'user')
-     showMessage('Yummy! How about something sweet, what would you like for dessert?', "bot")
+     showMessage(value, 'user');
+     showMessage('Yummy! How about something sweet, what would you like for dessert?', "bot");
      increaseCounter();
    } else if (currentQuestion === 3) {
      clearInput();
-     showMessage(value, 'user')
-     showMessage(`${value} sounds delicious, and here's the bill!`, "bot")
+     showMessage(value, 'user');
+     showMessage(`${value} sounds delicious, and here's the bill!`, "bot");
      increaseCounter();
    }
   });
@@ -83,4 +84,4 @@ nameForm.addEventListener("submit", (event)=> {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 500)
+setTimeout(greeting, 500);
