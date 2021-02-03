3c7f332d7c550c269cdd06c70aa0a5d5350d204e
mmolliss <malin.vannesjo@gmail.com>
2021-02-03 20:42:00 +0100

    fixed bot and added counter

--- code/script.js
+++ code/script.js
@@ -2,62 +2,18 @@
 const chat = document.getElementById('chat')
 let userAnswer = document.getElementById('name-input')
 const nameForm = document.getElementById('name-form')
-const value = document.getElementById('name-input')
+let currentQuestion = 0;
 // Global variables, if you need any, declared here
 
 // Functions declared here
-const beerQuestion = () => {
-  if (value === "ipa") {
-    showMessage(value, 'user')
-    showMessage('Yummy', "bot") 
-  } else if (value === "lager") {
-    showMessage (value, 'user')
-    showMessage('Great choice!', "bot")
-  } else if (value === "stout") {
-    showMessage (value, 'user')
-    showMessage ('Coming right up', "bot")
-  } else { 
-    showMessage (value,'user')
-    showMessage ('Sorry, we are out of that', "bot")
-  }
-}
-
-const wineQuestion = () => {
-  if (value === "meat") {
-    showMessage(value, 'user')
-    showMessage('I suggest you get a bottle of red wine!', "bot") 
-  } else if (value === "fish") {
-    showMessage (value, 'user')
-    showMessage('Get some white wine!', "bot")
-  } else if (value === "vegetarian") {
-    showMessage (value, 'user')
-    showMessage ('Rose will go great with that!', "bot")
-  } else { 
-    showMessage (value,'user')
-    showMessage ('Sorry, we are out of that', "bot")
-  }
-}
-
-const bubblesQuestion = () => {
-  if (value === "cava") {
-    showMessage(value, 'user')
-    showMessage("I'll get you a bottle straight away!", "bot") 
-    console.log(value);
-  } else if (value === "Prosecco") {
-    showMessage (value, 'user')
-    showMessage('Yumm italian!', "bot")
-  } else if (value === "champagne") {
-    showMessage (value, 'user')
-    showMessage ('What a celebration!', "bot")
-  } else { 
-    showMessage (value,'user')
-    showMessage ('Sorry, we are out of that', "bot")
-  }
-}
 
 const clearInput = () => {
   const clearText = document.getElementById("name-form");
   clearText.reset();
+}
+
+const increaseCounter = () => {
+  currentQuestion = currentQuestion + 1
 }
 
 // This function will add a chat bubble in the correct place based on who the sender is
@@ -89,37 +45,37 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, what do you fancy today? Beer, Wine or Bubbles?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage(`Hello there, what's your name?`, 'bot')
 }
 
-// Set up your eventlisteners here
 
+// Set up your eventlisteners here
 nameForm.addEventListener("submit", (event)=> {
   event.preventDefault();
   
    // Assign input value to variable
    const value = document.getElementById('name-input').value;
-      if (value === "beer") {
-      clearInput()
-      showMessage(value, 'user')
-      showMessage('What do you prefer: IPA, Lager or Stout?', "bot")
-      beerQuestion();
-    } else if (value === "wine") {
-      clearInput()
-      showMessage(value, 'user')
-      showMessage('What are you eating: meat, fish or vegetarian?', 'bot')
-      wineQuestion();
-    } else if (value === "bubbles") {
-      clearInput()
-      showMessage(value, 'user')
-      showMessage('What do you prefer: Cava, Prosecco or Champagne?', 'bot')
-      bubblesQuestion();
-    } else {
-      clearInput()
-      showMessage(value, 'user')
-      showMessage("We don't sell that!", 'bot')
-    }    
+     if (currentQuestion === 0) {
+     clearInput();
+     showMessage(value, 'user')
+     showMessage(`Hi ${value}, nice to meet you! I'm Bot and I'll be your waiter today. What do you feel like drinking?`, "bot")
+     increaseCounter();
+   } else if (currentQuestion === 1) {
+     clearInput();
+     showMessage(value, 'user')
+     showMessage(`Nice! ${value} is a great choice! What do you feel like eating with that?`, "bot")
+     increaseCounter();
+   } else if (currentQuestion === 2) {
+     clearInput();
+     showMessage(value, 'user')
+     showMessage('Yummy! How about something sweet, what would you like for dessert?', "bot")
+     increaseCounter();
+   } else if (currentQuestion === 3) {
+     clearInput();
+     showMessage(value, 'user')
+     showMessage(`${value} sounds delicious, and here's the bill!`, "bot")
+     increaseCounter();
+   }
   });
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -128,6 +84,3 @@ nameForm.addEventListener("submit", (event)=> {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 500)
-
-// delete input after sent
-// add more (nested?) questions
