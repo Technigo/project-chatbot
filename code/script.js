d050e59d9dde58343205223a51cf66996e7e153c
Mabel Liaw <Owner@Mabels-MacBook-Pro.local>
2021-02-03 20:37:49 +0100

    create step, input

--- code/script.js
+++ code/script.js
@@ -1,42 +1,23 @@
-// All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
-
-// Global variables, if you need any, declared here
-
-// Functions declared here
-
-// This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
-  if (sender === 'user') {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `
-  } else if (sender === 'bot') {
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
-
-// Starts here
-const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
-
+//const
+const form = document.getElementById('form');
+const chat = document.getElementById('chat');
+//global variables/scope('bubble within bubble, biggest bubble')
+let step = 1;
 // Set up your eventlisteners here
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(step);
+
+    //input
+    const value = document.getElementById('input-value').value;
+
+    Step = 2;
+    chat.innerHTML += ` <div><p>Hi ${value}, Nice Name you got!</p></div>`;
+
+});
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -44,4 +25,3 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
