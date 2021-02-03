bfcd199af2ef5cf0b9961479ea3783386f10d427
Mir Albertioni <fethullah.dincer@gmail.com>
2021-02-03 14:56:32 +0100

    groupwork

--- MyTest/test copy.js
+++ MyTest/test copy.js
@@ -1,19 +1,27 @@
 const form = document.getElementById('form');
 const chat = document.getElementById('chat');
-const send = document.getElementById('send')
-const input = document.getElementById('input-value')
+const send = document.getElementById('send');
+const input = document.getElementById('input-value'); //hello
 
 let questionNumber = 1; 
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent default 
+
+    // Assign input value to variable
+    const value = document.getElementById('input-value').value; // Vi skriver .value så vi når själva värdet, istället för t.ex. .length, som är längden bokstäver som ett heltal 
+    nextQuestion(value);
+
+}
+);  
 
 const nextQuestion = (message) => {
     /*console.log('questionNumber', questionNumber) */
   
     if (questionNumber === 1) {
       input.value = ''
-      setTimeout(() => questionOne(message), 1000)
-      
-    } else if (questionNumber === 2) {
-        
+      setTimeout(() => questionOne(message), 1000) //timeout innebär att den väntar på att gå vidare
+    } else if (questionNumber === 2) { 
         input.value = ''
       setTimeout(() => questionTwo(message), 1000)
     } else if (questionNumber === 3) {
@@ -29,28 +37,13 @@ const nextQuestion = (message) => {
       }
   }
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent default 
 
-    // Assign input value to variable
-    const value = document.getElementById('input-value').value;
-    nextQuestion(value);
-
-}
-);  
-
-
-/*send.addEventListener('click', () => nextQuestion()) // Denna är SUPERVIKTIG, det ÄR DENNA SOM SÄTTER IGÅNG FRÅGORNA
-input.addEventListener('keypress', (event) => { nextQuestion()
-}) */
-
-
-const questionOne = () => { 
+const questionOne = (name) => { 
     questionNumber++;
 
     chat.innerHTML += `
         <div>
-            <p> Welcome to our booking system!</p>
+            <p> Welcome to our booking system, ${name}!</p>
             <p> 
             Please select what floor you would like to book your room in number, 
             (1 for floor 1, 2 for floor 2, 3 for floor 3)
@@ -63,20 +56,22 @@ const questionOne = () => {
     
 
 
-const questionTwo = (value) => {
+const questionTwo = (floor) => {
 
-    console.log(value);
-    questionNumber++;
-    if (value == 1) {
+    console.log(floor);
+    
+    if (floor == 1) {
+        questionNumber++;
         chat.innerHTML += `
         <div>
-            <p> Thank you. You have selected 1st Floor. Now please select what room </p>
+            <p> Thank you. You have selected ${floor}st Floor. Now please select what room </p>
             <p> For Room in floor 1 Zebra Press: z</p>
             <p> For Room in floor 1 Lions Press: l</p>
             <p> For Room in floor 1 tiger Press: t</p>
         </div>
         `;
-    } else if (value == 2) {
+    } else if (floor == 2) {
+        questionNumber++;
         chat.innerHTML += `
             <div>
                 <p> Thank you. You have selected 2nd Floor. Now please select what room </p>
@@ -86,8 +81,9 @@ const questionTwo = (value) => {
             </div>
         `;
     
-    } else if (value == 3) {
-            chat.innerHTML += `
+    } else if (floor == 3) {
+        questionNumber++;
+            chat.innerHTML = `
             <div>
                 <p> Thank you. You have selected 3rd Floor. Now please select what room </p>
                 <p> For Room in floor 3 Zebra Press: z</p>
@@ -99,18 +95,19 @@ const questionTwo = (value) => {
     } else {
             chat.innerHTML += `
             <div>
-                <p>Select a number vvvvvbetween 1-3</p>
+                <p>Select a number vvvvvbetween 1-3 QUESTION 2</p>
             </div>
         `;}
         
-    }
+}
 
     
 
 const questionThree = (value) => {
     console.log(value);
-    questionNumber++;
+    
         if (value == "z") {
+            questionNumber++;
             chat.innerHTML += `
                 <div>
                     <p> QUESTION 3 </p>
@@ -120,6 +117,7 @@ const questionThree = (value) => {
                 </div>
             `;
         } else if (value == "l") {
+            questionNumber++;
             chat.innerHTML += `
                 <div>
                     <p> QUESTION 3 </p>
@@ -130,6 +128,7 @@ const questionThree = (value) => {
             `;
         
         } else if (value == "t") {
+            questionNumber++;
             chat.innerHTML += `
                 <div>
                     <p> QUESTION 3 </p>
