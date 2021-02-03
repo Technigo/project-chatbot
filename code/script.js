7ad80859b69b9093ad1ef028c337cc7396655bdb
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-02-03 09:54:07 +0100

    Changed name and greeting

--- code/script.js
+++ code/script.js
@@ -1,10 +1,13 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat');
 const form = document.getElementById('name-form');
+const inputWrapper = document.getElementById('input-wrapper');
+const input = document.getElementById('name-input');
 
 // Global variables, if you need any, declared here
 
 // Functions declared here
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -30,15 +33,20 @@ const showMessage = (message, sender) => {
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
-}
+};
+
+
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  bot(`Hello sweet tooth, what's your name?`)
   // Just to check it out, change 'bot' to 'user' here 👆
-}
+};
+
+
 
 // Set up your eventlisteners here
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -46,4 +54,5 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+
+setTimeout(greeting, 1000);
