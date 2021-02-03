05d1ea6c0f8d9569ae2ee09b54dcf29c8b5c2959
sirihemsley <siri.hemsley@gmail.com>
2021-02-03 16:56:58 +0100

    Iteration 4, basic structure with 2 questions and button answers

--- code/script.js
+++ code/script.js
@@ -1,24 +1,21 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const nameForm = document.getElementById('name-form')
-const nameInput = document.getElementById('name-input')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // Global variables, if you need any, declared here
+let stepNumber = 1
 
 // Functions declared here
 
-const handleNameInput = (event) => {
-  event.preventDefault()
-  // Store the value in a variable so we can access it after we 
-  // clear it from the input
-  const name = nameInput.value
-  showMessage(name, 'user')
-  nameInput.value = ''
-// After 1 second, show the next question by invoking the next function.
-// passing the name into it to have access to the user's name if we want
-// to use it in the next question from the bot.
-setTimeout(() => showMessage(name), 1000)
+const botAnswer = (message) => {
+  showMessage (message, 'bot')
 }
+
+const userAnswer = (message) => {
+  showMessage (message, 'user') 
+}
+
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -43,25 +40,74 @@ const showMessage = (message, sender) => {
     `
   }
 
-
-
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
+const nextStep = (message) => {
+  console.log( 'stepNumber', stepNumber)
+
+  if (answerNumber === 1) {
+    userAnswer (message)
+    setTimeout (() => showVibe(message),1000)
+  } else if (answerNumber === 2) {
+    userAnswer (message)
+    setTimeout (() => showOutfit(message),1000)
+  }
+}
+
+
+
+
 // Starts here
-const greeting = () => {
-  showMessage(`Hello, where's the party at?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+const showPlace = (place) => {
+  answerNumber = 1
+  botAnswer(`Hello, where's the party at?`)
+
+  inputWrapper.innerHTML = `
+    <button id="nightclubBtn">Nightclub</button>
+    <button id="cocktailBtn">Cocktail bar</button>
+  `
+  document.getElementById('nightclubBtn')
+  .addEventListener('click', () => nextStep('Nightclub'))
+  document.getElementById('cocktailBtn')
+  .addEventListener('click', () => nextStep('Cocktail bar'))
 } 
 
-const foodoption = () => {
-  showMessage(`Do you want pizza or pasta?`, 'bot')
+const showVibe = (place) => {
+  answerNumber++
+
+  if (place === 'nightclub') {
+    botAnswer(`Are we talking Berghain or Studio54?`)
+
+    inputWrapper.innerHTML = `
+    <button id="berghainBtn">Berghain</button>
+    <button id="studio54Btn">Studio 54</button>
+  `
+  document.getElementById('berghainBtn')
+  .addEventListener('click', () => nextStep('Berghain'))
+  document.getElementById('studio54Btn')
+  .addEventListener('click', () => nextStep('Studio54'))
+
+  } else {
+    botAnswer(`In the mood for Cosmopolitan or Old Fashioned?`)
+
+    inputWrapper.innerHTML = `
+    <button id="cosmoBtn">Cosmopolitan</button>
+    <button id="oldfashionBtn">Old Fashioned</button>
+  `
+  document.getElementById('cosmoBtn')
+  .addEventListener('click', () => nextStep('Cosmopolitan'))
+  document.getElementById('oldfashionBtn')
+  .addEventListener('click', () => nextStep('Old fashioned'))
+  }
 
 }
 
+
+
 // Set up your eventlisteners here
-nameForm.addEventListener('submit', handleNameInput)
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -69,4 +115,4 @@ nameForm.addEventListener('submit', handleNameInput)
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(showPlace, 1000)
