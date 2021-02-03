40edd79f78cde10de6f719c458dcd32b2624359f
Irina <jekabsone.irina@gmail.com>
2021-02-03 16:57:50 +0100

    First two questions

--- code/script.js
+++ code/script.js
@@ -1,7 +1,11 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+const chat = document.getElementById("chat")
+const form = document.getElementById("name-form")
+const nameInput = document.getElementById("name-input")
+const inputWrapper = document.getElementById("input-wrapper")
 
 // Global variables, if you need any, declared here
+let question = 1;
 
 // Functions declared here
 
@@ -31,12 +35,74 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
+// First message
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage(`Welcome to Technigo Cinema! What is your name?`, 'bot')
 }
 
+//Question 1
+const handleNameInput = (event) => {
+  event.preventDefault()
+  const userName = nameInput.value
+  nameInput.value = ""
+  showMessage (userName, "user")
+  setTimeout (() => showMessage (`Hi ${userName}!`, 'bot'), 1000)
+  setTimeout (() => askMovieOptions (`What kind of movie would you like to see today? Choose from the list below.`, 'bot'), 2000)
+  };
+
+//Question 2
+const askMovieOptions = () => {
+  showMessage (`What kind of movie would you like to see today? Choose from the list below: `, 'bot')
+  inputWrapper.innerHTML=`
+<button id="animation">Animation</button>
+<button id="action">Action</button>
+<button id="comedy">Comedy</button>
+<button id="horror">Horror</button>`
+
+document.getElementById("animation").addEventListener("click", () => {
+  showMessage("Great choice!", "bot")
+  setTimeout (() => inputWrapper.innerHTML=`
+<button id="the-invisible-girl">The Invisible Girl</button>
+<button id="we-bare-bears">We Bare Bears: The Movie</button>
+<button id="the-croods">The Croods: A New Age</button>
+`, 1000)
+})
+
+document.getElementById("action").addEventListener("click", () => {
+  showMessage("Great choice! Go ahead and pick the best one.", "bot")
+  setTimeout (() => inputWrapper.innerHTML=`
+<button id="honest-thief">Honest Thief</button>
+<button id="tenet">Tenet</button>
+<button id="bloodshot">Bloodshot</button>
+`, 1000)
+})
+
+document.getElementById("comedy").addEventListener("click", () => {
+  showMessage("Great choice! Go ahead and pick the best one.", "bot")
+  setTimeout (() => inputWrapper.innerHTML=`
+<button id="babyteeth">Babyteeth</button>
+<button id="downhill">Downhill</button>
+<button id="friendsgiving">Friendsgiving</button>
+`, 1000)
+})
+
+document.getElementById("horror").addEventListener("click", () => {
+  showMessage("Great choice! Go ahead and pick the best one.", "bot")
+  setTimeout (() => inputWrapper.innerHTML=`
+<button id="antebellum">Antebellum</button>
+<button id="brahms">Brahms: The Boy 2</button>
+<button id="come-play">Come Play</button>
+`, 1000)
+})
+
+};
+
+//Question 3
+
+
 // Set up your eventlisteners here
+form.addEventListener('submit', handleNameInput)
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
