b07f31d08a00aff0af7b762dc73ae78f86c75864
Irina <jekabsone.irina@gmail.com>
2021-02-04 16:13:05 +0100

    Question nr 3

--- code/script.js
+++ code/script.js
@@ -1,13 +1,31 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById("chat")
 const form = document.getElementById("name-form")
-const nameInput = document.getElementById("name-input")
+const input = document.getElementById("name-input")
 const inputWrapper = document.getElementById("input-wrapper")
 
 // Global variables, if you need any, declared here
-let question = 1;
+let question = 0;
+let userName = ""
+let ticketNumber = ""
 
 // Functions declared here
+// This function will lead to the next question
+const nxtQuestion = (answer) => {
+  usrInput(answer)
+    if (questionNumber === 1) {
+      handleNameInput(answer)
+      input = ""
+    } 
+    else if (questionNumber === 2) {
+      askMovieOptions(answer)
+      input =""
+    } 
+    else if (questionNumber === 3) {
+      askForTickets(answer)
+      input =""
+    } 
+  }
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -37,71 +55,137 @@ const showMessage = (message, sender) => {
 // Starts here
 // First message
 const greeting = () => {
+  questionNumber = 1;
+  console.log(questionNumber)
   showMessage(`Welcome to Technigo Cinema! What is your name?`, 'bot')
+}
+// This means the greeting function will be called one second after the website is loaded.
+setTimeout(greeting, 1000)
+
+//Answer from the user
+const usrInput = (answer) => {
+    showMessage(answer, 'user');
 }
 
 //Question 1
-const handleNameInput = (event) => {
-  event.preventDefault()
-  const userName = nameInput.value
-  nameInput.value = ""
-  showMessage (userName, "user")
-  setTimeout (() => showMessage (`Hi ${userName}!`, 'bot'), 1000)
-  setTimeout (() => askMovieOptions (`What kind of movie would you like to see today? Choose from the list below.`, 'bot'), 2000)
+const handleNameInput = () => {
+  questionNumber = 2
+  console.log(questionNumber)
+  let userName = document.getElementById('name-input').value;
+  setTimeout (() => showMessage(`Hi ${userName}!`, 'bot'), 1000)
+  setTimeout (() => askMovieOptions (), 2000)
   };
 
 //Question 2
-const askMovieOptions = () => {
-  showMessage (`What kind of movie would you like to see today? Choose from the list below: `, 'bot')
-  inputWrapper.innerHTML=`
-<button id="animation">Animation</button>
-<button id="action">Action</button>
-<button id="comedy">Comedy</button>
-<button id="horror">Horror</button>`
+const askMovieOptions = (question) => {
+  questionNumber = 3
+  console.log(questionNumber)
+  showMessage(`What kind of movie would you like to see today? Choose from the list below: `, 'bot')
+  inputWrapper.innerHTML = `
+    <button id="animation">Animation</button>
+    <button id="action">Action</button>
+    <button id="comedy">Comedy</button>
+    <button id="horror">Horror</button>`
 
-document.getElementById("animation").addEventListener("click", () => {
-  showMessage("Great choice!", "bot")
-  setTimeout (() => inputWrapper.innerHTML=`
-<button id="the-invisible-girl">The Invisible Girl</button>
-<button id="we-bare-bears">We Bare Bears: The Movie</button>
-<button id="the-croods">The Croods: A New Age</button>
-`, 1000)
-})
+  document.getElementById("animation").addEventListener("click", () => {
+    showMessage("Great choice! Go ahead and pick the best one.", "bot")
+    setTimeout(() => {
+      whatMovie()
+    }, 1000)
+    let whatMovie = () => {
+      inputWrapper.innerHTML =  `
+      <button class="choice-btn" id="the-invisible-girl">The Invisible Girl</button>
+      <button class="choice-btn" id="we-bare-bears">We Bare Bears: The Movie</button>
+      <button class="choice-btn" id="the-croods">The Croods: A New Age</button>`
+     
+      const choiceBtn = document.querySelectorAll(".choice-btn")
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
+      }
+    }
+  })
 
-document.getElementById("action").addEventListener("click", () => {
-  showMessage("Great choice! Go ahead and pick the best one.", "bot")
-  setTimeout (() => inputWrapper.innerHTML=`
-<button id="honest-thief">Honest Thief</button>
-<button id="tenet">Tenet</button>
-<button id="bloodshot">Bloodshot</button>
-`, 1000)
-})
+  document.getElementById("action").addEventListener("click", () => {
+    showMessage("Great choice! Go ahead and pick the best one.", "bot")
+    setTimeout(() => {
+      whatMovie()
+    }, 1000)
+      let whatMovie = () => {
+      inputWrapper.innerHTML =  `
+      <button class="choice-btn" id="honest-thief">Honest Thief</button>
+      <button class="choice-btn" id="tenet">Tenet</button>
+      <button class="choice-btn" id="bloodshot">Bloodshot</button>`
+    
+      const choiceBtn = document.querySelectorAll(".choice-btn")
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
+      }
+    }
+  })
 
-document.getElementById("comedy").addEventListener("click", () => {
-  showMessage("Great choice! Go ahead and pick the best one.", "bot")
-  setTimeout (() => inputWrapper.innerHTML=`
-<button id="babyteeth">Babyteeth</button>
-<button id="downhill">Downhill</button>
-<button id="friendsgiving">Friendsgiving</button>
-`, 1000)
-})
+  document.getElementById("comedy").addEventListener("click", () => {
+    showMessage("Great choice! Go ahead and pick the best one.", "bot")
+    setTimeout(() => {
+      whatMovie()
+    }, 1000)
+      let whatMovie = () => {
+      inputWrapper.innerHTML = `
+        <button class="choice-btn" id="babyteeth">Babyteeth</button>
+        <button class="choice-btn" id="downhill">Downhill</button>
+        <button class="choice-btn" id="friendsgiving">Friendsgiving</button>`
+    
+      const choiceBtn = document.querySelectorAll(".choice-btn")
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
+      }
+    }
+  })
 
-document.getElementById("horror").addEventListener("click", () => {
-  showMessage("Great choice! Go ahead and pick the best one.", "bot")
-  setTimeout (() => inputWrapper.innerHTML=`
-<button id="antebellum">Antebellum</button>
-<button id="brahms">Brahms: The Boy 2</button>
-<button id="come-play">Come Play</button>
-`, 1000)
-})
+  document.getElementById("horror").addEventListener("click", () => {
+    showMessage("Great choice! Go ahead and pick the best one.", "bot")
+    setTimeout(() => {
+      whatMovie()
+    }, 1000)
+    let whatMovie = () => {
+      inputWrapper.innerHTML = `
+        <button class="choice-btn" id="antebellum">Antebellum</button>
+        <button class="choice-btn" id="brahms">Brahms: The Boy 2</button>
+        <button class="choice-btn" id="come-play">Come Play</button>
+        `
+      const choiceBtn = document.querySelectorAll(".choice-btn")
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
+      }
+    }
+  })
 
 };
 
 //Question 3
+const askForTickets = (question) => {
+  questionNumber = 4
+  console.log(questionNumber)
+  showMessage (`How many tickets would you like to have?`, 'bot')
+  inputWrapper.innerHTML = `
+        <form id="name-form">
+          <input id="name-input" type="text" />
+          <button class="send-btn" type="submit">
+            Send
+          </button>
+        </form>` 
+
+}
+
 
 
 // Set up your eventlisteners here
-form.addEventListener('submit', handleNameInput)
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const input = document.getElementById("name-input").value;
+  nxtQuestion(input)
+})
+
+
 
 
 // When website loaded, chatbot asks first question.
@@ -109,5 +193,4 @@ form.addEventListener('submit', handleNameInput)
 // greeting()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+
