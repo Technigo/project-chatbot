4b6adee97b10a5187d5fac9bc7c98af711c7858a
Elaine Bergstrom <elainebergstrom26@gmail.com>
2021-02-04 15:12:49 +0100

    Create nextQuestion function, add buttons to question2, reloadBot function (issue with userReply)

--- code/script.js
+++ code/script.js
@@ -6,10 +6,19 @@ const sendBtn = document.getElementById('send')
 const yesButton = document.getElementById('yes-btn')
 const noButton = document.getElementById('no-btn')
 
+//Makes a function for showMessage 'user' (cleaner code)
+const userReply = (msg) => {
+  showMessage(msg, 'user');
+}
+//Makes a function for showMessage 'bot' (cleaner)
+const botReply = (msg) => {
+  showMessage(msg, 'bot');
+}
+
 // Global variables, if you need any, declared here
-
+//Numerically labels questions so nextQuestion function can find them.
+let indexDoggos = 1
 // Functions declared here
-
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
@@ -37,23 +46,69 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+const reloadBot = () => {
+  botReply(`see ya`)
+  
+  inputWrapper.innerHTML = 
+  `<div>
+    <button id="reloadBtn">Restart</button>
+  </div>`
+
+  document.getElementById('reloadBtn').addEventListener('click', () => {
+    location.reload()
+
+})
+}
+
+const nextQuestion = (message) => {
+  console.log(indexDoggos);
+  if (indexDoggos === 1){
+    userReply(message)
+    setTimeout(() => dogSize(message), 1000);
+  } 
+ else {
+  setTimeout(() => greeting(message), 1000);
+}
+}
+ 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there! Are you read to see some bestest doggos?!`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  indexDoggos = 1
+  botReply('Hello there! Are you ready to see some bestest doggos?!')
 }
 
-const answerYes = () => {
-  showMessage('hell yes', 'user')
-}
-// Set up your eventlisteners here
-yesButton.addEventListener('click', answerYes) 
+//Functions to answer yes to the first question
+/* const answerYes = () => {
+  userReply('Hell yes!');
+} */
 
-const answerNo = () => {
+
+// Set up your eventlisteners here - user answer yes when click
+yesButton.addEventListener('click', () => nextQuestion('Hells yes!')) 
+noButton.addEventListener('click', () => reloadBot('Fy fan nej'))
+
+//Functions to answer no to the first question
+
+/* const answerNo = () => {
   showMessage('Fy fan nej!', 'user')
 }
-// Set up your eventlisteners here
-noButton.addEventListener('click', answerNo) 
+*/
+// Set up your eventlisteners here - user answer no when clicked
+
+
+const dogSize = () => {
+  indexDoggos++
+  botReply('Do you want to see a small, medium, or large doggo?')
+
+  inputWrapper.innerHTML = `
+    <button id="small-btn">Small</button>
+    <button id="medium-btn">Medium</button>
+    <button id="large-btn">Large</button>  
+  `
+}
+
+
+
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
