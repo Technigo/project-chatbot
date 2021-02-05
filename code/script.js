74e441b354a20313686511f374f2c2f5db0eae48
unknown <lindgren.anna.m@gmail.com>
2021-02-05 09:37:40 +0100

    we added functions to the chatbot

--- code/script.js
+++ code/script.js
@@ -1,13 +1,19 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const inputWrapper = document.getElementById('input-wrapper'); //input sektion + submit
+const nameForm = document.getElementById('name-form');
+const nameInput = document.getElementById('name-input'); //User input sektion
 
 // Global variables, if you need any, declared here
+let nameUser =''
 
 // Functions declared here
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  if (sender === 'user') {
+  
+  if (sender === 'user') {   
+    
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,7 +23,8 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
-    chat.innerHTML += `
+  
+     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
@@ -30,13 +37,82 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+
 // Starts here
-const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+function greeting() {
+  questionNumber = 1
+  showMessage(`Hi! What is your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+const handleNameInput = (event) => {
+  event.preventDefault()
+  const nameUser = nameInput.value //sparar namnet
+  nameInput.value = '' //rensar text rutan
+  showMessage(nameUser, 'user') //visar userinput i chatten
+  setTimeout(() => showMessage(`So nice to meet you ${nameUser}`, 'bot'), 1000)
+  setTimeout(() => askForYes(nameUser), 2500)
+}
+
+const askForYes = () => {
+  showMessage(`My name is Yes-bot. I beleive that there is no such thing as a no-answer. And I am here to see if you beleive the same. Do you agree?`, 'bot')
+
+  inputWrapper.innerHTML = `
+    <button id="yesButton">Yes</button>
+    <button id="noButton">No</button>
+  `
+  document.getElementById("yesButton").addEventListener('click', () => {
+    showMessage(`Yes`, 'user')
+    inputWrapper.innerHTML = ""
+    setTimeout(() => askForFeeling("Yes"), 1000)
+  })
+
+  document.getElementById("noButton").addEventListener('click', () => {
+    showMessage(`No`, 'user')
+    inputWrapper.innerHTML =""
+    setTimeout(() => askForFeeling("No"), 1000)
+  })
+}
+
+const askForFeeling = (answer) => {
+  showMessage(`So your answer is ${answer}`, 'bot')
+
+  inputWrapper.innerHTML =`
+  <button id="yeahButton">Heck Yeah</button>
+  <button id="notReallyButton">Not really</button> 
+`
+
+  if (answer === "Yes"){
+  showMessage(`Thats what i wanted to hear. Do you feel like a winner today?`, 'bot')
+
+  document.getElementById("yeahButton").addEventListener('click', () => {
+    showMessage(`Heck Yeah`, 'user')
+    inputWrapper.innerHTML = ""
+    setTimeout(() => askForPepp("Heck Yeah"), 1000)
+  })
+  } if(answer === "No" ) {
+    showMessage(`That was not the answer i expected`, 'bot')
+
+    document.getElementById("notReallyButton").addEventListener('click', () => {
+      showMessage(`Not really`, 'user')
+      inputWrapper.innerHTML = ""
+      setTimeout(() => thanksBye("Not really"), 1000)
+    })
+  }
+}
+
+const thanksBye = (bye) => {
+  showMessage(`So your answer is ${bye}`, 'bot')
+  showMessage (`It was lovely to speak with you, bye`, 'bot') //avslutar programmet
+  inputWrapper.innerHTML =``
+}
+
+
+
 // Set up your eventlisteners here
+
+nameForm.addEventListener('submit', handleNameInput)
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
