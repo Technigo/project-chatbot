a1dd0bc3c962d3b767ae9985ab28b2452bf00d63
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-02-06 00:26:33 +0100

    added more functions

--- code/script.js
+++ code/script.js
@@ -4,6 +4,12 @@ const nameForm = document.getElementById('name-form')
 const nameInput = document.getElementById('name-input')
 const inputWrapper = document.getElementById('input-wrapper')
 
+
+
+
+
+let confirmation =''
+
 // Global variables, if you need any, declared here
 const pizzaChoice = `
   <select id='select'>
@@ -56,24 +62,92 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 // Functions declared here
+
+
+//Summary
+const askForConfirmation = () => {
+  showMessage(confirmation, 'bot')
+
+  inputWrapper.innerHTML = `
+  <button id="yes-button">Yes</button>
+  <button id="no-button">No</button>
+  `
+  document
+  .getElementById('yes-button')
+  .addEventListener('click', () => {
+  showMessage('Yes', 'user')
+  inputWrapper.innerHTML = '' 
+  setTimeout(() => showMessage('Good, we will have your order ready in 20 minutes', 'bot'), 2000)
+})
+document
+  .getElementById('no-button')
+  .addEventListener('click', () => {
+  showMessage('No', 'user')
+  document.getElementById("restart").addEventListener("click",() => location.reload())
+})
+}
+
+//Question 6
+const drinkSelect = (drinkChoice) => {
+  if (drinkChoice === 'a') {
+    showMessage('Okey, you added a drink to your order', 'bot')
+  } else {
+    showMessage("Okey, you don't want a drink added to your order", 'bot')
+  }
+confirmation += `and ${drinkChoice} drink is that correct?`
+  setTimeout(() => askForConfirmation(drinkChoice), 1000)
+}
+
 //Question 5
 const drinkChoice = () => {
-
+  showMessage('Do you want a drink with your order?', 'bot')
+  inputWrapper.innerHTML = `
+  <button id="yes-button">Yes</button>
+  <button id="no-button">No</button>
+  `
+  document
+  .getElementById('yes-button')
+  .addEventListener('click', () => {
+  showMessage('Yes', 'user')
+  inputWrapper.innerHTML = '' 
+  setTimeout(() => drinkSelect('a'), 1000)
+})
+document
+  .getElementById('no-button')
+  .addEventListener('click', () => {
+  showMessage('No', 'user')
+  inputWrapper.innerHTML = '' 
+  setTimeout(() => drinkSelect('no'), 1000)
+})
 }
+
+
 //Question 4
-const askForAmount = (selectedFood) => {
+const askForAmount = selectedFood => {
   showMessage(`How many ${selectedFood} do you want?`, 'bot')
-  const foodAmount = nameInput.value
-  showMessage(foodAmount, 'user')
-  nameForm.addEventListener('submit', drinkChoice)
+  inputWrapper.innerHTML =`
+  <form id="name-form">
+  <input id="name-input" type="text" />
+  <button class="send-btn" type="submit">
+    Send
+  </button>
+</form>
+  `
+  const foodForm = document.getElementById('name-form')
+  const foodInput = document.getElementById('name-input')
+  let foodAmount = foodInput.value
+ 
+  foodForm.addEventListener('submit', (event) => {
+  event.preventDefault()  
+  confirmation += `and you want ${foodInput.value} of them `
+  showMessage(foodInput.value, 'user') 
+  drinkChoice(foodInput.value)})
+  }
 
-
-} 
 
 // Question 3
 const askFoodChoice = foodChoice => {
   showMessage(`${foodChoice} sounds good! What kind of ${foodChoice} do you want?`, 'bot')
-
 
 if (foodChoice === 'pizza') {
   inputWrapper.innerHTML = pizzaChoice
@@ -82,9 +156,12 @@ if (foodChoice === 'pizza') {
 } else {
   inputWrapper.innerHTML = salladChoice
 }
+
 const selectedFood = document.getElementById('select')
+
 selectedFood.addEventListener('change', () => {
 showMessage(selectedFood.value, 'user')
+confirmation += `So you want ${selectedFood.value} `
 inputWrapper.innerHTML=''
 setTimeout(() => askForAmount(selectedFood.value), 1000)
 }) 
@@ -129,6 +206,7 @@ const handleNameInput = (event) => {
   showMessage(userName, 'user')
   nameInput.value = ''
   setTimeout (() => showMessage(`Nice to meet you ${userName}! I hope you're hungry`, 'bot'), 1000 )
+  setTimeout(() => showMessage('What would you like to eat?', 'bot'), 2000)
   setTimeout (() => handleFoodInput(userName), 2000)
 }
 
@@ -157,3 +235,12 @@ nameForm.addEventListener('submit', handleNameInput)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
 // setTimeout(() => handleFoodInput, 1000)
+
+
+
+
+
+
+
+
+
