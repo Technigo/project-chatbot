2a899678440e987b991fb30139f68a68652d729e
Rasmus Persson <rrasmuspersson@gmail.com>
2021-02-05 09:52:54 +0100

    finalised stocks and fund question and answers.

--- code/script.js
+++ code/script.js
@@ -59,41 +59,50 @@ const greeting = () => {
 
 const nextQuestion = (message) => {
   if (questionNumber === 1) {
-
-    userReply(message); // Speech bubble name
-    formMessageBox.value = "";
-    setTimeout(() => stockOrFundsQuestion(message), chatDelay) // Move to question2
-
+      greentingAnswer(message);
   } else if (questionNumber === 2) {
-    userReply(message); // Speech bubble STOCKS or FUNDS
-    formMessageBox.value = "";
-
-    if (message === "stocks") {
-      botReply("Perfect. Would you like high risk or low risk stocks?");
-      questionNumber++; // Add 1 to questionNumber. questionNumber is now 3
-    } else if (message === "funds") {
-      botReply("Perfect. Would you like high risk or low risk funds?");
-      questionNumber++; // Add 1 to questionNumber. questionNumber is now 3
-    } else {
-      botReply('You have to type either "stocks" or "funds".');
-    };
+      stockOrFundsQuestionAnswer(message);
   } else if (questionNumber === 3) {
-    highRiskOrLowRiskStockQuestion(message);
+      highRiskOrLowRiskStockQuestion(message);
   } else if (questionNumber === 4) {
-    finalQuestion(message);
+    highRiskOrLowRiskFundQuestion(message);
+  } else if (questionNumber === 99) {
+      finalQuestion(message);
   } else {
-    botReply("questionNumber invalid value!");
+      botReply("questionNumber invalid value!");
   }
 };
 
+const greentingAnswer = (message) => {
+    userReply(message); // Speech bubble name
+    formMessageBox.value = "";
+    setTimeout(() => stockOrFundsQuestion(message), chatDelay) // Move to question2
+};
+
 const stockOrFundsQuestion = (message) => {
-  questionNumber++; // Add 1 to questionNumber. questionNumber is now 2
+  questionNumber = 2; // Add 1 to questionNumber. questionNumber is now 2
   botReply(`Hello ${message}. Would you like to buy "stocks" or "funds"?`) // Speech bubble question
   // Jump to nextQuestion function because we clicked the button
 };
 
+const stockOrFundsQuestionAnswer = (message) => {
+    
+    userReply(message); // Speech bubble STOCKS or FUNDS
+     formMessageBox.value = "";
+
+    if (message === "stocks") {
+      botReply("Perfect. Would you like high risk or low risk stocks?");
+      questionNumber = 3; // Add 1 to questionNumber. questionNumber is now 3
+    } else if (message === "funds") {
+      botReply("Perfect. Would you like high risk or low risk funds?");
+      questionNumber = 4; // Add 1 to questionNumber. questionNumber is now 4
+    } else {
+      botReply('You have to type either "stocks" or "funds".');
+    }; 
+};
+
 const highRiskOrLowRiskStockQuestion = (message) => {
-  questionNumber++
+  questionNumber = 99;
   userReply(message); // Speechbubble high
   formMessageBox.value = "";
   if (message === "high" || message === "high risk") {
@@ -106,10 +115,10 @@ const highRiskOrLowRiskStockQuestion = (message) => {
     <div class="selectOptionDiv" id="selectOptionDiv">
       <select id="selectMenuOptions">
         <option value="" selected disabled>Select a stock</option>
-        <option value="provideIT">Provide IT</option>
-        <option value="sagaxD">Sagax D</option>
-        <option value="naxs">Naxs</option>
-        <option value="lunding energy">Lundin Energy</option>
+        <option value="Provide IT">Provide IT</option>
+        <option value="Sagax D">Sagax D</option>
+        <option value="Naxs">Naxs</option>
+        <option value="Lundin Energy">Lundin Energy</option>
       </select>
       <button id="selectOptionButton" class="send-btn" type="button">Buy</button>
     </div>
@@ -131,10 +140,10 @@ const highRiskOrLowRiskStockQuestion = (message) => {
     <div class="selectOptionDiv" id="selectOptionDiv">
     <select id="selectMenuOptions">
     <option value="" selected disabled>Select a stock</option>
-    <option value="investor">Investor</option>
-    <option value="latour">Latour</option>
-    <option value="kinnevik">Kinnevik</option>
-    <option value="castellum">Castellum</option>
+    <option value="Investor">Investor</option>
+    <option value="Latour">Latour</option>
+    <option value="Kinnevik">Kinnevik</option>
+    <option value="Castellum">Castellum</option>
     </select>
     <button id="selectOptionButton" class="send-btn" type="button">Buy</button>
     </div>
@@ -144,21 +153,70 @@ const highRiskOrLowRiskStockQuestion = (message) => {
 
     selectOptionButton.addEventListener('click', () => nextQuestion(selectMenuOptions.value));
 
-
-
-
-
-
-
-  } else if (message === "high" && questionNumber === 50) {
-    botReply("High Risk Funds");
   } else {
     botReply("You have to type either low risk or high risk.");
   };
 };
 
-const highRiskOrLowRiskFundkQuestion = (message) => {
-  
+const highRiskOrLowRiskFundQuestion = (message) => {
+
+  questionNumber = 99;
+  userReply(message); // Speechbubble high
+  formMessageBox.value = "";
+  if (message === "high" || message === "high risk") {
+    botReply("oh, so you want high risk!");
+    setTimeout(chatDelay)
+    botReply("Here you go!");
+
+    formInputWrapper.innerHTML = `
+  <div class="input-wrapper" id="input-wrapper">
+    <div class="selectOptionDiv" id="selectOptionDiv">
+      <select id="selectMenuOptions">
+        <option value="" selected disabled>Select a stock</option>
+        <option value="BGF World Gold">BGF World Gold</option>
+        <option value="Allianz Global">Allianz Global</option>
+        <option value="Amundi Fds">Amundi Fds</option>
+        <option value="Shroder ISF">Shroder ISF</option>
+      </select>
+      <button id="selectOptionButton" class="send-btn" type="button">Buy</button>
+    </div>
+  </div>`;
+
+    let selectOptionButton = document.getElementById("selectOptionButton");
+    let selectMenuOptions = document.getElementById("selectMenuOptions");
+
+    selectOptionButton.addEventListener('click', () => nextQuestion(selectMenuOptions.value));
+
+  } else if (message === "low" || message === "low risk") {
+
+    botReply("You want low risk");
+    setTimeout(chatDelay)
+    botReply("Here you go!");
+
+    formInputWrapper.innerHTML = `
+    <div class="input-wrapper" id="input-wrapper">
+    <div class="selectOptionDiv" id="selectOptionDiv">
+    <select id="selectMenuOptions">
+    <option value="" selected disabled>Select a stock</option>
+    <option value="Spiltan Räntefond Sverige">Spiltan Räntefond Sverige</option>
+    <option value="SPP Obligationsfond">SPP Obligationsfond</option>
+    <option value="Spiltan Högräntefond">Spiltan Högräntefond</option>
+    <option value="IKC avkastningsfond">IKC avkastningsfond</option>
+    </select>
+    <button id="selectOptionButton" class="send-btn" type="button">Buy</button>
+    </div>
+    </div>`;
+    let selectOptionButton = document.getElementById("selectOptionButton");
+    let selectMenuOptions = document.getElementById("selectMenuOptions");
+
+    selectOptionButton.addEventListener('click', () => nextQuestion(selectMenuOptions.value));
+
+  } else {
+    botReply("You have to type either low risk or high risk.");
+  };
+
+
+
 };
 
 const finalQuestion = (message) => {
