fe0799e3ccb0a33c280e916657d9e8b30e4f8e51
Hannah Jesinkey <h.jesinkey@gmail.com>
2021-02-05 08:55:00 +0100

    Declared functions at top of page

--- code/script.js
+++ code/script.js
@@ -6,8 +6,13 @@ const userInput = document.getElementById('user-input')
 // Global variables, if you need any, declared here
 
 // Functions declared here
-
-// This function will add a chat bubble in the correct place based on who the sender is
+const userReply = () => {
+    showMessage(userInput.value, 'user')
+}
+const botReply = (message) => {
+        showMessage(message, 'bot')
+    }
+    // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
     if (sender === 'user') {
         chat.innerHTML += `
@@ -45,49 +50,23 @@ docBotForm.addEventListener('submit', event => {
     event.preventDefault()
 
     if (questionCounter === 0) {
-        const userReply = () => {
-            showMessage(userInput.value, 'user')
-        }
         userReply();
 
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         botReply(`Welcome ${userInput.value}! How are you feeling today?`, 'bot')
-
     } else if (questionCounter === 1) {
-        const userReply = () => {
-            showMessage(userInput.value, 'user')
-        }
         userReply();
-
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         if (userInput.value.length < 6) {
             botReply(`Can you expand on that?`)
         } else {
             botReply(`Why do you think you feel like that today?`)
         }
     } else if (questionCounter === 2) {
-        const userReply = () => {
-            showMessage(userInput.value, 'user')
-        }
         userReply();
 
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         botReply(`Hmmm... I’ m listening. Please tell me more`)
     } else if (questionCounter === 3) {
-        const userReply = () => {
-            showMessage(userInput.value, 'user')
-        }
         userReply();
 
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         setTimeout(botReply, 1000, [`I see! No one really ever asks me how I feel...`])
         setTimeout(botReply, 3000, [`Sometimes I'm very low on energy. I have alot to process you know. It makes me stressed and affects my mood.`])
         setTimeout(botReply, 5000, [`Sleep is very important. So are hugs.`])
@@ -95,14 +74,8 @@ docBotForm.addEventListener('submit', event => {
         setTimeout(botReply, 9000, [`That feels better, don’t you think?`])
         setTimeout(botReply, 11000, [`So back to you. How would you describe your wellbeing on a scale of 1 to 3 where 1 is “Like Shit” and 3 is “On Top Of The World”?`])
     } else if (questionCounter === 4) {
-        const userReply = () => {
-            showMessage(userInput.value, 'user');
-        }
         userReply();
 
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         if (userInput.value === `1`) {
             botReply(`Take a deep breath, then you should probably contact your Vårdcentral and get some more professional help. There is help out there!`)
         } else if (userInput.value === `2`) {
@@ -113,14 +86,8 @@ docBotForm.addEventListener('submit', event => {
             botReply(`I am afraid I don't understand you answer, please try again and contact another health professional`)
         }
     } else {
-        const userReply = () => {
-            showMessage(userInput.value, 'user');
-        }
         userReply();
 
-        const botReply = (message) => {
-            showMessage(message, 'bot')
-        }
         botReply(`I am afraid there is nothing more I can do for you, please contact 112`)
     }
     userInput.value = '';
