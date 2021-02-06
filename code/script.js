fbe2de2a911dcdd51b7a3b5d629b2690ed44d8b4
Marielle Ohlsson <ohlssonmarielle@gmail.com>
2021-02-06 19:45:36 +0100

    finished writing summary and switched the order of the questions

--- code/script.js
+++ code/script.js
@@ -4,15 +4,10 @@ const nameForm = document.getElementById('name-form')
 const nameInput = document.getElementById('name-input')
 const inputWrapper = document.getElementById('input-wrapper')
 
-
-
-
-
 let confirmation =''
 
-// Global variables, if you need any, declared here
 const pizzaChoice = `
-  <select id='select'>
+  <select class="food-select" id='select'>
     <option value='' selected disabled>Click here to choose pizza</option>
     <option value='Margarita'>Margarita</option>
     <option value='Hawaii'>Hawaii</option>
@@ -21,7 +16,7 @@ const pizzaChoice = `
 `
 
 const pastaChoice = `
-  <select id='select'>
+  <select class="food-select" id='select'>
     <option value='' selected disabled>Click here to choose Pasta</option>
     <option value='Carbonara'>Carbonara</option>
     <option value='Bolognese'>Bolognese</option>
@@ -30,7 +25,7 @@ const pastaChoice = `
 `
 
 const salladChoice = `
-  <select id='select'>
+  <select class="food-select" id='select'>
     <option value='' selected disabled>Click here to choose Salad</option>
     <option value='Cesar'>Cesar</option>
     <option value='Shrimp'>Shrimp</option>
@@ -52,7 +47,7 @@ const showMessage = (message, sender) => {
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/Pizzabot.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -61,143 +56,13 @@ const showMessage = (message, sender) => {
   }
   chat.scrollTop = chat.scrollHeight
 }
-// Functions declared here
 
-
-//Summary
-const askForConfirmation = () => {
-  showMessage(confirmation, 'bot')
-
-  inputWrapper.innerHTML = `
-  <button id="yes-button">Yes</button>
-  <button id="no-button">No</button>
-  `
-  document
-  .getElementById('yes-button')
-  .addEventListener('click', () => {
-  showMessage('Yes', 'user')
-  inputWrapper.innerHTML = '' 
-  setTimeout(() => showMessage('Good, we will have your order ready in 20 minutes', 'bot'), 2000)
-})
-document
-  .getElementById('no-button')
-  .addEventListener('click', () => {
-  showMessage('No', 'user')
-  document.getElementById("restart").addEventListener("click",() => location.reload())
-})
+// Starts here
+const greeting = () => {
+  showMessage(`Hello, what's your name?`, 'bot') 
 }
 
-//Question 6
-const drinkSelect = (drinkChoice) => {
-  if (drinkChoice === 'a') {
-    showMessage('Okey, you added a drink to your order', 'bot')
-  } else {
-    showMessage("Okey, you don't want a drink added to your order", 'bot')
-  }
-confirmation += `and ${drinkChoice} drink is that correct?`
-  setTimeout(() => askForConfirmation(drinkChoice), 1000)
-}
-
-//Question 5
-const drinkChoice = () => {
-  showMessage('Do you want a drink with your order?', 'bot')
-  inputWrapper.innerHTML = `
-  <button id="yes-button">Yes</button>
-  <button id="no-button">No</button>
-  `
-  document
-  .getElementById('yes-button')
-  .addEventListener('click', () => {
-  showMessage('Yes', 'user')
-  inputWrapper.innerHTML = '' 
-  setTimeout(() => drinkSelect('a'), 1000)
-})
-document
-  .getElementById('no-button')
-  .addEventListener('click', () => {
-  showMessage('No', 'user')
-  inputWrapper.innerHTML = '' 
-  setTimeout(() => drinkSelect('no'), 1000)
-})
-}
-
-
-//Question 4
-const askForAmount = selectedFood => {
-  showMessage(`How many ${selectedFood} do you want?`, 'bot')
-  inputWrapper.innerHTML =`
-  <form id="name-form">
-  <input id="name-input" type="text" />
-  <button class="send-btn" type="submit">
-    Send
-  </button>
-</form>
-  `
-  const foodForm = document.getElementById('name-form')
-  const foodInput = document.getElementById('name-input')
-  let foodAmount = foodInput.value
- 
-  foodForm.addEventListener('submit', (event) => {
-  event.preventDefault()  
-  confirmation += `and you want ${foodInput.value} of them `
-  showMessage(foodInput.value, 'user') 
-  drinkChoice(foodInput.value)})
-  }
-
-
-// Question 3
-const askFoodChoice = foodChoice => {
-  showMessage(`${foodChoice} sounds good! What kind of ${foodChoice} do you want?`, 'bot')
-
-if (foodChoice === 'pizza') {
-  inputWrapper.innerHTML = pizzaChoice
-} else if (foodChoice === 'pasta') {
-  inputWrapper.innerHTML = pastaChoice
-} else {
-  inputWrapper.innerHTML = salladChoice
-}
-
-const selectedFood = document.getElementById('select')
-
-selectedFood.addEventListener('change', () => {
-showMessage(selectedFood.value, 'user')
-confirmation += `So you want ${selectedFood.value} `
-inputWrapper.innerHTML=''
-setTimeout(() => askForAmount(selectedFood.value), 1000)
-}) 
-
-}
-// Question 2
-const handleFoodInput = () => {
-  inputWrapper.innerHTML = `
-  <button id="Pizza-button">Pizza</button>
-  <button id="Pasta-button">Pasta</button>
-  <button id="Salad-button">Salad</button>
-  `
-  document
-  .getElementById('Pizza-button')
-  .addEventListener('click',() => {
-   showMessage('I want pizza', 'user')
-   inputWrapper.innerHTML = '' 
-   setTimeout(() => askFoodChoice('pizza'), 1000)
-  })
-
-  document
-  .getElementById('Pasta-button')
-  .addEventListener('click',() => {
-   showMessage('I want pasta', 'user')
-   inputWrapper.innerHTML = '' 
-   setTimeout(() => askFoodChoice('pasta'), 1000) 
-  })
-
-  document
-  .getElementById('Salad-button')
-  .addEventListener('click',() => {
-   showMessage('I want Salad', 'user') 
-   inputWrapper.innerHTML = '' 
-   setTimeout(() => askFoodChoice('sallad'), 1000)
-  })
-} 
+setTimeout(greeting, 1000)
 
 // Question 1
 const handleNameInput = (event) => {
@@ -210,37 +75,145 @@ const handleNameInput = (event) => {
   setTimeout (() => handleFoodInput(userName), 2000)
 }
 
-// This function will add a chat bubble in the correct place based on who the sender is
-
-  
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
- 
-
-
-// Starts here
-const greeting = () => {
-  showMessage(`Hello, what's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
-
-// Set up your eventlisteners here
-
 nameForm.addEventListener('submit', handleNameInput)
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
-// setTimeout(() => handleFoodInput, 1000)
+// Question 2
+const handleFoodInput = () => {
+  inputWrapper.innerHTML = `
+    <button id="Pizza-button">Pizza</button>
+    <button id="Pasta-button">Pasta</button>
+    <button id="Salad-button">Salad</button>
+  `
+  
+  document
+  .getElementById('Pizza-button')
+  .addEventListener('click',() => {
+    showMessage('I want pizza', 'user')
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => askFoodChoice('pizza'), 1000)
+  })
 
+  document
+  .getElementById('Pasta-button')
+  .addEventListener('click',() => {
+    showMessage('I want pasta', 'user')
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => askFoodChoice('pasta'), 1000) 
+  })
 
+  document
+  .getElementById('Salad-button')
+  .addEventListener('click',() => {
+    showMessage('I want Salad', 'user') 
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => askFoodChoice('sallad'), 1000)
+  })
+} 
 
+// Question 3
+const askFoodChoice = foodChoice => {
+  showMessage(`${foodChoice} sounds good! What kind of ${foodChoice} do you want?`, 'bot')
 
+  if (foodChoice === 'pizza') {
+    inputWrapper.innerHTML = pizzaChoice
+  } else if (foodChoice === 'pasta') {
+    inputWrapper.innerHTML = pastaChoice
+  } else {
+    inputWrapper.innerHTML = salladChoice
+  }
 
+  const selectedFood = document.getElementById('select')
 
+  selectedFood.addEventListener('change', () => {
+    showMessage(selectedFood.value, 'user')
+    confirmation += `So you want ${selectedFood.value} `
+    inputWrapper.innerHTML=''
+    setTimeout(() => askForAmount(selectedFood.value), 1000)
+  }) 
+}
 
+//Question 4
+const askForAmount = selectedFood => {
+  showMessage(`How many ${selectedFood} do you want?`, 'bot')
+  inputWrapper.innerHTML =`
+    <form id="name-form">
+      <input id="name-input" type="text" />
+      <button class="send-btn" type="submit">
+        Send
+      </button>
+    </form>
+  `
 
+  const foodForm = document.getElementById('name-form')
+  const foodInput = document.getElementById('name-input')
+ 
+  foodForm.addEventListener('submit', (event) => {
+    event.preventDefault()  
+    confirmation += `and you want ${foodInput.value} of them `
+    showMessage(foodInput.value, 'user') 
+    drinkChoice(foodInput.value)
+  })
+}
+
+//Question 5
+const drinkChoice = () => {
+  setTimeout(() => showMessage('Do you want a drink with your order?', 'bot'), 1000)
+  inputWrapper.innerHTML = `
+    <button id="yes-button">Yes</button>
+    <button id="no-button">No</button>
+  ` 
+
+  document
+  .getElementById('yes-button')
+  .addEventListener('click', () => {
+    showMessage('Yes', 'user')
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => drinkSelect('a'), 1000)
+  }) 
+
+  document
+  .getElementById('no-button')
+  .addEventListener('click', () => {
+    showMessage('No', 'user')
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => drinkSelect('no'), 1000)
+  })
+}
+
+//Question 6
+const drinkSelect = (drinkChoice) => {
+
+  if (drinkChoice === 'a') {
+    showMessage('Okey, you added a drink to your order', 'bot')
+  } else {
+    showMessage("Okey, you don't want a drink added to your order", 'bot')
+  }
+
+  confirmation += `and ${drinkChoice} drink is that correct?`
+  setTimeout(() => askForConfirmation(drinkChoice), 1000)
+}
+
+//Summary
+const askForConfirmation = () => {
+  showMessage(confirmation, 'bot')
+
+  inputWrapper.innerHTML = `
+    <button id="yes-button">Yes</button>
+    <button id="no-button">No</button>
+  `
+  document
+  .getElementById('yes-button')
+  .addEventListener('click', () => {
+    showMessage('Yes', 'user')
+    inputWrapper.innerHTML = '' 
+    setTimeout(() => showMessage('Good, we will have your order ready in 20 minutes', 'bot'), 2000)
+  })
+
+  document
+  .getElementById('no-button')
+  .addEventListener('click', () => {
+    showMessage('No', 'user')
+    location.reload()
+  })
+}
 
