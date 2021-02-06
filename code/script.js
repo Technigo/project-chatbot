991e6a468035ca22a862a397aac204bfb1efad4e
unknown <lindgren.anna.m@gmail.com>
2021-02-06 17:43:36 +0100

    organized the code and removec setTimeOut that didnt work

--- code/script.js
+++ code/script.js
@@ -1,16 +1,11 @@
-// All the DOM selectors stored as short variables
-const chat = document.getElementById('chat');
-const inputWrapper = document.getElementById('input-wrapper'); //input sektion + submit
-const nameForm = document.getElementById('name-form');
-const nameInput = document.getElementById('name-input'); //User input sektion
+const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+const nameForm = document.getElementById('name-form')
+const nameInput = document.getElementById('name-input')
 
-// Global variables, if you need any, declared here
+
 let nameUser =''
 
-
-// Functions declared here
-
-// This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   
   if (sender === 'user') {   
@@ -34,40 +29,39 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+ 
   chat.scrollTop = chat.scrollHeight
 }
 
 
-// Starts here
+
 // Question 1
 function greeting() {
   showMessage(`Hi! What is your name?`, 'bot')
-  
-  // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 // Handle Name
 const handleNameInput = (event) => {
   event.preventDefault()
-  const nameUser = nameInput.value //sparar namnet
-  nameInput.value = '' //rensar text rutan
-  showMessage(nameUser, 'user') //visar userinput i chatten
+  const nameUser = nameInput.value 
+  nameInput.value = '' 
+  showMessage(nameUser, 'user') 
   setTimeout(() => showMessage(`So nice to meet you ${nameUser}`, 'bot'), 1000)
   setTimeout(() => askForYes(nameUser), 2500)
 }
 
 //Question 2 
 const askForYes = () => {
-  setTimeout (showMessage(`Hello! My name is Yes-bot. I beleive that there is no such thing as a no-answer. And I am here to make you beleive the same. Are you up for that?`, 'bot'), 1000)
+  setTimeout (() => showMessage(`My name is Yes-bot. I beleive that there is no such thing as a no-answer. And I am here to make you beleive the same. Are you up for that?`, 'bot'), 1000)
 
   inputWrapper.innerHTML = `
     <button id="yesButton">Yes</button>
     <button id="yeahButton">Yeah</button>
   `
+
   document.getElementById("yesButton").addEventListener('click', () => {
     showMessage(`Yes`, 'user')
-    inputWrapper.innerHTML = ""
+    inputWrapper.innerHTML =""
     setTimeout(() => askForFeeling("Yes"), 2000)
   })
 
@@ -81,53 +75,56 @@ const askForYes = () => {
 //Question 3
 const askForFeeling = (answer) => {
   showMessage(`So your answer is ${answer}. Of course, I'm so happy you are up for that. You rock!🤘`, 'bot')
-  showMessage(`Are you feeling happy?`, 'bot')
+  setTimeout(() => showMessage(`Are you feeling happy?`, 'bot'), 1500) 
 
   inputWrapper.innerHTML =`
     <button id="yasButton">Yas...</button>
     <button id="ouiButton">Oui</button> 
   `
-  
+
   document.getElementById("yasButton").addEventListener('click', () => {
-      showMessage(`Yas...`, 'user')
-      inputWrapper.innerHTML = ""
-      setTimeout(() => askForPepp("Yas..."), 1000)
+    showMessage(`Yas...`, 'user')
+    inputWrapper.innerHTML = ""
+    setTimeout(() => askForPepp("Yas..."), 2000)
   })   
 
   document.getElementById("ouiButton").addEventListener('click', () => {
-     showMessage(`Oui`, 'user')
-     inputWrapper.innerHTML = ""
-     setTimeout(() => askForPepp("Oui"), 1000)
+    showMessage(`Oui`, 'user')
+    inputWrapper.innerHTML = ""
+    setTimeout(() => askForPepp("Oui"), 2000)
   }) 
 }
 
+//Question 4
 const askForPepp = (feeling) => {
   if (feeling === "Oui") {
     showMessage(`Cool, you're confident! If you're happy than I'm happy`, 'bot')
-    showMessage(`Would you like a pep talk? Even though you don't seem to need it 😜`, 'bot')
+    setTimeout(() => showMessage(`Would you like a pep talk? Even though you don't seem to need it 😜`, 'bot'), 1500)
   } else {
     showMessage(`That seems a bit modest, don't you think?🙊`, 'bot')
-    showMessage(`Maybe you need a pep talk?`, 'bot')
+    setTimeout(() => showMessage(`Maybe you need a pep talk?`, 'bot'), 1500) 
   }
 
   inputWrapper.innerHTML =`
     <button id="pepButton">Yes Pep</button>
     <button id="noButton">No because I'm boring</button> 
   `
+
   document.getElementById("pepButton").addEventListener('click', () => {
     showMessage(`Yes Pep`, 'user')
     inputWrapper.innerHTML =""
-    setTimeout(() => getRandomPepp("Yes Pep"), 1000)
+    setTimeout(() => getRandomPepp("Yes Pep"), 2000)
   })
 
   document.getElementById("noButton").addEventListener('click', () => {
     showMessage(`No because I'm boring`, 'user')
     inputWrapper.innerHTML =""
-    setTimeout(() => thanksBye("No because I'm boring"), 1000)
+    setTimeout(() => thanksBye("No because I'm boring"), 2000)
   })
 
 }
 
+//Question 5
 const getRandomPepp = () => {
   let random = Math.floor(Math.random() * 8);
   let pepp =''
@@ -158,12 +155,12 @@ const getRandomPepp = () => {
   }
   
   showMessage(pepp, 'bot')
+  setTimeout(() => thanksBye(), 3000)
 }
  
-//Last thing the user see.
+// The end
 const thanksBye = () => {
-  showMessage(`Its shame that you dont want to talk 😔... `, 'bot')
-  showMessage (`If you feel like talking again, press the restart button`, 'bot') //avslutar programmet
+  showMessage (`We have come to the end of the road. If you feel like talking again, press the restart button`, 'bot') 
   inputWrapper.innerHTML =`
   <button id="restartButton">Actually i feel like talking again!</button>
   `
@@ -174,14 +171,6 @@ const thanksBye = () => {
   
 }
 
-// Set up your eventlisteners here
 nameForm.addEventListener('submit', handleNameInput)
 
-
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
