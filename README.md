4c53bffb8d7eed6d2aa65264bcbcdd20b360e769
Irina <jekabsone.irina@gmail.com>
2021-02-24 23:32:16 +0100

    HandleEventSetup reusable function created

--- README.md
+++ README.md
@@ -4,8 +4,21 @@ Our project for this week was to work with pair programming and build a chatbot 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We decided to create a chatbot where the user can order tickets for the cinema, after making several choices.
+On the first day, we decided on the idea for the project, and we also created all the content, so that we had that part ready and could focus only on code for the rest of the week. That turned out to be a good approach which really did speed up the work for the remaining days.
+
+We used Visual Studio Code Live Share in order to be able to pair program which worked well for us. We met on Zoom for a few hours most days of the week, and worked on the code together. Some evenings we also made a few smaller updates separately, but always made sure that both approved and understood what had been done.
+
+We started with the greeting and created one question at a time, and made sure each new addition worked before proceeding. We decided early on to use buttons for choices, and put a class on some buttons in order to create a loop to look for a click on those, with the purpose to be able to proceed to the next question after any of them was selected.
+
+We have a function that makes sure that the chatbot continues on to the next question when called, and another function that adds a chat bubble in the right place depending on if it's the bot or the user who is saying something. We have also added a few global variables.
+
+During our entire pair programming we've made sure to look for problems in the terminal, in order to fix everything before testing.
+We have worked with functions, variables, eventlisteners and also looping. We have asked for help on StackOverflow when needed and have received some really good advice and feedback from our classmates.
+
+We discussed to possibly use if and else and calculate the price for the tickets depending on the amount of tickets the user chooses, so that could be something we'd do if we had more time. For now we decided that the approach we have used works well considering the content. We would also like to extend our knowledge on how to use looping the best way, since we was really helped by using it for our chatbot, but need to know more in order to use it in any context.
 
 ## View it live
 
-Have you deployed your project somewhere? Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+Our chatbot can be viewed here:
+https://youthful-fermat-4a634a.netlify.app/
--- code/script.js
+++ code/script.js
@@ -4,25 +4,25 @@ const form = document.getElementById("name-form")
 const input = document.getElementById("name-input")
 const inputWrapper = document.getElementById("input-wrapper")
 // Global variables, if you need any, declared here
-let question = 0;
+let question = 1;
 let userName = ""
 let ticketNumber = ""
 // Functions declared here
 // This function will lead to the next question
 const nxtQuestion = (answer) => {
   usrInput(answer)
-    if (questionNumber === 1) {
-      handleNameInput(answer)
-      input = ""
-    } 
-    else if (questionNumber === 2) {
-      askMovieOptions(answer)
-      input =""
-    } 
-    else if (questionNumber === 3) {
-      askForTickets(answer)
-      input =""
-    } 
+  if (question === 1) {
+    handleNameInput(answer)
+    input.value = ""
+  } 
+  else if (question === 2) {
+    askMovieOptions()
+    input.value =""
+  } 
+  else if (question === 3) {
+    askForTickets(answer)
+    input.value =""
+  } 
 }
 
 // This function will add a chat bubble in the correct place based on who the sender is
@@ -52,8 +52,6 @@ const showMessage = (message, sender) => {
 // Starts here
 // First message
 const greeting = () => {
-  questionNumber = 1;
-  console.log(questionNumber)
   showMessage(`Welcome to Technigo Cinema! What is your name?`, 'bot')
 }
 // This means the greeting function will be called one second after the website is loaded.
@@ -62,104 +60,55 @@ setTimeout(greeting, 1000)
 const usrInput = (answer) => {
   showMessage(answer, 'user')
 }
-//Question 2
+//Question 1
 const handleNameInput = () => {
-  questionNumber = 2
-  console.log(questionNumber)
   let userName = document.getElementById('name-input').value;
   setTimeout (() => showMessage(`Hi ${userName}!`, 'bot'), 1000)
   setTimeout (() => askMovieOptions (), 2000)
+  question = 2
 };
 
-//Question 3
-const askMovieOptions = (question) => {
-  questionNumber = 3
-  console.log(questionNumber)
+//This function will listen for movie option click events
+const handleEventSetup = (optionChosen, button1, button2, button3) => {
+  document.getElementById(`${optionChosen}`).addEventListener("click", () => {
+    setTimeout (() => showMessage(`I will go for ${optionChosen}.`, "user"), 1000)
+    setTimeout (() => showMessage("Great choice! Go ahead and pick the best one.", "bot"), 2000)
+    setTimeout(() => {
+      whatMovie()
+    }, 3000)
+    let whatMovie = () => {
+      inputWrapper.innerHTML =  `
+        <button class="choice-btn">${button1}</button>
+        <button class="choice-btn">${button2}</button>
+        <button class="choice-btn">${button3}</button>`
+      //Looping through each button and checking when it is clicked
+      const choiceBtn = document.querySelectorAll(".choice-btn")
+      for (let i = 0; i < choiceBtn.length; i++) {
+        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
+      }
+    }
+  })
+
+}
+//Question 2
+const askMovieOptions = () => {
   showMessage(`What kind of movie would you like to see today? Choose from the list below: `, 'bot')
   inputWrapper.innerHTML = `
     <button id="animation">Animation</button>
     <button id="action">Action</button>
     <button id="comedy">Comedy</button>
-    <button id="horror">Horror</button>`
-  
-    document.getElementById("animation").addEventListener("click", () => {
-      setTimeout (() => showMessage("I will go for animation.", "user"), 1000)
-      setTimeout (() => showMessage("Great choice! Go ahead and pick the best one.", "bot"), 2000)
-      setTimeout(() => {
-        whatMovie()
-      }, 3000)
-      let whatMovie = () => {
-        inputWrapper.innerHTML =  `
-          <button class="choice-btn" id="the-invisible-girl">The Invisible Girl</button>
-          <button class="choice-btn" id="we-bare-bears">We Bare Bears: The Movie</button>
-          <button class="choice-btn" id="the-croods">The Croods: A New Age</button>`
-        //Looping through each button and checking when it is clicked
-        const choiceBtn = document.querySelectorAll(".choice-btn")
-        for (let i = 0; i < choiceBtn.length; i++) {
-          choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
-        }
-      }
-    })
-    document.getElementById("action").addEventListener("click", () => {
-      setTimeout (() => showMessage("I will go for action.", "user"), 1000)
-      setTimeout (() => showMessage("Great choice! Go ahead and pick the best one.", "bot"), 2000)
-      setTimeout(() => {
-        whatMovie()
-      }, 3000)
-      let whatMovie = () => {
-        inputWrapper.innerHTML =  `
-          <button class="choice-btn" id="honest-thief">Honest Thief</button>
-          <button class="choice-btn" id="tenet">Tenet</button>
-          <button class="choice-btn" id="bloodshot">Bloodshot</button>`
-        //Looping through each button and checking when it is clicked
-        const choiceBtn = document.querySelectorAll(".choice-btn")
-        for (let i = 0; i < choiceBtn.length; i++) {
-        choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
-        }
-      }
-    })
-    document.getElementById("comedy").addEventListener("click", () => {
-      setTimeout (() => showMessage("I will go for comedy.", "user"), 1000)
-      setTimeout (() => showMessage("Great choice! Go ahead and pick the best one.", "bot"), 2000)
-      setTimeout(() => {
-        whatMovie()
-      }, 3000)
-      let whatMovie = () => {
-        inputWrapper.innerHTML = `
-          <button class="choice-btn" id="babyteeth">Babyteeth</button>
-          <button class="choice-btn" id="downhill">Downhill</button>
-          <button class="choice-btn" id="friendsgiving">Friendsgiving</button>`
-        //Looping through each button and checking when it is clicked
-        const choiceBtn = document.querySelectorAll(".choice-btn")
-        for (let i = 0; i < choiceBtn.length; i++) {
-          choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
-        }
-      }
-    })
-    document.getElementById("horror").addEventListener("click", () => {
-      setTimeout (() => showMessage("I will go for horror.", "user"), 1000)
-      setTimeout (() => showMessage("Great choice! Go ahead and pick the best one.", "bot"), 2000)
-      setTimeout(() => {
-        whatMovie()
-      }, 3000)
-      let whatMovie = () => {
-        inputWrapper.innerHTML = `
-          <button class="choice-btn" id="antebellum">Antebellum</button>
-          <button class="choice-btn" id="brahms">Brahms: The Boy 2</button>
-          <button class="choice-btn" id="come-play">Come Play</button> `
-        //Looping through each button and checking when it is clicked
-        const choiceBtn = document.querySelectorAll(".choice-btn")
-        for (let i = 0; i < choiceBtn.length; i++) {
-          choiceBtn[i].addEventListener("click", () => nxtQuestion(choiceBtn[i].textContent))
-        }
-      }
-    })
+    <button id="horror">Horror</button>
+  `
+  handleEventSetup("animation", "Toy Story 4", "Coco", "The Incredibles")
+  handleEventSetup("action", "Honest Thief", "Tenet", "Bloodshot")
+  handleEventSetup ("comedy", "Babyteeth", "Lucky You", "Meet The Parents 3")
+  handleEventSetup ("horror", "Antebellum", "Brahms: The Boy 2", "Come Play")
+
+  question = 3
 };
 
-//Question 4
-const askForTickets = (question) => {
-  questionNumber = 4
-  console.log(questionNumber)
+//Question 3
+const askForTickets = () => {
   setTimeout (() => showMessage (`How many tickets would you like to have?`, 'bot'), 1000)
   setTimeout(() => {
     numberOfTickets()
@@ -292,8 +241,7 @@ const askForTickets = (question) => {
 // Even listener to listen for submitt button
 form.addEventListener("submit", (event) => {
   event.preventDefault()
-  const input = document.getElementById("name-input").value;
-  nxtQuestion(input)
+  nxtQuestion(input.value)
 })
 
 
