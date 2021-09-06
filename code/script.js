d1ddd3933294abcece580a752c2d406ce592faff
HedvigM <hedvig@mejstedt.se>
2021-09-06 14:05:57 +0200

    Iteration 1
    Testing the built in code to understand the code.

--- code/script.js
+++ code/script.js
@@ -8,6 +8,7 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log(showMessage)
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -33,6 +34,7 @@ const showMessage = (message, sender) => {
 // Starts here
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
+  
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
@@ -44,4 +46,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 500)
