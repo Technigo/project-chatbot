3027ba81fb0354e6add34aabb675067fd96efe69
Sabrina Zancotti <zancotti@gmail.com>
2021-09-06 16:24:55 +0200

    started with the move out cleaning button option

--- code/script.js
+++ code/script.js
@@ -47,7 +47,7 @@ const sendUserMessageName = () => {
   sendBotMessage(
     `Hi ${message.value}, what cleaning service do you want us to provide for you?`
   );
-  message.value = "";
+  // message.value = "";
 
   const submitContainer = document.getElementById("submitNameContainer");
   submitContainer.classList.add("hidden");
@@ -114,3 +114,132 @@ document
   });
 
 // -------------------MOVE OUT CLEANING------------------------------
+
+const moveOutCleaningBtn = document.getElementById("moveOutCleaning");
+moveOutCleaningBtn.addEventListener(`click`, () => {
+  showMessage(`Move-out cleaning`, `user`);
+  sendBotMessage(`How many square meters is your home?`);
+
+  const cleanBtnsContainer = document.getElementById("cleanBtnsContainer");
+  cleanBtnsContainer.classList.remove("active");
+
+  const submitTimesCleaningContainer = document.getElementById(
+    "submitTimesCleaningContainer"
+  );
+  submitTimesCleaningContainer.classList.remove("active");
+
+  const submitSquareMetersContainer = document.getElementById(
+    "submitSquareMetersContainer"
+  );
+  submitSquareMetersContainer.classList.add("active");
+});
+
+const sendUserMessageSquareMetersBtn = () => {
+  const message = document.getElementById("submitSquareMetersInput");
+  showMessage(message.value, `user`);
+
+  sendBotMessage(
+    `Super, we have registered that your home is ${message.value} square meters. We will contact you with an offer you can't refuse!`
+  );
+
+  message.value = "";
+};
+
+const submitSquareMetersBtn = document.getElementById("submitSquareMetersBtn");
+submitSquareMetersBtn.addEventListener("click", sendUserMessageSquareMetersBtn);
+
+document
+  .getElementById("submitSquareMetersInput")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      sendUserMessageSquareMetersBtn();
+    }
+  });
+
+// ----------------------------------------Window Cleaning --------------------------------------------
+
+// const weeklyCleaningBtn = document.getElementById("weeklyCleaning");
+// weeklyCleaningBtn.addEventListener(`click`, () => {
+//   showMessage(`Weekly cleaning`, `user`);
+//   sendBotMessage(
+//     `Excellent! How many times a month do you want us to clean your home?`
+//   );
+
+//   const cleanBtnsContainer = document.getElementById("cleanBtnsContainer");
+//   cleanBtnsContainer.classList.remove("active");
+
+//   const submitTimesCleaningContainer = document.getElementById(
+//     "submitTimesCleaningContainer"
+//   );
+//   submitTimesCleaningContainer.classList.add("active");
+// });
+
+// const sendUserMessageTimesMonthBtn = () => {
+//   const message = document.getElementById("submitTimesCleaningInput");
+//   showMessage(message.value, `user`);
+
+//   sendBotMessage(
+//     `Perfect, we will clean ${
+//       message.value
+//     } times a month. That will cost approximately ${
+//       100 * message.value
+//     } dollars a month.`
+//   );
+
+//   message.value = "";
+// };
+
+// const submitTimesCleaningBtn = document.getElementById(
+//   "submitTimesCleaningBtn"
+// );
+// submitTimesCleaningBtn.addEventListener("click", sendUserMessageTimesMonthBtn);
+
+// document
+//   .getElementById("submitTimesCleaningInput")
+//   .addEventListener("keypress", function (e) {
+//     if (e.key === "Enter") {
+//       sendUserMessageTimesMonthBtn();
+//     }
+//   });
+
+// v;
+// const moveOutCleaningBtn = document.getElementById("moveOutCleaning");
+// moveOutCleaningBtn.addEventListener(`click`, () => {
+//   showMessage(`Move-out cleaning`, `user`);
+//   sendBotMessage(`How many square meters is your home?`);
+
+//   const cleanBtnsContainer = document.getElementById("cleanBtnsContainer");
+//   cleanBtnsContainer.classList.remove("active");
+
+//   const submitTimesCleaningContainer = document.getElementById(
+//     "submitTimesCleaningContainer"
+//   );
+//   submitTimesCleaningContainer.classList.remove("active");
+
+//   const submitSquareMetersContainer = document.getElementById(
+//     "submitSquareMetersContainer"
+//   );
+//   submitSquareMetersContainer.classList.add("active");
+// });
+
+// const sendUserMessageSquareMetersBtn = () => {
+//   const message = document.getElementById("submitSquareMetersInput");
+//   showMessage(message.value, `user`);
+
+//   sendBotMessage(
+//     `Super, we have registered that your home is ${message.value} square meters. We will contact you with an offer you can't refuse!`
+//   );
+
+//   message.value = "";
+// };
+
+// const submitSquareMetersBtn = document.getElementById("submitSquareMetersBtn");
+// submitSquareMetersBtn.addEventListener("click", sendUserMessageSquareMetersBtn);
+
+// document
+//   .getElementById("submitSquareMetersInput")
+//   .addEventListener("keypress", function (e) {
+//     if (e.key === "Enter") {
+//       sendUserMessageSquareMetersBtn();
+//     }
+//   });
