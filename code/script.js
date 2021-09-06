7ae444a7154d3bf6d1172f748c327f9c6b647de2
Helena Wiklund <helena.wiklund@live.se>
2021-09-06 17:27:57 +0200

    made the bot put another question and made some variabels for name and inputfield, Eventlistener and function

--- code/script.js
+++ code/script.js
@@ -1,5 +1,7 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const userInput = document.getElementById('name-input')
+const form = document.getElementById('name-form')
 
 // Global variables, if you need any, declared here
 
@@ -8,7 +10,6 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
-    console.log(message)
      chat.innerHTML += `
        <section class="user-msg">
          <div class="bubble user-bubble">
@@ -18,7 +19,6 @@ const showMessage = (message, sender) => {
        </section>
      `
   } else if (sender === 'bot') {
-    console.log(message)
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -34,12 +34,22 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  showMessage('Hej', 'user')
+  showMessage(`Hello and welcome to the cake factory &#x1F370 What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+  form.addEventListener('submit', (event) => {
+   event.preventDefault()
+   const name = userInput.value 
+   console.log(name)  
+  showMessage(name, 'user')
+  userInput.value = ''
+  setTimeout(() => showFoodOptions(name), 1000)
+  });
 
+  const showFoodOptions = (name) => {
+    showMessage(`Wish cake are you upp to mode for today ${name}`, 'bot')
+  }
 
 // Set up your eventlisteners here
 
@@ -49,4 +59,5 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 1000);
+
