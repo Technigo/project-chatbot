a1814577a8bfdcfc3b1aa09d50bbf531f04803ef
fatigr26 <fatigr2692@gmail.com>
2021-09-07 18:30:09 +0200

    added some javascript

--- code/script.js
+++ code/script.js
@@ -1,12 +1,17 @@
-// All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+// DOM SELECTORS 
+const chat = document.getElementById('chat');
+const button = document.getElementById('btn')
+const nameInput = document.getElementById('name-input');
 
-// Global variables, if you need any, declared here
+let currentQuestion = 1;
 
-// Functions declared here
+//EVENTS LISTENER
 
-// This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
+// FUNCTIONS:
+
+// 1.Function showMessage: This function will add a chat bubble in the correct place based on who the sender is
+const showMessage = (message, sender) => { 
+
   if (sender === 'user') {
     chat.innerHTML += `
       <section class="user-msg">
@@ -15,7 +20,7 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    ` 
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
@@ -26,17 +31,43 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight// This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
 }
 
-// Starts here
+// 2. Function greeting: This function invokes ('calls') the first function, so when the function greeting is invokes, invokes the first function.
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+showMessage(`What's your name?`, 'bot')
 }
 
-// Set up your eventlisteners here
+setTimeout(greeting,1000);
+
+// 3. Function HandleNameInput
+ const handleNameInput = (event) => {
+  event.preventDefault()
+
+  if (currentQuestion === 1) {
+    let name = nameInput.value
+    showMessage(name, 'user')
+    nameInput.value = ''
+  
+    setTimeout(() => showFoodOptions(name), 1000)
+    setTimeout(() => {
+      showMessage(`What type of flowers do you need, roses,tulips,sunflowers? ${name}?`, 'bot')
+    }, 2000);
+    currentQuestion = 2;
+
+  } else if (currentQuestion === 2){
+  console.log('Question2')
+  }
+  
+}
+
+button.addEventListener('click',handleNameInput);
+
+
+
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -44,4 +75,3 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
