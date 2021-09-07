66096787b1221748078bdb453faab12426b7325c
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-09-07 17:23:24 +0200

    Iteration 2

--- code/script.js
+++ code/script.js
@@ -34,7 +34,7 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there, What are you craving?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
