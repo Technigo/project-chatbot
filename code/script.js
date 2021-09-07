c5dcd9e3c356a765bfdba86e016cc00d2e31ea92
Никитина Юля <nik.iuliiasw@gmail.com>
2021-09-07 16:13:09 +0200

    first, second, third questions are done

--- code/script.js
+++ code/script.js
@@ -3,8 +3,12 @@ const chat = document.getElementById('chat')
 const inputWrapper = document.getElementById('input-wrapper')
 const nameInput = document.getElementById('name-input')
 const form = document.getElementById('name-form')
+// const sendButton = document.getElementById('send')
 
 // Global variables, if you need any, declared here
+// let questionStep = 1
+
+
 
 // Functions declared here
 
@@ -46,21 +50,79 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  botAnswer(`Welcome to our salon! <br> What's your name?`)
+  botAnswer(`Welcome to our salon! What's your name?`)
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 // Set up your eventlisteners here
-
+// 1st user answer
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   const name = nameInput.value
   userAnswer(name)
   nameInput.value = ''
-  setTimeout(() => showPetTypes(name), 1000)
+  setTimeout(() => petTypes(name), 1000)
+  //   questionStep = 2
+  // } else if (questionStep === 2) {
+  //   userAnswer(form.pet.value)
+  // }
 })
 
 
+
+// 2nd question and answer
+const usersPet = (type) => {
+  userAnswer(type)
+  setTimeout(() => petServices(type), 1000)
+}
+
+
+const petTypes = (name) => {
+  botAnswer(`Nice to meet you ${name} :) What type of pet do you have?`)
+
+  inputWrapper.innerHTML = `
+  <button id= 'dogButton'>Dog</button>
+  <button id= 'catButton'>Cat</button>
+  `
+  document.getElementById('dogButton').addEventListener('click', () => usersPet('dog'))
+  document.getElementById('catButton').addEventListener('click', () => usersPet('cat'))
+
+}
+
+
+// form.innerHTML = `  
+//   <label> Dog
+//   <input type='radio' value='dog' name='pet' />
+//   </label>
+//   <label> Cat
+//   <input type='radio' value='cat' name='pet' />
+//   </label>
+//   <button type='submit'>Submit</button>
+// `
+
+
+// third question and answer
+const usersService = (service) => {
+  userAnswer(service)
+  setTimeout(() => petPayment(service), 1000)
+}
+
+
+const petServices = (type) => {
+  botAnswer(`So you are the ${type} person :) Please select a procedure for your pet.`)
+
+  inputWrapper.innerHTML = `
+    <button id='firstService'>Bathing</button>
+    <button id='secondService'>Brushing</button>
+    <button id='thirdService'>Nail trimming</button>
+    <button id='forthService'>Haircut</button>
+  `
+  document.getElementById('firstService').addEventListener('click', () => usersService('Bathing'))
+  document.getElementById('secondService').addEventListener('click', () => usersService('Brushing'))
+  document.getElementById('thirdService').addEventListener('click', () => usersService('Nail trimming'))
+  document.getElementById('forthService').addEventListener('click', () => usersService('Hair cut'))
+
+}
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -68,4 +130,4 @@ form.addEventListener('submit', (event) => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded. Tried to changed to 2000
-setTimeout(greeting, 2000)
+setTimeout(greeting, 1000)
