86934dc09cec0e944f94deb3c41fe011df733a2a
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-09-08 09:17:22 +0200

    added bot reply and some logic to the bot

--- code/script.js
+++ code/script.js
@@ -1,18 +1,28 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const welcomeButton = document.getElementById('welcomeBtn')
+const sendBtn = document.getElementsByClassName('send-btn')
+const input = document.getElementById('input')
+const inputWrapper = document.getElementById('input-wrapper')
+
 
 // Global variables, if you need any, declared here
 
 // Functions declared here
 
+let questionNumber = 1
+
+const botReply = (message) => {
+  showMessage(message, 'bot')
+}
+
+const userReply = (message) => {
+  showMessage(message, 'user')
+}
+
 // This function will add a chat bubble in the correct place based on who the sender is
 
 
-  //welcomeButton.addEventListener('click', (greeting) => {
-    //button.style.display = 'none';
-  //})
-//}
 
 const showMessage = (message, sender) => {
   if (sender === 'user') {
@@ -42,17 +52,136 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
+ questionNumber = 0
   showMessage(`Hello there Beat Bot!`, 'user')
+  setTimeout (() => botGreeting(), 1000)
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 welcomeButton.onclick = function () {
   if (welcomeButton.style.display !== 'none') {
     welcomeButton.style.display = 'none'; {
-      greeting(showMessage, 'bot')
+      greeting(showMessage, 'user')
       }
     };
 }
+
+const botGreeting = () => {
+  showMessage(`Hello there human, what's your name?`, 'bot')
+  nextQuestion()
+  
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
+
+
+/*const nextQuestion = ()=>{
+  console.log('questionNumber', questionNumber)
+ if (questionNumber === 1){
+    botGreeting(`Hello there human, what's your name?`, 'bot')
+    input.value =''
+    
+  }
+}*/
+
+const handleNameInput = (event) => {
+  event.preventDefault()
+  
+  const name = nameInput.value
+  showMessage(name, 'user')
+  nameInput.value = ''
+
+  setTimeout(() => whatMood(name), 1000)
+}
+
+const whatMood = () => {
+  questionNumber = 1
+  botReply(`Oh ${name} what mood are you in?`)
+}
+
+const moods = (type) => {
+  questionNumber++
+
+  botReply(`Totally understand your ${type} mood, let me give you some beats to match it`)
+
+  if(type === 'sad', 'depressed', 'blue') {
+   inputWrapper.innerHTML = `
+   <select id="select">
+    <option value="" selected disabled> Select a beat..</option>
+    <option value="Hassle">Erik Hassle - Hurtful</option>
+    <option value="TheFray">The Fray - How to save a life</option>
+    <option value="Coldplay">Coldplay - Fix you</option>
+  </select>
+  `
+  } else if (type === 'happy', 'glad', 'excited') {
+    inputWrapper.innerHTML = `
+   <select id="select">
+    <option value="" selected disabled> Select a beat..</option>
+    <option value="Pharell">Pharell Williams - Happy</option>
+    <option value="Timberlake">Justin Timberlake - Can't stop the feeling</option>
+    <option value="Beyonce">Beyonce - Single ladies</option>
+  </select>
+  `
+  } else if (type === 'angry', 'mad', 'frustrated', 'irritated') {
+    inputWrapper.innerHTML = `
+   <select id="select">
+    <option value="" selected disabled> Select a beat..</option>
+    <option value="Pink">Pink - So what</option>
+    <option value="Swift">Taylor Swift - I knew you were trouble</option>
+    <option value="CudiWest">Kid Cudi, Kanye West - Erase Me</option>
+  </select>
+  `
+ } else (type === 'in love', 'love', 'romantic'); {
+  inputWrapper.innerHTML = `
+ <select id="select">
+  <option value="" selected disabled> Select a beat..</option>
+  <option value="Legend">John Legend - All of me</option>
+  <option value="Houston">Whitney Houston - I will always love you</option>
+  <option value="Carey">Mariah Carey - We Belong Together</option>
+</select>
+`
+ }
+
+
+
+/*const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber)
+
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => whatMood(message), 1000)
+  } else if (questionNumber === 2) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => moods(message), 1000)
+  } else if (questionNumber === 3) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => showDishSize(message), 1000)
+  } else if (questionNumber === 4) {
+    userReply(message)
+    setTimeout(() => showPrice(message), 1000)
+  } else {
+    userReply(message)
+    setTimeout(thankYou, 1000)
+  }
+}*/
+
+
+/*const nextQuestion = ()=>{
+  console.log('questionNumber', questionNumber)
+  switch(questionNumer){
+    case 1: //botens nästa sak
+      break;
+    case 2:
+      break;
+      default:
+        break;
+  }
+  
+}*/
+
+
 // Set up your eventlisteners here
 
 // When website loaded, chatbot asks first question.
@@ -62,3 +191,8 @@ welcomeButton.onclick = function () {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 //setTimeout(greeting, 1000)
+
+sendBtn.addEventListener('click', () => nextQuestion(input.value))
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter' && input.value) nextQuestion(input.value)
+})
