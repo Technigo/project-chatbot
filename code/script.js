efc103f83362b09e48da4d78245e3611e1a6825d
Eddie Lundgren <Eddie.lundgren@hotmail.com>
2021-09-08 18:28:19 +0200

    Iteration 3 donezo!

--- code/script.js
+++ code/script.js
@@ -1,18 +1,18 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn') 
 const form = document.getElementById('name-form')
-const nameInput = document.getElementById('name-input')
-const button = document.getElementById('button')
-const inputBox = document.getElementById('input-wrapper')
-const startButton = document.getElementById('start-btn')
+const inputValue = document.getElementById('name-input')
+
 // Global variables, if you need any, declared here
-let questionNumber = 1 
-// Functions declared here
+let currentQuestion = 1 
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log('user is sending') 
+
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -22,6 +22,8 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
+    console.log('bot is sending') 
+
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -35,30 +37,59 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-
-  
 // Starts here
 const greeting = () => {
-  questionNumber = 1
-  botReply(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-const complimentQuestion = () => {
-  showMessage(`What type of compliment do you want?`, 'bot')
- 
+const ageQuestion = () => {
+  showMessage('How old are you?', 'bot')
+}
+
+const petQuestion = () => {
+  showMessage('Do you have pets?', 'bot')
+}
+
+const handleInput = (event) => {
+  event.preventDefault()
+  
+  console.log('our currentQuestion variable is:', currentQuestion)
+
+  if (currentQuestion === 1) {
+    handleNameQuestion()
+  } else if (currentQuestion === 2) {
+    handleAgeQuestion()
+  } else if (currentQuestion === 3) {
+    handlePetQuestion()
+  }
+  
+  currentQuestion++
+  console.log('handing over to the bot with a new currentQuestion value', currentQuestion)
+}
+
+const handleNameQuestion = () => {
+  const name = inputValue.value
+
+  showMessage(`My name is ${name}`, 'user')
+  inputValue.value = ''
+
+  setTimeout(ageQuestion, 1000) 
+
+}
+
+const handleAgeQuestion = () => {
+  const age = inputValue.value
+
+  showMessage(`I am ${age} years young`, 'user')
+  inputValue.value = ''
+
+  setTimeout(petQuestion, 1000)
 }
 
 // Set up your eventlisteners here
-  form.addEventListener('submit', (event) => {
-    event.preventDefault ()
-    const name = nameInput.value
-    showMessage(name, 'user')
-    nameInput.value = ''
-  })
-  startButton.addEventListener('click', greeting)
-  
-  //Tack Patrik & Jessi
+form.addEventListener('submit', handleInput)
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -67,3 +98,4 @@ const complimentQuestion = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
+
