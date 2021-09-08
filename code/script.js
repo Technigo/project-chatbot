0cee116c07430e5f670d1f1bd9c3b6ced5281c86
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-09-08 18:11:52 +0200

    added some styling. added choices for hairdresser, date and time and confirmation. added goodbye message

--- code/script.js
+++ code/script.js
@@ -1,11 +1,20 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+const input = document.getElementById('input')
+const sendBtn = document.getElementById('send')
 
 // Global variables, if you need any, declared here
+let questionNumber = 1
 
-// Functions declared here
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
+}
 
-// This function will add a chat bubble in the correct place based on who the sender is
+const userReply = (msg) => {
+  showMessage(msg, 'user')
+}
+
 const showMessage = (message, sender) => {
   if (sender === 'user') {
     chat.innerHTML += `
@@ -30,10 +39,189 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber)
+
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => bookOrCancel(message), 1000)
+  } else if (questionNumber === 2) {
+    userReply(message)
+    setTimeout(() => showMenu(message), 1000)
+  } else if (questionNumber === 3) {
+    userReply(message)
+    setTimeout(() => showHairdresser(message), 1000)
+  } else if (questionNumber === 4) {
+    userReply(message)
+    setTimeout(() => showDate(message), 1000)
+  } else if (questionNumber === 5) {
+    userReply(message)
+    setTimeout(() => showTime(message), 1000)
+  } else if (questionNumber === 6) {
+    userReply(message)
+    setTimeout(() => showConfirmation(message), 1000)
+  } else {
+    userReply(message)
+    setTimeout(thankYou, 1000)
+  }
+}
+
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  questionNumber = 1
+  botReply(`Welcome to Urban Hairdo! What is your name?`)
+}
+
+const handleNameInput = document.getElementById('name-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  const nameInput = document.getElementById('name-input')
+  const userName = nameInput.value
+
+  userReply(userName, 'user')
+  nameInput.value = ''
+  setTimeout(() => bookOrCancel(userName), 1000)
+}
+)
+
+// Question2 (booking or canceling)
+
+const bookOrCancel = (msg) => {
+  questionNumber++
+  botReply(`Hello ${msg}! Do you wish to book or cancel an appointment?`)
+
+  inputWrapper.innerHTML = `
+  <button id="bookBtn">Book</button>
+  <button id="cancelBtn">Cancel</button>
+`
+
+  document
+    .getElementById('bookBtn')
+    .addEventListener('click', () => nextQuestion('Book'))
+  document
+    .getElementById('cancelBtn')
+    .addEventListener('click', () => nextQuestion('Cancel'))
+}
+
+// Question3 (choose treatment)
+
+const showMenu = (type) => {
+  questionNumber++
+
+  if (type === 'Book') {
+    botReply(`We can help you with that! Please choose type of appointment.`)
+    inputWrapper.innerHTML = `
+        <select id="select">
+          <option value="" selected disabled>Select treatment </option>
+          <option value="haircut">Haircut</option>
+          <option value="coloring">Coloring</option>
+          <option value="hair treatment">Hair Treatment</option>
+        </select>
+      `
+  } else {
+    botReply(`Please call 08-123 45 67 to cancel your appointment. Sorry to see you go!`)
+    inputWrapper.innerHTML = ``
+  }
+
+  const select = document.getElementById('select')
+  select.addEventListener('change', () => nextQuestion(select.value))
+}
+
+// Question4 (choose hairdresser)
+
+const showHairdresser = (treatment) => {
+  questionNumber++
+
+  botReply(`Awesome! You wish to book ${treatment}! Which hairdresser would you like to book?`)
+
+  inputWrapper.innerHTML = `
+  <button id="bookAnna">Anna</button>
+  <button id="bookHaru">Haru</button>
+`
+  document
+    .getElementById('bookAnna')
+    .addEventListener('click', () => nextQuestion('Anna'))
+  document
+    .getElementById('bookHaru')
+    .addEventListener('click', () => nextQuestion('Haru'))
+}
+
+// Question5 (choose date)
+
+const showDate = (hairdresser) => {
+  questionNumber++
+  botReply(`${hairdresser} is available for booking! Which date do you want to come?`)
+
+  if (hairdresser === 'Anna') {
+    inputWrapper.innerHTML = `
+    <form id="input">
+      <label for="date"></label>
+      <input type="date" id="date" name="date">
+    </form>
+    `
+  } else {
+    inputWrapper.innerHTML = `
+    <form id="input">
+      <label for="date"></label>
+      <input type="date" id="date" name="date">
+    </form>
+    `
+  }
+
+  const input = document.getElementById('date')
+  input.addEventListener('change', () => nextQuestion(input.value))
+}
+
+//Question6 (choose time)
+const showTime = (date) => {
+  questionNumber++
+  botReply(`These are the available times on ${date}.`)
+
+  inputWrapper.innerHTML = `
+  <button id="time1" value="10.30">10.30</button>
+  <button id="time2" value="11.00">11.00</button>
+  <button id="time3" value="13.30">13.30</button>
+  <button id="time4" value="14.30">14.30</button>
+  <button id="time5" value="16.00">16.00</button>
+  `
+  let button = document.getElementById('time1')
+  button.addEventListener('click', () => nextQuestion(button.value))
+
+  let button2 = document.getElementById('time2')
+  button2.addEventListener('click', () => nextQuestion(button2.value))
+
+  let button3 = document.getElementById('time3')
+  button3.addEventListener('click', () => nextQuestion(button3.value))
+
+  let button4 = document.getElementById('time4')
+  button4.addEventListener('click', () => nextQuestion(button4.value))
+
+  let button5 = document.getElementById('time5')
+  button5.addEventListener('click', () => nextQuestion(button5.value))
+}
+
+//Question7 (confirm booking)
+const showConfirmation = (time) => {
+  questionNumber++
+  botReply(`Are you sure you want to book this ${time}?`)
+
+  inputWrapper.innerHTML = `
+  <button id="confirm">Yes</button>
+  <button id="restart">No</button>
+  `
+  document
+    .getElementById('confirm')
+    .addEventListener('click', () => nextQuestion('Yes'))
+
+  document.getElementById('restart').addEventListener('click', () => {
+    location.reload()
+    return false
+  })
+}
+
+const thankYou = () => {
+  botReply(`Thank you for choosing Urban Hairdo! Looking forward to seeing you!`)
+  inputWrapper.innerHTML = ``
 }
 
 // Set up your eventlisteners here
