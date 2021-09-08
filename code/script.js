0545f7828837b13fe7a4d5812abdac57ddb6f512
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-09-08 13:31:10 +0200

    iteration 1, changed bot to user row 37, tried different greeting-timeouts, row 15 removed the template literals, row 11 & 21 added console.log to see how it works.

--- code/script.js
+++ code/script.js
@@ -8,15 +8,17 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log('this is the user talking') //this is shown because we changed to 'user' in const greeting.
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
-          <p>${message}</p>
+          <p>message</p>
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    ` // at row 15 - when we removed the ${}/template literals the bot writes "message" since we no longer are calling the message-string.
   } else if (sender === 'bot') {
+    console.log('this is the bot talking')//this is NOT showing because we changed to 'user' in const greeting.
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -32,7 +34,7 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there, What's your name?`, 'user')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
@@ -44,4 +46,10 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+  // setTimeout(greeting, 1000)
+
+// makes the greeting take 6 seconds to show after the page is loaded
+// setTimeout(greeting, 6000)
+
+// makes the greeting appear directly when the page is loaded
+setTimeout(greeting)
