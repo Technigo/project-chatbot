9c0cf5b49801886036218706734d82aecc71e21f
Darya <darya.lapata@gmail.com>
2021-09-08 15:50:22 +0200

    added some new functions

--- code/script.js
+++ code/script.js
@@ -1,8 +1,12 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat');
-const form = document.getElementById('name-form')
+const form = document.getElementById('name-form'); 
+const button = document.getElementById('button');
+const nameInput = document.getElementById('name-input');
+const inputWrapper = document.getElementById('input-wrapper');
 
 // Global variables, if you need any, declared here
+let currentQuestion = 0
 
 // Functions declared here
 
@@ -31,25 +35,81 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber)
 
-// Starts here (starter code)
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => showFoodTypes(message), 1000)
+  } 
+}
+
+
+const handleNameInput = () => {
+  currentQuestion ++ ; // fr nr 1
+  console.log(currentQuestion)
+
+  const name = nameInput.value
+  showMessage(name, 'user')
+  nameInput.value = '';
+
+
+  setTimeout( () => 
+    showMessage(`Nice to meet you ${name} `, 'bot'), 1000);
+  
+
+  setTimeout(() => showFoodOptions(name), 2000)
+  
+  
+}
+
+const showFoodOptions = () => {
+  currentQuestion++ ; // fråga nr 2
+  console.log(currentQuestion)
+  showMessage(`What would you like to order? `, 'bot')
+  inputWrapper.innerHTML = `
+  <button id="pizza-button">Pizza</button>
+  `
+ 
+  const pizzaButton = document.getElementById('pizza-button')
+
+  pizzaButton.addEventListener('click', () => {
+    // console.log("clicked pizza button")
+    // handleNameInput()
+    showMessage(`pizza`, 'user');
+    setTimeout( () => 
+    showMessage(`What type of pizza would you like to order?`, 'bot'), 1000);
+    inputWrapper.innerHTML = `
+    <select id="select">
+    <option value="" selected disabled>👇 Select a pizza...</option>
+    <option value="margerita">Margerita</option>
+    <option value="vesuvio">Vesuvio</option>
+    <option value="peperoni">Peperoni</option>
+  </select>
+  `
+  })
+
+}
+
+const handlePizzaButton = () => {
+
+}
+
+// Starts here 
 
 const greeting = () => {
   showMessage(`Hi, my name is Pizzatone! What's your name?`, 'bot');
-  showMessage(`Hi, my name is Pizzatone! What's your name?`, 'user');
-  showMessage(`Hi, my name is Pizzatone! What's your name?`, 'bot');
 
- 
-  // showMessage(`Hi, my name is Pizzatone! What's your name?`, 'user');
-  
-
-  // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 
 // Set up your eventlisteners here
-form.addEventListener('click', () => {
-  setTimeout(greeting, 2000)
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleNameInput()
+
+
 })
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -58,3 +118,4 @@ form.addEventListener('click', () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 // setTimeout(greeting, 1000)
+setTimeout(greeting, 1000);
