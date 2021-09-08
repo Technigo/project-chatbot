2ebd15a246ee3503debb5c5c8944e8037c4849fe
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2021-09-08 18:36:07 +0200

    start button + question 2

--- code/script.js
+++ code/script.js
@@ -1,24 +1,15 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const form = document.getElementById('name-form')
-const input = document.getElementById('user-input')
+const input = document.getElementById('name-input')
 const inputWrapper = document.getElementById('input-wrapper')
+const startButton = document.getElementById ('start-btn')
 
 // Global variables, if you need any, declared here
 
-const questionNumber = 1
+let currentQuestion = 1
 
 // Functions declared here
-
-let nextQuestion = (message) => {
-  if (questionNumber === 1) {
-    handleNameInput(message)
-    input.value = ''
-  }
-  else if (questionNumber === 2){
-    pizzaOption (message)
-  }
-}
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -47,40 +38,50 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there! What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-const handleNameInput = () => {
-const name = input.value
-  console.log(name)
-  showMessage(name, 'user')
-  input.value = ''
-  showMessage (`Hello, ${name}!`, 'bot')
+const handleNameQuestion = () => {  
+  currentQuestion = 1
+  const name = input.value
+    console.log(name)
+    showMessage(name, 'user')
+    input.value = ''
+    showMessage (`Hello, ${name}! We're ready to take your order`, 'bot')
+    setTimeout(pizzaOption, 1000)
+    }
+
+const pizzaOption =() => {
+  currentQuestion = 2
+  showMessage (`Which pizza would you like?`, 'bot')
+  // const whatPizza = () => {
+  //     <button id="peperoni">peperoni</button>
+  //     <button id="funghi">funghi</button>
+  //     <button id="margharita">margharita</button>
+  //     }
+    // }
+
 }
 
+const handleInput = () => {     //this is where we add new questions
+  event.preventDefault()
 
+  console.log(currentQuestion)
+  if(currentQuestion === 1){
+    handleNameQuestion()
+  } else if(currentQuestion === 2){
+    pizzaOption()
+  }
 
-// const pizzaOptions = () => {
-//   questionNumber++
-//   showMessage (`Which pizza would you like?`, 'bot')
-//   inputWrapper.innerHTML =
-
-//   const whatPizza = () => {
-//     <button id="peperoni">peperoni</button>
-//     <button id="funghi">funghi</button>
-//     <button id="margharita">margharita</button>
-//   }
-// }
-
+}
 
 
 // Set up your eventlisteners here
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  handleNameInput()
-})
+startButton.addEventListener('click', () => setTimeout (greeting, 500))
+
+form.addEventListener('submit', handleInput)
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -88,4 +89,6 @@ form.addEventListener('submit', (event) => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+
+
+// setTimeout(greeting, 1000)
