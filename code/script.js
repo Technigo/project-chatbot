1bc11200c71e810cb1954026d06ab0a421fcb0a9
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-09-08 21:02:30 +0200

    Iteration 4 started

--- code/script.js
+++ code/script.js
@@ -1,14 +1,55 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const form = document.getElementById('name-form')
+const userName = document.getElementById('name-input')
+const sendBtn = document.getElementById('send-btn')
+//const foodOptions = document.querySelector('.food-options')
 
 // Global variables, if you need any, declared here
+let currentQuestion = 1
 
+
+ 
 // Functions declared here
+const handelInput = (event) => {
+  event.preventDefault()
+  
+  if (currentQuestion===1) {
+  const userInput = userName.value
+  userName.value = ""
+  showMessage(userInput, 'user')
+  setTimeout(() => {
+    nextQuestion (userInput)
+  }, 1000)
+  } else if (currentQuestion === 2) {
+    const userInput2= event.target.textContent
+    showMessage(`${userInput2}`, 'user')
+  }
+  
+}
+
+const nextQuestion = (preAnswer) => {
+  
+  if (currentQuestion === 1) {
+    showMessage(`Welcome ${preAnswer}! What are you craving for today?`, 'bot')
+    userName.style.display = "none"
+    sendBtn.style.display = "none"
+    
+    form.innerHTML += `
+
+   <div class="food-options" id="optionBtn">
+     <button id="optionBtn">Phad Thai</button>
+    <button id="optionBtn">Paneng Curry</button>
+    <button id="optionBtn">Tom Kha Gai</button>
+    </div>
+   `
+    currentQuestion++
+  }
+}
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
-    console.log('user sending message')
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -18,7 +59,6 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
-    console.log('bot sending message')
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -33,77 +73,18 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
-const confirmation = (btnType) => {
-  
-  if (btnType === 1) {
-    showMessage ('Awesome, Pad Thai it is!', 'bot')
-  } else if (btnType === 2) {
-    showMessage ('Yay, we love Paneng too!', 'bot')
-  } else  {
-    showMessage('Good Choice, you will love it!', 'bot')
-  }
-}
-
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there!, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-const userName = document.getElementById("name-input")
-const sendButton=document.getElementById("send-btn")
-
-sendButton.addEventListener("click", (event) => { 
-  event.preventDefault();
-  const name = userName.value
-  showMessage(name, 'user')
-  userName.value = ""
-   
-  setTimeout(() =>{
-    showMessage('${name}! What are you craving?', 'bot')
-    },1000) //delay is in milliseconds 
-})
-  
-const foodOption=document.getElementById("phadThaiBtn")
-foodOption.addEventListener("click", (event) => {
-  event.preventDefault();
-  showMessage ('1 Pad Thai, please', 'user')
-  document.getElementById("buttons").style.display = "none";
-  setTimeout(() =>{
-    showMessage('Awesome, Pad thai it is!', 'bot')
-    },1000) //delay is in milliseconds 
-  
- 
-})
-
-const foodOption2=document.getElementById("panengBtn")
-foodOption2.addEventListener("click", (event) => {
-  event.preventDefault();
-  showMessage('1 Paneng, please', 'user')
-  document.getElementById("buttons").style.display = "none";
-  setTimeout(() =>{
-    showMessage ('Yay, we love Paneng!', 'bot')
-    },1000) //delay is in milliseconds 
-  
-})
-
-const foodOption3=document.getElementById("tomKhaGaiBtn")
-foodOption3.addEventListener("click", (event) => {
-  event.preventDefault();
-  showMessage ('1 Tom Kha Gai, please', 'user')
-  document.getElementById("buttons").style.display = "none";
-  setTimeout(() =>{
-    showMessage('Good Choice, you will love it!', 'bot')
-    },1000) //delay is in milliseconds 
-  
- 
-  
-})
-
-
-
-
-
 // Set up your eventlisteners here
+
+form.addEventListener('submit', handelInput)
+
+//foodOptions.addEventListener('click', handelInput)
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -111,4 +92,4 @@ foodOption3.addEventListener("click", (event) => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 500)
+setTimeout(greeting, 1000)
