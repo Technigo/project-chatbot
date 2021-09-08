0762ad9742b0c521946637843657299a07d6028a
Jenny Quach <quach.jen@gmail.com>
2021-09-08 20:59:00 +0200

    Co-authored-by: vrill <vrill@users.noreply.github.com>

--- code/script.js
+++ code/script.js
@@ -1,13 +1,17 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-
+// const btn = document.getElementsByClassName('send-btn')
+// const name = document.getElementById('name-input')
 // Global variables, if you need any, declared here
 
 // Functions declared here
 
+
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    
+
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,11 +21,12 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
+    console.log('hello')
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
-          <p>${message}</p>
+          <p>message</p>
         </div>
       </section>
     `
@@ -31,12 +36,16 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
+
+// setTimeout(greeting, 1000)
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 // Set up your eventlisteners here
+// btn.addEventListener('Click', showMessage())
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
