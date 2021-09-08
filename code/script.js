b898fee3a0c225d998f3e12f9c2e6e9935984052
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-09-08 19:22:10 +0200

    Iteration 4 - added buttons with multiple answering alternatives, added DOM selectors

--- code/script.js
+++ code/script.js
@@ -3,6 +3,7 @@ const chat = document.getElementById('chat')
 const userInput = document.getElementById('user-input')
 const form = document.getElementById('chat-form')
 const send = document.getElementById('send')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // Global variables, if you need any, declared here
 
@@ -10,6 +11,7 @@ let questionNumber = 0
 
 
 // Functions declared here
+
 
 
 // This function will add a chat bubble in the correct place based on who the sender is
@@ -49,13 +51,49 @@ const greeting = () => {
   showMessage(`Hello there! I'm Plot, the plant bot. What's your name?`, 'bot')
 }
   
-const favColor = () => {
-  showMessage(`What is your favourite color?`, 'bot')
+// const favColor = () => {
+//   showMessage(`What is your favourite color?`, 'bot')
+// }
+
+const whatPlant = () => {
+  showMessage(`Nice to meet you!`, 'bot') 
+  showMessage(`What kind of plants do you like?`, 'bot')
+
+    inputWrapper.innerHTML = `
+    <button id="flowers">🌺</button>
+    <button id="edibles">🌶</button>
+    <button id="thorny">🌵</button>
+  `
+
+    document.getElementById('flowers')
+    .addEventListener('click', () => plantSelection('flowers'))
+    document.getElementById('edibles')
+    .addEventListener('click', () => plantSelection('edibles'))
+    document.getElementById('thorny')
+    .addEventListener('click', () => plantSelection('thorny'))
 }
 
-const likesFlowers = () => {
-  showMessage(`Do you like flowers?`, 'bot')
-}
+const plantSelection = (choice) => {
+  questionNumber++
+
+
+  if (choice === 'flowers') {
+    inputWrapper.innerHTML = `
+    <button id="oneBig">One BIG flower 🌸</button>
+    <button id="plentySmall">A bouquet 💐</button>
+    `
+  } else if (choice === 'edibles') {
+      inputWrapper.innerHTML = `
+      <button id="sweet">I have sweet tooth 🍓</button>
+      <button id="hot">I like it HOT 🔥</button>
+      `
+    } else if (choice === 'thorny') {
+      inputWrapper.innerHTML = `
+      <button id="cactus">Low maintenance 🌵</button>
+      <button id="rose">Something deceivingly beautiful 🌹</button>
+      ` 
+    }
+  }
 
 const handleInput = (event) => {
   event.preventDefault()
@@ -63,7 +101,7 @@ const handleInput = (event) => {
     if (questionNumber === 1) {
       handleNameQuestion()
     } else if (questionNumber === 2) {
-      handleColorQuestion()   
+      handlePlantQuestion()   
     }
 }
 
@@ -71,14 +109,14 @@ const handleNameQuestion = () => {
   const name = userInput.value
   showMessage(`My name is ${name}!`, 'user')
   userInput.value = ''
-  setTimeout(favColor, 1000)
+  setTimeout(whatPlant, 1000)
 }
 
-const handleColorQuestion = () => {
+const handlePlantQuestion = () => {
   const color = userInput.value
   showMessage(`My favourite color is ${color}!`, 'user')
   userInput.value = ''
-  setTimeout(likesFlowers, 1000)
+  setTimeout(1000)
 }
 
 // Set up your eventlisteners here
