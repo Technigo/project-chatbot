a1668a48ff7b02efd87fa715d669385fd208b872
Reinis <reinis@Reiniss-MacBook-Pro.local>
2021-09-08 18:09:34 +0300

    done with two questions

--- code/script.js
+++ code/script.js
@@ -1,9 +1,29 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const ourForm = document.getElementById("name-form");
 
 // Global variables, if you need any, declared here
-
+let question=1;
 // Functions declared here
+
+const whichPlanet = () =>{
+  ourForm.innerHTML= `
+  <select>
+    <option name="planet" value="Technigo" id="technigo">Technigo</option>
+    <option name="planet" value="Tatooine" id="tatooine">Tatooine</option>
+    <option name="planet" value="Abafar" id="abafar">Abafar</option>
+  </select>
+  <button class="send-btn" type="submit">Select</button>
+`
+}
+
+const threeButtons = () =>{
+  ourForm.innerHTML=`
+  <button class="send-btn" type="submit">I´m lost</button>
+  <button class="send-btn" type="submit">Looking for an answer</button>
+  <button class="send-btn" type="submit">I´m good</button>
+  `
+}
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -13,7 +33,7 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user-small.gif" alt="User" />  
       </section>
     `
   } else if (sender === 'bot') {
@@ -36,29 +56,31 @@ const greeting = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-// // Set up your eventlisteners here
-let answer= document.getElementById('name-form');
 
-// Adding event listnener and displaying the answer in the chat
-  answer.addEventListener('submit', (event) =>{
+const answer= (event)=>{
   event.preventDefault();
-  showMessage(`You are: ${event.target[0].value}`, 'user');
-  const respond = () =>{
-    showMessage(`Hello ${event.target[0].value}`, 'bot')
+  if(question=== 1){
+    showMessage(`${event.target[0].value}`, "user");
+    setTimeout(() => showMessage(`Where do you come from ${event.target[0].value}?`, 'bot'), 1000);
+    setTimeout(whichPlanet, 1500);
+    question=2
   }
-  setTimeout(respond, 2000);
-  // setTimeout(whichPlanetYouAreFrom, 2000);
-});
-// // .target[0].value
+  else if (question===2){
+    
+    showMessage(`${event.target[0].value}`, "user");
+    setTimeout(() => showMessage(`${event.target[0].value}, a great school of jedi. What are you looking for?`, 'bot'), 1000);
+    setTimeout(threeButtons, 1500);
+    question=3
+  }
+  // else if (question===3){
+    
+  //   showMessage(`${event.target[0].value}`, "user");
+  //   setTimeout(() => showMessage(`Ah, to the right place you come? I was expecting you and I know your question!`, 'bot'), 1000);
+  //   question=4
+  // }
+}
+// // Set up your eventlisteners here
+document.getElementById('name-form').addEventListener('submit', answer);
 
 
-
-
-
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 500)
