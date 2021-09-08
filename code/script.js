e9c2f7cdd70c3fccf3bb4e0efe5dead78a493ac4
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2021-09-08 09:25:20 +0200

    first iteration

--- code/script.js
+++ code/script.js
@@ -1,5 +1,10 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+const input = document.getElementById('input')
+const sendBtn = document.getElementById('send')
+const nameInput = document.getElementById('name-input')
+const nameForm = document.getElementById('name-form')
 
 // Global variables, if you need any, declared here
 
@@ -8,6 +13,7 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log("I am user!")
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,6 +23,7 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
+    console.log("I am bot!")
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -37,11 +44,16 @@ const greeting = () => {
 }
 
 // Set up your eventlisteners here
-
+nameForm.addEventListener('submit', (event) => {
+    event.preventDefault ()
+    const name = nameInput.value
+    showMessage(name, 'user')
+    nameInput.value = ''
+    })
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 500)
