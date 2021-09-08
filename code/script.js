1b87b12734bdb4dc332eb7220aea2e6cb66793a3
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-09-08 17:22:22 +0200

    Iteration 3, added more questions, added if--statements, added eventlistener, added global variable questionNumber

--- code/script.js
+++ code/script.js
@@ -1,9 +1,16 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const userInput = document.getElementById('user-input')
+const form = document.getElementById('chat-form')
+const send = document.getElementById('send')
 
 // Global variables, if you need any, declared here
 
+let questionNumber = 0
+
+
 // Functions declared here
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -28,16 +35,56 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
+
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
+
+// if (questionNumber === 1) {
+
+
 
 // Starts here, introducing Plot the plant bot
 const greeting = () => {
   showMessage(`Hello there! I'm Plot, the plant bot. What's your name?`, 'bot')
 }
+  
+const favColor = () => {
+  showMessage(`What is your favourite color?`, 'bot')
+}
+
+const likesFlowers = () => {
+  showMessage(`Do you like flowers?`, 'bot')
+}
+
+const handleInput = (event) => {
+  event.preventDefault()
+  questionNumber++
+    if (questionNumber === 1) {
+      handleNameQuestion()
+    } else if (questionNumber === 2) {
+      handleColorQuestion()   
+    }
+}
+
+const handleNameQuestion = () => {
+  const name = userInput.value
+  showMessage(`My name is ${name}!`, 'user')
+  userInput.value = ''
+  setTimeout(favColor, 1000)
+}
+
+const handleColorQuestion = () => {
+  const color = userInput.value
+  showMessage(`My favourite color is ${color}!`, 'user')
+  userInput.value = ''
+  setTimeout(likesFlowers, 1000)
+}
 
 // Set up your eventlisteners here
+
+form.addEventListener('submit', handleInput)
+
 
 // When website loaded, chatbot asks first question.
 // setTimeout makes the greeting take 0.5 seconds to show after the page is loaded
