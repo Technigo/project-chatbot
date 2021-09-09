5ca3addb12b358443500ffae4635ebb612b97eb2
dandeloid <82587220+dandeloid@users.noreply.github.com>
2021-09-09 18:18:05 +0200

    Iteration 3

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const startButton = document.getElementById('start-btn') //for Bot greet start button
+const form = document.getElementById('name-form')
+const inputValue = document.getElementById('name-input')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // Global variables, if you need any, declared here
 
@@ -26,7 +28,6 @@ const showMessage = (message, sender) => {
         </div>
       </section>
     `
-    //Removed ${} up above for test. Bot then just writes out "message" wont get the variable message.
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
@@ -34,12 +35,30 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hey! I'm your virtual bartender. What's your name?`, 'bot') //changed msg
+  showMessage(`Hey! I'm your virtual bartender. What's your name?`, 'bot') 
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+const handleNameInput = (event) => {
+  event.preventDefault() //prevents the page from refreshing
+  const name = inputValue.value  //input from user gets stored in name
+  showMessage(name, 'user') //shows the msg the user typed in
+  inputValue.value = '' //clears form
+
+  if (name === ""){ //If no user name
+    setTimeout(() => showMessage(`That's rude! Please give me your name.`, 'bot'), 1000)
+  }
+  else{ //If user puts in name... continue...
+    setTimeout(() => showMessage(`Hey ${name}!`, 'bot'), 1000) //bot says Hey to 'name'
+    setTimeout(() => drinkMenu(), 2000) // continues to drinkMenu
+  }
+}
+
+
 // Set up your eventlisteners here
-startButton.addEventListener('click', greeting) //calls greeting on button click Start
+
+form.addEventListener('submit', handleNameInput) //listens to submit button, goes to handleNameInput func.
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -48,4 +67,4 @@ startButton.addEventListener('click', greeting) //calls greeting on button click
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 
-//setTimeout(greeting, 1000)
+setTimeout(greeting, 1000)
