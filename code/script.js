e05ad754d5f7f53647ec7199ec9381a677d612ee
Jenny Quach <quach.jen@gmail.com>
2021-09-10 01:43:49 +0200

    select tag for sushi done

--- code/script.js
+++ code/script.js
@@ -1,30 +1,53 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const btn = document.getElementsByClassName('send-btn')
 const nameInput = document.getElementById('name-input')
-const nameForm = document.getElementById('name-form')
+const form = document.getElementById('name-form')
+
+const btnSelectMain = `
+    <button id='sushi' type='submit'> Sushi</button>
+    <button id='pokebowl' type='submit'> Pokebowl</button>
+    <button id='yakisoba' type='submit'> Yakisoba</button>
+    `
+
+const selectSushi = `
+<select name="sushi" id="select">
+    <option value="" selected disabled>--Please choose an option--</option>
+    <option value="9">Mixed 9 pcs</option>
+    <option value="12">Mixed 12 pcs</option>
+    <option value="16">Mixed 16 pcs</option>
+</select>`
+
+const selectPokebowl = `
+<select name="pokebowl" id="select">
+    <option value="" selected disabled>--Please choose an option--</option>
+    <option value="salmon">Pokebowl salmon</option>
+    <option value="schrimp">Pokebowl shrimp</option>
+    <option value="tofu">Pokebowl tofu</option>
+</select>
+`
+
+const selectYakisoba = `
+<select name="Yakisoba" id="select">
+    <option value="" selected disabled>--Please choose an option--</option>
+    <option value="salmon">Yakisoba salmon</option>
+    <option value="pork">Yakisoba pork</option>
+    <option value="tofu">Yakisoba tofu</option>
+</select>
+`
 
 // Global variables, if you need any, declared here
 
 // Functions declared here
 
-/*
-const showFoodOptions = userName => {
-  console.log('${userName}')
-} 
-*/
 
-const handleNameInput = (event) => {
-  event.preventDefault()
-  const name = nameInput.value
-  showMessage(`My name is ${name}`, 'user')
-  nameInput.value = ''
-  setTimeout(() => showFoodOptions(name), 1000)
-}  
+
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log('user is sending')
+    
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -34,7 +57,8 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
-    console.log('hello')
+    console.log('bot is sending')
+
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -50,21 +74,81 @@ const showMessage = (message, sender) => {
 
 // Starts here
 
-// setTimeout(greeting, 1000)
+// Interaction 1, initial greeting
+
 const greeting = () => {
-  showMessage(`Welcome! We have sushi, pokebowls and noodles. What is your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage(`Hello and welcome to Migaro! What is your name?`, 'bot')
+}
+  setTimeout(greeting, 1000) // This means the greeting function will be called one second after the website is loaded.
+
+const handleNameInput = (event) => {
+  event.preventDefault()
+
+  const name = nameInput.value
+  console.log(name)
+
+  showMessage(name, 'user') 
+  nameInput.value = '' //clearing name input setting it to an empty string
+  
+  setTimeout(() => foodOptions(btnSelectMain, name), 1000)
+}  
+
+// Interaction 2, Food options
+
+const foodOptions = (selectMain, name) => {
+  showMessage(`Nice to meet you ${name}! What are you craving today? See our options below.`, 'bot')
+  form.innerHTML = selectMain
+  document
+  .getElementById('sushi')
+  .addEventListener('click', () => {
+    showMessage(`Sushi would make my day!`,'user')
+    form.innerHTML = ''
+    setTimeout(() => selectFinalOption(selectSushi), 1000)
+  })
+
+  document
+  .getElementById('pokebowl')
+  .addEventListener('click', () => {
+    showMessage(`A pokebowl would make my day!`,'user')
+    form.innerHTML = ''
+    setTimeout(() => selectFinalOption(selectPokebowl), 1000)
+  })
+
+  document
+  .getElementById('yakisoba')
+  .addEventListener('click', () => {
+    showMessage(`Yakisoba would make my day!`,'user')
+    form.innerHTML = ''
+    setTimeout(() => selectFinalOption(selectYakisoba), 1000)
+  })
+}
+
+// Interaction 3 - choose what to eat
+
+const selectFinalOption = (selectFinal) => {
+  showMessage(`Of course, everybody loves sushi! Make your choice below`, 'bot')
+  
+  form.innerHTML = selectFinal
+  const mainChoice = document.getElementById('select')
+    mainChoice.addEventListener('change', () => {
+      console.log(mainChoice)
+      showMessage(`${mainChoice.value}`,'user')
+
+      form.innerHTML = ''
+      setTimeout(() => payment(), 1000)
+    })
+}
+
+// Interaction 4 payment
+
+const payment = () => {
+  console.log('pay')
+}
+
+const handleFoodInput = (event) => {
+  event.preventDefault()
 }
 
 // Set up your eventlisteners here
-// btn.addEventListener('Click', showMessage())
 
-nameForm.addEventListener('submit', handleNameInput)
-
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+form.addEventListener('submit', handleNameInput)
