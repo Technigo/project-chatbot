257195a22b57d6221475e3c4c21ba434be5330f5
Sarah Mottram <smelbows@gmail.com>
2021-09-09 12:26:57 +0200

    added celeb images

--- code/script.js
+++ code/script.js
@@ -7,6 +7,117 @@ const inputWrapper = document.getElementById('input-wrapper')
 
 
 // Global variables, if you need any, declared here
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug Jones"
+
+const christopher = document.createElement("img");
+christopher.src = "./assets/celebs/Christopher_Lee.jpeg"
+christopher.alt = "Pic of Christopher Lee"
+
+const anna = document.createElement("img");
+anna.src = "./assets/celebs/anna_wintour.jpeg"
+anna.alt = "Pic of Anna Wintour"
+
+const carrot = document.createElement("img");
+carrot.src = "./assets/celebs/Carrot_top.jpeg"
+carrot.alt = "Pic of Carrot Top"
+
+const charles = document.createElement("img");
+charles.src = "./assets/celebs/Charles_Dickens.png"
+charles.alt = "Pic of Charles Dickens"
+
+const ed = document.createElement("img");
+ed.src = "./assets/celebs/Ed_Sheeran.webp"
+ed.alt = "Pic of Ed Sheeran"
+
+const french = document.createElement("img");
+french.src = "./assets/celebs/french_and_saunders.jpg"
+french.alt = "Pic of French and Saunders"
+
+const halle = document.createElement("img");
+halle.src = "./assets/celebs/Halle_Berry.jpg"
+halle.alt = "Pic of Doug"
+
+const helena = document.createElement("img");
+helena.src = "./assets/celebs/Helena_Bonham_Carter.jpg"
+helena.alt = "Pic of Helena Bonham Carter"
+
+const jamie = document.createElement("img");
+jamie.src = "./assets/celebs/Jamie_Lee_Curtis.jpg"
+jamie.alt = "Pic of Jamie Lee Curtis"
+
+const jim = document.createElement("img");
+jim.src = "./assets/celebs/Jim_ Carrey.jpeg"
+jim.alt = "Pic of Jim Carrey"
+
+const karl = document.createElement("img");
+karl.src = "./assets/celebs/Karl_lagerfeld.jpg"
+karl.alt = "Pic of Karl Lagerfeld"
+
+const keira = document.createElement("img");
+keira.src = "./assets/celebs/Kiera_Knightley.jpeg"
+keira.alt = "Pic of Kiera Knightley"
+
+const kiss = document.createElement("img");
+kiss.src = "./assets/celebs/kiss.jpg"
+kiss.alt = "Pic of kiss"
+
+const margaret = document.createElement("img");
+margaret.src = "./assets/celebs/Margaret_Atwood"
+margaret.alt = "Pic of Margaret Atwood"
+
+const marilyn = document.createElement("img");
+marilyn.src = "./assets/celebs/Marilyn_Manson.jpg"
+marilyn.alt = "Pic of Marilyn Manson"
+
+const neil = document.createElement("img");
+neil.src = "./assets/celebs/neil-gaiman.jpg"
+neil.alt = "Pic of Neil Gaiman"
+
+const prince = document.createElement("img");
+prince.src = "./assets/celebs/Prince.jpg"
+prince.alt = "Pic of Prince"
+
+const taylor = document.createElement("img");
+taylor.src = "./assets/celebs/Taylor_Swift.webp"
+taylor.alt = "Pic of Taylor Swift"
+
+const tupac = document.createElement("img");
+tupac.src = "./assets/celebs/Tupac_Shakur.jpg"
+tupac.alt = "Pic of Tupac Shakur"
+
+const u2 = document.createElement("img");
+u2.src = "./assets/celebs/u2.jpg"
+u2.alt = "Pic of u2"
+
+const versace = document.createElement("img");
+versace.src = "./assets/celebs/versace.jpg"
+versace.alt = "Pic of Versace"
+
+const cent = document.createElement("img");
+cent.src = "./assets/celebs/50_Cent.jpg"
+cent.alt = "Pic of 50cent"
+
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug"
+
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug"
+
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug"
+
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug"
+
+const doug = document.createElement("img");
+doug.src = "./assets/celebs/Doug_Jones_2015.jpg"
+doug.alt = "Pic of Doug"
 
 // Functions declared here
 const wantToPlay = (user) => {
@@ -212,61 +323,126 @@ const showSocialMedia = (genre, hobby) => {
 const showMyAnswer = (media, genre, hobby) => {
   if (media === 'Some') {
     if (hobby === 'movie') {
-      if (genre === 'horror') { 
-        console.log(media, hobby, genre)
+      if (genre === 'horror') {
+        showCelebImg(jamie)
       }
-      else if (genre === 'drama') { }
-      else if (genre === 'comedy') { }
+      else if (genre === 'drama') { 
+        showCelebImg(helena)
+      }
+      else if (genre === 'comedy') { 
+        showCelebImg(carrot)
+      }
     }
     else if (hobby === 'music') {
-      if (genre === 'hiphop') { }
-      else if (genre === 'pop') { }
-      else if (genre === 'rock') { }
+      if (genre === 'hiphop') { 
+        showCelebImg(jennifer)
+      }
+      else if (genre === 'pop') { 
+        showCelebImg(ed)}
+      else if (genre === 'rock') { 
+        showCelebImg(marilyn)
+      }
     }
     else if (hobby === 'books') {
-      if (genre === 'novels') { }
-      else if (genre === 'magazines') { }
-      else if (genre === 'academic') { }
+      if (genre === 'novels') { 
+        showCelebImg(margaret)
+      }
+      else if (genre === 'magazines') { 
+        showCelebImg(anna)
+      }
+      else if (genre === 'academic') { 
+        showCelebImg(jennie)
+      }
     }
   } else if (media === 'Lots') {
     if (hobby === 'movie') {
-      if (genre === 'horror') { }
-      else if (genre === 'drama') { }
-      else if (genre === 'comedy') { }
+      if (genre === 'horror') { 
+        showCelebImg(christopher)
+      }
+      else if (genre === 'drama') { 
+        showCelebImg(keira)
+      }
+      else if (genre === 'comedy') { 
+        showCelebImg(jim)
+      }
     }
     else if (hobby === 'music') {
-      if (genre === 'hiphop') { }
-      else if (genre === 'pop') { }
-      else if (genre === 'rock') { }
+      if (genre === 'hiphop') { 
+        showCelebImg(cent)
+      }
+      else if (genre === 'pop') { 
+        showCelebImg(taylor)
+      }
+      else if (genre === 'rock') { 
+        showCelebImg(u2)
+      }
     }
     else if (hobby === 'books') {
-      if (genre === 'novels') { }
-      else if (genre === 'magazines') { }
-      else if (genre === 'academic') { }
+      if (genre === 'novels') { 
+        showCelebImg(charles)
+      }
+      else if (genre === 'magazines') { 
+        showCelebImg(donnatella)
+      }
+      else if (genre === 'academic') { 
+        showCelebImg(elon)
+      }
     }
   } else if (media === 'My grandma') {
     if (hobby === 'movie') {
-      if (genre === 'horror') { }
-      else if (genre === 'drama') { }
-      else if (genre === 'comedy') { }
+      if (genre === 'horror') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'drama') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'comedy') { 
+        showCelebImg(doug)
+      }
     }
     else if (hobby === 'music') {
-      if (genre === 'hiphop') { }
-      else if (genre === 'pop') { }
-      else if (genre === 'rock') { }
+      if (genre === 'hiphop') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'pop') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'rock') { 
+        showCelebImg(doug)
+      }
     }
     else if (hobby === 'books') {
-      if (genre === 'novels') { }
-      else if (genre === 'magazines') { }
-      else if (genre === 'academic') { }
+      if (genre === 'novels') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'magazines') { 
+        showCelebImg(doug)
+      }
+      else if (genre === 'academic') { 
+        showCelebImg(doug)
+      }
     }
   }
 }
-    // This function will add a chat bubble in the correct place based on who the sender is
-    const showMessage = (message, sender) => {
-      if (sender === 'user') {
-        console.log(message, sender);
-        chat.innerHTML += `
+
+const showCelebImg = (celeb) => {
+  chat.innerHTML += `
+  <section class="bot-msg">
+  <img src="assets/bot.png" alt="Bot" /> 
+   <div class="bubble bot-bubble ">
+      <p>Your celebrity soulmate is!</p>
+      <img class="celeb-img" src="${celeb.src}" alt="${celeb.alt}" />  
+    </div>
+  </section>
+`
+chat.scrollTop = chat.scrollHeight
+
+}
+// This function will add a chat bubble in the correct place based on who the sender is
+const showMessage = (message, sender) => {
+  if (sender === 'user') {
+    console.log(message, sender);
+    chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
@@ -274,9 +450,9 @@ const showMyAnswer = (media, genre, hobby) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-      } else if (sender === 'bot') {
-        console.log(message, sender);
-        chat.innerHTML += `
+  } else if (sender === 'bot') {
+    console.log(message, sender);
+    chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
@@ -284,29 +460,29 @@ const showMyAnswer = (media, genre, hobby) => {
         </div>
       </section>
     `
-      }
-      // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-      chat.scrollTop = chat.scrollHeight
-    }
+  }
+  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+  chat.scrollTop = chat.scrollHeight
+}
 
-    // Starts here
-    const greeting = () => {
-      showMessage(`Hello there, What's your instagram username?`, 'bot')
-      // Just to check it out, change 'bot' to 'user' here 👆
-    }
+// Starts here
+const greeting = () => {
+  showMessage(`Hello there, What's your instagram username?`, 'bot')
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
 
-    // Set up your eventlisteners here
-    sendBtn.addEventListener('click', (e) => {
-      e.preventDefault()
-      const name = userName.value;
-      showMessage(name, 'user');
-      wantToPlay(name);
-    })
-    // When website loaded, chatbot asks first question.
-    // normally we would invoke a function like this:
-    // greeting()
-    // But if we want to add a little delay to it, we can wrap it in a setTimeout:
-    // setTimeout(functionName, timeToWaitInMilliSeconds)
-    // This means the greeting function will be called one second after the website is loaded.
-    setTimeout(greeting, 1000)
+// Set up your eventlisteners here
+sendBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const name = userName.value;
+  showMessage(name, 'user');
+  wantToPlay(name);
+})
+// When website loaded, chatbot asks first question.
+// normally we would invoke a function like this:
+// greeting()
+// But if we want to add a little delay to it, we can wrap it in a setTimeout:
+// setTimeout(functionName, timeToWaitInMilliSeconds)
+// This means the greeting function will be called one second after the website is loaded.
+setTimeout(greeting, 1000)
 
