8b6fba224f1f7879967826f2a36ea970df08a426
Darya <darya.lapata@gmail.com>
2021-09-10 14:19:31 +0200

    last changes, added some new styling

--- code/script.js
+++ code/script.js
@@ -6,116 +6,115 @@ const nameInput = document.getElementById('name-input');
 const inputWrapper = document.getElementById('input-wrapper');
 
 // Global variables, if you need any, declared here
-let currentQuestion = 0
+let currentQuestion = 1; 
 
-// Functions declared here
+
+// FUNCTIONS
+
+// 4., 6., 8.
+    const nextQuestion = (message) => {
+
+      if(currentQuestion === 1) {
+        nameInput.value = ''
+        setTimeout(() => showFoodOptions(message), 1000)
+
+      } else if(currentQuestion === 2) {
+        showMessage(message, 'user')
+        setTimeout(() => showMessage(`So you want to order ${message}?`, 'bot'), 1000)
+        showPriceSushi() 
+
+      } else if(currentQuestion === 3) {
+        showMessage(message, 'user')
+        ending(message)
+      } 
+      
+
+    }
 
 // This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
-  if (sender === 'user') {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `
-  } else if (sender === 'bot') {
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+    const showMessage = (message, sender) => {
+      if (sender === 'user') {
+        chat.innerHTML += ` 
+          <section class="user-msg">
+            <div class="bubble user-bubble">
+              <p>${message}</p>
+            </div>
+            <img src="./assets/user.png" alt="User" />  
+          </section>
+        `
 
-const nextQuestion = (message) => {
-  console.log('questionNumber', questionNumber)
+      } else if (sender === 'bot') {
+        chat.innerHTML += ` 
+          <section class="bot-msg">
+          <img src="./assets/sushi-roll-icon.jpeg" alt="">
+            <div class="bubble bot-bubble">
+              <p>${message}</p>
+            </div>
+          </section>
+        `
+      chat.scrollTop = chat.scrollHeight
+    }
+ }
 
-  if (questionNumber === 1) {
-    userReply(message)
-    input.value = ''
-    setTimeout(() => showFoodTypes(message), 1000)
-  } 
-}
+//5.
+    const showFoodOptions = () => {
+      currentQuestion++ // current question is 2
+      setTimeout(() => showMessage(`What sushi would you like to order? `, 'bot'), 1000)
+      inputWrapper.innerHTML = `
+        <button id="small-sushi">8 pieces</button>
+        <button id="big-sushi">12 pieces</button>
+      `
 
+      const smallSushi = document.getElementById('small-sushi')
+      smallSushi.addEventListener('click', () => nextQuestion('8 pieces'))
 
-const handleNameInput = () => {
-  currentQuestion ++ ; // fr nr 1
-  console.log(currentQuestion)
+      const bigSushi = document.getElementById('big-sushi')
+      bigSushi.addEventListener('click', () => nextQuestion('12 pieces'))
+     
+    }
+// 7.
+    const showPriceSushi = () => {
+      currentQuestion++// current question is 3
+      inputWrapper.innerHTML = `
+        <button id="yes">Yes</button>
+        <button id="no">No</button>
+      ` 
+      const yesAnswer = document.getElementById('yes'); 
+      yesAnswer.addEventListener('click', () => nextQuestion('yes'));
 
-  const name = nameInput.value
-  showMessage(name, 'user')
-  nameInput.value = '';
+      const noAnswer = document.getElementById('no'); 
+      noAnswer.addEventListener('click', () => nextQuestion('no'));
+    
+    }
 
+//3. The bot shows a response to the user 
+    const handleNameInput = () => {
+      const name = nameInput.value
+      showMessage(name, 'user')
+      setTimeout(() => showMessage(`Nice to meet you ${name}`, 'bot'), 1000)
+      nextQuestion()
+    }
 
-  setTimeout( () => 
-    showMessage(`Nice to meet you ${name} `, 'bot'), 1000);
-  
+    // 1.Starts here 
+    const greeting = () => {
+      showMessage(`Hi, my name is SushiBot! What's your name?`, 'bot'); // question nr 1
+    }
+    setTimeout(greeting, 1000)
 
-  setTimeout(() => showFoodOptions(name), 2000)
-  
-  
-}
+// 9.
+    const ending = (message) => {
+      if (message === 'yes') {
+        showMessage('Thank you for your order', 'bot')
+        setTimeout(() => location.reload(), 5000)
+      } else  
+        setTimeout(() => location.reload(), 1500)
+      
 
-const showFoodOptions = () => {
-  currentQuestion++ ; // fråga nr 2
-  console.log(currentQuestion)
-  showMessage(`What would you like to order? `, 'bot')
-  inputWrapper.innerHTML = `
-  <button id="pizza-button">Pizza</button>
-  `
- 
-  const pizzaButton = document.getElementById('pizza-button')
+    }
+// EVENT LISTENERS 
 
-  pizzaButton.addEventListener('click', () => {
-    // console.log("clicked pizza button")
-    // handleNameInput()
-    showMessage(`pizza`, 'user');
-    setTimeout( () => 
-    showMessage(`What type of pizza would you like to order?`, 'bot'), 1000);
-    inputWrapper.innerHTML = `
-    <select id="select">
-    <option value="" selected disabled>👇 Select a pizza...</option>
-    <option value="margerita">Margerita</option>
-    <option value="vesuvio">Vesuvio</option>
-    <option value="peperoni">Peperoni</option>
-  </select>
-  `
-  })
-
-}
-
-const handlePizzaButton = () => {
-
-}
-
-// Starts here 
-
-const greeting = () => {
-  showMessage(`Hi, my name is Pizzatone! What's your name?`, 'bot');
-
-}
-
-
-// Set up your eventlisteners here
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  handleNameInput()
-
-
-})
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-// setTimeout(greeting, 1000)
-setTimeout(greeting, 1000);
+// 2.When the user writes a name in the input and pushes the button the bot continues to the next step (3)
+    button.addEventListener('click', (e) => { 
+    e.preventDefault(); // this prevents the browser to reload
+    handleNameInput();
+    })
