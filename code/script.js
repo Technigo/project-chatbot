4c4a7ef07ae0c17cf77499ce95ae3d88cf7101c9
Birgit Nehrwein <nehrwein@posteo.de>
2021-09-10 13:57:24 +0200

    Iteration 4

--- code/script.js
+++ code/script.js
@@ -2,11 +2,13 @@
 const chat = document.getElementById('chat')
 const inputForm = document.getElementById('input-form')
 const textInput = document.getElementById('text-input')
-// Global variables, if you need any, declared here
+const inputWrapper = document.getElementById('input-wrapper')
 
+// Global variables, if you need any, declared here
+let dogName = "";
 // Functions declared here
 
-// The showMessage function will add a chat bubble in the correct place based on who the sender is. The showMessage function takes two arguments - the message, and the sender. You can pass any text as the message and either 'user' or 'bot' as the sender. Depending on which sender you choose, it'll build up some HTML and append it to the 'chat' div with a different image and class name.
+// The showMessage function will add a chat bubble in the correct place based on who the sender is. 
 const showMessage = (message, sender) => {
   if (sender === 'user') {
     chat.innerHTML += `
@@ -34,34 +36,122 @@ const showMessage = (message, sender) => {
 // The Bot starts here
 //The greeting function sends a message as the bot asking for your name.
 const greeting = () => {
-  showMessage(`Hello and welcome to Pretty Pets, What's the name of your furry friend?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage(`Hello and welcome to Pretty Pets! What's the name of your furry friend?`, 'bot')
 }
 
+// The user enters dog name in the handleInput function
 const handleInput = (event) => {
   event.preventDefault()
-  // Store the value in a variable so we can access it after we 
-	// clear it from the input
+  // Store the value in a variable so we can access it after we clear it from the input
   const userInput = textInput.value
+  dogName = userInput;
   showMessage(userInput, 'user')
   textInput.value = ''
 
-  // After 1 second, show the next question by invoking the next function.
-	// passing the name into it to have access to the user's name if we want
-	// to use it in the next question from the bot.
+  // After 1 second, show the next question by invoking the next function passing the name into it to have access to the user's name if we want to use it in the next question from the bot.
   setTimeout(() => nameOfPet(userInput), 1000)
 }
 
+// Questions from the Bot about the user selection and creating two buttons (Book appointment and View treamtment)
 const nameOfPet = () => {
   showMessage(`Would you like to book an appointment or look at our treatments?`, 'bot')
+  
+  inputWrapper.innerHTML = `
+  <button id="appointmentBtn">Book Appointment</button>
+  <button id="treatmentBtn">View Treatments</button>
+  `
+  document
+  .getElementById('treatmentBtn')
+  .addEventListener('click', () => viewTreatments())
+
+  document
+  .getElementById('appointmentBtn')
+  .addEventListener('click', () => bookTreatment())
 }
+
+// User choose View treatment and two new buttons gets displayed (Big Lebowski and Michelle Pfieffer) 
+const viewTreatments = () => {
+  showMessage(`View treatments`, 'user')
+
+  setTimeout(() => showMessage(`Click on the buttons for more information`, 'bot'), 1000)
+
+  inputWrapper.innerHTML = `
+  <button id="bigLebowskiBtn">Big Lebowski-Treatment</button>
+  <button id="michellePfeifferBtn">Michelle Pfeiffer-Package</button>
+`
+document
+  .getElementById('bigLebowskiBtn')
+  .addEventListener('click', () => lebowskiTreatment())
+ 
+  document
+  .getElementById('michellePfeifferBtn')
+  .addEventListener('click', () => pfeifferTreatment())
+}
+
+// User choose Big Lebowski and two new buttons gets displayed (Book treatment and Other treatment option)
+const lebowskiTreatment = () => {
+  showMessage(`I want to know about the Big Lebowski treatment`, 'user')
+
+  setTimeout(() => showMessage(`The Big Lebowski treatment consists of a flee scrub and a rugh hair-do`, 'bot'), 1000)
+
+  inputWrapper.innerHTML = `
+  <button id="bookTreatmentBtn">Book treatment</button>
+  <button id="otherOptionsBtn">Other treatment options</button>
+`
+  document
+    .getElementById('bookTreatmentBtn')
+    .addEventListener('click', () => bookTreatment())
+}
+
+// User choose Michelle Pfieffer and two new buttons gets displayed (Book treatment and Other treatment option)
+const pfeifferTreatment = () => {
+  showMessage(`I want to know about the Michelle Pfeiffer treatment`, 'user')
+
+  setTimeout(() => showMessage(`The Michelle Pfeiffer treatment is our luxury grooming including furr washing and nail SPA`, 'bot'), 1000)
+
+  inputWrapper.innerHTML = `
+  <button id="bookTreatmentBtn">Book treatment</button>
+  <button id="otherOptionsBtn">Other treatment options</button>
+`
+  document
+    .getElementById('bookTreatmentBtn')
+    .addEventListener('click', () => bookTreatment())
+}
+
+// User gets booking options
+const bookTreatment = () => {
+  showMessage(`I want to book a treatment`, 'user')
+
+  setTimeout(() => showMessage(`Please pick one of the available appointments`, 'bot'), 1000)
+
+  inputWrapper.innerHTML = `
+  <button id="mondayBookBtn">Monday, 10:00 - 11:00</button>
+  <button id="tuesday1BookBtn">Tuesday, 13:00 - 14:00</button>
+  <button id="tuesday2BookBtn">Tuesday, 15:00 - 16:00</button>
+`
+  document
+    .getElementById('mondayBookBtn')
+    .addEventListener('click', () => checkOut())
+  document
+    .getElementById('tuesday1BookBtn')
+    .addEventListener('click', () => checkOut())
+  document
+    .getElementById('tuesday2BookBtn')
+    .addEventListener('click', () => checkOut())  
+}
+
+//Treatment is booked and bot sends checkout-message
+const checkOut = () => {
+  showMessage(`Great, looking forward to meet you and ${dogName} soon!`, 'bot')
+  inputWrapper.innerHTML = ''
+}
+
+
 
 // Set up your eventlisteners here
 inputForm.addEventListener('submit', handleInput)
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
+
+// When website loaded, chatbot asks first question. normally we would invoke a function like this: greeting(). But if we want to add a little delay to it, we can wrap it in a setTimeout:
 setTimeout(greeting, 1000) 
+
+
