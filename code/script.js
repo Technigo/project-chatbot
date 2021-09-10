b438f8e935d53651ab20c83d2ffc278d7a25e09b
Ida Utterström <80784577+Idautterstrom@users.noreply.github.com>
2021-09-10 09:26:48 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,13 +1,21 @@
-// All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
+// All the DOM selectors stored as short variables (only selectors that have an event to it)
+const chat = document.getElementById('chat') 
+const form = document.getElementById('name-form') //The form
+const inputValue = document.getElementById('name-input') //Input field
+const inputWrapper = document.getElementById('input-wrapper')
+const sendBtn = document.getElementById('submit')
 
 // Global variables, if you need any, declared here
 
-// Functions declared here
 
-// This function will add a chat bubble in the correct place based on who the sender is
+// FUNCTIONS declared here (always has to be created before we can call it-->
+
+
+//This function will add a chat bubble in the correct place based on who the sender is
+
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -26,22 +34,79 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+
+//Makes chat scroll to the last message
   chat.scrollTop = chat.scrollHeight
 }
 
-// Starts here
+
+//All questions and answers from bot
+
+//1st
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-// Set up your eventlisteners here
+//2nd
+const handleFoodInput = (name) => { //Why does it say undefined?
+  showMessage(`Hi ${name}, what would you like to order?`, 'bot') 
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
+  inputWrapper.innerHTML = `
+  <button id="Pizza">Pizza</button>
+  <button id="Pasta">Pasta</button>
+  <button id="Hamburger">Hamburger</button>
+ `
+
+ inputWrapper.addEventListener('click', handleRequest)
+ 
+}
+
+//3rd
+const handleRequest = (type) => { 
+  showMessage (`Thanks for choosing ${type}, would you like a drink with that?`, 'bot')
+
+  inputWrapper.innerHTML = `
+  <button id="Yes">Yes</button>
+  <button id="No">No</button>
+  `
+  //To get different replies from bot depending on answer:
+document.getElementById('Yes').addEventListener('click', () => drinkOrder ('Yes'))
+document.getElementById('No').addEventListener('click', () => drinkOrder ('No'))
+
+}
+
+//4th
+const drinkOrder = (type) => { //depending on answer, different messages will occur!
+  
+  //To stop messages from appearing again:
+  inputWrapper.removeEventListener('click', handleRequest)
+
+  if (type === 'Yes') { 
+    showMessage(`Thanks, food and drinks are coming your way!`, 'bot')
+  } else if (type === 'No') {
+    showMessage(`Ok, no drink for you! See you soon!`, 'bot') 
+  }
+
+  inputWrapper.innerHTML=''
+}
+
+
+//Function that shows the name of the user in first interaction
+const handleNameInput = (event) => { 
+  event.preventDefault()
+
+  const name = inputValue.value
+
+  showMessage(`Hey, my name is ${name}!`, 'user')
+  inputValue.value = '' //this is to take away the name from the form and call on another function
+
+  setTimeout(handleFoodInput, 1000)
+}
+
+  
+//The button on the form, envoked every time we submit the form
+form.addEventListener('submit', handleNameInput)
+
+
+//When website loaded, chatbot asks first question with a 1sec delay:
 setTimeout(greeting, 1000)
