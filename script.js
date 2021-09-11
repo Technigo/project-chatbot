da90beefc1a6f8dfc172ee98475293b66bdd6361
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-09-11 13:26:43 +0200

    added more questions and alternatives, cleaned up code and added comments, added ternary operator

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let inputFromUser = ''
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
-    console.log('this is the user talking') //this NOT is shown because we changed to 'user' in const greeting.
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -28,7 +27,6 @@ const showMessage = (message, sender) => {
       </section>
     ` 
   } else if (sender === 'bot') {
-    console.log('this is the bot talking')//this is showing because we changed to 'user' in const greeting.
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -43,18 +41,10 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-// if (questionNumber === 1) {
-
-
-
 // Starts here, introducing Plot the plant bot
 const greeting = () => {
   showMessage(`Hello there! I'm Plot, the plant bot. What's your name?`, 'bot')
 }
-  
-// const favColor = () => {
-//   showMessage(`What is your favourite color?`, 'bot')
-// }
 
 const whatPlant = () => {
   showMessage(`Nice to meet you ${inputFromUser}!`, 'bot') 
@@ -66,86 +56,116 @@ const whatPlant = () => {
     <button id="thorny">🌵</button>
   `
     document.getElementById('flowers')
-    .addEventListener('click', () => plantSelection('flowers'))
+    .addEventListener('click', () => plantSelection('flowers 🌺'))
     document.getElementById('edibles')
-    .addEventListener('click', () => plantSelection('edibles'))
+    .addEventListener('click', () => plantSelection('edibles 🌶'))
     document.getElementById('thorny')
-    .addEventListener('click', () => plantSelection('thorny'))
-  }, 1000)
+    .addEventListener('click', () => plantSelection('thorny 🌵'))
+  }, 1500)
   }
 
 const plantSelection = (plantChoice) => {
   questionNumber++
-showMessage(`I prefer ${plantChoice}!`, 'user')
-setTimeout (() => {showMessage(`Aha... ${plantChoice} you say.. Let's find out more!`, 'bot')
+  showMessage(`I prefer ${plantChoice}!`, 'user')
+  const editedText = plantChoice.slice(0, -2) //removes the last two characters in the information sent through plantChoice.
+  setTimeout (() => {showMessage(`Aha... ${editedText} you say.. let's find out more!`, 'bot')
 
-  if (plantChoice === 'flowers') {
-    inputWrapper.innerHTML = `
-    <button id="oneBig">One BIG flower 🌸</button>
-    <button id="plentySmall">A bouquet 💐</button>
-    `
-  } else if (plantChoice === 'edibles') {
+    if (plantChoice === 'flowers 🌺') {
+      inputWrapper.innerHTML = `
+      <button id="oneBig">One BIG flower 🌸</button>
+      <button id="plentySmall">A bouquet 💐</button>
+      `
+      document.getElementById('oneBig')
+      .addEventListener('click', () => lastChoice('One BIG flower 🌸'))
+      document.getElementById('plentySmall')
+      .addEventListener('click', () => lastChoice('A bouquet 💐'))
+    } else if (plantChoice === 'edibles 🌶') {
       inputWrapper.innerHTML = `
       <button id="sweet">I have sweet tooth 🍓</button>
       <button id="hot">I like it HOT 🔥</button>
       `
-    } else if (plantChoice === 'thorny') {
+      document.getElementById('sweet')
+      .addEventListener('click', () => lastChoice('I have sweet tooth 🍓'))
+      document.getElementById('hot')
+      .addEventListener('click', () => lastChoice('I like it HOT 🔥'))
+    } else {
       inputWrapper.innerHTML = `
       <button id="cactus">Low maintenance 🌵</button>
       <button id="rose">Something deceivingly beautiful 🌹</button>
       ` 
+      document.getElementById('cactus')
+      .addEventListener('click', () => lastChoice('Low maintenance 🌵'))
+      document.getElementById('rose')
+      .addEventListener('click', () => lastChoice('Something deceivingly beautiful 🌹')) 
     }
+  }, 1000)
   }
-  , 1000)
 
-  document.getElementById('oneBig')
-  .addEventListener('click', () => lastChoice('oneBig'))
-  document.getElementById('plentySmall')
-  .addEventListener('click', () => lastChoice('plentySmall'))
-  document.getElementById('sweet')
-  .addEventListener('click', () => lastChoice('sweet'))
-  document.getElementById('hot')
-  .addEventListener('click', () => lastChoice('hot'))
-  document.getElementById('cactus')
-  .addEventListener('click', () => lastChoice('cactus'))
-  document.getElementById('rose')
-  .addEventListener('click', () => lastChoice('rose'))
+  const lastChoice = (finalChoice) => {
+    questionNumber++
+    showMessage(finalChoice, 'user')
+    setTimeout (() => {
+    if (finalChoice === 'One BIG flower 🌸') {
+      showMessage(`Based on your`, 'bot')
+    } else if (finalChoice === 'A bouquet 💐') {
+      showMessage(`Based on your`, 'bot')
+    } else if (finalChoice === 'I have sweet tooth 🍓') {
+      showMessage(`Based on your`, 'bot')
+    } else if (finalChoice === 'I like it HOT 🔥') {
+      showMessage(`Based on your`, 'bot')
+    } else if (finalChoice === 'Low maintenance 🌵') {
+      showMessage(`You obviously `, 'bot')
+    } else {
+      showMessage(`Alright, a plant with a touch of poison for you my dear...`, 'bot')
+      setTimeout (() => {showMessage(`Hmmm..`, 'bot')}, 1000)
+      setTimeout (() => {showMessage(`I suggest a `, 'bot')}, 2000)
+    }
+    }, 1500)
+
+     setTimeout (() => {inputWrapper.innerHTML =`
+      <button id="yes">I'm happy with your choice!</button>
+      <button id="no">Naah, try again!</button>`
+      //  `
+      // <input id="yes" type="radio" name="choice" value="yes" /> I'm happy with your choice!
+      // <input id="no" type="radio" name="choice" value="no"/> Naah, try again!
+      // <button class="send-btn" id="send" type="submit">Send</button>
+      // `
+      document.getElementById('yes')
+      .addEventListener('click', () => playAgain(`I'm very satisfied with your expertise!`))
+      document.getElementById('no')
+      .addEventListener('click', () => playAgain(`It seems like you don't know me at all...`))
+     }, 4500)
+  }
+
+    const playAgain = (happy) => {
+    questionNumber++
+    showMessage(happy, 'user')
+    // trying out a different approach to the if/else statement
+    happy === `I'm very satisfied with your expertise!` ? showMessage(`Perfect! Let's wrap it up in paper for you!`, 'bot') : showMessage(`I'm sorry to hear that... let's try again, then 😌`, 'bot')
   }
 
 
 
 const handleInput = (event) => {
   event.preventDefault()
-// setTimeout (() => 
   questionNumber++
     if (questionNumber === 1) {
       handleNameQuestion()
     } else if (questionNumber === 2) {
-      handlePlantQuestion()   
+      setTimeout(plantSelection, 1500)
     } else if (questionNumber === 3) {
-      handlePlantChoice()
-    }
-  // }, timeOutTime)
+      setTimeout(lastChoice, 1500)
+    } else if (questionNumber === 4) { 
+      setTimeout(playAgain, 1500) 
+    } 
 }
 
 const handleNameQuestion = () => {
-  inputFromUser = userInput.value
+  inputFromUser = userInput.value // ska vi göra om till konstant, jag tror inte vi använder denna mer än 1 gång
   showMessage(`My name is ${inputFromUser}!`, 'user')
-  userInput.value = ''
+  inputWrapper.innerHTML = ``
   setTimeout(whatPlant,1000)
 }
-
-const handlePlantQuestion = () => {
-  inputFromUser = userInput.value  
-  showMessage(`I prefer ${inputFromUser}!`, 'user')
-  userInput.value = ''
-  setTimeout(plantSelection, 1500)
-}
-
-const lastChoice = () => {
-//fortsätt på if else för plantSelection/lastChoice
-} 
-
 
 // Set up your eventlisteners here
 
