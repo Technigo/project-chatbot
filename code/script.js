42961a9c07ec1e41b75635df3f9caf9a8a70f892
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-09-12 11:17:23 +0200

    final changes to UI and dropdown and buttons fixed

--- code/script.js
+++ code/script.js
@@ -10,7 +10,7 @@ const select1 = document.getElementById('select1')
 var coll = document.getElementsByClassName('collapsible');
 
  
-
+//to toggle the chatbot
 for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener('click', function() {
         this.classList.toggle('active');
@@ -80,36 +80,31 @@ const botGreeting = () => {
   showMessage(`Hello there human, what's your name?`, 'bot')
   questionNumber = 1
 }
-//gives the h
+//user tells their name
 const handleNameInput = () => {
-  console.log('handleNameInput')
   showMessage(`My name is ${input.value}`, 'user');
   setTimeout(() => whatMood(), 1000);
 }
 
 //Pick your mood
 const whatMood = () => {
-  console.log('whatMood')
   showMessage(`Oh ${input.value} describe your mood in one word!`, 'bot')
   questionNumber = 3
   input.value=''
 }
 //User choose their mood
 const moodAnswer = () => {
-  console.log('moodAnswer');
-
-  console.log('moodAnswer() ', input.value)
   const inputMood = input.value
   questionNumber = 4
   showMessage(`I am ${inputMood}`, 'user')
   setTimeout(() => songSelection(input.value), 1000);
 }
 
-//Select a song from the list
+//Selects a song from the list
 const songSelection = (input) => {
   questionNumber = 5
   
-
+//if sad, depressed or blue is selected these songs will be avalible
   if(input ==='sad'|| input === 'depressed'|| input === 'blue') {
     showMessage(`So you are ${input}, here are some song recommendations`, 'bot')
 
@@ -120,32 +115,32 @@ const songSelection = (input) => {
      <option value="TheFray">The Fray - How to save a life</option>
      <option value="Coldplay">Coldplay - Fix you</option>
    </select>
-   `
+   `//bot answers when music is playing
    const songDropDown = document.getElementById('select1')
    songDropDown.addEventListener('change', (event) => {
      setTimeout (() => showMessage(`Great choice, we're now listening to ${event.target.value}! Do you like the song?`, 'bot'),1000);
 
-      
+      //plays song
      if (event.target.value === 'Coldplay') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/Coldplay_-_Fix_You_Official_Video_1.mp3" type="audio/mp3">
         </audio>
     `
-
+//gives the human a chance to continue listening och start over
   inputWrapper.innerHTML += `
     <button type="submit" id="yes">Yes</button>
     <button type="submit" id="no">No</button>
     `
-
+//choice buttons
     const yesBtn = document.getElementById('yes')
     const noBtn = document.getElementById('no')
-  
+  //Listens to the yes button
     yesBtn.addEventListener('click', () => {
       showMessage(`Yes!`, 'user')
       ending()
     })
-
+//hides button when clicked
     yesBtn.onclick = function () {
       if (yesBtn.style.display !== 'none') {
         yesBtn.style.display = 'none';
@@ -158,7 +153,7 @@ const songSelection = (input) => {
         noBtn.style.display = 'none';
       };
     }
-
+//when no takes human back to begining
     noBtn.addEventListener('click', () => {
       input.value=''
       setTimeout (() => showMessage(`Too bad! I will make you start over`, 'bot'),1000);
@@ -166,7 +161,7 @@ const songSelection = (input) => {
     })
 
     } else if (event.target.value === 'Hassle') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/Hurtful.mp3" type="audio/mp3">
         </audio>
@@ -203,7 +198,7 @@ const songSelection = (input) => {
       setTimeout(() => beginAgain(), 5000);
     })
     } else if (event.target.value === 'TheFray') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/fray.mp3" type="audio/mp3">
         </audio>
@@ -260,7 +255,7 @@ const songSelection = (input) => {
 
       
      if (event.target.value === 'Pharell') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/Williams.mp3" type="audio/mp3">
         </audio>
@@ -298,7 +293,7 @@ const songSelection = (input) => {
     })
     }
     else if (event.target.value === 'Timberlake') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/Timberlake.mp3" type="audio/mp3">
         </audio>
@@ -336,7 +331,7 @@ const songSelection = (input) => {
     })
     }
     else if (event.target.value === 'Beyonce') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/singleLadies.mp3.mp3" type="audio/mp3">
         </audio>
@@ -391,7 +386,7 @@ const songSelection = (input) => {
     setTimeout (() => showMessage(`Great choice, we're now listening to ${event.target.value}! Do you like the song?`, 'bot'),1000);
       
      if (event.target.value === 'Pink') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/P!nk.mp3" type="audio/mp3">
         </audio>
@@ -429,7 +424,7 @@ const songSelection = (input) => {
     })
     }
     else if (event.target.value === 'Swift') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/swift.mp3" type="audio/mp3">
         </audio>
@@ -467,7 +462,7 @@ const songSelection = (input) => {
     })
     }
     else if (event.target.value === 'CudiWest') {
-      inputWrapper.innerHTML += `
+      inputWrapper.innerHTML = `
         <audio autoplay> 
           <source src="./assets/CudiWest.mp3" type="audio/mp3">
         </audio>
@@ -522,7 +517,7 @@ const songSelection = (input) => {
   setTimeout (() => showMessage(`Great choice, we're now listening to ${event.target.value}! Do you like the song?`, 'bot'),1000);
     
    if (event.target.value === 'Legend') {
-    inputWrapper.innerHTML += `
+    inputWrapper.innerHTML = `
       <audio autoplay> 
         <source src="./assets/Ledgend.mp3">
       </audio>
@@ -560,7 +555,7 @@ const songSelection = (input) => {
     })
   }
   else if (event.target.value === 'Houston') {
-    inputWrapper.innerHTML += `
+    inputWrapper.innerHTML = `
       <audio autoplay> 
         <source src="./assets/houston.mp3" type="audio/mp3">
       </audio>
@@ -598,7 +593,7 @@ const songSelection = (input) => {
     })
   }
   else if (event.target.value === 'Carey') {
-    inputWrapper.innerHTML += `
+    inputWrapper.innerHTML = `
       <audio autoplay> 
         <source src="./assets/Carey.mp3" type="audio/mp3">
       </audio>
@@ -638,8 +633,8 @@ const songSelection = (input) => {
  })
 
   } else { //if the bot does not recognize the mood
-    console.log('im here')
     showMessage(`I don't recognize that mood..`,'bot')
+    //dropdown with all the moods
     inputWrapper.innerHTML = `
     <select id="select5">
      <option value="" selected disabled> Select a mood..</option>
@@ -659,9 +654,8 @@ const songSelection = (input) => {
    </select>
    <button id="rerun"> Go </button>
    `
-  // const selectedValue = document.getElementById('select5').value
+//human can chose a mood in the dropdown and press go to listen
    document.getElementById('rerun').onclick = function() {
-     //songSelection(document.getElementById('select5').value)
      console.log('now I have clicked', document.getElementById('select5').value);
      songSelection(document.getElementById('select5').value)
    }
@@ -669,18 +663,18 @@ const songSelection = (input) => {
   }
 
 }
-
+//bot ending question
     const ending = () => {
       questionNumber = 6
       showMessage(`Thank you for chatting with me!`, 'bot')
     }
-
+//reloads the page when pressing no when listening to music
     const beginAgain = () => {
       questionNumber = 7
       location.reload();
     }
     
-
+//Logic that sends the human forward in the chat
 const handleInput = (event) => {
   event.preventDefault()
   console.log(questionNumber)
