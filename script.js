7474042f9fabf0f1cb604a6267bce4faf396c26d
Skrosen <76884712+Skrosen@users.noreply.github.com>
2021-09-12 12:59:35 +0200

    Final edition. Added bg-img, styled chat form, changed avatars, added possibility to reload chatbot, added all the final bot-answers

--- script.js
+++ script.js
@@ -2,19 +2,12 @@
 const chat = document.getElementById('chat')
 const userInput = document.getElementById('user-input')
 const form = document.getElementById('chat-form')
-const send = document.getElementById('send')
 const inputWrapper = document.getElementById('input-wrapper')
-// const timeOutTime = 1500
 
 // Global variables, if you need any, declared here
-
 let questionNumber = 0
-let inputFromUser = ''
 
 // Functions declared here
-
-
-
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
@@ -23,13 +16,13 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="https://images.vexels.com/media/users/3/232355/isolated/preview/2d9853778878b3fe4099b52d7411eb1f-cute-baby-caterpillar-illustration.png" alt="User" />  
       </section>
     ` 
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="https://static.vecteezy.com/system/resources/previews/002/571/082/non_2x/cute-cactus-plant-in-pot-kawaii-character-icon-free-vector.jpg" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -46,8 +39,8 @@ const greeting = () => {
   showMessage(`Hello there! I'm Plot, the plant bot. What's your name?`, 'bot')
 }
 
-const whatPlant = () => {
-  showMessage(`Nice to meet you ${inputFromUser}!`, 'bot') 
+const whatPlant = (name) => {
+  showMessage(`Nice to meet you ${name}!`, 'bot') 
   setTimeout (() => {showMessage(`What kind of plants do you like?`, 'bot')
 
     inputWrapper.innerHTML = `
@@ -82,12 +75,12 @@ const plantSelection = (plantChoice) => {
     } else if (plantChoice === 'edibles 🌶') {
       inputWrapper.innerHTML = `
       <button id="sweet">I have sweet tooth 🍓</button>
-      <button id="hot">I like it HOT 🔥</button>
+      <button id="hot">I like it ZINGY 🔥</button>
       `
       document.getElementById('sweet')
       .addEventListener('click', () => lastChoice('I have sweet tooth 🍓'))
       document.getElementById('hot')
-      .addEventListener('click', () => lastChoice('I like it HOT 🔥'))
+      .addEventListener('click', () => lastChoice('I like it ZINGY 🔥'))
     } else {
       inputWrapper.innerHTML = `
       <button id="cactus">Low maintenance 🌵</button>
@@ -106,44 +99,51 @@ const plantSelection = (plantChoice) => {
     showMessage(finalChoice, 'user')
     setTimeout (() => {
     if (finalChoice === 'One BIG flower 🌸') {
-      showMessage(`Based on your`, 'bot')
+      showMessage(`Based on your fondness for big flowers I suggest a SUNFLOWER 🌻`, 'bot')
     } else if (finalChoice === 'A bouquet 💐') {
-      showMessage(`Based on your`, 'bot')
+      showMessage(`Cool, you want it all... How about a hip bouqet of dried flowers? 🌾`, 'bot')
     } else if (finalChoice === 'I have sweet tooth 🍓') {
-      showMessage(`Based on your`, 'bot')
-    } else if (finalChoice === 'I like it HOT 🔥') {
-      showMessage(`Based on your`, 'bot')
+      showMessage(`I found this seed for a cherry tree 🍒...`, 'bot')
+      setTimeout (() => {showMessage(`It might take a couple of years to give fruit but good things comes to those who wait!`, 'bot')}, 1000)
+    } else if (finalChoice === 'I like it ZINGY 🔥') {
+      showMessage(`Wow, a little edgy I see.. Here you have some sprouted cloves of garlic to plant 🧄`, 'bot')
     } else if (finalChoice === 'Low maintenance 🌵') {
-      showMessage(`You obviously `, 'bot')
+      showMessage(`If you have the patience you can grow your own christmas tree.. Here is a baby spruce to get you started 🌲`, 'bot')
     } else {
       showMessage(`Alright, a plant with a touch of poison for you my dear...`, 'bot')
-      setTimeout (() => {showMessage(`Hmmm..`, 'bot')}, 1000)
-      setTimeout (() => {showMessage(`I suggest a `, 'bot')}, 2000)
+      setTimeout (() => {showMessage(`Hmm.... I suggest a carnivorous plant for you 🐲`, 'bot')}, 1000)
     }
     }, 1500)
 
      setTimeout (() => {inputWrapper.innerHTML =`
       <button id="yes">I'm happy with your choice!</button>
       <button id="no">Naah, try again!</button>`
-      //  `
-      // <input id="yes" type="radio" name="choice" value="yes" /> I'm happy with your choice!
-      // <input id="no" type="radio" name="choice" value="no"/> Naah, try again!
-      // <button class="send-btn" id="send" type="submit">Send</button>
-      // `
+ 
       document.getElementById('yes')
       .addEventListener('click', () => playAgain(`I'm very satisfied with your expertise!`))
       document.getElementById('no')
       .addEventListener('click', () => playAgain(`It seems like you don't know me at all...`))
-     }, 4500)
+     }, 3000)
   }
 
     const playAgain = (happy) => {
     questionNumber++
     showMessage(happy, 'user')
     // trying out a different approach to the if/else statement
-    happy === `I'm very satisfied with your expertise!` ? showMessage(`Perfect! Let's wrap it up in paper for you!`, 'bot') : showMessage(`I'm sorry to hear that... let's try again, then 😌`, 'bot')
+    
+    if (happy === `I'm very satisfied with your expertise!`) {
+      showMessage(`Perfect! Let's wrap it up in paper for you!`, 'bot') 
+      inputWrapper.innerHTML = ''
+      setTimeout (() => {
+        showMessage(`Great, here you go! 🎁`, 'bot')
+      }, 1500)
+     } else {
+       showMessage(`I'm sorry to hear that... let's try again, then 🙄`, 'bot')
+       setTimeout (() => {location.reload()
+       return false
+     }, 2500)
+    }
   }
-
 
 
 const handleInput = (event) => {
@@ -161,10 +161,10 @@ const handleInput = (event) => {
 }
 
 const handleNameQuestion = () => {
-  inputFromUser = userInput.value // ska vi göra om till konstant, jag tror inte vi använder denna mer än 1 gång
-  showMessage(`My name is ${inputFromUser}!`, 'user')
+  const name = userInput.value 
+  showMessage(`My name is ${name}!`, 'user')
   inputWrapper.innerHTML = ``
-  setTimeout(whatPlant,1000)
+  setTimeout (() => {whatPlant(name)},1000)
 }
 
 // Set up your eventlisteners here
