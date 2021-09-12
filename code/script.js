19b3c42690409cc4d9b5478c8df24e59884a1960
AschwinSiaila <aschwin76@hotmail.com>
2021-09-12 23:50:13 +0200

    SetTimeOut 1500

--- code/script.js
+++ code/script.js
@@ -44,4 +44,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 1500)
