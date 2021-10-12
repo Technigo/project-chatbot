bc057bc865dbf592e0aeb283918e6e0758efb829
Gustav Frid <gustav.frid@gmail.com>
2021-10-12 14:34:44 +0200

    change indentation

--- code/script.js
+++ code/script.js
@@ -1,8 +1,8 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById('chat');
-const yesBtn = document.getElementById('yes-btn');
-const noBtn = document.getElementById('no-btn');
-const inputWrapper = document.getElementById('input-wrapper');
+const chat = document.getElementById('chat')
+const yesBtn = document.getElementById('yes-btn')
+const noBtn = document.getElementById('no-btn')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // Global variables, if you need any, declared here
 
@@ -10,50 +10,50 @@ const inputWrapper = document.getElementById('input-wrapper');
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-	if (sender === 'user') {
-		// console.log('is this the user?');
-		chat.innerHTML += `
+  if (sender === 'user') {
+    // console.log('is this the user?');
+    chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
         <img src="assets/dj.png" alt="User" />  
       </section>
-    `;
-	} else if (sender === 'bot') {
-		// console.log('is this the bot?');
-		chat.innerHTML += `
+    `
+  } else if (sender === 'bot') {
+    // console.log('is this the bot?');
+    chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/scuba-diver.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
       </section>
-    `;
-	}
-	// This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-	chat.scrollTop = chat.scrollHeight;
-};
+    `
+  }
+  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+  chat.scrollTop = chat.scrollHeight
+}
 
 // Starts here
 const greeting = () => {
-	showMessage(`Hello there, are you ready to place your order?`, 'bot');
-	// Just to check it out, change 'bot' to 'user' here 👆
-};
+  showMessage(`Hello there, are you ready to place your order?`, 'bot')
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
 
 const clearInput = () => {
-	inputWrapper.innerHTML = ``;
-};
+  inputWrapper.innerHTML = ``
+}
 
 const handleNotReady = () => {
-  showMessage(`Not ready yet...`, 'user');
+  showMessage(`Not ready yet...`, 'user')
   setTimeout(greeting, 1000)
 }
 
 const selectTheme = () => {
-	showMessage(`Sure!`, 'user');
-	showMessage(`That's awesome!`, 'bot');
-	inputWrapper.innerHTML = `
+  showMessage(`Sure!`, 'user')
+  showMessage(`That's awesome!`, 'bot')
+  inputWrapper.innerHTML = `
   <select id="select-theme">
     <option value="" selected disabled>👇 Select a theme...</option>
     <option value="Architecture">Architecture</option>
@@ -97,68 +97,68 @@ const selectTheme = () => {
     <option value="VIDIYO™">VIDIYO™</option>
     <option value="Xtra">Xtra</option>
   </select>
-`;
-	const select = document.getElementById('select-theme');
-	select.addEventListener('change', () => evaluateThemeSelection(select.value));
-};
+`
+  const select = document.getElementById('select-theme')
+  select.addEventListener('change', () => evaluateThemeSelection(select.value))
+}
 
-const evaluateThemeSelection = (selectedTheme) => {
-	showMessage(selectedTheme, 'user');
-	if (selectedTheme === 'Star Wars™') {
-		showMessage(`Awesome! You picked the only valid choice!`, 'bot');
-		starWarsItems();
-	} else {
-		showMessage(`Shoot! ${selectedTheme} is out of stock. How about Star Wars lego?`, 'bot');
-		inputWrapper.innerHTML = `
+const evaluateThemeSelection = selectedTheme => {
+  showMessage(selectedTheme, 'user')
+  if (selectedTheme === 'Star Wars™') {
+    showMessage(`Awesome! You picked the only valid choice!`, 'bot')
+    starWarsItems()
+  } else {
+    showMessage(`Shoot! ${selectedTheme} is out of stock. How about Star Wars lego?`, 'bot')
+    inputWrapper.innerHTML = `
     <button id="yes-btn">Sure! Show me what you got!</button>
     <button id="no-btn">Not, not interested.</button>
-    `;
-		document.getElementById('yes-btn').addEventListener('click', () => {
-			showMessage('Sure! Show me what you got!', 'user');
-			starWarsItems();
-		});
-		document.getElementById('no-btn').addEventListener('click', () => {
-			showMessage('No, not interested.', 'user');
-			showMessage('Ok. Good bye.', 'bot');
-			clearInput();
-		});
-	}
-};
+    `
+    document.getElementById('yes-btn').addEventListener('click', () => {
+      showMessage('Sure! Show me what you got!', 'user')
+      starWarsItems()
+    })
+    document.getElementById('no-btn').addEventListener('click', () => {
+      showMessage('No, not interested.', 'user')
+      showMessage('Ok. Good bye.', 'bot')
+      clearInput()
+    })
+  }
+}
 
 const starWarsItems = () => {
-	showMessage(`May the force be with you! Choose one of our top picks:`, 'bot');
-	inputWrapper.innerHTML = `
+  showMessage(`May the force be with you! Choose one of our top picks:`, 'bot')
+  inputWrapper.innerHTML = `
   <button id="btn-1">Millenium Falcon</button>
   <button id="btn-2">R2D2</button>
   <button id="btn-3">Death Star</button>
-    `;
-	document.getElementById('btn-1').addEventListener('click', () => confirmOrder('Millenium Falcon'));
-	document.getElementById('btn-2').addEventListener('click', () => confirmOrder('R2D2'));
-	document.getElementById('btn-3').addEventListener('click', () => confirmOrder('Death Star'));
-};
+    `
+  document.getElementById('btn-1').addEventListener('click', () => confirmOrder('Millenium Falcon'))
+  document.getElementById('btn-2').addEventListener('click', () => confirmOrder('R2D2'))
+  document.getElementById('btn-3').addEventListener('click', () => confirmOrder('Death Star'))
+}
 
-const confirmOrder = (item) => {
-	showMessage(item, 'user');
-	showMessage(`One ${item} added to you cart. Are you happy with order?`, 'bot');
-	inputWrapper.innerHTML = `
+const confirmOrder = item => {
+  showMessage(item, 'user')
+  showMessage(`One ${item} added to you cart. Are you happy with order?`, 'bot')
+  inputWrapper.innerHTML = `
   <button id="yes-btn">Yes!</button>
   <button id="no-btn">No!</button>
-  `;
-	document.getElementById('yes-btn').addEventListener('click', () => {
-		showMessage('Yes', 'user');
-		showMessage(`Awesome! One ${item} will be sent to you from a galaxy far far away.`, 'bot');
-		clearInput();
-	});
-	document.getElementById('no-btn').addEventListener('click', () => {
-		showMessage('No.', 'user');
-		showMessage('Ok. Good bye.', 'bot');
-		clearInput();
-	});
-};
+  `
+  document.getElementById('yes-btn').addEventListener('click', () => {
+    showMessage('Yes', 'user')
+    showMessage(`Awesome! One ${item} will be sent to you from a galaxy far far away.`, 'bot')
+    clearInput()
+  })
+  document.getElementById('no-btn').addEventListener('click', () => {
+    showMessage('No.', 'user')
+    showMessage('Ok. Good bye.', 'bot')
+    clearInput()
+  })
+}
 
 // Set up your eventlisteners here
-yesBtn.addEventListener('click', () => selectTheme());
-noBtn.addEventListener('click', () => handleNotReady());
+yesBtn.addEventListener('click', () => selectTheme())
+noBtn.addEventListener('click', () => handleNotReady())
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -166,4 +166,4 @@ noBtn.addEventListener('click', () => handleNotReady());
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000);
+setTimeout(greeting, 1000)
