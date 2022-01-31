b75fa5852e3a39261d1440a055a79e021bb558aa
vanhaj <vanessa.hajek@hotmail.com>
2022-01-31 21:06:40 +0100

    iteration 2, added name and button

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 
+const quizButton = document.querySelector(".quiz-button")
+
 // Global variables, if you need any, declared here
 
 // Functions declared here
@@ -32,12 +34,18 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
+
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
+   showMessage(`Hello there, What's your name?`, 'bot')
+   // Just to check it out, change 'bot' to 'user' here 👆
+ }
+
+
+
 
 // Set up your eventlisteners here
+
+quizButton.addEventListener("click", greeting)
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -45,4 +53,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+// setTimeout(greeting, 1000)
