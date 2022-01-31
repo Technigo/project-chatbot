47e5d0e61edeb7a1aeb288098811c89d982f52fd
eyahya-khan <eyahyakhan@gmail.com>
2022-01-31 20:22:31 +0100

    added course button

--- js/script.js
+++ js/script.js
@@ -32,9 +32,109 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'user')
+  showMessage(`Hello, <br>What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
+//display time
+const getTime = ()=> {
+  let today = new Date();
+  let hours =  today.getHours();
+  let minutes = today.getMinutes();
+  
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+  if(minutes < 10){
+    minutes = "0" + minutes;
+  }
+  let time = hours + ":" + minutes
+
+   return time;
+}
+
+let displayTime = document.getElementById("chat")
+displayTime.append (getTime())
+
+/*audio sound*/
+let sound = document.getElementById("myAudio");
+/*take user input */
+let user1 = document.getElementById("send-btn");
+let userText = document.getElementById("name-input");
+user1.addEventListener("click", sendText);
+
+function sendText(event){
+  event.preventDefault();
+let txt = userText.value;
+  if(txt){
+    showMessage(`${txt}`, 'user');
+    userText.value = "";
+    sound.play();
+    setTimeout(() => showMessage(`Thanks ${txt}<br> Select a course that you want!`, 'bot'), 2000)
+    /*setTimeout(() => document.getElementById('course').removeAttribute('class','course'),2000)
+    document.getElementById('send-btn').setAttribute('disabled','true')
+    document.getElementById('name-input').setAttribute('disabled','true')*/
+    setTimeout(() => document.getElementById('name-form').innerHTML =`<div id="course" class="course">
+    <input id="html" type="button" value="HTML">
+    <input id="css" type="button" value="CSS">
+    <input id="javascript" type="button" value="JavaScript">
+  </div>`,2000)
+    
+    
+    //select course
+    let htmlBtn = document.getElementById('html')
+    let cssBtn = document.getElementById('css')
+    let javascriptBtn = document.getElementById('javascript')
+    
+    
+    htmlBtn.addEventListener('click', () =>{
+      htmlBtn = htmlBtn.value
+      console.log(htmlBtn)
+      showMessage(`${htmlBtn}`, 'user');
+      setTimeout(() => showMessage(`Thanks <br> Select price!`, 'bot'), 2000)
+      setTimeout(() => document.getElementById('name-form').innerHTML =`<div id="priceSlect" class="priceSelect">
+      <input id="priceSelect-one" type="button" value="3500 Kr">
+      <input id="priceSelect-two" type="button" value="4000 Kr">
+      <input id="priceSelect-three" type="button" value="10000 Kr">
+    </div>`,2000)
+      /*
+      setTimeout(() => document.getElementById('course').setAttribute('class','course'),2000)
+      setTimeout(() => document.getElementById('priceSlect').removeAttribute('class','priceSlect'),2000)
+      document.getElementById('send-btn').setAttribute('disabled','true')
+      document.getElementById('name-input').setAttribute('disabled','true')*/
+
+
+    })
+    
+    cssBtn.addEventListener('click', () =>{
+      cssBtn = cssBtn.value
+      showMessage(`${cssBtn}`, 'user');
+      setTimeout(() => showMessage(`Thanks <br> Select price!`, 'bot'), 2000)
+      setTimeout(() => document.getElementById('course').setAttribute('class','course'),2000)
+    })
+    
+    javascriptBtn.addEventListener('click', () =>{
+      javascriptBtn = javascriptBtn.value
+      showMessage(`${javascriptBtn}`, 'user');
+      setTimeout(() => showMessage(`Thanks <br> Select price!`, 'bot'), 2000)
+      setTimeout(() => document.getElementById('course').setAttribute('class','course'),2000)
+    })
+  
+  
+  
+  }
+}
+/*
+const courseSelect = ()=>{
+  let htmlBtn = document.getElementById('html')
+  let cssBtn = document.getElementById('css')
+  let javascriptBtn = document.getElementById('javascript')
+  htmlBtn.addEventListener('click', () =>{
+    htmlBtn = htmlBtn.value
+  })
+  
+}*/
+
+
 
 // Set up your eventlisteners here
 
