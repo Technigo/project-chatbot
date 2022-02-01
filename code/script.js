85062583caffde955ce5945094720be41a091b9e
TereseClaesson <terese.claesson@gmail.com>
2022-02-01 19:38:09 +0100

    Change const showEmojis

--- code/script.js
+++ code/script.js
@@ -1,8 +1,19 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const inputWrapper = document.getElementById('inputWrapper')
+const input = document.getElementById('input')
+const sendBtn = document.getElementById('send')
 
 
+let questionNumber = 1
+
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
+}
+
+const userReply = (msg) => {
+  showMessage(msg, 'user')
+}
 // Global variables, if you need any, declared here
 
 // Functions declared here
@@ -33,13 +44,64 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber)
+
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => showEmojis(message), 1000)
+  } else {
+    userReply(message)
+    setTimeout(thankYou, 1000)
+  }
+}
+
+
 // Starts here
 const greeting = () => {
   showMessage(`Hi! How you doing?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+const showEmojis = (msg) => {
+  questionNumber++
+  botReply(
+    `I see. ${msg}. How would you rate your day?`
+  )
+  inputWrapper.innerHTML = `
+    <button id="brilliantBtn">Brilliant</button>
+    <button id="okBtn">OK</button>
+    <button id="terribleBtn">Terrible</button>
+  `
+
+  document
+    .getElementById('brilliantBtn')
+    .addEventListener('click', () => nextQuestion('brilliant'))
+  document
+    .getElementById('okBtn')
+    .addEventListener('click', () => nextQuestion('ok'))
+  document
+    .getElementById('terribleBtn')
+    .addEventListener('click', () => nextQuestion('terrible'))
+}
+
+
 // Set up your eventlisteners here
+
+const handleFeelingInput = (event) => {
+  event.preventDefault()
+  // Store the value in a variable so we can access it after we 
+	// clear it from the input
+  const feeling = feelingInput.value
+  showMessage(feeling, 'user')
+  feelingInput.value = ''
+
+  // After 1 second, show the next question by invoking the next function.
+	// passing the name into it to have access to the user's name if we want
+	// to use it in the next question from the bot.
+  setTimeout(() => showFoodOptions(feeling), 1000)
+}
 
 
 // When website loaded, chatbot asks first question.
