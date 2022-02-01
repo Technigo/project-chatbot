d31f3dc296a262f78d1d5dd93ca421576b5dabfa
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-02-01 13:44:51 +0100

    1st iteration done

--- code/script.js
+++ code/script.js
@@ -1,6 +1,7 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 
+
 // Global variables, if you need any, declared here
 
 // Functions declared here
@@ -8,6 +9,7 @@ const chat = document.getElementById('chat')
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log()
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,6 +19,7 @@ const showMessage = (message, sender) => {
       </section>
     `
   } else if (sender === 'bot') {
+    console.log()
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -32,11 +35,14 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hello there, What's your name?`, 'bot') // denna är kopplad till <p>message</p>
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
 // Set up your eventlisteners here
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -44,4 +50,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+setTimeout(greeting, 1200) 
