f3fa07950b95a9ac0555aaf9a9f24b4db0335ba4
Michael Dohmann Chang <michaelchang@Michaels-MBP.lan>
2022-02-01 19:16:37 +0100

    finished integrating their CSS with our CSS. Yay! -Michael

--- code/script.js
+++ code/script.js
@@ -1,30 +1,25 @@
 // Our Javascript
 
 // Page 1 - Login Page
-
 // Get Hours & Minutes
 let findTime = new Date().toTimeString().substr(0,5);
 let time = findTime.replace(":", ".");
-console.log(time);
 
 //Get Weekday (Define array, pull date, subtract 1 for array)
 const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const weekday = new Date();
 let today = weekdays[weekday.getDay() - 1];
-console.log(today);
 
 //Get Date (Fixed to remove 0 from 01-09 in date)
 let date = new Date().toDateString().substr(8,2);
 if (date < 10) {
   date = date.replace("0", "");
 }
-console.log(date);
 
 // Get Month (Define Array, Choose Array item)
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let currentMonth = new Date();
 const month = months[currentMonth.getMonth()];
-console.log(month);
 
 //Insert Time into Unlock Scren
 const insertTime = document.getElementById("time");
@@ -40,9 +35,14 @@ usernameInput.addEventListener('click', function() {
   this.classList.toggle("login-container--active")
 })
 
-// Add event listener for login button push
-// Login Button Behaviour
+// Page 2 - Chat Page
+// DOM selectors & Global Variables
+const chat = document.getElementById('chat')
 const loginButton = document.querySelector('.login__button');
+let userName = "";
+
+
+//Event Listener for Login Button Push
 loginButton.addEventListener('click', function() {
   // prevent.Default stops page from reloading when clicking icon
   event.preventDefault();
@@ -54,25 +54,18 @@ loginButton.addEventListener('click', function() {
   // Correct nameString to userName (cut string after first name, correct Case)
   const nameString2 = nameString.split(" ")[0];
   console.log(nameString2);
-  const userName = nameString2.charAt(0).toUpperCase() + nameString2.substr(1).toLowerCase();
-  console.log(userName);
+
+  userName = nameString2.charAt(0).toUpperCase() + nameString2.substr(1).toLowerCase();
 
   // Hide Login Screen
   document.querySelector(".unlock-screen").style.visibility = "hidden";
 
   //Show Chat Screen
   document.querySelector(".chat-screen").style.visibility = "visible";
+
+  greeting(userName);
+  console.log(userName);
 })
-
-
-// Their JavaScript
-
-// All the DOM selectors stored as short variables
-const chat = document.getElementById('chat')
-
-// Global variables, if you need any, declared here
-
-// Functions declared here
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -82,13 +75,13 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/profile.png" alt="User" class="chat-icon" />  
+        <img src="assets/profile.png" alt="User Icon" class="chat-icon user-icon" />  
       </section>
     `
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/sassy.png" alt="Bestie Icon" class="chat-icon" />
+        <img src="assets/sassy.png" alt="Bestie Icon" class="chat-icon bestie-icon" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -100,8 +93,8 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
-const greeting = () => {
-  showMessage(`Hello there ${userName}`, 'bot')
+const greeting = (userName) => {
+  showMessage(`Heyyy ${userName}, I heard you just got dumped by that asshole... what was his name again?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
@@ -113,4 +106,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+// setTimeout(greeting, 1000)
