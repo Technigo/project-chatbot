73a373092cb04370774de509c02d91c4561fabe8
MattLjunggren <72102921+MattLjunggren@users.noreply.github.com>
2022-02-01 21:26:47 +0100

    Until second question in the chatbot exchange

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const chat = document.getElementById('chat')
 const inputWrapper = document.getElementById('input-wrapper')
-const input = document.getElementById('input')
-const sendBtn = document.getElementById('send')
+const input = document.getElementById('nameInput')
+const sendBtn = document.getElementById('sendBtn')
 
-// let questionNumber = 1
+let questionNumber = 1
 
 const botReply = (msg) => {
   showMessage(msg, 'bot')
@@ -35,7 +34,7 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user.png" alt="User" />
       </section>
     `
   } else if (sender === 'bot') {
@@ -54,10 +53,93 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
-const greeting = () => {
-  showMessage(`Good day and welcome to Bot Theatre! What is your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+// const greeting = () => {
+//   showMessage(`Good day and welcome to Bot Theatre! What is your name?`, 'bot')
+//   // Just to check it out, change 'bot' to 'user' here 👆
+// }
+
+ const greeting = () => {
+  questionNumber = 1
+  botReply(`Good day and welcome to Bot Theatre! What is your name?`, 'bot')
 }
+
+ const nextQuestion = (message) => {
+ console.log('questionNumber', questionNumber)
+ 
+if (questionNumber === 1) {
+userReply(message)
+nameInput.value = ''
+setTimeout(() => showFoodTypes(message), 1000)
+}
+else if (questionNumber === 2) {
+  userReply(message)
+  setTimeout(() => showMenu(message), 1000)
+} else if (questionNumber === 3) {
+  userReply(message)
+  setTimeout(() => showDishSize(message), 1000)
+} else if (questionNumber === 4) {
+  userReply(message)
+  setTimeout(() => showPrice(message), 1000)
+} else {
+  userReply(message)
+  setTimeout(thankYou, 1000)
+}
+}
+
+
+ const showFoodTypes = (msg) => {
+ questionNumber++
+  botReply(
+    `Nice to meet you ${msg}. What mood are you in?`
+  )
+  }
+ 
+ 
+ sendBtn.addEventListener('click', () => nextQuestion(input.value))
+
+
+
+ //const handleNameInput = (event) => {
+  //event.preventDefault()
+  // Store the value in a variable so we can access it after we 
+ //clear it from the input
+  // const name = nameInput.value
+  // showMessage(name, 'user')
+  // nameInput.value = ''
+
+  // After 1 second, show the next question by invoking the next function.
+	// passing the name into it to have access to the user's name if we want
+	// to use it in the next question from the bot.
+  //setTimeout(() => showFoodOptions(name), 1000)
+//}
+
+
+ 
+
+// const showFoodTypes = (msg) => {
+//   questionNumber++
+//   botReply(
+//     `Hello ${msg}. What type of mood are you in?`
+//   )
+
+//   inputWrapper.innerHTML = `
+//   <button id="happyBtn">🤣</button>
+//   <button id="romanticBtn">🥰</button>
+//   <button id="horrorBtn">😱</button>
+//   <button id="bollywoodBtn">💃🏼</button>
+//   `
+
+// document
+//   .getElementById('pizzaBtn')
+//   .addEventListener('click', () => nextQuestion('pizza'))
+// document
+//   .getElementById('pastaBtn')
+//   .addEventListener('click', () => nextQuestion('pasta'))
+// document
+//   .getElementById('saladBtn')
+//   .addEventListener('click', () => nextQuestion('salad'))
+// }
+
 
 // Set up your eventlisteners here
 
@@ -71,4 +153,3 @@ const greeting = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
-
