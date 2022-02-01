6cdcddaf829d068dcbb561ef0defa72d95e5f22c
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-02-01 11:17:28 +0200

    made a plan for the chatbot structure, modified html titles and add the first function to js

--- code/script.js
+++ code/script.js
@@ -1,13 +1,25 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+//const input = document.getElementById('input')
+//const sendBtn = document.getElementById('send')
 
 // Global variables, if you need any, declared here
+let questionNumber = 1
 
 // Functions declared here
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
+}
+
+const userReply = (msg) => {
+  showMessage(msg, 'user')
+}
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
+    console.log("user is speaking")
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -31,18 +43,67 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-// Starts here
-const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+const nextQuestion=(message)=>{
+  if (questionNumber === 1) {
+    userReply (message)
+    setTimeout(()=> deliveryAddress (message), 1000)
+  }
+
+  else if (questionNumber === 2) {
+    userReply(message)
+    input.value = ''
+    setTimeout(()=> deliveryPerson (message), 1000)
+  }
 }
 
-// Set up your eventlisteners here
+// Starts here
+const greeting = () => {
+  questionNumber=1
+  showMessage(`Hello there! Tomorrows options are tulips, roses and lilies`, 'bot')
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
+inputWrapper.innerHTML=
+`<button id="tulips">Tulips</button>
+<button id="roses">Roses</button>
+<button id="lilies">Lilies</button>`
+
+// Set up your eventlisteners here
+document
+    .getElementById('tulips')
+    .addEventListener('click', () => nextQuestion())
+document
+    .getElementById('roses')
+    .addEventListener('click', () => nextQuestion())
+document
+    .getElementById('lilies')
+    .addEventListener('click', () => nextQuestion())
+}
+
+/* To make the flowerbuttons disapear after click
+const select = document.getElementById('select')
+  select.addEventListener('change', () => nextQuestion(select.value))
+*/ 
+
+const deliveryAddress = ()=> {
+  questionNumber++
+  botReply(`What is the delivery address?`)
+}
+
+const deliveryPerson = ()=> {
+  questionNumber++
+  botReply(`Are the flowers for yourself or someone else?`)
+  inputWrapper.innerHTML=
+`<button id="myself">Myself</button>
+<button id="someoneElse">Someone Else</button>`
+
+document
+    .getElementById('myself')
+    .addEventListener('click', () => nextQuestion())
+document
+    .getElementById('someoneElse')
+    .addEventListener('click', () => nextQuestion())
+}
+
+
+
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
