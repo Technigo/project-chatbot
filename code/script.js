9f644d3c94c27ca8265615d8f290acabf14ce68f
JaEngd <95588205+JaEngd@users.noreply.github.com>
2022-02-01 20:00:50 +0100

    Done with Iteration 2 and part 1-2 of Iteration 3

--- code/script.js
+++ code/script.js
@@ -1,6 +1,8 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-
+const nameInput = document.getElementById('name-input')
+const form = document.getElementById('name-form')
+const submit = document.getElementById('submit')
 // Global variables, if you need any, declared here
 // Functions declared here
 // This function will add a chat bubble in the correct place based on who the sender is
@@ -31,12 +33,41 @@ const showMessage = (message, sender) => {
 }
 
 // Starts here
-const greeting = () => {
+  const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+  const handleNameInput = (event) => { //at submit this function will be invoked
+  event.preventDefault() //prevents website refresh at submit
+
+  const name = nameInput.value //input value will be stored in the const name
+  console.log(name)
+
+  showMessage(`My name is ${name}.`, 'user') //users answer
+  nameInput.value = '' //clearing name input setting it to an empty string
+
+  setTimeout(() => foodOptions(btnSelectMain, name), 1000) //passing the arguments to foodOptions function with 1s delay
+}  
+
+form.addEventListener('submit', handleNameInput)
+
 // Set up your eventlisteners here
+/*form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  nxtQuestion(input.value)
+})*/
+
+  // Store the value in a variable so we can access it after we 
+	// clear it from the input
+  
+
+  // After 1 second, show the next question by invoking the next function.
+	// passing the name into it to have access to the user's name if we want
+	// to use it in the next question from the bot.
+  //
+ 
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -45,3 +76,11 @@ const greeting = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
+
+
+
+
+
+
+
+
