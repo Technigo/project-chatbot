fb8f17ec17454e8e265248de8b508631ca748089
Amanda Tange <amandatange@hotmail.com>
2022-02-02 10:00:23 +0100

    added functions for question 2, 3, 4

--- code/script.js
+++ code/script.js
@@ -3,29 +3,147 @@ const chat = document.getElementById('chat');
 const input = document.getElementById("name-input");
 const sendButton = document.getElementById("send-btn");
 const inputWrapper = document.getElementById("input-wrapper");
+let buttons = document.getElementsByTagName("button");
+
 // Global variables, if you need any, declared here
-
+let numberOfQuestionsAsked = 0
 const greetingMessage = `Hello there, What's your name?`;
-
+let userNameDict = {}
 // Functions declared here
 
-const form = document.getElementById("name-form")
- form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let userName = input.value
-    showMessage(userName, "user")
-    input.value = '';
-    setTimeout(questionAge(userName), 1000)
-  })
+const form = document.getElementById("name-form");
+form.addEventListener("submit", (event) => {
+  //prevent reload of page
+  event.preventDefault();
+});
+
+const buttonClicked = () => {
+  numberOfQuestionsAsked++
+  
+
+  if (numberOfQuestionsAsked < 5) {
+    console.log(numberOfQuestionsAsked)
+    if (numberOfQuestionsAsked === 1) {
+      showMessage(input.value, "user")
+      //saves the user's name
+      userNameDict["name"] = input.value
+      questionAge(userNameDict["name"])
+    } else if (numberOfQuestionsAsked === 2) {
+      showMessage(event.target.innerHTML, "user")
+      questionEnergy()
+    } else if (numberOfQuestionsAsked === 3) {
+      console.log("hej")
+    } else if (numberOfQuestionsAsked === 4) {
+      console.log(numberOfQuestionsAsked)
+    }
+    // } else if (numberOfQuestionsAsked === 5) {
+      // console.log(numberOfQuestionsAsked)
+    // }
+    
+  }
+  input.value = '';
+};
+
+
+for (let button of buttons) {
+button.addEventListener("click", buttonClicked)
+console.log(buttons);
+};
+
+
+
+
+// form.addEventListener("submit", (event) => {
+//   //prevent reload of page
+//   event.preventDefault()
+//   numberOfQuestionsAsked++
+//   //posts user's message
+//   showMessage(input.value, "user")
+  
+//   if (numberOfQuestionsAsked < 6) {
+//     console.log(numberOfQuestionsAsked)
+//     if (numberOfQuestionsAsked === 1) {
+//       //saves the user's name
+//       let userName = input.value
+//       questionAge(userName)
+//     } else if (numberOfQuestionsAsked === 2) {
+//       console.log(numberOfQuestionsAsked)
+//     } else if (numberOfQuestionsAsked === 3) {
+//       console.log(numberOfQuestionsAsked)
+//     } else if (numberOfQuestionsAsked === 4) {
+//       console.log(numberOfQuestionsAsked)
+//     } else if (numberOfQuestionsAsked === 5) {
+//       console.log(numberOfQuestionsAsked)
+//     }
+    
+//   }
+//   input.value = '';
+// })
 
 const questionAge = (name) => {
   showMessage(`Hello ${name}! How old are you?`,"bot")
   inputWrapper.innerHTML = ''
+  
   let button1 = document.createElement('button')
-  button1.innerHTML = '18'
-  inputWrapper.appendChild(button1)
+  let button2 = document.createElement('button')
+  let button3 = document.createElement('button')
+
+  button1.innerHTML = '0-29'
+  button2.innerHTML = '30-59'
+  button3.innerHTML = '60+'
+
+  let buttonsToAppend = [button1, button2, button3]
+  for (let button of buttonsToAppend) {
+    inputWrapper.appendChild(button)
+    button.onclick = buttonClicked
+  }
+
+  // makeButtonsClickable();
 }
 
+const questionMood = (name) => {
+  showMessage(`So, ${name}.. Which smiley best describes your mood?`,"bot")
+  inputWrapper.innerHTML = ''
+  
+  let smileyDiv1 = document.createElement('div')
+  let smileyDiv2 = document.createElement('div')
+  let smileyDiv3 = document.createElement('div')
+  let smileyDiv4 = document.createElement('div')
+  let smileyDiv5 = document.createElement('div')
+
+  smileyDiv1.innerHTML = '&#128520'
+  smileyDiv2.innerHTML = '&#128525'
+  smileyDiv3.innerHTML = '&#128549'
+  smileyDiv4.innerHTML = '&#128556'
+  smileyDiv5.innerHTML = '&#128513'
+
+  let smileys = [smileyDiv1, smileyDiv2, smileyDiv3, smileyDiv4, smileyDiv5]
+  for (let smiley of smileys) {
+    smiley.classList.add("smiley-div")
+    inputWrapper.appendChild(smiley)
+  }
+}
+const questionEnergy=()=>{
+  showMessage(`What's your energy level?`,"bot")
+  inputWrapper.innerHTML=""
+  const slideContainer=document.createElement("div")
+  slideContainer.className="slide-container"
+  const slider =document.createElement("input")
+  slider.type="range"
+  slider.min="1" 
+  slider.max="5"
+  slider.className="slider"
+  slideContainer.appendChild(slider)
+  inputWrapper.appendChild(slideContainer)
+ // const slide = document.getElementById("myRange");
+const output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+}
 
 
 // const form = document.getElementById("name-form");
@@ -110,3 +228,5 @@ function makeColors () {
 }
 // makeColors();
 // document.getElementsByTagName("body")[0].style.backgroundColor = `rgb(${values[0]}, ${values[1]}, ${values[2]})`;
+
+
