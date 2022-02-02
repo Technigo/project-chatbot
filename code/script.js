818b651b11376213c6cf01da92d6bc85d71fdecd
Arianna Allahyar <anallahyar@gmail.com>
2022-02-02 15:05:10 +0100

    Question counter created

--- code/script.js
+++ code/script.js
@@ -1,8 +1,9 @@
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
-const input = document.getElementById('name-input')
+const nameInput = document.getElementById('name-input')
 
 // Global variables, if you need any, declared here
+
 
 // Functions declared here
 
@@ -28,7 +29,8 @@ const showMessage = (message, sender) => {
     `
   } 
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight;
+//  questionFlow(); 
 }
 
 // Starts here
@@ -40,33 +42,38 @@ const greeting = () => {
 // Set up your eventlisteners here
 document.getElementById('name-form').onsubmit = event => {
   event.preventDefault();
-  showMessage(input.value, 'user');
-  input.value ='';
+  showMessage(nameInput.value, 'user');
+  nameInput.value ='';
+  questionFlow();
+  // call function to move through questions
 }
 
 // ------- PSEUDOCODE ------- //
 
 // Define our variables
-// let questionCounter ...
-// const inputWrapper ...
+const inputWrapper = document.getElementById('input-wrapper');
+let questionCounter = 0;
 
 // declare function to move through questions that is called from event listeners
 // obs naming a function should decribe what it is doing. ex 'messageHandler" or 'messageDelivery'
-  // display user input message
   // update questionCounter to keep track of what question comes next
   // move through questionnaire based on which question we're on usung if else statement
     // if questionCounter === 1 {
-      // display bot-message here OR in the question itself (add set-timeout?)
       // call first question function
     // } else if... {
       // call next question function
     // }
 
+const questionFlow = () => {
+  questionCounter++;
+}
+
 // Question 1 function
-  // display bot-message here OR in if-else statement (add set-timeout?)
+  // display bot-message here (add set-timeout?)
   // html form input type (i.e. how to answer)
-  // do something on submit (event listener) that calls looping function
+  // do something on submit (event listener) that calls showMessage
     // submit can have logic built in depending on how user answers
+
 
 // Question 2 function
   // repeat above question structure
@@ -81,10 +88,11 @@ document.getElementById('name-form').onsubmit = event => {
 
 // End message
 
+
+setTimeout(greeting, 500)
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 500)
