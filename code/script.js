1e450c68a34e63da16762a19b90ba39c769ac594
Katarina Elgerus <katarina.elgerus@liv.se>
2022-02-02 19:57:13 +0100

    iteration 2

--- code/script.js
+++ code/script.js
@@ -32,7 +32,7 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = () => {
-  showMessage(`Hello there, What's your name?`, 'bot')
+  showMessage(`Hi there Icecream lover, What's your name?`, 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
