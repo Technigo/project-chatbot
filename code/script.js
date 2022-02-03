b57bb426e4a7de8a75046786b9f39e82d6c1f4df
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-02-03 12:13:09 +0200

    added new images fot bot and the user and made styling for mobile version, and filized the js code for different questions with if / else scenarios, there is one mistake we need to fix in the js code

--- code/script.js
+++ code/script.js
@@ -6,7 +6,12 @@ const sendBtn = document.getElementById('send')
 const form = document.getElementById('name-form')
 
 // Global variables, if you need any, declared here
-let questionNumber = 1
+let questionNumber = 1 //variable for counting which question we are dealing (defined in Figma)
+let flowers = "" //variable for flower to be used in the final message of the bot 
+let address ="" //variable for address to be used in the final message of the bot 
+let cardText ="" //variable for greeting card's greeting to be used in the final message of the bot 
+let delTime = ""//variable for delivery time to be used in the final message of the bot 
+let cardYes = "" //variable for card to be used in the final message of the bot 
 
 // Functions declared here
 const botReply = (msg) => {
@@ -26,14 +31,14 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/smile_user.png" alt="User" />  
       </section>
     `
   } else if (sender === 'bot') {
     console.log("bot is speaking")
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/smile.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -80,23 +85,26 @@ const nextQuestion = (message) => {
 // Question number 1
 const greeting = () => {
   questionNumber=1
-  showMessage(`Hello there! Tomorrows options are tulips, roses and lilies`, 'bot')
+  showMessage(`Hello there! Tomorrow's options are the following:`, 'bot')
 
 inputWrapper.innerHTML=
-`<button id="tulipsBtn">Tulips</button>
-<button id="rosesBtn">Roses</button>
-<button id="liliesBtn">Lilies</button>`
+`<button id="tulipsBtn">Bouquet of Tulips 15€</button>
+<button id="rosesBtn">Bouquet of Roses 25€</button>
+<button id="liliesBtn">Bouquet of Lilies 20€</button>`
 
 // Set up your eventlisteners here
-document
-    .getElementById('tulipsBtn')
-    .addEventListener('click', () => nextQuestion('Tulips'))
-document
-    .getElementById('rosesBtn')
-    .addEventListener('click', () => nextQuestion('Roses'))
-document
-    .getElementById('liliesBtn')
-    .addEventListener('click', () => nextQuestion('Lilies'))
+document.getElementById('tulipsBtn').addEventListener('click', () => {
+  flowers="Bouquet of Tulips 15€";
+  nextQuestion('Bouquet of Tulips 15€')
+})
+document.getElementById('rosesBtn').addEventListener('click', () => {
+  flowers="Bouquet of Roses 25€";
+  nextQuestion('Bouquet of Roses 25€')
+})
+document.getElementById('liliesBtn').addEventListener('click', () => {
+  flowers="Bouquet of Lilies 20€";
+  nextQuestion('Bouquet of Lilies 20€')
+})
 }
 
 // Question number 2
@@ -105,27 +113,33 @@ const deliveryAddress = ()=> {
   botReply(`What is the delivery address?`)
 
   inputWrapper.innerHTML=
-  `<input id="inputField" type="text"/>
+  `<input id="inputAddress" type="text"/>
   <button id="sendBtn" class="send-btn" type="submit">Send</button>
   `
-  document.getElementById('sendBtn').addEventListener('click', () => nextQuestion(inputField.value))
+
+  document.getElementById('sendBtn').addEventListener('click', () => {
+    address=inputAddress.value
+    nextQuestion(inputAddress.value)
+  })
 }
 
 // Question number 3
 const deliveryPerson = ()=> {
-  questionNumber++
+  questionNumber++ //this is the same than writing questionNumber = questionNumber + 1
   botReply(`Are the flowers for yourself or someone else?`)
 
   inputWrapper.innerHTML=
 `<button id="myselfBtn">Myself</button>
-<button id="someoneElseBtn">Someone Else</button>`
+<button id="someoneElseBtn">Someone else</button>`
 
-document
-    .getElementById('myselfBtn')
-    .addEventListener('click', () => nextQuestion('myself'))
-document
-    .getElementById('someoneElseBtn')
-    .addEventListener('click', () => nextQuestion('someoneElse'))
+//if the flowers are for myself bot does not ask the card questions and goes straight to question number 5
+document.getElementById('myselfBtn').addEventListener('click', () => {
+  questionNumber=5;nextQuestion('myself')
+})
+
+document.getElementById('someoneElseBtn').addEventListener('click', () =>{
+  nextQuestion('someone else')
+})
 }
 /*Trying the if else function 
   
@@ -134,27 +148,34 @@ document
 // Question number 4
 const card = () => {
   questionNumber++
-  botReply(`Would you like to add a card with your flowers?`)
+  botReply(`Would you like to add a greeting card with your flowers?`)
   inputWrapper.innerHTML=
   `<button id="cardYesBtn">Yes</button>
   <button id="cardNoBtn">No</button>`
 
-document
-  .getElementById('cardYesBtn')
-  .addEventListener('click', () => nextQuestion('Yes'))
-document
-  .getElementById('cardNoBtn')
-  .addEventListener('click', () => nextQuestion('No')) 
+  document.getElementById('cardYesBtn').addEventListener('click', () => {
+  cardYes="Yes"
+
+  nextQuestion('Yes')
+  })
+
+  document.getElementById('cardNoBtn').addEventListener('click', () => {
+    questionNumber=5;
+    nextQuestion('No')
+  }) 
 }
 
 // Question number 5
 const cardGreeting = () => {
   questionNumber++
-  botReply (`Type your greeting here, for example ''Happy Birthday Lisa''`)
+  botReply (`Type your greeting here, for example ''Happy Birthday Lisa, Love Tom''`)
   inputWrapper.innerHTML=
   `<input id="inputField" type="text"/>
   <button id="sendBtn" class="send-btn" type="submit">Send</button>`
-  document.getElementById('sendBtn').addEventListener('click', () => nextQuestion(inputField.value))
+  document.getElementById('sendBtn').addEventListener('click', () => {
+  cardText= inputField.value
+  nextQuestion(inputField.value)
+  })
 }
 
 // Question number 6
@@ -164,17 +185,47 @@ const deliveryTime = () => {
   inputWrapper.innerHTML=
   `<select id="deliveryTime">
     <option value="" selected disabled>👇 Select a delivery time...</option>
-    <option value="10-12PM">10-12PM</option>
-    <option value="12-2PM">12-2PM</option>
-    <option value="2-4PM">2-4PM</option>
-    <option value="4-6PM">4-6PM</option>
+    <option value="10AM-12PM">10AM-12PM</option>
+    <option value="12PM-2PM">12PM-2PM</option>
+    <option value="2PM-4PM">2PM-4PM</option>
+    <option value="4PM-6PM">4PM-6PM</option>
   </select>`
-
-  const select = document.getElementById('deliveryTime')
-  select.addEventListener('change', () => nextQuestion(select.value))
+  let select = document.getElementById('deliveryTime')
+  select.addEventListener('change', () => {
+   delTime= select.value
+   nextQuestion(select.value)
+  })
 }
 
-// Question number 7 *THIS IS NOT FINISHED
- // const finalMessage = (Question 7) => {}
-// This means the greeting function will be called one second after the website is loaded.
+//Question number 7 
+const finalMessage = () => {
+  if (cardYes= "Yes"){
+    botReply(`Thank you for your order. We will deliver ${flowers} with 
+    a card reading ${cardText} to address ${address} between ${delTime}. 
+    Please press "confirm" to continue with the order or "restart" to make changes`)
+  }
+  else {
+    botReply(`Thank you for your order. We will deliver ${flowers} to address ${address} between ${delTime}. 
+    Please press "confirm" to continue with the order or "restart" to make changes`)
+  }
+  inputWrapper.innerHTML=
+  `<button id="confirmBtn">Confirm</button>
+  <button id="restartBtn">Restart</button>`
+
+  document.getElementById('confirmBtn').addEventListener('click', () => {
+    botReply(`Thank you for your order. We will now redirect you to the payment page`)
+  })
+
+  document.getElementById('restartBtn').addEventListener('click', () => {
+    questionNumber=1
+    flowers = "" //resetting variable to empty for new choices
+    address = "" //resetting variable to empty for new choices
+    cardText = "" //resetting variable to empty for new choices
+    delTime = "" //resetting variable to empty for new choices 
+    cardYes = "" //resetting variable to empty for new choices
+    greeting()
+    }) 
+}
+
+ // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000)
