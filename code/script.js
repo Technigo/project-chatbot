1ff180d0aca4c35d296c1d6c2717e04f1ebaacf7
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2022-02-03 12:14:35 +0100

    added some conditionals

--- code/script.js
+++ code/script.js
@@ -2,13 +2,16 @@
 const chat = document.getElementById('chat');
 const inputWrapper = document.getElementById('input-wrapper');
 const sendBtn = document.getElementById('send-btn');
-const input = document.getElementById('name-input');
+const inputField = document.getElementById('input-field');
 
 // Global variables, if you need any, declared here
 let questionNumber = 1;
 let userName = '';
+let type = '';
 let icing = '';
-let pieces = '';
+let colour = '';
+let size = '';
+let receivedInput = true; // double click: variable for preventing double click
 
 // Functions declared here
 const botReply = (msg) => {
@@ -29,14 +32,14 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user1.png" alt="User" />  
       </section>
     `;
   } else if (sender === 'bot') {
     console.log();
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/bot1.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -50,33 +53,42 @@ const showMessage = (message, sender) => {
 const nextQuestion = (message) => {
   console.log('questionNumber', questionNumber);
 
-  if (questionNumber === 1) {
-    userName = message;
-    userReply(message);
-    input.value = '';
-    setTimeout(() => showCakes(message), 1000);
-  } else if (questionNumber === 2) {
-    userReply(message);
-    setTimeout(() => showIcing(message), 1000);
-  } else if (questionNumber === 3) {
-    icing = message;
-    userReply(message);
-    if (message === "without icing") {
-      questionNumber++;
+  if (receivedInput == true) { // double click: prevent double click
+    receivedInput = false; // double click: prevent double click
+    if (questionNumber === 1) {
+      userName = message;
+      if (userName == '') {
+        userReply(`I don't want to say my name`);
+      } else {
+        userReply(`My name is ${userName}`);
+      }
+      inputField.value = '';
+      setTimeout(() => showCakes(message), 1000);
+    } else if (questionNumber === 2) {
+      type = message;
+      userReply(message);
+      setTimeout(() => showIcing(message), 1000);
+    } else if (questionNumber === 3) {
+      icing = message;
+      userReply(message);
+      if (message === "without icing") {
+        questionNumber++;
+        setTimeout(() => showSize(message), 1000);
+      } else {
+        setTimeout(() => showColour(message), 1000);
+      }
+    } else if (questionNumber === 4) {
+      colour = message;
+      userReply(message);
       setTimeout(() => showSize(message), 1000);
+    } else if (questionNumber === 5) {
+      size = message;
+      userReply(message);
+      setTimeout(() => showOrder(message), 1000);
     } else {
-      setTimeout(() => showColour(message), 1000);
+      userReply(message);
+      setTimeout(() => thankYou(size, icing), 1000);
     }
-  } else if (questionNumber === 4) {
-    userReply(message);
-    setTimeout(() => showSize(message), 1000);
-  } else if (questionNumber === 5) {
-    pieces = message
-    userReply(message);
-    setTimeout(() => showOrder(message), 1000);
-  } else {
-    userReply(message);
-    setTimeout(() => thankYou(pieces, icing), 1000);
   }
 };
 
@@ -85,18 +97,23 @@ const nextQuestion = (message) => {
 // Starts here
 const greeting = () => {
   questionNumber = 1;
-  botReply(`Welcome, what's your name?`);
+  botReply(`Welcome!<br/>What is your name?`);
   // Just to check it out, change 'bot' to 'user' here 👆
 };
 
 const showCakes = (msg) => {
   questionNumber++;
-  botReply(`What type of cake would you like, ${msg}`);
+  if (userName == '') {
+    botReply(`What type of cake would you like, mysterious customer?`);
+  } else {
+    botReply(`What type of cake would you like, ${msg}?`);
+  }
   inputWrapper.innerHTML = `
   <button id="chocolateBtn">Chocolate</button>
   <button id="vanillaBtn">Vanilla</button>
   <button id="fruitBtn">Fruit</button>
   `;
+  receivedInput = true; // double click: re-enable clicking on button for this question
   document.getElementById('chocolateBtn').addEventListener('click', () => nextQuestion('chocolate'));
   document.getElementById('vanillaBtn').addEventListener('click', () => nextQuestion('vanilla'));
   document.getElementById('fruitBtn').addEventListener('click', () => nextQuestion('fruit'));
@@ -104,23 +121,25 @@ const showCakes = (msg) => {
 
 const showIcing = (msg) => {
   questionNumber++;
-  botReply(`A ${msg} cake, what a great choice! Do you want icing?`);
+  botReply(`A ${msg} cake, what a great choice! Do you want icing?<br/>(extra charge: 5 €)`);
   inputWrapper.innerHTML = `
-  <button id="YesBtn">Yes please!</button>
-  <button id="NoBtn">No, it would be to sweet </button>
+  <button id="yesBtn">Yes please!</button>
+  <button id="noBtn">No, it would be to sweet </button>
   `;
-  document.getElementById('YesBtn').addEventListener('click', () => nextQuestion('with icing'));
-  document.getElementById('NoBtn').addEventListener('click', () => nextQuestion('without icing'));
+  receivedInput = true; // double click: re-enable clicking on button for this question
+  document.getElementById('yesBtn').addEventListener('click', () => nextQuestion('with icing'));
+  document.getElementById('noBtn').addEventListener('click', () => nextQuestion('without icing'));
 };
 
 const showColour = (msg) => {
   questionNumber++;
-  botReply(`A cake with icing is always better! What colour do you want for the icing?`);
+  botReply(`A cake with icing is always better! What colour do you want?`);
   inputWrapper.innerHTML = `
   <button id="pinkBtn">Pink</button>
   <button id="greenBtn">Green</button>
   <button id="yellowBtn">Yellow</button>
   `;
+  receivedInput = true; // double click: re-enable clicking on button for this question
   document.getElementById('pinkBtn').addEventListener('click', () => nextQuestion('pink'));
   document.getElementById('greenBtn').addEventListener('click', () => nextQuestion('green'));
   document.getElementById('yellowBtn').addEventListener('click', () => nextQuestion('yellow'));
@@ -130,60 +149,67 @@ const showSize = (msg) => {
   console.log();
   questionNumber++;
   botReply(`It's noted ${userName}! What size do you want?`);
-  inputWrapper.innerHTML = 
-  `
-  <button id="4Btn">4 pieces</button>
-  <button id="8Btn">8 pieces</button>
-  <button id="12Btn">12 pieces</button>
-  <button id="16Btn">16 pieces</button>
-  `;
-  document.getElementById('4Btn').addEventListener('click', () => nextQuestion('4 pieces'));
-  document.getElementById('8Btn').addEventListener('click', () => nextQuestion('8 pieces'));
-  document.getElementById('12Btn').addEventListener('click', () => nextQuestion('12 pieces'));
-  document.getElementById('16Btn').addEventListener('click', () => nextQuestion('16 pieces'));
+  inputWrapper.innerHTML =
+  //   `
+  // <button id="4Btn">4 pieces</button>
+  // <button id="8Btn">8 pieces</button>
+  // <button id="12Btn">12 pieces</button>
+  // <button id="16Btn">16 pieces</button>
+  // `;
+  // receivedInput = true; // double click: re-enable clicking on button for this question
+  // document.getElementById('4Btn').addEventListener('click', () => nextQuestion('4 pieces'));
+  // document.getElementById('8Btn').addEventListener('click', () => nextQuestion('8 pieces'));
+  // document.getElementById('12Btn').addEventListener('click', () => nextQuestion('12 pieces'));
+  // document.getElementById('16Btn').addEventListener('click', () => nextQuestion('16 pieces'));
 
-  // `
-  // <select id="select">
-  //   <option value="" selected disabled>Select the number of pieces</option>
-  //   <option value="4">4 pieces</option>
-  //   <option value="8">8 pieces</option>
-  //   <option value="12">12 pieces</option>
-  //   <option value="16">16 pieces</option>
-  // </select>
-  // `
-  // const select = document.getElementById('select')
-  // select.addEventListener('change', () => nextQuestion(select.value))
+  `
+  <select id="select">
+    <option value="" selected disabled>Select the number of pieces</option>
+    <option value="4 pieces">4 pieces</option>
+    <option value="8 pieces">8 pieces</option>
+    <option value="12 pieces">12 pieces</option>
+    <option value="16 pieces">16 pieces</option>
+  </select>
+  `
+  const select = document.getElementById('select')
+    receivedInput = true // double click: re-enable clicking on button for this question
+  select.addEventListener('change', () => nextQuestion(select.value))
 
 };
 
 const showOrder = (msg) => {
   questionNumber++;
-  botReply(`Are you sure your want to order a ${msg} cake?`);
+  let icingString = ''; // variable for the recap of the order
+  if (colour !== '') {
+    icingString = ' with ' + colour + ' icing';
+  }
+  botReply(`Are you sure your want to order a ${size} ${type} cake${icingString}?`);
   inputWrapper.innerHTML = `
   <button id="yesBtn">Yes</button>
   <button id="noBtn">No</button>
   `;
+  receivedInput = true; // double click: re-enable clicking on button for this question
   document.getElementById('noBtn').addEventListener('click', () => {
     location.reload();
     return false;
   });
-
+  receivedInput = true; // double click: re-enable clicking on button for this question
   document.getElementById('yesBtn').addEventListener('click', () => nextQuestion('Yes!'));
 };
 
-const thankYou = (pieces, icing) => {
-  let price = ''
-  if (pieces === "4 pieces"){
-    price = 10 
-  } else if (pieces === "8 pieces"){
-    price = 20
-  } else if (pieces === "12 pieces"){
-    price = 30
+const thankYou = (size, icing) => {
+  let price = '';
+  if (size === "4 pieces") {
+    price = 10;
+  } else if (size === "8 pieces") {
+    price = 20;
+  } else if (size === "12 pieces") {
+    price = 30;
   } else {
-    price = 40
+    price = 40;
   }
-  if (icing === "with icing"){
-    price += 5
+  if (icing === "with icing") {
+    price += 5;
   }
   botReply(`Thanks you for your order, it will be ${price} €`);
   inputWrapper.innerHTML = ``;
@@ -192,14 +218,14 @@ const thankYou = (pieces, icing) => {
 // Set up your eventlisteners here
 
 sendBtn.addEventListener('click', () => {
-  nextQuestion(input.value);
+  nextQuestion(inputField.value);
   // Disable button & input on Enter
 });
 
 // maybe this part is not useful? seems to work without
-// input.addEventListener('keypress', (event) => {
-//   if (event.key === 'Enter' && input.value) {
-//     nextQuestion(input.value)
+// inputField.addEventListener('keypress', (event) => {
+//   if (event.key === 'Enter' && inputField.value) {
+//     nextQuestion(inputField.value)
 //     // Disable button & input on Enter
 //   }
 // });
