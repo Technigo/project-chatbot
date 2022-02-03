1b8b107e1a18468de5248e2e39b379804cd95cd8
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-02-03 18:24:55 +0100

    added css and some more javaScript eg buttons and choices

--- code/script.js
+++ code/script.js
@@ -59,9 +59,54 @@ const showDecade = (name) => {
     <button id="2000Btn">2000s</button>
   `
 
-  // document.getElementById('1980Btn').addEventListener('click', () => nextQuestion('1980'))
-  // document.getElementById('1990Btn').addEventListener('click', () => nextQuestion('1990'))
-  // document.getElementById('2000Btn').addEventListener('click', () => nextQuestion('2000'))
+  document.getElementById('1980Btn').addEventListener('click', () => chooseGenre('1980s'))
+  document.getElementById('1990Btn').addEventListener('click', () => chooseGenre('1990s'))
+  document.getElementById('2000Btn').addEventListener('click', () => chooseGenre('2000s'))
+  setTimeout(() => showGenre(type), 1000)
+}
+
+const chooseGenre = (type) => {
+  showMessage(type, 'user')
+  showMessage(`${type}? Good taste. Please pick a preferred category`, 'bot') 
+
+  inputWrapper.innerHTML = `
+  <button id="comedyBtn">Comedy</button>
+  <button id="horrorBtn">Horror</button>
+  <button id="documentaryBtn">Documentary</button>
+`
+  document.getElementById('comedyBtn').addEventListener('click', () => selectMovie('Comedy'))
+  document.getElementById('horrorBtn').addEventListener('click', () => selectMovie('Horror'))
+  document.getElementById('documentaryBtn').addEventListener('click', () => selectMovie('Documentary'))
+  setTimeout(() => selectMovie(genre), 1000)
+}
+
+
+const selectMovie = (genre) => {
+  showMessage(genre, 'user')
+  showMessage (`${genre} you say! Cool, here are my top three choices, please pick the one you want to watch!`, 'bot')
+  
+  inputWrapper.innerHTML = `
+    <button id="movieOne">Jaws</button>
+    <button id="movieTwo">Movie two</button>
+    <button id="movieThree">Movie three</button>
+    `
+
+  document.getElementById('movieOne').addEventListener('click', () => movieChoice('Jaws'))
+  document.getElementById('movieTwo').addEventListener('click', () => movieChoice('Movie two'))
+  document.getElementById('movieThree').addEventListener('click', () => movieChoice('Movie three'))
+  setTimeout(() => movieChoice(choice), 1000)
+
+}
+
+
+const movieChoice = (choice) => {
+  showMessage(choice, 'user')
+  showMessage (`${choice}!! You're too fancy! Here's the link to that, enjoy!`, 'bot')
+  
+  inputWrapper.innerHTML = `
+<button id="jaws"><a href="https://www.netflix.com/watch/60001220?trackId=253863245&tctx=0%2C3%2Ce680cd6d-886b-40f6-92fc-fdde43f8e78a-293626776%2C6b3ee427-8d85-4938-8904-492c18bee7be_35566007X19XX1643892807490%2C6b3ee427-8d85-4938-8904-492c18bee7be_ROOT%2C%2C%2C" target="_blank">JAWS</a></button>
+
+`
 }
 
 // Set up your eventlisteners here. 
