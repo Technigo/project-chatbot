7cee512b9e00e18357b3a4aa3d78b2f49d217410
Fay <faypistik@gmail.com>
2022-02-04 14:00:15 +0200

    fixed some CSS

--- code/script.js
+++ code/script.js
@@ -11,7 +11,6 @@ let area
 let taxiType
 // Functions declared here
 
-
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   if (sender === 'user') {
@@ -23,7 +22,7 @@ const showMessage = (message, sender) => {
         <img src="assets/user-new-reversed.png" alt="User" />  
       </section>
     `
-  
+
   } else if (sender === 'bot') {
     console.log(sender)
     chat.innerHTML += `
@@ -39,14 +38,11 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-
-// QUESTION 1
+// Question 1
 // Starts here
 const askQuestion1 = () => {
   showMessage(`Hello, what's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
 }
-
 setTimeout(askQuestion1, 500)
 
 const handleQuestion1 = event => { //the handleQuestion is only used in event listeners ie if we want to do something when we click or submit
@@ -56,15 +52,14 @@ const handleQuestion1 = event => { //the handleQuestion is only used in event li
   showMessage(`My name is ${username}`, 'user')
   nameInput.value = '' //clears name input to empty string
   setTimeout(() => askQuestion2(), 1000)
-  // setTimeout(() => selectDestination(name), 1000)  //pass arguments to selectDestination function with 1s delay
+  // setTimeout(() => selectDestination(name), 1000)  //pass arguments to selectDestination function with 1s delay --- update: no need to pass argument since we store it in global var
 }
 
 // Question 2
-
 const askQuestion2 = () => {
   showMessage(`Where do you want to be picked up from ${username} ?`, 'bot') //show next quesiton from bot
   inputWrapper.innerHTML = //create a new form - the previous form is deleted ALSO added autofocus to the input so the cursor goes to the input and the user doesn't have to click to type an answer
-  ` 
+    ` 
     <form id="address-form">
       <input id="address-input" type="text" autofocus /> 
       <button id= "send" class="send-btn" type="submit" >
@@ -75,7 +70,7 @@ const askQuestion2 = () => {
 
   const addressForm = document.getElementById('address-form') //find the form with id = address-form (the one created above)
   // console.log(addressForm)
-  addressForm.addEventListener('submit',  handleQuestion2)//when submitting the address form run the handleAnswer2 function -- this is to run the next function
+  addressForm.addEventListener('submit', handleQuestion2)//when submitting the address form run the handleQuestion2 function -- this is to run the next function
 }
 
 const handleQuestion2 = event => {
@@ -87,10 +82,9 @@ const handleQuestion2 = event => {
   setTimeout(() => askQuestion3(), 1000)
 }
 
-// QUESTION 3
-
+// Question 3
 const askQuestion3 = () => {
-  showMessage(`Which area do you want to go?`, 'bot') 
+  showMessage(`Which area do you want to go?`, 'bot')
   // console.log(`${username} address: ${address}`)
   inputWrapper.innerHTML = `
   <button id="northArea">North</button>
@@ -98,33 +92,30 @@ const askQuestion3 = () => {
   <button id="eastArea">East</button>
   <button id="westArea">West</button>
 `
-const northAreaButton = document.getElementById("northArea") //find HTML button and store it to northAreaButton const
-northAreaButton.addEventListener('click', () => setTimeout (() => handleQuestion3("North"),1000)) //when button is clicked, run this function with this value and add a timeout
-const southAreaButton = document.getElementById("southArea")
-southAreaButton.addEventListener('click', () => handleQuestion3("South")) //when button is clicked, run this function with this value
-const eastAreaButton = document.getElementById("eastArea")
-eastAreaButton.addEventListener('click', () => handleQuestion3("East"))
-const westAreaButton = document.getElementById("westArea")
-westAreaButton.addEventListener('click', () => handleQuestion3("West"))
-// document.getElementById('westArea').addEventListener('click', () => areaOfDestination('West'))
+  const northAreaButton = document.getElementById("northArea") //find HTML button and store it to northAreaButton const
+  northAreaButton.addEventListener('click', () => setTimeout(() => handleQuestion3("North"), 1000)) //when button is clicked, run this function with this value and add a timeout - keeping the timeout here to have it as an example syntax
+  const southAreaButton = document.getElementById("southArea")
+  southAreaButton.addEventListener('click', () => handleQuestion3("South")) //when button is clicked, run this function with this value
+  const eastAreaButton = document.getElementById("eastArea")
+  eastAreaButton.addEventListener('click', () => handleQuestion3("East"))
+  const westAreaButton = document.getElementById("westArea")
+  westAreaButton.addEventListener('click', () => handleQuestion3("West"))
+  // document.getElementById('westArea').addEventListener('click', () => areaOfDestination('West'))
 }
 
 const handleQuestion3 = (inputArea) => {
-
   showMessage(inputArea, 'user')
   area = inputArea
   inputWrapper.innerHTML = ""
-//CONDITIONAL
+  //CONDITIONAL
   // if (inputArea == "West") {
   //   setTimeout(declineMessage, 1000)
-    
+
   // }else {
   //   setTimeout(confirmMessage, 1000)
   // }
   setTimeout(() => askQuestion4(), 1000)
-  
 }
-
 // const declineMessage = () => {
 //   showMessage(`Sorry ${username}
 //   we cannot transport you to ${area} area
@@ -137,8 +128,9 @@ const handleQuestion3 = (inputArea) => {
 //   `, 'bot')
 // }
 
+//Question 4
 const askQuestion4 = () => {
-  showMessage(`What type of car would you like?`, 'bot') 
+  showMessage(`What type of car would you like?`, 'bot')
   inputWrapper.innerHTML = `
  
   <select class="box" name="cars" id="cars">
@@ -149,7 +141,7 @@ const askQuestion4 = () => {
   </select>
   `
   const carSelection = document.getElementById("cars")
-  carSelection.addEventListener('change', () => handleQuestion4(carSelection.value))
+  carSelection.addEventListener('change', () => handleQuestion4(carSelection.value)) //Used the change eventlistener since it is a dropdown selection.Instead of writing code for every answer, get the value of the carSelection const
   // const luxury = document.getElementById("luxury")
   // cars.addEventListener('click', () => handleQuestion4("luxury"))
   // const limousine = document.getElementById("limousine")
@@ -158,52 +150,30 @@ const askQuestion4 = () => {
 
 const handleQuestion4 = (inputCar) => {
   showMessage(inputCar, 'user')
-  taxiType = inputCar 
-
+  taxiType = inputCar
+  //Conditional
   if (inputCar == "Limousine") {
     setTimeout(offerChampagne, 1000)
-
   } else {
     setTimeout(goodbyeMessage, 1000)
   }
+}
 
-  }
+const offerChampagne = () => {
+  showMessage(`Beautiful choice ${username}! Here is a bottle of champagne for your ride, enjoy!`, 'bot')
+  setTimeout(goodbyeMessage, 1000) //call the goodbyeMessage after someone has selected Limousine and is presented with the above message.
+  inputWrapper.innerHTML = ""
+}
 
-  const offerChampagne = () => {
-    showMessage(`Beautiful choice ${username}! Here is a bottle of champagne for your ride, enjoy!`, 'bot')
-    setTimeout(goodbyeMessage, 1000)
-    inputWrapper.innerHTML = ""
-  }
-
-  const goodbyeMessage = () => {
-    showMessage(`Thank you for booking your ride with Taxi Tomorrow, ${username}! We will send a ${taxiType} taxi to ${address} to transport you to ${area}.`, 'bot')
-    inputWrapper.innerHTML = ""
-  }
-
-
-
-
+const goodbyeMessage = () => {
+  showMessage(`Thank you for booking your ride with Taxi Tomorrow, ${username}! We will send a ${taxiType} taxi to ${address} to transport you ${area}.`, 'bot')
+  inputWrapper.innerHTML = ""
+  
+}
 // //REPLY 2
 // const areaOfDestination = (area) => {
 //   console.log(area)
 //   showMessage(area, 'user')
 // }
-
-// QUESTION 3
-
-
 // Set up your eventlisteners here
-
-form.addEventListener('submit', handleQuestion1)
-
-
-
-
-
-
-
-//   // After 1 second, show the next question by invoking the next function.
-// 	// passing the name into it to have access to the user's name if we want
-// 	// to use it in the next question from the bot.
-//   setTimeout(() => showFoodOptions(name), 1000)
-// }
+form.addEventListener('submit', handleQuestion1) //This is the first event listener. Code will not run until it gets to that line. After that, it goes to the top and runs the handleQuestion1 function
