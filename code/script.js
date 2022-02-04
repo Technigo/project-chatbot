782ace3cbb1c25103eb6838a2771c1fa956ac1ae
Nhung Phan <nhungphan@Joel-Lov.local>
2022-02-04 10:22:55 +0200

    organizing Javascript

--- code/script.js
+++ code/script.js
@@ -1,7 +1,7 @@
 const chat = document.getElementById('chat')
 const sendBtn = document.querySelector(".send-btn");
 const inputWrapper = document.getElementById("input-wrapper");
-
+const music = new Audio ("music.mp3");
   
 // Functions declared here
 
@@ -23,7 +23,7 @@ const showMessage = (message, sender) => {
       <section class="bot-msg">
         <img src="https://images.unsplash.com/photo-1634909924531-4daae117dbc1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2062&q=80" alt="Bot" />
         <div class="bubble bot-bubble">
-          <p>${message}</p>
+          <p class="bot-text">${message}</p>
         </div>
       </section>
     `
@@ -45,41 +45,33 @@ const userReply = (msg) => {
 }
 
 // Starts here
- // Question 4 - thank you note
 
- 
+
 
 const nextQuestion = (message) => {
     if (questionNumber === 1) {
         userReply(message);
-        setTimeout(() => (ambulanceCase(message)), 300); 
+        setTimeout(() => (ambulanceCase(message)), 500); 
     } else if (questionNumber === 2) {
         userReply(message);
-        setTimeout(() => (questionThree(message)), 300); 
-    } else if (questionNumber === 3) {
-        userReply(message);
-    } else {
-    }
+        setTimeout(() => (confirmMessage(message)), 800); 
+    } else if (questionNumber=== 3) {
+       setTimeout(() => botReply(message),500);
+        setTimeout(() => (restart()), 500); 
+    } 
 }
 
-
-function call(message) {
-    questionNumber++;
-    inputWrapper.innerHTML = `
-    <button type="button" id="a"> 🚑 a </button>
-    <button type="button" id="b"> 🤕 b </button>
-    `
-}
 
 // greeting
  const greeting = (message) => {
   questionNumber = 1;
-  showMessage(`Hi, I am Dr.Strange, what can I help with today?`, 'bot')
+  botReply(`Hi, I am Dr.Strange, what can I help with today?`);
   showInjuryCase();
 }
   
  //Question 1 -- show Injury Case
  function showInjuryCase(message) {
+
   //Show two button
   inputWrapper.innerHTML = `
   <button type="button" id="ambulance"> 🚑 In need of an ambulance</button>
@@ -95,113 +87,112 @@ function call(message) {
    //Add event lisner for injury case
    const injuryBtn = document.getElementById("minor-injury");
    injuryBtn.addEventListener("click", ()=> {
-    nextQuestion("Minor Injury")
+   nextQuestion("Minor Injury");
+
 });
 }
 
  // Question 2 - Ambulance - Input address
  function ambulanceCase (message) {
     questionNumber++;
+   
     // Type 1
     if (message === "In need of an ambulance") {
-    setTimeout(() => showMessage(`oh, you are ${message}`, 'bot'), 300);
-    setTimeout(() => showMessage("Where can we pick you up?", "bot"), 500);
+       setTimeout(() =>  botReply(`oh, you are ${message} 🥺`),300);
+       setTimeout(() => botReply(`Where can we pick you up? 🤔🏡`), 800);
 
-    // Change the button to address input 
+        inputWrapper.innerHTML = `
+        <form id="address-form">
+        <input id="address-input" type="text" />
+        <button class="send-btn" type="submit"> Send </button>
+        </form>`;
+
+        //Add eventlistner to the address form
+        const addressInput = document.querySelector("#address-input");
+        const addressForm = document.getElementById("address-form");
+
+        //Set condition for address input
+        addressForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (addressInput.value == ""){
+          showMessage(`RIP 🥲`, 'bot')}     
+        else {
+          nextQuestion(`${addressInput.value}`)
+          botReply(`We will pick you up at ${addressInput.value}, when we feel we like it 🤷`); 
+          addressInput.value ="";
+        }
+       })
+  } 
+   else if (message === "Minor Injury") {
+    setTimeout(() =>  botReply(`oh, you have ${message}`), 300);
+    setTimeout(() =>  botReply(`Do you want to see a doctor today?`),800);
+
     inputWrapper.innerHTML = `
-    <form id="address-form">
-    <input id="address-input" type="text" />
-    <button class="send-btn" type="submit"> Send </button>
-    </form>`;
-
-    //Add eventlistner to the address form
-    const addressInput = document.querySelector("#address-input");
-    const addressForm = document.getElementById("address-form");
-
-    //Set condition for address input
-    addressForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (addressInput.value == ""){
-        showMessage(`RIP 🥲`, 'bot')}     
-   //If user input address, show confirm message
-   else {
-       nextQuestion(`${addressInput.value}`);
-       showMessage(`We will pick you up at ${addressInput.value}, when we feel we like it`, "bot");
-       addressInput.value ="";
-      }
-    })
-    } 
-    
-    // If type = "minor injury"
-    else {
-     setTimeout(() => showMessage(`oh, you have a ${message}`, 'bot'), 300)
-     setTimeout(() => showMessage(`Do you want to see a doctor today?`, "bot"), 700);
-     inputWrapper.innerHTML = `
-     <button type="button" id="yes"> Yes </button>
-     <button type="button" id="no"> No </button>
+    <button type="button" id="yes"> Yes </button>
+    <button type="button" id="no"> No </button>
     `
-    document.getElementById("yes").addEventListener("click",() => {
-    nextQuestion("yes");
-    //setTimeout(doctorOption, 500);
-    });
-    document.getElementById("no").addEventListener("click",()=> {
-    location.reload()
-    });
-    }
-}
-
-
-
-// Question  4 
-function questionThree(message) {
-    questionNumber++;
-    if (message === "yes") {
-        doctorOption();
-    } else {
-        botReply(`Thank you`);
-        restart();
-    }
-}
-
-//Question 5
-function restart() {
-    inputWrapper.innerHTML = `
-    <button type="button" id="restart-btn"> Restart </button>
-    `
-    document.getElementById("restart-btn").addEventListener("click", () => {
-        location.reload()
-    })
-}
-  
-
-
- //  Choose a doctor 
- function doctorOption() {
      
-    showMessage(`Which doctor do you prefer?`, "bot");
+    document.getElementById("no").addEventListener("click", () => {
+      nextQuestion("no");
+    })
+     document.getElementById("yes").addEventListener("click",() => {
+     nextQuestion("yes");
+  })
+
+ }
+
+ }
+
+ function confirmMessage (message) { 
+  questionNumber++;     
+  if (message === "yes") {
+    botReply(`Which doctor do you prefer?`);
     inputWrapper.innerHTML = `
     <button type="button" id="doctor-1">
-      <img class="doctor-img" src="https://www.stylectory.net/wp-content/uploads/2021/01/1172adcf30a2ce4f024dd41805189b51.jpeg" alt="Christ"> 
+      <img class="doctor-img" src="https://www.esquireme.com/cloud/2021/09/09/chris-hemsworth-poses-during-a-photo-call-for-thor-ragnarok-on-october-15-2017-in-sydney-australia-photo-by-mark-metcalfe_getty-images-for-disney-square.jpg" alt="Christ"> 
     </button>
     <button type="button" id="doctor-2">
     <img class="doctor-img" src="https://theubj.com/wp-content/uploads/2021/12/Henry-Cavill.jpg" alt="Henry">
     </button>
-    `
-    document.getElementById("doctor-1").addEventListener("click",() => {
-    showMessage(`Dr.Chris will see later today 😘`,"bot");
+   `
+   document.getElementById("doctor-1").addEventListener("click",() => {
+    botReply(`Dr.Chris will see later today 😘`);
+    nextQuestion("Thank you ❤️");
+
    })
    document.getElementById("doctor-2").addEventListener("click", () => {
-   showMessage(`Dr.Henry will see later today 😘`, "bot");
+   botReply(`Dr.Henry will see later today 😘`);
+   nextQuestion("Thank you ❤️");
+
    })
-
-
+   } 
+   else {
+   nextQuestion("Thank you ❤️");
+   }
   }
 
-
-
- 
-
+function restart() {
+  inputWrapper.innerHTML = `
+  <button type="button" id="restart-btn"> Restart 👀 </button>
+  `
+  document.getElementById("restart-btn").addEventListener("click", () => {
+      location.reload()
+  })
+}
 
 setTimeout(greeting, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
