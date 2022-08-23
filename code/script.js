0b283b2ff4eeeb645ef2ae31159a81b1ac0b9c64
Elin Segelöv <elin.segelov@gmail.com>
2022-08-23 16:08:47 +0200

    possible to use item buttons

--- code/script.js
+++ code/script.js
@@ -1,8 +1,14 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
 
-// If you need any global variables that you can use across different functions, declare them here:
 
+// If you need any global variables that you can use across different functions, declare them here:
+let heroName;
+let heroHp = 2
+const heroInventory = [];//🅰️ 2 is the nuber HP you start with
+
+
+//Tänker att vi kan deklarera inventoryn här, typ let invemtory = [item1, item2]
 
 // Declare your functions after this comment
 
@@ -31,6 +37,7 @@ const showMessage = (message, sender) => {
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
+  
 }
 
 // Starts here
@@ -40,6 +47,7 @@ const greeting = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
 // Set up your eventlisteners here
 
 // When website loaded, chatbot asks first question.
@@ -48,23 +56,61 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000);
+
+
+setTimeout(greeting, 1);
 
 //START OF OUR CODE
 const submitButton = document.querySelector(".send-btn");
-const inputField = document.querySelector("#name-input")
-submitButton.addEventListener("click", function(e) {
-    e.preventDefault(); 
-    let inputContens = inputField.value;
-    showMessage(`My name is ${inputContens}`, "user");
-    showMessage(`${inputContens}, you are about to embark on an adventure! Choose one item below to aid you on you quest. I advise you to choose wisely!`, "bot");  
+const inputField = document.querySelector("#name-input");
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    heroName = inputField.value; //defines the heros name 
+    showMessage(`My name is ${heroName}`, "user"); 
 
-    // Make buttons visible
-    const buttonWrapper = document.querySelector("#button-wrapper");
-    buttonWrapper.classList.add("visible-wrapper");
-    
-    // Attempt to hide the input line and send button - fixed. used wrong const name when trying to add classlist.
+    // Hide the input line and send button 
     const inputWrapper = document.querySelector(".input-wrapper");
     inputWrapper.classList.add("hidden-wrapper");
 
+   //After showMessages is called, setTimout waits 1000 ms to call nameReply 
+  setTimeout(nameReply, 0);
 })
+
+/* This is the first reply from the bot */
+const nameReply = () => {
+  showMessage(`${heroName}, you are about to embark on an adventure! Choose one item below to aid you on you quest. I advise you to choose wisely!`, "bot");
+  setTimeout(showButtons, 0);
+}
+
+// Make buttons visible for next step
+const showButtons = () => {
+      const buttonWrapper = document.querySelector("#button-wrapper");
+      buttonWrapper.classList.add("visible-wrapper");   
+}
+
+// Add items to Hero Inventory choosing between button options
+  let itemButton = document.querySelectorAll(".answer-button");
+  
+//When the items are clicked and selected, they will be added to the inventory
+itemButton.forEach(button => button.addEventListener("click", function (event) {
+  event.preventDefault();
+  heroInventory.push(this.innerHTML); 
+  console.log(`Added ${this.innerHTML} to backpack`);
+  console.log("inventory", heroInventory);
+}))
+  
+  
+  
+  /* 
+    e.preventDefault();
+  }
+    /*console.log(hero);
+    if (hero.includes("Rope🪢")) {
+      console.log("yay")    
+    }
+    else if (!hero.includes("Rope🪢")) {
+      console.log("you died")
+    }
+  )}
+
+ */
