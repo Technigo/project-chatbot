356b0e366e3d2d5f5faf8c408a69d9d130078916
Iracuzzi <arnau.monforte@gmail.com>
2022-08-23 19:15:56 +0200

    add first iteration for chat-bot project, console.log row 23

--- code/script.js
+++ code/script.js
@@ -1,10 +1,14 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
-
+const nameInput = document.getElementById('name-input')
+const submitBtn = document.getElementById('send-btn')     // we created the variables to work with it.
+const nameForm = document.getElementById('name-form')
+const inputWrapper = document.getElementById('input-wrapper')
 // If you need any global variables that you can use across different functions, declare them here:
 
 
 // Declare your functions after this comment
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -20,6 +24,7 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log("Hello world")
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -39,8 +44,18 @@ const greeting = () => {
   showMessage("Hello there, What's your name?", 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
 }
+const handleNameInput = (event) => {      // Here we creat a function to avoid the submit when we click or press the button sumbit.
+  event.preventDefault()                  // .preventDefault() avoits the refresh the page.
+  const name = nameInput.value            // we create a new variable with the typed message in the input bar to use for the chatbot.
+  showMessage(name, 'user')               // here calls the function showMessage() to see what was typed before.
+  nameInput.value = ''                    // converts the info of nameInput.value to a stirng.
+  setTimeout(() => showMessage(`Welcome ${name}. Want to talk about you price, or have some problem?`, 'bot'), 750)
+}
+
 
 // Set up your eventlisteners here
+
+nameForm.addEventListener('submit', handleNameInput)   // callback the new  created function.
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
