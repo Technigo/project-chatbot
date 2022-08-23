22542aff3beae58c677b9f67b4434ffd040fd4ec
ViktorSvenssonN <viktor.tommy.svensson@gmail.com>
2022-08-23 13:44:21 +0200

    Co-authored-by: SofiaErikaIngeborg <SofiaErikaIngeborg@users.noreply.github.com>

--- code/script.js
+++ code/script.js
@@ -1,8 +1,20 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
+const inputWrapper = document.getElementById('input-wrapper')
+const input = document.getElementById('input')
+const sendBtn = document.getElementById('send')
 
 // If you need any global variables that you can use across different functions, declare them here:
 
+let questionNumber = 1
+
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
+}
+
+const userReply = (msg) => {
+  showMessage(msg, 'user')
+}
 
 // Declare your functions after this comment
 
@@ -33,14 +45,53 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight;
 }
 
-// Starts here
-const greeting = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
+// Test Sofia //
+
+ const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber) 
+
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => showFoodTypes(message), 1000)
+  } else if (questionNumber === 2) {
+    userReply(message)
+    setTimeout(() => showMenu(message), 1000)
+  } else if (questionNumber === 3) {
+    userReply(message)
+    setTimeout(() => showDishSize(message), 1000)
+  } else if (questionNumber === 4) {
+    userReply(message)
+    setTimeout(() => showPrice(message), 1000)
+  } else {
+    userReply(message)
+    setTimeout(thankYou, 1000)
+  }
 }
 
+
+// Starts here
+const greeting = () => {
+  questionNumber = 1
+  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
+  botReply("Hello there, What's your name?")
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
+const showFoodTypes = (msg) => {
+  questionNumber++
+  botReply(
+    `Nice to meet you ${msg}. What type of food would you like to order?`
+  ) 
+}
+ /* const userReplay = (msg) => {
+  showMessage(`Nice to meet you ${msg}. What would you like to have help with?', 'user`);
+*/
+
 // Set up your eventlisteners here
+sendBtn.addEventListener('click', () => nextQuestion(input.value))
+input.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter' && input.value) nextQuestion(input.value)
+})
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
