22abbcf797cab340d3f7525370cd413d356bc872
SofieRydmark <you@example.com>
2022-08-24 16:32:29 +0200

    Updated Javascript to fix flow to confirmations-message/question

--- code/script.js
+++ code/script.js
@@ -5,10 +5,8 @@ const form = document.getElementById('room-form');
 const roomInput = document.getElementById('room-input'); //user writes their room
 const sendBtn = document.getElementById('send-btn');
 const collapse = document.getElementsByClassName("main-collapse");
-const wakeUpInput = document.getElementById('wakeUp-input');
+const wakeUpInput = document.getElementById('wakeUpinput');
 const wakeUpForm = document.getElementById('wakeUp-form');
-
-  // added variables pointing to html Ids, changed it to room numbers instad of name //
 
   
 // If you need any global variables that you can use across different functions, declare them here:
@@ -16,8 +14,8 @@ const wakeUpForm = document.getElementById('wakeUp-form');
 
 // Declare your functions after this comment
 
-
-  // This function will add a chat bubble in the correct place based on who the sender is
+// This function will open and close chatbar //not workning right now
+  
   for (let i = 0; i < collapse.length; i++) {
     collapse[i].addEventListener("click", function(){
         this.classList.toggle("active");
@@ -31,6 +29,7 @@ const wakeUpForm = document.getElementById('wakeUp-form');
 
     });
 }
+// This function will add a chat bubble in the correct place based on who the sender is
 
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html section inside the chat with the posted message
@@ -64,32 +63,50 @@ const showMessage = (message, sender) => {
 // Question 1 - What is your room number?
 
 const greeting = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello, what is your room number?", 'bot'); //changed message/S//
+  showMessage("Welcome to Hotel Vosje. What is your room number?", 'bot'); 
 };
 
 // Answer with room number from user 
 
-const handleFormInput = (event) => { // at submit this function will be invoked
-  event.preventDefault() // prevents website refresh at submit
-  const room = roomInput.value // input value will be stored in the const name
-  showMessage(`My room number is ${room}`, 'user') // users answer 
-  roomInput.value = '' // clearing room input setting it to an empty string
-  setTimeout(() => helpSelection(room), 1000) // passing the arguments to next function with 1sec delay
+const handleFormInput = (event) => {        // at submit this function will be invoked
+  event.preventDefault()        // prevents website refresh at submit
+  const room = roomInput.value        // input value will be stored in the const name
+  showMessage(`My room number is ${room}`, 'user')      // users answer 
+  roomInput.value = ''        // clearing room input setting it to an empty string
+
+  if (room === '') {       // this will prompt the user to answer correctly
+    setTimeout(() => showMessage(`Please, give me your room number.`, 'bot'), 1000) 
+  }
+  else{
+    setTimeout(() => helpSelection(room), 1000)
+  }
 }
+
 
 // Question 2 - What do you need help with today?
 
-const helpSelection = () => {
-  showMessage(`Hello "room", what can I help you with today?`, 'bot');
+const helpSelection = (room) => {
+  showMessage(`Hello room ${room}, what can I help you with today?`, 'bot');
   inputWrapper.innerHTML = `
   <button id="amenities" type="sbumit" class="chat-btn">Amenities </button>
-  <button id="room-service" type="submit" class="chat-btn">Rooom service </button>
+  <button id="room-service" type="submit" class="chat-btn">Room service </button>
   <button id="wake-up-call" type="submit" class="chat-btn">Wake-up call </button>
   `
-  document.getElementById('amenities').addEventListener('click',() => selectionAnswer ('amenities'));
-  document.getElementById('room-service').addEventListener('click',() => selectionAnswer ('room-service'));
-  document.getElementById('wake-up-call').addEventListener('click',() => selectionAnswer ('wake-up-call'));
+  document.getElementById('amenities').addEventListener('click',() => {
+    showMessage(`I need amenities, please`, 'user')
+    setTimeout(() => selectionAnswer ('amenities'),1000);
+})
+
+  document.getElementById('room-service').addEventListener('click',() => {
+    showMessage(`I need food, please`, 'user')
+    setTimeout(() => selectionAnswer ('room-service'),1000)
+})
+
+  document.getElementById('wake-up-call').addEventListener('click',() => {
+    showMessage(`I need a wake-up-call.`, 'user')
+    setTimemout(() => selectionAnswer ('wake-up-call'),1000);
+  })
+
 }
 
 // functions for what the guest chooses
@@ -97,45 +114,61 @@ const helpSelection = () => {
 const selectionAnswer = (selection) => {
 
   if (selection === 'amenities') {
-    showMessage(`I need amenities, please`, 'user')
-    setTimeout(() => showMessage(`Sure, what do you need?`, 'bot'),1000);
+    showMessage(`Sure, what do you need?`, 'bot');
     inputWrapper.innerHTML = `
     <button id="toothbrush" type="submit" class="chat-btn">Toothbrush & paste </button>
     <button id="shampoo" type="submit" class="chat-btn">Schampoo & Conditioner </button>
     <button id="toiletrykit" type="submit" class="chat-btn">Toiletry Kit </button>
     <button id="shavingkit" type="submit" class="chat-btn">Shaving Kit </button>
     `
-    document.getElementById('toothbrush').addEventListener('click',() => 
-      setTimeout(() => showMessage(`I need some toothbrush and toothpaste`, 'user')), 1000);
-    document.getElementById('shampoo').addEventListener('click',() =>
-      setTimeout(() => showMessage(`I need some shampoo and conditioner`, 'user')),1000);
-    document.getElementById('toiletrykit').addEventListener('click',() => 
-      setTimeout(() => showMessage(`I need a toiletry kit`, 'user')), 1000);
-    document.getElementById('shavingkit').addEventListener('click',() => 
-      setTimeout(() => showMessage(`I need a shaving kit`, 'user')), 1000);
+    document.getElementById('toothbrush').addEventListener('click',() => {
+      showMessage(`I need toothbrush and toothpaste`, 'user')
+      setTimeout(() => goodbye (selection), 2000)
+    })
+
+    document.getElementById('shampoo').addEventListener('click',() => {
+      showMessage(`I need shampoo and conditioner`, 'user')
+      setTimemout(() => goodbye (selection), 2000)
+    })
+
+    document.getElementById('toiletrykit').addEventListener('click',() => {
+      showMessage(`I need a toiletry kit`, 'user')
+      setTimeout(() => goodbye (selection), 2000)
+    })
+
+    document.getElementById('shavingkit').addEventListener('click',() => {
+      showMessage=(`I need a shaving kit`, 'user')
+      setTimeout(() => goodbye (selection), 2000)
+    })
+    
 
   }
 
   else if (selection === 'room-service') {
-    showMessage(`I need food, please`, 'user')
-    setTimeout(() => showMessage(`Of course, what are you in the mood for?`, 'bot'),1000);
+    showMessage(`Of course, what are you in the mood for?`, 'bot');
     inputWrapper.innerHTML = `
     <button id="pasta" type="submit" class="chat-btn">Pasta Carbonara </button>
-    <button id="sallad" type="submit" class="chat-btn">Ceaser Salad</button>
+    <button id="sallad" type="submit" class="chat-btn">Ceasar Salad</button>
     <button id="tomatoesoup" type="submit" class="chat-btn">Tomatoe Soup </button>
    `
-    document.getElementById('pasta').addEventListener('click', () =>
-    setTimeout(() => showMessage(`I would love some Pasta Carbonara`, 'user')), 1000);
-    document.getElementById('sallad').addEventListener('click', () =>
-    setTimeout(() => showMessage(`I could eat a Ceaser Salad`, 'user')), 1000);
-    document.getElementById('tomatoesoup').addEventListener('click', () =>
-    setTimeout(() => showMessage(`I crave some Tomatoe Soup`, 'user')), 1000);
+    document.getElementById('pasta').addEventListener('click', () => {
+      showMessage(`I would love some Pasta Carbonara`, 'user')
+      setTimeout(() => goodbnye(selection), 2000)
+    }) 
+
+    document.getElementById('sallad').addEventListener('click', () => {
+      showMessage(`I would love a Ceasar Salad`, 'user')
+      setTimeout(() => goodbye (selection), 2000)
+    })
+
+    document.getElementById('tomatoesoup').addEventListener('click', () => {
+      showMessage(`I crave some Tomatoe Soup`, 'user')
+      setTimeout(() => goodbye(selection), 2000)
+    })
   }
 
-  else  {
-    showMessage(`I need a wake-up-call.`, 'user');
-     
-    setTimeout(() => showMessage(`Sure, at what time?`, 'bot'),1000);
+  else if (selection === 'wake-up-call') {   
+    showMessage(`Sure, at what time?`, 'bot');
     inputWrapper.innerHTML = `
       <form id="wakeUp-form"> 
         <input id="wakeUpInput" type="text" placeholder="wake-up time.."/>  
@@ -144,23 +177,30 @@ const selectionAnswer = (selection) => {
         </button>
       </form>
     `
-    const handleWakeUpInput = (event) => { // at submit this function will be invoked
-      event.preventDefault() // prevents website refresh at submit
-      const wakeUp = wakeUp.value // input value will be stored in the const name
-      showMessage(`Wake me up at ${wakeUp}`, 'user') // users answer 
-      roomInput.value = '' // clearing room input setting it to an empty string
-      setTimeout(() => goodbye, 1000) // passing the arguments to next function with 1sec delay
-    }
+    document.getElementById(wakeUpInput).addEventListener('submit', () => {
+      showMessage(`Wake me up at ${wakeUpInput.value}`, 'user')
+      setTimeout(() => goodbye(selection), 1000)
+    })
   }
+  
+  //const wakeUpInput = (event) => { // at submit this function will be invoked
+      //event.preventDefault() // prevents website refresh at submit
+      //const wakeUp = wakeUpInput.value // input value will be stored in the const name
+     // showMessage(`Wake me up at ${wakeUp}`, 'user') // users answer 
+      //wakeUpInput.value = '' // clearing room input setting it to an empty string
+     // passing the arguments to next function with 1sec delay
+    //}
+
+
 }
 
 // Thank you message and asking if anything else is needed?
 
-const goodbye = () => {
-showMessage('We will take care of that for you right away. Do you need anything else?', 'bot')
+const goodbye = (selection) => {
+showMessage(`We will take care of ${selection} for you right away. Do you need anything else?`, 'bot')
 inputWrapper.innerHTML = `
-  <button id="yes" type="sbumit" class="chat-btn">Yes, please </button>;
-  <button id="no" type="submit" class="chat-btn">No, thank you</button>;
+  <button id="yes" type="sbumit" class="chat-btn">Yes, please </button>
+  <button id="no" type="submit" class="chat-btn">No, thank you</button>
   `
 document.getElementById('yes').addEventListener('click',() => goodbyeAnswer ('yes'));
 document.getElementById('no').addEventListener('click',() => goodbyeAnswer ('no'));
