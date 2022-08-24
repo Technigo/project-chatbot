821bca8691f0f2cd7af766a542ae94fe2fe483f1
Julia Ostedt <julia.ostedt@gmail.com>
2022-08-24 10:08:49 +0200

    We have updated the bot with the first answer, changed the questions and the name

--- code/script.js
+++ code/script.js
@@ -1,5 +1,7 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById("chat");
+const input = document.getElementById("user-input");
+const form = document.getElementById("workout-form");
 
 // If you need any global variables that you can use across different functions, declare them here:
 
@@ -19,7 +21,6 @@ const showMessage = (message, sender) => {
     `;
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === "bot") {
-    console.log("hello there");
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -36,16 +37,40 @@ const showMessage = (message, sender) => {
 // Starts here
 const greeting = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", "bot");
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage("Hello there, I'm your personal trainer bot.", "bot");
 };
+
+const ageQuestion = () => {
+  showMessage("Firstly, how old are you?", "bot");
+};
+
+const handleAgeInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  const age = input.value;
+  showMessage(age, "user");
+  input.value = "";
+};
+
+/* const ageReaction = (age) => {
+  if (age >= 16) {
+    console.log("age");
+  } else {
+    //do that
+  }
+}; */
+
+// After 1 second, show the next question by invoking the next function.
+// passing the name into it to have access to the user's name if we want
+// to use it in the next question from the bot.
+
+//setTimeout(() => showFoodOptions(name), 1000);
 
 // Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
+form.addEventListener("submit", handleAgeInput);
+
 // setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000);
+setTimeout(ageQuestion, 2000);
