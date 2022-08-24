0fba9f53d772a532578ec76f8b8127c2d019d306
Elin Segelöv <elin.segelov@gmail.com>
2022-08-24 18:05:47 +0200

    progress forward,working all over

--- code/script.js
+++ code/script.js
@@ -5,10 +5,9 @@ const chat = document.getElementById('chat');
 // If you need any global variables that you can use across different functions, declare them here:
 let heroName;
 let heroHp = 2
-const heroInventory = [];//🅰️ 2 is the nuber HP you start with
+const heroInventory = [];// 2 is the nuber HP you start with
 
 
-//Tänker att vi kan deklarera inventoryn här, typ let invemtory = [item1, item2]
 
 // Declare your functions after this comment
 
@@ -37,7 +36,6 @@ const showMessage = (message, sender) => {
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
-  
 }
 
 // Starts here
@@ -47,18 +45,13 @@ const greeting = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+setTimeout(greeting, 1);
 
 // Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 
 
-setTimeout(greeting, 1);
+
 
 //START OF OUR CODE
 const submitButton = document.querySelector(".send-btn");
@@ -67,53 +60,119 @@ submitButton.addEventListener("click", function(event) {
     event.preventDefault(); 
     heroName = inputField.value; //defines the heros name 
     showMessage(`My name is ${heroName}`, "user"); 
-
-    // Hide the input line and send button 
-    const inputWrapper = document.querySelector(".input-wrapper");
-    inputWrapper.classList.add("hidden-wrapper");
-
    //After showMessages is called, setTimout waits 1000 ms to call nameReply 
-  setTimeout(nameReply, 0);
-})
+   setTimeout(nameReply, 0);
+  })
 
-/* This is the first reply from the bot */
+  /* This is the first reply from the bot */
 const nameReply = () => {
   showMessage(`${heroName}, you are about to embark on an adventure! Choose one item below to aid you on you quest. I advise you to choose wisely!`, "bot");
-  setTimeout(showButtons, 0);
+  setTimeout(itemSelection, 0);
 }
 
-// Make buttons visible for next step
+  // List to select items 
+  const itemSelection = () => {
+  const inputWrapper = document.querySelector(".input-wrapper");
+  inputWrapper.innerHTML = `
+    <select id="item1">
+          <option value="" selected disabled>👇 Select an item...</option>;
+          <option value="Water-bottle">Water bottle🥤</option>
+          <option value="Umbrella">Umbrella☂</option>
+          <option value="Rope">Rope🪢</option>
+          <option value="Helmet">Helmet ⛑</option>
+          <option value="Boat">Boat ⛵️</option>
+          <option value="Life-Jacket">Life jacket 🦺</option>
+          <option value="Gutar">Guitar 🎸</option>
+          <option value="Meat">Piece of meat 🍗</option>
+          <option value="Sword">Sword 🗡</option>
+          <option value="Shield">Shield 🛡</option>
+          <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
+          <option value="Moccain">Moccasin 🧦</option>
+    </select>
+    
+    <select id="item2">
+          <option value="" selected disabled>👇 Select another item...</option>;
+          <option value="Water-bottle">Water bottle🥤</option>
+          <option value="Umbrella">Umbrella☂</option>
+          <option value="Rope">Rope🪢</option>
+          <option value="Helmet">Helmet ⛑</option>
+          <option value="Boat">Boat ⛵️</option>
+          <option value="Life-Jacket">Life jacket 🦺</option>
+          <option value="Gutar">Guitar 🎸</option>
+          <option value="Meat">Piece of meat 🍗</option>
+          <option value="Sword">Sword 🗡</option>
+          <option value="Shield">Shield 🛡</option>
+          <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
+          <option value="Moccain">Moccasin 🧦</option>
+    </select>
+    
+   <button id="select-item-btn">Select</button>`;
+   
+   let itemList1 = document.querySelector("#item1");
+   let itemList2 = document.querySelector("#item2");
+   let selectItemBtn = document.querySelector("#select-item-btn");
+   selectItemBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+  
+   console.log(itemList1.selectedOption);
+  })}
+
+
+/*     // Make buttons visible for next step
 const showButtons = () => {
       const buttonWrapper = document.querySelector("#button-wrapper");
       buttonWrapper.classList.add("visible-wrapper");   
 }
+ */
 
-// Add items to Hero Inventory choosing between button options
-  let itemButton = document.querySelectorAll(".answer-button");
-  
+  // Add items to Hero Inventory choosing between button options
 //When the items are clicked and selected, they will be added to the inventory
-itemButton.forEach(button => button.addEventListener("click", function (event) {
+/* selectItemBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  heroInventory.push(this.innerHTML); 
-  console.log(`Added ${this.innerHTML} to backpack`);
+  heroInventory.push(item1.value, item2.value); 
+  console.log(`Added ${item1.value, item2.value} to backpack`);
   console.log("inventory", heroInventory);
-  showMessage (`I choose ${this.innerHTML}`,"user");
-  setTimeout(hideButtons, 1000);
-}))
+  showMessage (`${item1.value, item2.value}`,"user");
+    setTimeout(hideButtons, 10);
+  }) */
 
 //When selected item, the buttons will disapprear and show next message. 
 const hideButtons = () => {
   const buttonWrapper = document.querySelector("#button-wrapper");
-  buttonWrapper.classList.remove("visible-wrapper");   
-  showMessage (`You added ${heroInventory} to your packpack. Now you need to decide which way to go!`,"bot");
+  buttonWrapper.classList.remove("visible-wrapper");  
+  setTimeout(botMonologue, 1000);
+}
+
+const botMonologue = () => {
+showMessage (`Excellent choice! Your backpack is stuffed and you are ready to to embark on your journey!`,"bot");
+setTimeout(showReusableButtons, 1000);
+// Make buttons visible for next step
+}
+
+const showReusableButtons = () => {
+  const reusableButtons = document.querySelector("#reusable-buttons");
+  reusableButtons.classList.add("visible-wrapper"); 
+ /*  setTimeout(botIntersection1, 1000); */
 }
 
 
+const botIntersection1 = () => {
+showMessage (`Walking kilometer after kilometer down a winding road you come to an intersection. Where do you want to go?`,"bot");
+setTimeout(pathChoice, 1000);
+}
+/* botIntersection1 (); */
 
 
+// Change item buttons to new text. Make buttons visible for next step.
 
+const pathChoice = () => {
+document.getElementById("reusable-button-1").innerHTML = "Dessert🌵";
+document.getElementById("reusable-button-2").innerHTML = "Mountain🌋";
+document.getElementById("reusable-button-3").innerHTML = "Creek🌊";
+setTimeout(showReusableButtons, 1000);
+}
 
-  
   /* 
 
     /*console.log(hero);
@@ -123,6 +182,4 @@ const hideButtons = () => {
     else if (!hero.includes("Rope🪢")) {
       console.log("you died")
     }
-  )}
-
- */
+  )} */
