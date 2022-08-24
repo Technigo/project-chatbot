e24aeaa6d34515176734ff86f983699f26aec7ba
a-well <amandaemkwellington@gmail.com>
2022-08-24 16:31:47 +0200

    Add questions

--- code/script.js
+++ code/script.js
@@ -1,7 +1,11 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById("chat");
+const nameForm = document.getElementById("name-form");
+const nameInput = document.getElementById("name-input");
 
 // If you need any global variables that you can use across different functions, declare them here:
+
+let questionNumber = 1;
 
 // Declare your functions after this comment
 
@@ -9,7 +13,7 @@ const chat = document.getElementById("chat");
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html section inside the chat with the posted message
   if (sender === "user") {
-    console.log("User saying something");
+    console.log("User saying this:", message);
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -20,7 +24,7 @@ const showMessage = (message, sender) => {
     `;
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === "bot") {
-    console.log("Bot saying something");
+    console.log("Bot saying this:", message);
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -34,14 +38,70 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight;
 };
 
+const nextQuestion = () => {
+  if (questionNumber === 1) {
+    botGreetsUserAndAsksName();
+  } else if (questionNumber === 2) {
+    botAskWhichDay();
+  } else if (questionNumber === 3) {
+    botAskWhatTime();
+  } else if (questionNumber === 4) {
+    botAskHowManyPeople();
+  }
+
+  if (questionNumber > 10) {
+    showMessage("Oh, you're talkative aren't you...", "bot");
+  }
+  questionNumber++;
+};
+
 // Starts here
-const greeting = () => {
+const botGreetsUserAndAsksName = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there, What's your name?", "bot");
   // Just to check it out, change 'bot' to 'user' here 👆
 };
 
+//function asking about what day
+const botAskWhichDay = () => {
+  //Here we call the next question about what day they would like to visit
+  showMessage("What day would like to book your table?", "bot");
+};
+
+//funciton asking about what time of the day
+const botAskWhatTime = () => {
+  //Here we call the next question about what day they would like to visit
+  showMessage("For what TIME would like to book your table?", "bot");
+};
+
+const botAskHowManyPeople = () => {
+  // How many people? (Buttons)
+  //   - 2
+  //   - 3
+  //   - 4
+  showMessage("How many people?", "bot");
+  // @TODO show 2-3-4 button picker UI for user
+};
+
+//function handling the name-input
+const handleUserInput = (event) => {
+  console.log(nameInput);
+  let userMsg = nameInput.value;
+  //console.log(userMsg);
+
+  showMessage(userMsg, "user");
+  nameInput.value = "";
+
+  // if (userMsg === "") {
+  //   showMessage("Sorry, I didnt get that ", "bot");
+  //   return;
+  // }
+  nextQuestion();
+};
+
 // Set up your eventlisteners here
+
+nameForm.addEventListener("submit", handleUserInput);
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -49,4 +109,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 900);
+setTimeout(nextQuestion, 1000);
