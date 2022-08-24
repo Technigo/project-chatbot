dd1c82b29b23c0f3a1ced85bb9d87968b7ba5695
Archkrull <102250838+Archkrull@users.noreply.github.com>
2022-08-24 11:28:22 +0200

    Co-authored-by: lindanorberg <lindanorberg@users.noreply.github.com>

--- code/script.js
+++ code/script.js
@@ -57,13 +57,12 @@ const handleNameInput = (event) => {
   const name = nameInput.value
   showMessage(name, 'user')
   nameInput.value = ''
-  setTimeout(() => chooseTourGuide(name), 1000)
+  setTimeout(() => chooseTourGuide(name), 500)
 }
 
 const chooseTourGuide = (name) => {
- // const tourGuide = ???.value
+  //const tourGuide = inputWrapper.value
   showMessage(`${name}, please choose your tour guide`, 'bot')
- // tourGuide = 
   inputWrapper.innerHTML=` 
   <button id="pirateBtn">Pirate</button>
   <button id="vampireBtn">Vampire</button>
@@ -71,12 +70,74 @@ const chooseTourGuide = (name) => {
 
   document
   .getElementById('pirateBtn')
-  .addEventListener('click',()=> showMessage ("Pirate", 'bot'))
+  .addEventListener('click',()=> handleTourGuide ('Pirate'))
+
+  document
+  .getElementById('vampireBtn')
+  .addEventListener('click',()=> handleTourGuide ('Vampire'))
+
+  document
+  .getElementById('witchBtn')
+  .addEventListener('click',()=> handleTourGuide ('Witch'))
+}
+
+const handleTourGuide = (tourGuide) => {
+  showMessage(tourGuide, 'user')
+  setTimeout(() => chooseActivity(tourGuide), 500)
+}
+
+const chooseActivity = (tourGuide) => {
+    showMessage (`You have chosen the ${tourGuide} What do you want to do together?`, 'bot')
+
+  if (tourGuide === 'Pirate') {
+    inputWrapper.innerHTML =`
+    <select id="select">
+     <option value="" selected disabled>Choose your activity</option>
+     <option value="Sailing">Sailing</option>
+     <option value="Robbing">Robbing</option>
+     <option value="Fencing">Fencing</option>
+    </select>`
+  }
+
+  else if (tourGuide === 'Vampire') {
+    inputWrapper.innerHTML =`
+    <select id="select">
+     <option value="" selected disabled>Choose your activity</option>
+     <option value="Explore the city at night">Explore the city at night</option>
+     <option value="Blood tasting">Blood tasting</option>
+     <option value="Stargazing">Stargazing</option>
+    </select>`
+  }
+
+  else {
+    inputWrapper.innerHTML =`
+    <select id="select">
+     <option value="" selected disabled>Choose your activity</option>
+     <option value="Potion making">Potion making</option> 
+     <option value="Broom riding">Broom riding</option>
+     <option value="Cat petting">Cat petting</option>
+    </select>`
+  }
+
+  const select = document.getElementById('select')
+  select.addEventListener('change', () => handleActivity(select.value));
+  
+}
+
+const handleActivity = (tourGuide) => {
+  showMessage(select.value, 'user')
+  const activity = select.value
+  setTimeout(() => confirmation(tourGuide, activity), 500)
+}
+
+const confirmation = (tourGuide, activity) => {
+  showMessage(`Are you sure you want to ${activity} with the ${tourGuide}?`, 'bot')
 }
 
 
 // Set up your eventlisteners here
 nameForm.addEventListener('submit', handleNameInput);
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -84,4 +145,4 @@ nameForm.addEventListener('submit', handleNameInput);
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000); //We satisfied with one second.
+setTimeout(greeting, 500); //We satisfied with one second.
