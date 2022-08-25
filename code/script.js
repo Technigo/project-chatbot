f60b2ace5c6c246a41047062768867e52f6804c0
Kim Svensson <svensson.kim88@gmail.com>
2022-08-25 12:59:15 +0200

    hej

--- code/script.js
+++ code/script.js
@@ -2,8 +2,8 @@
 const chat = document.getElementById('chat');
 const nameForm = document.querySelector("#name-form")
 const main = document.querySelector("main")
-const beerOptions = document.querySelector("beer-options")
-const beerTypeOptions = document.querySelector("#beer-type")
+const beerOptions = document.querySelector("#beer-options")
+const beerTypeOptions = document.querySelector("#beer-type-options")
 const lightBtn = document.querySelector("#light-btn")
 const darkBtn = document.querySelector("#dark-btn")
 const heinBtn = document.querySelector("#hein-btn")
@@ -54,27 +54,68 @@ const greeting = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-const submitButton = document.querySelector(".send-btn")
-const inputField = document.querySelector("#name-input")
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  nameUser = inputField.value;
-  showMessage(`hi ${nameUser}`, 'bot')
-})
+  let nameUser = null;
 
-// If the user doesnt type name:
-let nameUser = null;
-
-nameForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-  nameUser = document.querySelector("#name-input").value 
-  if (nameUser.length === 0 || nameUser === null) {
-    showMessage('Please tell us your name', "bot")
-  } else {
+  nameForm.addEventListener("submit", (event) => {
+   event.preventDefault()
+   nameUser = document.querySelector("#name-input").value 
+   if (nameUser.length === 0 || nameUser === null) {
+     showMessage('Please tell us your name', "bot")
+   } else {
     showMessage(nameUser, "user")
-    setTimeout(showTypes, 500)
-  }
-})
+    setTimeout(showBeer, 500)
+   }
+ })
+
+ //Show beer options (buttons) bot ask user to pick beer option
+ 
+  let isFirstTime = true
+
+  const showBeer = () => {
+    nameForm.classList.add('hide')
+    beerOptions.classlist.add('active')
+    beerTypeOptions.classList.remove('active')
+   
+    if (isFirstTime) {
+     showMessage(`Pleased to meet you ${nameUser}! what would you like to order?`, 'bot')  
+    isFirstTime = false
+    } else {
+    showMessage (`Choose another type, please`, "bot")
+     for (beers of beers) {
+       main.classList.remove(beers)
+       carlBtn.classlist.remove(`${beers}-btn`)
+      
+     }
+   }
+ }
+    
+
+//Denna under funkarw
+// const submitButton = document.querySelector(".send-btn")
+// const inputField = document.querySelector("#name-input")
+// submitButton.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   nameUser = inputField.value;
+//   showMessage(`hi ${nameUser} what could I get you?`, 'bot')
+  
+// })
+
+// if (nameUser.length === 0 || nameUser === null) {
+//   showMessage('Please enter your name', 'bot')
+// } 
+// If the user doesnt type name:
+// let nameUser = null;
+
+// nameForm.addEventListener("submit", (event) => {
+//   event.preventDefault()
+//   nameUser = document.querySelector("#name-input").value 
+//   if (nameUser.length === 0 || nameUser === null) {
+//     showMessage('Please tell us your name', "bot")
+//   } else {
+//     showMessage(nameUser, "user")
+//     setTimeout(showTypes, 500)
+//   }
+// })
 
 // let isFirstTime = true
 
