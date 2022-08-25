dc78c1f40f8cdd248f1b736c1430175c72530219
Petra Söderström <petrasoderstrom1612@gmail.com>
2022-08-25 17:06:45 +0200

    Itinerary 4

--- code/script.js
+++ code/script.js
@@ -65,41 +65,55 @@ const question1 = (userName) => {
   nameForm.addEventListener("submit", handleInput2);
 };
 
-//User answering the wished coffee type/getting coffee type answer 
+//User answering how thei'r doing.
 const handleInput2 = (event) => {
   event.preventDefault(); //prevents refreshing the page
-  let coffeeChoice = nameInput.value;
-  showMessage(`${coffeeChoice}`, "user"); //calling on function
+  let moodChoice = nameInput.value;
+  showMessage(`${moodChoice}`, "user"); //calling on function
   nameInput.value = ""; //erases the answer field
-  setTimeout(() => question2(coffeeChoice), 500);
+  setTimeout(() => question2(moodChoice), 500);
 };
 
-//Bot asking if milk wanted
-const question2 = (coffeeChoice) => {
-  showMessage(`I see, ${coffeeChoice}. Excellent choice! Would you like some milk with it?`, "bot");
+//Bot asking to make a coffee order
+const question2 = (event) => {
+  showMessage(`Mmm regardless your feeling your day can always get better with a cup of coffee! Would you like some?`, "bot");
   console.log("bot som skriver"); //can be omitted
-  nameForm.removeEventListener("submit", handleInput2);
-  nameForm.addEventListener("submit", handleInput3);
+  inputWrapper.innerHTML = `
+  <button id="ofCourse" type="submit" class="chat-btn">Of course!</button>
+  <button id="noThanks" type="submit" class="chat-btn">I will pass</button>
+  `
+  document.getElementById("ofCourse").addEventListener("click", ofCourseChoice);
+  document.getElementById("noThanks").addEventListener("click", noThanksChoice);
 };
 
-//User answering about milk/Getting milk answer
-const handleInput3 = (event) => {
+const noThanksChoice = (event) => {
   event.preventDefault(); //prevents refreshing the page
-  let milkChoice = nameInput.value;
-  showMessage(`${milkChoice}`, "user"); //calling on function
-  nameInput.value = ""; //erases the answer field
-  setTimeout(() => question3(milkChoice), 500);
+  let denierAnswer = inputWrapper.value;
+  showMessage("No thank you. I'll pass!", "user");
+  inputWrapper.value = "";
+  console.log("Denied coffee wish"); //can be omitted
+  setTimeout(() => byeChat(denierAnswer), 1000);
 };
 
-const question3 = (milkChoice,) => {
-  showMessage(`Great, then I am writing down ${milkChoice} for the milk choice. I'll fix it right a way!`, "bot");
- /*  console.log("bot som skriver"); //can be omitted
-  nameForm.removeEventListener("submit", handleInput3);
-  nameForm.addEventListener("submit", handleInput4);
+const byeChat = () => {
+  showMessage("Sure! No problem. Have a nice day tho!", "bot");
+  console.log("Ending chat"); //can be omitted
+  inputWrapper.innerHTML = `
+  <span>
+  `
 };
 
-const handleInput4 = (answerName) => {
-  showMessage(`Hello ${answerName}! What kind of coffee would you like to order?`, "bot"); */
+const ofCourseChoice = (event) => {
+  event.preventDefault(); //prevents refreshing the page
+  let doerAnswer = inputWrapper.value;
+  showMessage("Of course I want!", "user");
+  inputWrapper.value = "";
+  console.log("Confirmed coffee wish"); //can be omitted
+  setTimeout(() => question4(doerAnswer), 1000);
+};
+
+const question4 = (answerName) => {
+  showMessage("What kind of coffee would you like to order?", "bot");
   console.log("bot som skriver"); //can be omitted
   inputWrapper.innerHTML = `
   <button id="blackCoffee" type="submit" class="chat-btn">Black Coffee</button>
@@ -118,7 +132,7 @@ const blackChoice = (event) => {
   showMessage(`I would love some Black Coffe!`, "user");
   inputWrapper.value = "";
   console.log("black choice"); //can be omitted
-  setTimeout(() => question4(coffeeAnswer), 1000);
+  setTimeout(() => question5(coffeeAnswer), 1000);
 };
 
 const macchiatoChoice = (event) => {
@@ -127,36 +141,56 @@ const macchiatoChoice = (event) => {
   showMessage(`A macchiato will be!`, "user");
   inputWrapper.value = "";
   console.log("machiato choice"); //can be omitted
-  setTimeout(() => question4(coffeeAnswer), 1000);
+  setTimeout(() => question5(coffeeAnswer), 1000);
 };
 
 const withMilkChoice = (event) => {
   event.preventDefault(); //prevents refreshing the page
   let coffeeAnswer = inputWrapper.value;
-  showMessage(`Coffe with milk, plz!`, "user");
-  inputWrapper.value = "";
+  showMessage('Coffe with milk, plz!.', "user");
+  inputWrapper.value = '';
   console.log("with milk choice"); //can be omitted
-  setTimeout(() => question4(coffeeAnswer), 1000);
+  setTimeout(() => question5(coffeeAnswer), 1000);
 };
 
-const question4 = () => {
+const question5 = () => {
   showMessage("Stress or zen mode?", "bot");
   console.log("bot som skriver"); //can be omitted
   inputWrapper.innerHTML = `
   <button id="takeAway" type="submit" class="chat-btn">Take away</button>
   <button id="drinkHere" type="submit" class="chat-btn">Drink here</button>
   `
-  document.getElementById("takeAway").addEventListener("click", takeAway);
-  document.getElementById("drinkHere").addEventListener("click", drinkHere);
+  // Här ifrån fungerar ej
+  document.getElementById("takeAway").addEventListener("click", takeAwayChoice);
+  document.getElementById("drinkHere").addEventListener("click", drinkHereChoice);
+  setTimeout(() => question6(coffeeAnswer), 1000);
+  /* setTimeout(() => question6(coffeeAnswer), 1000); */
+};
+
+let question6 = () => {
+  if (question6 = 'takeAway') {
+  showMessage('take away', "user");
+  } else if (question6 === "drinkHere") {
+  showMessage('drink here', "user");
+  } else {
+    console.log("test")
+  }
 };
 
 
 
-
-
-
-
-
+/* let coffeChoice = () => {
+  if (coffeChoice === "blackCoffee") {
+    showMessage(`Nice choice!`, "bot")
+    setTimeout(() => question2(answerName), 1000);
+  } else if (coffeeChoice === "macchiato") {
+    showMessage(`Nice choice!`, "bot")
+    setTimeout(() => question2(answerName), 1000);
+  } else {
+    showMessage(`Nice choice!`, "bot")
+    setTimeout(() => question2(answerName), 1000);
+  }
+} */
 
 
 // Set up your eventlisteners here
