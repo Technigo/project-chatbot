29da40ae6506acbb6b089676d2ca32ba7a3a3c23
Elin Segelöv <elin.segelov@gmail.com>
2022-08-25 09:53:38 +0200

    started on first if/else

--- code/script.js
+++ code/script.js
@@ -1,6 +1,6 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
-
+const inputWrapper = document.querySelector(".input-wrapper");
 
 // If you need any global variables that you can use across different functions, declare them here:
 let heroName;
@@ -66,57 +66,73 @@ submitButton.addEventListener("click", function(event) {
 
   /* This is the first reply from the bot */
 const nameReply = () => {
-  showMessage(`${heroName}, you are about to embark on an adventure! Choose one item below to aid you on you quest. I advise you to choose wisely!`, "bot");
+  showMessage(`${heroName}, you are about to embark on an adventure! 
+  Choose one item below to aid you on you quest. I advise you to choose wisely!`, "bot");
   setTimeout(itemSelection, 0);
 }
 
   // List to select items 
   const itemSelection = () => {
-  const inputWrapper = document.querySelector(".input-wrapper");
+  //const inputWrapper = document.querySelector(".input-wrapper");🅰️ Flyttade const till toppen av dokumentet
   inputWrapper.innerHTML = `
-    <select id="item1">
-          <option value="" selected disabled>👇 Select an item...</option>;
-          <option value="Water-bottle">Water bottle🥤</option>
-          <option value="Umbrella">Umbrella☂</option>
-          <option value="Rope">Rope🪢</option>
-          <option value="Helmet">Helmet ⛑</option>
-          <option value="Boat">Boat ⛵️</option>
-          <option value="Life-Jacket">Life jacket 🦺</option>
-          <option value="Gutar">Guitar 🎸</option>
-          <option value="Meat">Piece of meat 🍗</option>
-          <option value="Sword">Sword 🗡</option>
-          <option value="Shield">Shield 🛡</option>
-          <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
-          <option value="Moccain">Moccasin 🧦</option>
+    <select id="item-one">
+      <option value="" selected disabled>👇 Select an item...</option>;
+      <option value="Water-bottle">Water bottle🥤</option>
+      <option value="Umbrella">Umbrella☂</option>
+      <option value="Rope">Rope🪢</option>
+      <option value="Helmet">Helmet ⛑</option>
+      <option value="Boat">Boat ⛵️</option>
+      <option value="Life-Jacket">Life jacket 🦺</option>
+      <option value="Gutar">Guitar 🎸</option>
+      <option value="Meat">Piece of meat 🍗</option>
+      <option value="Sword">Sword 🗡</option>
+      <option value="Shield">Shield 🛡</option>
+      <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
+      <option value="Moccain">Moccasin 🧦</option>
     </select>
     
-    <select id="item2">
-          <option value="" selected disabled>👇 Select another item...</option>;
-          <option value="Water-bottle">Water bottle🥤</option>
-          <option value="Umbrella">Umbrella☂</option>
-          <option value="Rope">Rope🪢</option>
-          <option value="Helmet">Helmet ⛑</option>
-          <option value="Boat">Boat ⛵️</option>
-          <option value="Life-Jacket">Life jacket 🦺</option>
-          <option value="Gutar">Guitar 🎸</option>
-          <option value="Meat">Piece of meat 🍗</option>
-          <option value="Sword">Sword 🗡</option>
-          <option value="Shield">Shield 🛡</option>
-          <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
-          <option value="Moccain">Moccasin 🧦</option>
+    <select id="item-two">
+      <option value="" selected disabled>👇 Select another item...</option>;
+      <option value="Water-bottle">Water bottle🥤</option>
+      <option value="Umbrella">Umbrella☂</option>
+      <option value="Rope">Rope🪢</option>
+      <option value="Helmet">Helmet ⛑</option>
+      <option value="Boat">Boat ⛵️</option>
+      <option value="Life-Jacket">Life jacket 🦺</option>
+      <option value="Gutar">Guitar 🎸</option>
+      <option value="Meat">Piece of meat 🍗</option>
+      <option value="Sword">Sword 🗡</option>
+      <option value="Shield">Shield 🛡</option>
+      <option value="Invicibility-cloak">Invicibility cloak 🧥</option>
+      <option value="Moccain">Moccasin 🧦</option>
     </select>
     
-   <button id="select-item-btn">Select</button>`;
+    <button id="select-item-btn">Select</button>`;
    
-   let itemList1 = document.querySelector("#item1");
-   let itemList2 = document.querySelector("#item2");
-   let selectItemBtn = document.querySelector("#select-item-btn");
-   selectItemBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    
-  
-   console.log(itemList1.selectedOption);
-  })}
+    let itemList1 = document.querySelector("#item-one");
+    let itemList2 = document.querySelector("#item-two");
+   
+    let selectItemBtn = document.querySelector("#select-item-btn");
+    selectItemBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      //https://ricardometring.com/getting-the-value-of-a-select-in-javascript hittade lösning för dett här 🅰️
+      // the value of the selected option in respective list is assign to variable item1 and item2.
+      let item1 = itemList1.options[itemList1.selectedIndex].innerHTML;
+      let item2 = itemList2.options[itemList2.selectedIndex].innerHTML;
+     
+      inputWrapper.innerHTML = (``);
+
+      console.log(item1);
+      console.log(item2);
+      showMessage (`I choose ${item1} and ${item2}`, 'user')
+      
+      heroInventory.push(item1.value, item2.value); 
+      console.log(`Added ${item1.value, item2.value} to backpack`);
+      console.log("inventory", heroInventory);
+
+      setTimeout(botMonologue, 1000);
+      }) 
+      }
 
 
 /*     // Make buttons visible for next step
@@ -126,41 +142,46 @@ const showButtons = () => {
 }
  */
 
-  // Add items to Hero Inventory choosing between button options
-//When the items are clicked and selected, they will be added to the inventory
-/* selectItemBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  heroInventory.push(item1.value, item2.value); 
-  console.log(`Added ${item1.value, item2.value} to backpack`);
-  console.log("inventory", heroInventory);
-  showMessage (`${item1.value, item2.value}`,"user");
-    setTimeout(hideButtons, 10);
-  }) */
+
 
 //When selected item, the buttons will disapprear and show next message. 
-const hideButtons = () => {
+/* const hideButtons = () => {
   const buttonWrapper = document.querySelector("#button-wrapper");
   buttonWrapper.classList.remove("visible-wrapper");  
   setTimeout(botMonologue, 1000);
-}
+} */
 
 const botMonologue = () => {
-showMessage (`Excellent choice! Your backpack is stuffed and you are ready to to embark on your journey!`,"bot");
-setTimeout(showReusableButtons, 1000);
+showMessage (`Excellent choice! Your backpack is stuffed. Are you ready to to embark on your journey?`,"bot");
+setTimeout(continueAdventure, 1000);
 // Make buttons visible for next step
 }
 
-const showReusableButtons = () => {
-  const reusableButtons = document.querySelector("#reusable-buttons");
-  reusableButtons.classList.add("visible-wrapper"); 
- /*  setTimeout(botIntersection1, 1000); */
-}
+const continueAdventure= () => {
+  inputWrapper.innerHTML = `
+  <button class="continue" id="continueYes">YES! Bring it on!</button>
+  <button class="continue" id="continueNo">I'm too scared!</button>
+  `;
+  let continueAdventure = document.querySelectorAll(".continue");
+  continueAdventure.forEach(button => button.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("continueYes");
+//https://javascript.plainenglish.io/how-to-get-the-id-of-the-clicked-element-in-the-javascript-click-handler-8ca398d848d6
+    /*     if (continueYes) {
+
+
+    } else {
+
+    } */
+    
+}))}
+ 
 
 
 const botIntersection1 = () => {
 showMessage (`Walking kilometer after kilometer down a winding road you come to an intersection. Where do you want to go?`,"bot");
 setTimeout(pathChoice, 1000);
-}
+} 
 /* botIntersection1 (); */
 
 
