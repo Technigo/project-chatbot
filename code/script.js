5b79b71aae899c35b5792bd2b7317d994e87dc72
Linda Malm <Linda@Lindas-MacBook-Pro.local>
2022-08-26 20:44:36 +0200

    got a working conversation going between the bot and the user

--- code/script.js
+++ code/script.js
@@ -1,116 +1,177 @@
-// Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
-const userInput = document.getElementById('user-input');
-const form = document.getElementById('user-form');
-const submit = document.getElementById('submit');
-const main = document.getElementById('main');
-const inputWrapper = document.getElementById('input-wrapper');
+document.addEventListener('DOMContentLoaded', () => {
 
-let = questionNumber = 1;
+  // Variables that point to selected DOM elements
+  const chat = document.getElementById('chat');
+  const userInput = document.getElementById('user-input');
+  const form = document.getElementById('user-form');
+  const submit = document.getElementById('submit');
+  const main = document.getElementById('main');
+  const inputWrapper = document.getElementById('input-wrapper');
+  let = questionNumber = 1;
 
-const botReply = (msg) => {
-  showMessage(msg, 'bot');
-};
+  const botReply = (msg) => {
+    showMessage(msg, 'bot');
+  };
 
-const userReply = (msg) => {
-  showMessage(msg, 'user');
-};
-
-
-
-// Declare your functions after this comment
-
-// This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
-  
-  if (sender === 'user') {
-    console.log('answer');
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `;
-  } else if (sender === 'bot') {
-    console.log('question');
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `;
+  // This function will add a chat bubble in the correct place based on who the sender is
+  const showMessage = (message, sender) => {
+    
+    if (sender === 'user') {
+      console.log('answer');
+      chat.innerHTML += `
+        <section class="user-msg">
+          <div class="bubble user-bubble">
+            <p>${message}</p>
+          </div>
+          <img src="assets/user.png" alt="User" />  
+        </section>
+      `;
+    } else if (sender === 'bot') {
+      console.log('question');
+      chat.innerHTML += `
+        <section class="bot-msg">
+          <img src="assets/bot.png" alt="Bot" />
+          <div class="bubble bot-bubble">
+            <p>${message}</p>
+          </div>
+        </section>
+      `;
+    };
+    chat.scrollTop = chat.scrollHeight;
   }
-  chat.scrollTop = chat.scrollHeight;
-}
 
-const generateRequest = (message) => {
-  console.log('questionNumber', questionNumber);
-  
-  if (questionNumber === 1) {
-    setTimeout (() => showHelpOptions(message), 1000);
-  } else if (questionNumber === 2) {
-    setTimeout (() => bookingId(message), 1000);
-  } else if (questionNumber === 3) {
-    setTimeout (() => bookingId(message), 1000);
-  } else if (questionNumber === 4) {
-    setTimeout (() => bookingId(message), 1000);
+  // This function will get the bot to progress the conversation
+  const generateRequest = (message) => {
+    console.log('questionNumber', questionNumber);
+    
+    if (questionNumber === 1) {
+      setTimeout (() => showOptions(message), 1000);
+    } else if (questionNumber === 2) {
+      setTimeout (() => passengers(message), 1000);
+    } else if (questionNumber === 3) {
+      setTimeout (() => addBagage(message), 1000);
+    } else if (questionNumber === 4) {
+      setTimeout (() => confirmBooking(message), 1000);
+    } else if (questionNumber === 5) {
+      setTimeout (() => goodbye(message), 1000);
+    };
+  };
+
+  // Greeting from the bot turns up when the page is loaded
+  const greeting = () => {
+    questionNumber = 1;
+    botReply('Hello there. What is your name?');
+  };
+
+  // The function that makes the users name-response turn up
+  const handleInput = (event) => {
+      event.preventDefault();
+      const reply = userInput.value;
+      showMessage(reply, 'user');
+      userInput.value = '';
+
+      setTimeout(() => generateRequest(reply), 500);
+  };
+
+  // the bots first question; a multiple choise question with three buttons with different alternatives
+  const showOptions = (message) => {
+    questionNumber++;
+    botReply(`Hi ${message}, How would you like to travel?`);
+
+      inputWrapper.innerHTML = `
+        <button id='firstBtn'>Boat</button>
+        <button id='secondBtn'>Bus</button>
+        <button id='thirdBtn'>Train</button>
+      `;
+
+      document.getElementById('firstBtn') .addEventListener('click', () => {
+        generateRequest('');
+        showMessage('Boat', 'user');
+      });
+      document.getElementById('secondBtn') .addEventListener('click', () => {
+        generateRequest('');
+        showMessage('Bus', 'user');
+      });
+      document.getElementById('thirdBtn') .addEventListener('click', () => {
+        generateRequest('');
+        showMessage('Train', 'user');
+      });
+  };
+
+  // the bots second question; a list of numbers to choose from
+  const passengers = () => {
+    questionNumber++;
+    botReply('How many passengers will you be?');
+      
+    inputWrapper.innerHTML =`
+      <select id='numberOfPassengers' onchange='changeFunc'>
+        <option value='0'>0</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+      </select>
+    `;
+
+    document.getElementById('numberOfPassengers') .onchange = () => {
+      generateRequest(numberOfPassengers.value);
+      showMessage(numberOfPassengers.value, 'user');
+    };
+  };
+
+  // the bots third question; a multiple choise question with 'yes'- and 'no'-button
+  const addBagage = () => {
+    questionNumber++;
+    botReply('Would you like to add bagage?');
+    
+    inputWrapper.innerHTML = `
+      <button id='yesBtn'>Yes</button>
+      <button id='noBtn'>No</button>
+    `;
+
+    document.getElementById('yesBtn') .addEventListener('click', () => {
+      generateRequest('');
+      showMessage('Yes', 'user');
+    });
+    document.getElementById('noBtn') .addEventListener('click', () => {
+      generateRequest('');
+      showMessage('No', 'user');
+    });
   }
-}
 
-// Starts here
-
-const greeting = () => {
-  questionNumber = 1;
-  botReply('Hello there, What is your name?');
-}
-
-const handleInput = (event) => {
-    event.preventDefault();
-    const reply = userInput.value;
-    showMessage(reply, 'user');
-    userInput.value = '';
-
-    setTimeout(() => generateRequest(reply), 1000);
-}
-
-const showHelpOptions = () => {
-  questionNumber++;
-  botReply(`How may I help you today?`);
+  // the bots fourth question; a multiple choise question with 'yes'- and 'no'-button, no generates a reload and it the user doesen't respond in 5 min the page reloads
+  const confirmBooking = () => {
+    questionNumber++;
+    botReply('Ok, all set then. Would you like to confirm booking?');
 
     inputWrapper.innerHTML = `
-      <button id="firstBtn">Boat</button>
-      <button id="secondBtn">Bus</button>
-      <button id="thirdBtn">Train</button>
+      <button id='yesBtn'>Confirm</button>
+      <button id='noBtn'>Restart</button>
     `;
 
-    document.getElementById('firstBtn') .addEventListener('click', () => {
-      generateRequest('Boat');
-      showMessage('Boat', 'user');
-    })
-    document.getElementById('secondBtn') .addEventListener('click', () => {
-      generateRequest('Bus');
-      showMessage('Bus', 'user');
-    })
-    document.getElementById('thirdBtn') .addEventListener('click', () => {
-      generateRequest('Train');
-      showMessage('Train', 'user');
-    })
-}
+    document.getElementById('yesBtn') .addEventListener('click', () => {
+      generateRequest('');
+      showMessage('Confirm', 'user');
+    });
+    document.getElementById('noBtn') .addEventListener('click', () => {
+      location.reload();
+    });
 
-const bookingId = () => {
-  questionNumber++;
-  botReply('testing');
-}
+    setTimeout(() => location.reload(), 300000);
+  }
 
-form.addEventListener('submit', handleInput);
+  // the bots first question; a multiple choise question with three buttons with different alternatives
+  const goodbye = () => {
+    questionNumber++;
+    botReply('Thank you for your booking!')
 
-// Set up your eventlisteners here
+    inputWrapper.innerHTML = ''
 
+  }
 
-setTimeout(greeting, 1000);
+  // Eventlistner for the submit button
+  form.addEventListener('submit', handleInput);
 
+  // the greeting-delay, so the bots greeting doesn't turn up straight away
+  setTimeout(greeting, 1500);
 
+});
