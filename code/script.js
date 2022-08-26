f1e9db84af2ff5f4296c7166a2b28677d53802d1
emilbergvik <emilbergvik@gmail.com>
2022-08-26 09:24:01 +0200

    got all the if/else if functions to work

--- code/script.js
+++ code/script.js
@@ -1,3 +1,4 @@
+
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const form = document.getElementById('name-form')
@@ -31,7 +32,7 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-//Question 1
+//Question 1: Bot greets and asks for the user's name
 const greeting = () => {
   showMessage(`Hello there, What's your name?`, 'bot')
 }
@@ -41,158 +42,215 @@ const handleResponse = () => {
   showMessage(`Nice to meet you ${userName}!`, 'bot') 
 }
 
-//Question 2++
+//Question 2: Bot asks if user is having trouble choosing a movie
 const watchMovie = () => {
-  //let userName = document.getElementById('name-input').value; Onödig kod-rad?
   showMessage(`I'm guessing you're having trouble finding something to watch on Netflix?`, 'bot')
-  inputWrapper.innerHTML = `
-    <button id="option1">Yes</button>
-    <button id="option2">No</button>  
-    `
-    document.getElementById("option1").addEventListener("click", () => {
-      showMessage("Yes, i can't decide what to watch, help me!", 'user')
-      setTimeout (() => showMessage("I'm happy to help! How are you feeling today?", "bot"), 1000)
-      inputWrapper.innerHTML=` 
-        <button id="optHappy">😄</button>
-        <button id="optSad">😥</button>
-        <button id="optAngry">😡</button>
-       `
-      
-      optHappy.addEventListener('click', () => {
-        console.log("Happy indeed!")
-        showMessage("I'm feeling happy today", "user")
-        setTimeout (() => showMessage("Great to hear that you are happy! Then one of these movies might suit your mood", "bot"), 1000)
-        inputWrapper.innerHTML=`
-          <label> Take a pick!
-              <button id="happyFeet">Happy feet</option>
-              <button id="singingInTheRain">Singing in the Rain</option>
-              <button id="greaseMovie">Grease</option>
-            </select>
-          </label>`
-
-          happyFeet.addEventListener('click', () => {
-            console.log("I hope this works!!!")
-            showMessage("i'll watch Happy feet tonight!", 'user')
-            setTimeout (() => showMessage("Great pick! Happy feet is one of my all time favorites. Enjoy the movie!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-
-          singingInTheRain.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Singing in the rain tonight!", 'user')
-            setTimeout (() => showMessage("Good choice! Singing in the Rain sure is 10/10!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-
-          })
-
-          greaseMovie.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Grease tonight!", 'user')
-            setTimeout (() => showMessage("Great pick! I LOVE Grease!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-        })
-
-      optSad.addEventListener('click', () => {
-        console.log("Sad :(")
-        showMessage("I'm feeling sad today", "user")
-        setTimeout (() => showMessage("I'm so sorry to   hear that you are not feeling good. Sometimes all you need is a good cry. I can help you get there.", "bot"), 1000)
-        inputWrapper.innerHTML=`
-          <label> Take a pick!
-              <button id="titanic">Titanic</option>
-              <button id="theNotebook">The Notebook</option>
-              <button id="brokebackMountain">Brokeback Mountain</option>
-            </select>
-          </label>`
-       
-          titanic.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Titanic tonight!", 'user')
-            setTimeout (() => showMessage("Classic, maybe this time Jack will fit on the door!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-
-          theNotebook.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch The Notebook tonight!", 'user')
-            setTimeout (() => showMessage("Great choice, make sure to bring napkins!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-          
-          brokebackMountain.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Brokeback Mountain tonight!", 'user')
-            setTimeout (() => showMessage("Nice one! One of my overall favorites!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-       
-        })
-    
-      optAngry.addEventListener('click', () => {
-        console.log("grrr!")
-        showMessage("I'm feeling angry today", "user")
-        setTimeout (() => showMessage("Oh dear, I can feel your anger through the screen. Perhaps one of these films could be a good fit for you?", "bot"), 1000)
-        inputWrapper.innerHTML=`
-          <label> Take a pick!
-              <button id="angerManagement">Anger Management</option>
-              <button id="terminator2">Terminator 2</option>
-              <button id="djangoUnchained">Django Unchained</option>
-            </select>
-          </label>`
-       
-          angerManagement.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Anger Management tonight!", 'user')
-            setTimeout (() => showMessage("Good one, sounds like it could be needed!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-
-          terminator2.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Terminator 2 tonight!", 'user')
-            setTimeout (() => showMessage('Great choice. Hasta la vista, baby!', 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-          
-          djangoUnchained.addEventListener('click', () => {
-            console.log("I'm sure this works too")
-            showMessage("i'll watch Django Unchained tonight!", 'user')
-            setTimeout (() => showMessage("Good movie, one of Tarantinos best works!", 'bot'), 1000)
-            setTimeout (() => inputWrapper.innerHTML=`
-              <p>Thanks for using this bot!</p>
-              `, 1000)
-          })
-       
-       
-       
-        })
-    })
-
-    document.getElementById("option2").addEventListener("click", () => {
-      showMessage("No thanks, i just wanted to try the functions of this bot!", 'user')
-      setTimeout (() => showMessage("Well, now that you know that it works, you're more than welcome back once you need my help!", "bot"), 1500)
-      setTimeout (() => inputWrapper.innerHTML=`
-      <p>Thanks for using this bot!</p>
-    `, 1500)
-    
-    })
+  setTimeout (() => yesOrNo(), 900)
 }
+
+//Question 2.1 The pop-up of the Yes- or No buttons
+const yesOrNo = () => {
+  inputWrapper.innerHTML = `
+  <button id="option1">Yes</button>
+  <button id="option2">No</button>  
+  `
+  document.getElementById("option1").addEventListener("click", () => {
+    showMessage("Yes, I can't decide what to watch, help me!", 'user')  
+    inputWrapper.innerHTML = ''
+    setTimeout (() => doYouNeedHelp('option1'), 1500)
+  })
+  
+  document.getElementById("option2").addEventListener("click", () => {
+    showMessage("No thanks, i just wanted to try the functions of this bot!", 'user')
+    inputWrapper.innerHTML = ''
+    setTimeout (() => doYouNeedHelp('option2'), 1500)
+  })
+}
+
+//Question 3: Bot's answer depending on if use chose Yes or No in Question 2
+const doYouNeedHelp = (selection) => {
+  if (selection === 'option1') {
+    showMessage("I'm happy to help! How are you feeling today?", "bot")
+    inputWrapper.innerHTML= 
+    `<button id="optHappy">😄</button>
+    <button id="optSad">😥</button>
+    <button id="optAngry">😡</button>`
+ 
+
+    document.getElementById("optHappy").addEventListener('click', () => {
+      showMessage("I'm feeling happy today", "user")
+      inputWrapper.innerHTML = ''
+      setTimeout (() => chooseByMood('optHappy'), 1000)
+    })
+
+    document.getElementById("optSad").addEventListener('click', () => {
+      showMessage("I'm feeling sad today", "user")
+      inputWrapper.innerHTML = ''
+      setTimeout (() => chooseByMood('optSad'), 1000)
+    })
+
+    document.getElementById("optAngry").addEventListener('click', () => {
+      showMessage("I'm feeling angry today", "user")
+      inputWrapper.innerHTML = ''
+      setTimeout (() => chooseByMood('optAngry'), 1000)
+    })
+  } 
+  
+  else if (selection === 'option2') {
+    showMessage("Well, now that you know that it works, you're more than welcome back once you need my help!", "bot")
+    inputWrapper.innerHTML=`
+    <p>Thanks for using this bot!</p>`
+      }
+    }
+//Question 4: Bot's answer depends on which Mood-button has been chosen
+const chooseByMood = (selection) => {
+ if (selection === "optHappy") {
+  showMessage("Great to hear that you are happy! Then one of these movies might suit your mood", "bot")
+  inputWrapper.innerHTML=`
+      <button id="happyFeet">Happy feet</button>
+      <button id="singingInTheRain">Singing in the Rain</button>
+      <button id="greaseMovie">Grease</button>`
+
+      document.getElementById("happyFeet").addEventListener('click', () => {
+        showMessage("I'll watch Happy Feet tonight!", "user")
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplyHappy('happyFeet'), 1000)
+      })
+
+      document.getElementById("singingInTheRain").addEventListener('click', () => {
+        showMessage("I'll watch Singing in the rain tonight!", 'user')
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplyHappy('singingInTheRain'), 1000)
+      })
+
+      document.getElementById("greaseMovie").addEventListener('click', () => {
+        showMessage("I'll watch Grease tonight", 'user')
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplyHappy('grease'), 1000)
+      })
+  }
+
+ else if (selection === "optSad") {
+  showMessage("I'm so sorry to hear that you are not feeling good. Sometimes all you need is a good cry. I can help you get there.", "bot"), 100
+  inputWrapper.innerHTML=`
+      <button id="titanic">Titanic</button>
+      <button id="theNotebook">The Notebook</button>
+      <button id="brokebackMountain">Brokeback Mountain</button>`
+ 
+        document.getElementById("titanic").addEventListener('click', () => {
+        showMessage("I'll watch Titanic tonight!", "user")
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplySad('titanic'), 1000)
+        })
+
+        document.getElementById("theNotebook").addEventListener('click', () => {
+        showMessage("I'll watch The Notebook tonight!", 'user')
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplySad('theNotebook'), 1000)
+        })
+
+        document.getElementById("brokebackMountain").addEventListener('click', () => {
+        showMessage("I'll watch Brokeback Mountain tonight", 'user')
+        inputWrapper.innerHTML = ''
+        setTimeout (() => lastReplySad('brokebackMountain'), 1000)
+        }) 
+ 
+ 
+ 
+    }
+
+ else if (selection === "optAngry") {
+  showMessage("Oh dear, I can feel your anger through the screen. Perhaps one of these films could be a good fit for you?", "bot")
+  inputWrapper.innerHTML=`
+        <button id="angerManagement">Anger Management</button>
+        <button id="terminator2">Terminator 2</button>
+        <button id="djangoUnchained">Django Unchained</button>`
+
+        document.getElementById("angerManagement").addEventListener('click', () => {
+          showMessage("I'll watch Anger Management tonight!", "user")
+          inputWrapper.innerHTML = ''
+          setTimeout (() => lastReplyAngry('angerManagement'), 1000)
+        })
+  
+        document.getElementById("terminator2").addEventListener('click', () => {
+          showMessage("I'll watch Terminator 2 tonight!", 'user')
+          inputWrapper.innerHTML = ''
+          setTimeout (() => lastReplyAngry('terminator2'), 1000)
+        })
+  
+        document.getElementById("djangoUnchained").addEventListener('click', () => {
+          showMessage("I'll watch Django Unchained tonight", 'user')
+          inputWrapper.innerHTML = ''
+          setTimeout (() => lastReplyAngry('djangoUnchained'), 1000)
+        })
+}
+}
+
+//Question 5: This is where the user chooses one of 3 movies that were suggested based on the user's mood
+const lastReplyHappy = (selection) => {
+  if (selection === "happyFeet") {
+    showMessage("Great pick! Happy feet is one of my all time favorites. Enjoy the movie!", 'bot');
+    inputWrapper.innerHTML = `
+      <p>Thank you for using this bot!</p>
+    `
+  }
+  else if (selection === "singingInTheRain") {
+    showMessage("Good choice! Singing in the Rain sure is 10/10!", 'bot')
+    inputWrapper.innerHTML = `
+      <p>Thank you for using this bot!</p>
+    `
+  }
+  else if (selection === "grease") {
+    showMessage("Great pick! I LOVE Grease!", 'bot')
+    inputWrapper.innerHTML = `
+      <p>Thank you for using this bot!</p>
+    `
+  }
+}
+
+const lastReplySad = (selection) => {
+  if (selection === "titanic") {
+    showMessage("Classic, maybe this time Jack will fit on the door!", 'bot')
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+  
+  else if (selection === "theNotebook") {
+    showMessage("Great choice, make sure to bring napkins!", 'bot')
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+
+  else if (selection === "brokebackMountain") {
+    showMessage("Nice, that's one of my overall favorites!", 'bot')
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+}
+
+const lastReplyAngry = (selection) => {
+  if (selection === "angerManagement") {
+    showMessage("Good one, sounds like it could be needed!", 'bot')
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+
+  else if (selection === "terminator2") {
+    showMessage('Great choice. Hasta la vista, baby!', 'bot')
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+
+  else if (selection === "djangoUnchained") {
+    showMessage("Good movie, one of Tarantinos best works!", 'bot') 
+    inputWrapper.innerHTML = `
+    <p>Thank you for using this bot!</p>`
+  }
+}
+
+
+
+
 
 // Set up your eventlisteners here
 form.addEventListener('submit', (event) => {
@@ -203,15 +261,6 @@ form.addEventListener('submit', (event) => {
   setTimeout (() => handleResponse(value, 'bot'), 1000)
   setTimeout (() => watchMovie(value, 'bot'), 2000)
 });
-
-
-const changeHTML = () => {
-  inputWrapper.innerHTML=` 
-    <button id="optHappy">😄</button>
-    <button id="optSad">😥</button>
-    <button id="optAngry">😡</button>
-  `
-}
 
 
 setTimeout(greeting, 1000)
