8fdcb9824aa469059b99585020f67e66accf4ca7
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-08-26 22:06:31 +0200

    Interation4

--- code/script.js
+++ code/script.js
@@ -1,8 +1,8 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
-const sendBtn = document.querySelector('.send-btn')
-const nameInput = document.getElementById('name-input')
-const inputWrapper = document.getElementById('input-wrapper')
+const sendBtn = document.querySelector('.send-btn');
+const nameInput = document.getElementById('name-input');
+const inputWrapper = document.getElementById('input-wrapper');
 // If you need any global variables that you can use across different functions, declare them here:
 
 
@@ -22,7 +22,6 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
-    console.log('Bot is the sender')
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -99,8 +98,146 @@ const showMenu = foodType => {
   </select>
   `
   }
+  // user answer
+  // eventListner on all buttons
+  // call next function set timeOut
+  document.getElementById('select').addEventListener('change', () => {
+    showMessage(select.value, 'user')
+    setTimeout(showSideMenu, 1000)
+  })
 }
 
+const showSideMenu = () => {
+  // bot reply
+  // new buttons for sideOrders
+  // eventListner buttons
+  // call on knock knock joke =)
+  showMessage('Excellent choice, and also my personal favorite! What side order would you like with that?', 'bot')
+  inputWrapper.innerHTML = `
+  <button id="fries">🥤🍟 </but ton>
+  <button id="apple">🥤🍏 </button>
+  <button id="carrots">🥤🥕</button>
+  `
+  const replySides = sideOrder => {
+    showMessage(sideOrder, 'user')
+    setTimeout(knockJoke, 2500)
+  }
+  document.getElementById('fries').addEventListener('click', ()=> replySides('I would like drink and fries'))
+  document.getElementById('apple').addEventListener('click', ()=> replySides('I would like drink and apple slices'))
+  document.getElementById('carrots').addEventListener('click', ()=> replySides('I would like drink and baby carrots'))
+
+}
+
+const knockJoke =() => {
+  showMessage('Knock, knock', 'bot')
+  inputWrapper.innerHTML = `
+  <button id="come-in"> Come in! </button>
+  <button id="who-there"> Who's there? </button>
+  <button id="what"> I'm sorry, what? </button>
+  `
+  // sorry, what
+  document.getElementById('what').addEventListener('click', () => {
+  showMessage("I'm sorry, what?", 'user')
+  setTimeout(knockJoke, 1000)  
+  } )
+
+  // come in
+  document.getElementById('come-in').addEventListener('click', () => {
+    showMessage("Come in!", 'user')
+    setTimeout(() => {
+      showMessage('Thanks!', 'bot')
+      setTimeout(confirmOrder,1000)
+      }, 1000)
+  } )
+  // Who's there
+
+  document.getElementById('who-there').addEventListener('click', () => {
+    showMessage("Who's there?", 'user')
+    setTimeout(joke, 1000)
+  })
+}
+
+const joke = () => {
+  showMessage('Europe', 'bot')
+  inputWrapper.innerHTML = `
+  <button id="come-in"> Come in! </button>
+  <button id="europe"> Europe who? </button>
+  <button id="order"> Can we get back to my order? </button>
+  `
+  // come in II
+  document.getElementById('come-in').addEventListener('click', () => {
+    showMessage("Come in!", 'user')
+    setTimeout(() => {
+      showMessage('Thanks!', 'bot')
+      setTimeout(confirmOrder,1000)
+      }, 1000)
+  } )
+  // Joke
+  document.getElementById('europe').addEventListener('click', () => {
+    showMessage("Europe who?", 'user')
+    setTimeout(() => {
+      showMessage("No YOU’RE A POO!", 'bot')
+      setTimeout(confirmOrder,1000)
+      }, 1000)
+  } )
+  // Get back to order
+  document.getElementById('order').addEventListener('click', () => {
+    showMessage("Can we get back to my order?", 'user')
+    setTimeout(sassyBot, 1000)
+  })
+
+}
+
+const sassyBot = () => {
+  showMessage("If you’re gonna be like that then I don’t want to help you", 'bot')
+  inputWrapper.innerHTML = `
+  <button id="fine"> Fine, Europe who? </button>
+  <button id="ticked-off"> Okay I’m going to Hippo Burgers across the street, bye. </button>
+  `
+  document.getElementById('fine').addEventListener('click', () => {
+    showMessage("Europe who?", 'user')
+    setTimeout(() => {
+      showMessage("No YOU’RE A POO!", 'bot')
+      setTimeout(confirmOrder,1000)
+      }, 1000)
+  } )
+
+  document.getElementById('ticked-off').addEventListener('click', () => {
+    showMessage("Okay I’m going to Hippo Burgers across the street, bye.", 'user')
+    setTimeout(reload, 1000)
+  } )
+}
+
+// confirm order
+// readback total order
+
+const confirmOrder = () => {
+  showMessage("Are you happy with this order?", 'bot')
+  inputWrapper.innerHTML = `
+  <button id="yes"> Yes, very happy! </button>
+  <button id="no"> Not happy.. </button>
+  `
+  document.getElementById('yes').addEventListener('click', () => {
+    showMessage("Yes very happy!", 'user')
+    setTimeout(end, 1000)
+  })
+
+  document.getElementById('no').addEventListener('click', () => {
+    showMessage("Not happy at all..", 'user')
+    setTimeout(reload, 1000)
+  })
+}
+
+// Reload
+
+const reload = () => location.reload()
+
+// The End
+
+const end = () => {
+  showMessage('Thank you Susan your order will be ready in a jiff!', 'bot')
+  inputWrapper.innerHTML = ''
+}
 
 // Set up your eventlisteners here
 sendBtn.addEventListener('click', (event) => {
@@ -119,3 +256,9 @@ sendBtn.addEventListener('click', (event) => {
 
 // Starts the initial greeting
 setTimeout(greeting, 1000);
+// knockJoke()
+
+//lördag, fördröjning punchline
+// css buttons, css ändra style bakgrund, stl ruta osv 
+// ta bort onödiga komentarer
+// refaktorering
