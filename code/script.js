e6eccc1054861da33771a5d987a388bd7056424f
Cecilia Råden <you@example.com>
2022-08-27 19:25:22 +0200

    tried to solve problems, didn't work

--- code/script.js
+++ code/script.js
@@ -6,12 +6,20 @@ const inputwrapper = document.getElementById("input-wrapper");
 const bodybuttons = document.getElementById("bodybuttons");
 const nameform = document.getElementById("name-form");
 const nameinput = document.getElementById("name-input");
+const submit = document.getElementById('submit');
+const sendbtn = document.getElementById('send-btn');
+
 // If you need any global variables that you can use across different functions, declare them here:
-let = questionNumber = 0;
+let questionNumber = 0;
 // Declare your functions after this comment
-const botReply = (msg) => {
-  showMessage(msg, 'bot');
+const botReply = (message) => {
+  showMessage(message, 'bot');
 }
+
+const userReply = (message) => {
+  showMessage(message, 'user');
+}
+
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
@@ -38,7 +46,7 @@ const showMessage = (message, sender) => {
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
 }
-
+//Conditionals to set order of the questions
 const nextQuestion = (message) => {
   if (questionNumber === 0) {
     setTimeout (() => whatsTheIssue(message), 1000);
@@ -48,10 +56,21 @@ setTimeout (() => booking(message), 1000);
     setTimeout (() => doctorIsBooked(message), 1000);
   }
 }
-//question counter
-// if för hela sidan
 
-// Öppningsfråga
+/*const handleNameInput = (event) => {      // Here we creat a function to avoid the submit when we click or press the button sumbit.
+  event.preventDefault()                  // .preventDefault() avoits the refresh the page.
+  const name = nameInput.value            // we create a new variable with the typed message in the input bar to use for the chatbot.
+  userReplay(name)               // here calls the function showMessage() to see what was typed before.
+  nameInput.value = ''                    
+  //counter += 1
+  setTimeout(() => showMessage(`Welcome ${name}. Want to talk about you Plan, or do you have some problem?`, 'bot'), 750)
+  setTimeout(() => nextMessage(showOptions()),750)
+  console.log('counter', counter)
+};*/
+
+
+
+// First question from the bot
 const greeting = () => {
   questionNumber = 1;
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
@@ -61,17 +80,18 @@ const greeting = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
-//Svar på öppningsfråga
+//Answer from the user
 const submitButton = document.querySelector(".send-btn")
 const inputField = document.querySelector("#name-input")
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  const name = inputField.value
- showMessage(`My name is ${name}`, 'user');
-})
+//submitButton.addEventListener('click', function(event)
+const handleNameInput = (event) => {
+ event.preventDefault();
+  const name = nameInput.value
+ userReply(`My name is ${name}`);
+}
 
 
-//Svar: "Hej...., vad har du problem med?: knappar för huvudvärk, halsont, ångest
+//Second question from the bot, where there will be one written question and then three buttons.
 const whatsTheIssue = (name) => {
   questionNumber++;
 submitButton.addEventListener('click', function(event) {
@@ -100,6 +120,7 @@ inputwrapper.innerHTML =+ `
           setTimeout(() => askForHelp('hungry'), 1000)
          })
 
+  //Answer from bot with two buttons
 const booking = () => {
   questionNumber++;
   botReply("Thats unfortunate! Should I book you a doctor?");
@@ -117,7 +138,7 @@ const booking = () => {
     showMessage('No', 'user');
   });
 }
-
+//Booking confirmation from the bot
 const doctorIsBooked = () => {
   questionNumber++;
   botReply("We have now booked you a doctor, please check your email for booking confirmation")
