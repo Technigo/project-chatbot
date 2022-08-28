52ea1ad7cbc94d59e06b7e65bd29fa5e687ba023
RosenholmJohanna <rosenholm.johanna88@gmail.com>
2022-08-28 15:46:40 +0200

    add html, css, JS, icons and audio

--- code/script.js
+++ code/script.js
@@ -2,12 +2,17 @@
 const chat = document.getElementById('chat')                  //this the whole chatbox area
 const inputWrapper = document.getElementById('inputWrapper')   //the whole chat-box
 const nameForm = document.getElementById('nameForm')          //input field
-const nameInput = document.getElementById ('nameInput')     
+const nameInput = document.getElementById ('nameInput')      //typed input
 const button = document.getElementById('button')            //button
 
-// If you need any global variables that you can use across different functions, declare them here:
 
-// Declare your functions after this comment
+  // Sound effect for chat
+  const chatSound = () => {
+    let audio = new Audio("chatTone.mp3")
+    audio.play();
+  }
+
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -18,14 +23,14 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="IconUser.png" alt="User" />  
       </section>
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="<img src="https://img.icons8.com/search"/>
+        <img src="IconBot.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -33,50 +38,66 @@ const showMessage = (message, sender) => {
     `
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
+  chat.scrollTop = chat.scrollHeight;   
+  chatSound();          // this inside the function for sounds
 }
 
-// Starts here       
 
-const greeting = () => {
+const greeting = () => {                  // Chat functions starts here    
   showMessage("Hello there, What's your name?", 'bot');  
 }
 
-let greetingAnswer = (event) => {
-  event.preventDefault()   //prevent website refresh at submit 
+const greetingAnswer = (event) => { 
+  event.preventDefault();   //prevent website refresh at submit 
   const answer = nameInput.value //storing the answer in a variable
-  showMessage(`${answer}`, 'user')   //the greeting answer with name from user 
-  nameInput.value = ''           //setting the users answer into an empty string
+  showMessage(`My name is ${answer}`, 'user');   //the greeting answer with name from user 
+  nameInput.value = ''          //setting the users answer into an empty string
+  showMessage(`Hello! ${answer} Show a topic you would like to know about?` ,  'bot');
   setTimeout(() => questionTwo(answer), 1000) //excecuting the function upon completion of timer in milliseconds
 }
 
-nameForm.addEventListener('submit', greetingAnswer) //event listenr, in this order:  variable of inputfield,listener,submit,function call
+nameForm.addEventListener('submit', greetingAnswer) //event listenr, in this order:variable of inputfield,listener,submit,function call
+
 
 const questionTwo = () => { //second question ,BOT
-    showMessage(`Hello! choose a topic of what you would like to know about?`, 'bot')
+    showMessage('Choose a topic of what you would like to know about?', 'bot'), 1000
     inputWrapper.innerHTML = `     
     <button id = "romance" type = "submit">Romance</button>
     <button id = "nature" type = "submit">Nature</button> 
     <button id = "unicorns" type = "submit">Unicorns</button>
-    <button id = "food" type = "submit">Food</button> `   
+    <button id = "food" type = "submit">Food</button> `
 
     //eventlisteners for choices                                
-    document.getElementById('romance').addEventListener('click' , () => setTimeout(() => answerTwo('romance' , 'user')),1000 )  
-    document.getElementById('nature').addEventListener('click' , () => setTimeout(() => answerTwo('nature' , 'user')),1000 ) 
-    document.getElementById('unicorns').addEventListener('click' , () => setTimeout(() => answerTwo('unicorns' , 'user')),1000 )  
-    document.getElementById('food').addEventListener('click' , () => setTimeout(() => answerTwo('food' , 'user')),1000 )                
+    document.getElementById('romance').addEventListener('click' , () => { showMessage("I would like to know more about romance", 'user')
+    setTimeout(() => answerTwo('romance' , 'user')),1000  
+    }) 
+    document.getElementById('nature').addEventListener('click' , () => { showMessage("Nature is my favourite choice!", 'user')
+    setTimeout(() => answerTwo('nature' , 'user')),1000  
+    }) 
+    document.getElementById('unicorns').addEventListener('click' , () => { showMessage("Give me the magic, picking unicorns ofc!", 'user')
+    setTimeout(() => answerTwo('unicorns' , 'user')),1000  
+    }) 
+    document.getElementById('food').addEventListener('click' , () => { showMessage("I'm excited to know about food.", 'user')
+    setTimeout(() => answerTwo('food' , 'user')),1000  
+    }) 
+
 } 
 
 const answerTwo = (choice) => {      //answer , passing the parameter
-            
     if ( choice === 'romance' ) { 
-        setTimeout(() => showMessage("Ah, romance my favorite choice! Choose your category.", 'bot'),2000)
+        setTimeout(() => showMessage("Ah, romance my favorite choice! Choose your category.", 'bot'),1000)
         inputWrapper.innerHTML = `
         <button id = "single" type = "submit">single</button>
         <button id = "marriage" type = "submit">marriage</button>
         `
-        document.getElementById('single').addEventListener('click', () => setTimeout(() => showMessage (`Here's why you may be better off single: There are 80 million germs exchanged in every kiss.` , 'bot')),1000 )
-        document.getElementById('marriage').addEventListener('click', () => setTimeout(() => showMessage (`The fact is that an average married couple has sex 68.5 times a year` , 'bot')),1000 )
+        document.getElementById('single').addEventListener('click' , () => { showMessage("Dying to know about singleness.", 'user')
+        setTimeout(() => facts('single' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+        document.getElementById('marriage').addEventListener('click' , () => { showMessage("Married facts for married people.", 'user')
+        setTimeout(() => facts('marriage' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+
+       
     }
     else if ( choice === 'nature') {
         setTimeout(() => showMessage("Let's give you some nature wisdom, category?", 'bot'),1000)
@@ -84,18 +105,31 @@ const answerTwo = (choice) => {      //answer , passing the parameter
         <button id = "ocean" type = "submit">ocean</button>
         <button id = "animals" type = "submit">animals</button>
         `
-        document.getElementById('ocean').addEventListener('click', () => setTimeout(() => showMessage (`Did you know that the ocean releases more oxygon than the forests?` , 'bot')),1000 )
-        document.getElementById('animals').addEventListener('click', () => setTimeout(() => showMessage (`I will tell you the truth: An ostrich's eyes are bigger than its brain!` , 'bot')),1000 )
+        document.getElementById('ocean').addEventListener('click' , () => { showMessage("Tell me about this, please", 'user')
+        setTimeout(() => facts('ocean' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+        document.getElementById('animals').addEventListener('click' , () => { showMessage("Animals, are my fave! Give me the info!", 'user')
+        setTimeout(() => facts('animals' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+    
+   
     }
+    else if ( choice === 'unicorns') {      
 
-    else if ( choice === 'unicorns') {
-        setTimeout(() => showMessage("DID YOU KNOW THAT UNICORNS ARE REAL!?!", 'bot'),1000)
+        setTimeout(() => showMessage("You did know unicorns are real?! Choose a magic category", 'bot'),1000)
+     
         inputWrapper.innerHTML = `
         <button id = "superpower" type = "submit">superpower</button>
-        <button id = "hunting" type = "submit">hunting</button>
+        <button id = "wings" type = "submit">wings</button>
         `
-        document.getElementById('superpower').addEventListener('click', () => setTimeout(() => showMessage (`With their horns, unicorn can neutralize posion, how cool!?` , 'bot')),1000 )
-        document.getElementById('hunting').addEventListener('click', () => setTimeout(() => showMessage (`In Michigan you can get a licence to hunt unicorns, well god luck...` , 'bot')),1000 )
+        document.getElementById('superpower').addEventListener('click' , () => { showMessage("Tell me something about their superpower!", 'user')
+        setTimeout(() => facts('superpower' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+
+        document.getElementById('wings').addEventListener('click' , () => { showMessage("Tell me something about their wings!", 'user')
+        setTimeout(() => facts('wings' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+
     }
     else if ( choice === 'food') {
         setTimeout(() => showMessage("Food is life, fastfood fact or something about health themed perhaps?", 'bot'),1000)
@@ -104,61 +138,80 @@ const answerTwo = (choice) => {      //answer , passing the parameter
         <button id = "fastfood" type = "submit">fastfood</button>
         <button id = "health" type = "submit">health themed</button>
         `
-        document.getElementById('fastfood').addEventListener('click', () => setTimeout(() => showMessage (`The world's largest toy distributor is none other than McDonald's, thanks to their Happy Meals` , 'bot')),1000 )
-        document.getElementById('health').addEventListener('click', () => setTimeout(() => showMessage (`There Is No Perfect Diet for Everyone.` , 'bot')),1000 )  
-        
+       
+        document.getElementById('fastfood').addEventListener('click' , () => { showMessage("Tell me about fastfood, please.", 'user')
+        setTimeout(() => facts('fastfood' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+        document.getElementById('health').addEventListener('click' , () => { showMessage("Health info for the healthbuff!", 'user')
+        setTimeout(() => facts('health' , 'user')),1000  // calling out the next answer in a new function named facts
+        }) 
+       
     }
-    //}
-    //else if (choice === 'nature') {
-       // showMessage ("We all need nature healing powers! Did you know that the ocean releases more oxygen that the forests?"   //}
-}
-
-const questionThree = () => { //third question ,BOT
-  showMessage(`Please chose a new topic`, 'bot')
-  inputWrapper.innerHTML = `     
-  
-   `   
-
-  //eventlisteners for choices                                
-  document.getElementById('romance').addEventListener('click' , () => setTimeout(() => answerTwo('romance' , 'user')),1000 )  
-               
-} 
-// ******* To  add **********//
-
-// choose new topic or end (go back to work) function
-
-//if (choice === 'new topic') {
-  //show the topics again. except for the choosen previous  one? }
-
-//else (choice === end/back to work) {
- // Happy I could teach you some about "choosen topics-collect chosen topics" }
 
 
+    const facts = (lastChoice) => {
+        if (lastChoice === 'single'){
+        setTimeout(() => showMessage("Fact for you: You may be better off single because... There are 80 million germs exchanged in every kiss.", 'bot'),1000)
+        setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE
+        
+        }
+        else if (lastChoice === 'marriage') {
+        setTimeout(() => showMessage("Fact: The average married couple has sex 68.5 times a year", 'bot'),1000)
+        setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE   
+        }
+        else if (lastChoice === 'ocean'){
+        setTimeout(() => showMessage("Did you know that the ocean releases more oxygen than the forests?", 'bot'),1000) 
+        setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE
+        }
+        else if (lastChoice === 'superpower'){
+          setTimeout(() => showMessage("Fact: Did you know that with their horns they can neutralize poison. How cool?!!", 'bot'),1000)
+          setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE   
+        }
 
+        else if (lastChoice === 'wings'){
+        setTimeout(() => showMessage("Fact: Unicorns actually don't have wings - thats Pegasus!", 'bot'),1000)
+        setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE   
+        }
+        else if (lastChoice === 'animals'){
+            setTimeout(() => showMessage("I will tell you the truth: An ostrich's eyes are bigger than its brain!", 'bot'),1000)   
+            setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE
+        }
+        else if (lastChoice === 'fastfood'){
+            setTimeout(() => showMessage("Fact: The world's largest toy distributor is none other than McDonald's, thanks to their Happy Meals", 'bot'),1000)  
+            setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE 
+        }
+        else if (lastChoice === 'health'){
+            setTimeout(() => showMessage("Fact: There Is No Perfect Diet for Everyone.", 'bot'),1000)   
+            setTimeout(() => closure('single', 'bot')),1000  // calling out the next answer in a new function named CLOSURE
+        }
 
+    
+    }
 
+    const closure = (end) => {
+        setTimeout(() => showMessage("Hope you had a good time, would you like to know more facts or end this chat?", 'bot'),3000)
 
+        inputWrapper.innerHTML = `
+        <button id = "yes" type = "submit">Yes, more facts.</button>
+        <button id = "no" type = "submit">End, chat.</button>
+        `
+        document.getElementById('yes').addEventListener('click' , () => { showMessage("I want to know more!", 'user')
+        setTimeout(() => questionTwo('yes' , 'user')),1000  // calling out the next answer in a new function named facts
+        })
+        document.getElementById('no').addEventListener('click' , () => { showMessage("No thank you, had fun though!", 'user')
+        setTimeout(() => bye('no' , 'user')),1000  // calling out the next answer in a new function named facts
+        })
+    }
 
+    const bye = (end) => {
+        
+        setTimeout(() => showMessage("Goodbye, thank you for your time!", 'bot'),1000)
+        inputWrapper.innerHTML = `
+        <h1>Chat closed<h1>
+        `
+        }
 
-
-
-
-
-
- //eventlisteners
-// setTimeout( () => functiontwo(nameInput), 1000) //maybe we have to set time to invoke, otherways maybe the chat setns on hold?? must decide if the inwoke shoul be by time or button--> check it up what to do with time
-//const nameF = document.querySelector("nameInput")
-//nameInput.addEventListener('click', greetingAnswer() => { console.log(true)})
-
-
-
-// Set up your eventlisteners here
-
+    }
 // When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greeting, 1000);
- 
