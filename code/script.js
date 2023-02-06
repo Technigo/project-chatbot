32f16397ce7579f4fae3690f843c0b6055fdd3e1
Malin Skill <andreasmalin@Malins-MacBook-Air.local>
2023-02-06 11:41:48 +0100

    Code to target the main chat section, if functions declared,depending on if it's user or bot. invoked with the parameter message, chat or bot.

--- code/script.js
+++ code/script.js
@@ -1,13 +1,17 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
-
+const inputWrapper = document.getElementById('input-wrapper');
+const input = document.getElementById('name-input');
+const sendButton = document.getElementById('send')
 // If you need any global variables that you can use across different functions, declare them here:
+//This will be let variables, to be able to have different numbers on question to keep track
 
 
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
+//without the ${message} the actual message(parameter) in const greetUser will not be passed, only string message. Only the parameter 'bot' 
+function showMessage(message, sender) {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
     chat.innerHTML += `
@@ -17,9 +21,10 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+    `;
+    // the else if statement checks if the sender is a bot and if that's the case it inserts an html section inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log("Now the bot is asking a question");
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -27,7 +32,7 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
@@ -39,8 +44,17 @@ const greetUser = () => {
   showMessage("Hello there, What's your name?", 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
 }
+//This function is for the first reply were the name of user is included
+const replyUser1 = () => {
+  //here I call the function showMessage, were I can pass both message and that is the bot replying
+  //I want to include the answer from previous question.
+  //also i want it to be invoked after first question is answered so I need some kind of trigger
+  showMessage("Welcome and somehow get the input from previous question", 'bot');
+}
 
 // Set up your eventlisteners here
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
