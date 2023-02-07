b1060f5bde51093e6250288355e88415b5b98d44
Andrea Hedström <andrea@Andreas-Air.lan>
2023-02-08 00:45:35 +0100

    added variables that point to selected DOM elements and changed h1,h2 and first message from bot

--- code/script.js
+++ code/script.js
@@ -1,5 +1,8 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
+const inputWrapper = document.getElementById('input-wrapper');
+const nameInput = document.getElementById('name-input');
+const submit = document.getElementById('send-btn') 
 
 // If you need any global variables that you can use across different functions, declare them here:
 
@@ -36,11 +39,15 @@ const showMessage = (message, sender) => {
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot');
+  showMessage("Goodmorning! What's your name?", 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
+
 // Set up your eventlisteners here
+
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -48,4 +55,4 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
+setTimeout(greetUser, 500);
