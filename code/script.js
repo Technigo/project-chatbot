7ccb25c214207a22d506608ac4a7e0ff5c4aa501
fannystenberg <ffannystenberg@gmail.com>
2023-02-08 17:42:14 +0100

    changed img for bot and user

--- code/script.js
+++ code/script.js
@@ -25,7 +25,7 @@ const showMessage = (message, sender) => {
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user1.png" alt="User" />  
       </section>
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
@@ -33,7 +33,7 @@ const showMessage = (message, sender) => {
     console.log("Yay")
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/bot1.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
