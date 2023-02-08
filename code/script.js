558e9c575d708178ef78ffa120dbe224c810b33b
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-02-08 19:22:16 +0100

    Added functions for dept/arr and question count

--- code/script.js
+++ code/script.js
@@ -1,11 +1,11 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
-
 const noButton = document.getElementById("no-btn");
 const inputWrapper = document.getElementById('input-wrapper');
 const chatInput = document.getElementById("chat-input");
+const datePicker = document.getElementById("travel-date");
 const sendButton = document.getElementById("send-btn");
-
+let questionCount = 0;
 
 // If you need any global variables that you can use across different functions, declare them here:
 
@@ -47,45 +47,78 @@ const greetUser = () => {
   chat.innerHTML += `
    <button id="yes-btn" class="inChatButton">Yes please</button>
    <button id="no-btn" class="inChatButton">No thanks</button>
-  `
+  ` 
   document.getElementById('yes-btn').addEventListener('click',() => {
     showMessage('Yes please.', 'user')
-    setTimeout(() => showMessage('Great! What is your city of departure?', 'bot'), 1200);
+    setTimeout(() => showMessage('Great! What is your city of departure?', 'bot'), 1000);
+    questionCount++;
+    console.log(questionCount);
+  })
+
+  document.getElementById('no-btn').addEventListener('click',() => {
+    showMessage('No, thank you.', 'user')
+    setTimeout(() => showMessage('Alright, have a good day then!', 'bot'), 1000);    
 })
 
+console.log(questionCount);
 }
 
+const handleDepartureCity = (event) =>{
+  const departureCity = chatInput.value;
+  showMessage(`${departureCity}`,'user')
+  chatInput.value='';
+  setTimeout(() => showMessage('Nice! And where are you headed?','bot'),1000);
+  console.log(questionCount);
+  console.log(departureCity);
+}
 
+const handleArrivalCity = (event) =>{
+  const arrivalCity = chatInput.value;
+  showMessage(`${arrivalCity}`,'user')
+  chatInput.value='';
+  setTimeout(() => showMessage(`Okay, I'll make a note of that.`,'bot'),1000);
+  setTimeout(() => showMessage(`When would you like to travel.`,'bot'),2000);
+  console.log(questionCount);
+}
+
+const handleTravelDate = (event) =>{
+  const travelDate  = datePicker.value;
+  showMessage(`${travelDate}`,'user')
+  chatInput.value='';
+  setTimeout(() => showMessage(`Okay.`,'bot'),1000);
+  console.log(questionCount);
+}
 
 
 const handleChatInput = (event) => {
   event.preventDefault();
+  if(questionCount == 1){
+    handleDepartureCity();
+    questionCount++;
+  } else if(questionCount == 2){
+    handleArrivalCity();
+    questionCount++;
+  } else if(questionCount == 3){
+    handleTravelDate();
+    questionCount++;
+  }
   // Store the value in a variable so we can access it after we clear it from the input
-  const inputstring = chatInput.value;
-  showMessage(`You age is ${inputstring}`, 'user'); 
+  // const inputstring = chatInput.value;
+  // showMessage(`You age is ${inputstring}`, 'user'); 
 
-  const age = parseInt(inputstring);
-  if(age > 18)
-    showMessage("You are an adult!", 'bot');
-  else
-  showMessage("You are a child!", 'bot');
-
-
-  chatInput.value = ''
-
-  
-  
+  // const age = parseInt(inputstring);
+  // if(age > 18)
+  //   showMessage("You are an adult!", 'bot');
+  // else
+  // showMessage("You are a child!", 'bot');
+  // chatInput.value = ''
+   
 }
 
-
-
-
-
-
-
 // Set up your eventlisteners here
-//yesButton.addEventListener("click", yesButtonOnClick);
+
 sendButton.addEventListener("click", handleChatInput);
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -94,5 +127,3 @@ sendButton.addEventListener("click", handleChatInput);
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 1000);
-
-
