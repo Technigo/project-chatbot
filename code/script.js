aed4fb1073e77c809e0d45b09e5cc58dfe682c44
fannystenberg <ffannystenberg@gmail.com>
2023-02-08 23:01:02 +0100

    removed form and only kept div  for input in HTML

--- code/script.js
+++ code/script.js
@@ -1,50 +1,48 @@
-// Variables that point to selected DOM elements 
-const chat = document.getElementById('chat');
-const inputWrapper = document.getElementById('input-wrapper');
-const input = document.getElementById('answer-input');
-const sendBtn = document.getElementById('send');
+// All the DOM selectors stored as short variables
+const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+const input = document.getElementById('answer-input')
+const sendBtn = document.getElementById('send')
 
-let questionNr = 1
+let questionNumber = 1
 
 // If you need any global variables that you can use across different functions, declare them here:
 
 // Declare your functions after this comment
-
-const botReply = (message) => {
-  showMessage(msg, 'bot');
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
 }
 
-const userReply = (message) => {
-  showMessage(msg, 'user');
+const userReply = (msg) => {
+  showMessage(msg, 'user')
 }
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
 
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html section inside the chat with the posted message
+// the if statement checks if the sender is 'user' and if that's the case it inserts an html section inside the chat with the posted message
   if (sender === 'user') {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user1.png" alt="User" />  
+        <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+// the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
-    console.log("Yay")
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot1.png" alt="Bot" />
+        <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
+// This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+  chat.scrollTop = chat.scrollHeight
 }
 
 //This function makes the bot continue the conversation
@@ -60,10 +58,10 @@ const nextQuestion = (message) => {
     setTimeout(() => ShowCuisineTypes(message), 1000)
   } else if (questionNr === 2) {
     userReply(message)
-    setTimeout(() => ShowOptions(message), 1000)
-  } else if (questionNr === 3){
+    setTimeout(() => showOptions(message), 1000)
+  } else if (questionNr === 3) {
     userReply(message)
-    setTimeout(() => HapyChoice(message), 1000)
+    setTimeout(() => HappyChoice(message), 1000)
   } else {
     userReply(message)
     setTimeout(thankYou, 1000)
@@ -74,7 +72,7 @@ const nextQuestion = (message) => {
  //Greeting and questions
 const greetUser = () => {
   questionNr = 1
-  botReply("Hello there, what's your name?")
+  botReply(`Hello there, what's your name?`)
 }
 
 const ShowCuisineTypes = (msg) => {
@@ -88,27 +86,27 @@ const ShowCuisineTypes = (msg) => {
   `
 // Set up your eventlisteners here
   document
-  .getElementById(italianBtn);
-  .addEventListener('click', () => nextQuestion('italian'));
+    .getElementById('italianBtn')
+    .addEventListener('click', () => nextQuestion('italian'))
   document
-  .getElementById(asianBtn);
-  .addEventListener('click', () => nextQuestion('asian'));
+    .getElementById('asianBtn')
+    .addEventListener('click', () => nextQuestion('asian'))
   document
-  .getElementById(middleEasternBtn);
-  .addEventListener('click', () => nextQuestion('middleeastern'));
+    .getElementById('middleEasternBtn')
+    .addEventListener('click', () => nextQuestion('middleeastern'))
 }
 
-const ShowOptions = (type) => {
+const showOptions = (type) => {
   questionNr++
-  botReply(`Oh so you're in the mood for ${type}? Great choice. Select a place from the options!`);
+  botReply(`Oh so you're in the mood for ${type}? Great choice. Select a place from the options!`)
 
   if (type === 'italian') {
     inputWrapper.innerHTML = `
     <select id="select">
      <option value="" selected disabled>👇 Select a restaurant..</option>
-     <option value="lanonna1">La Nonna</option>
+     <option value="lanonna">La Nonna</option>
      <option value="olli">OLLI</option>
-     <option value="capricci">Capricci/option>
+     <option value="capricci">Capricci</option>
     </select>
     `
   } else if (type === 'asian') {
@@ -120,7 +118,7 @@ const ShowOptions = (type) => {
      <option value="sinramen">Sin ramen</option>
     </select>
     `
-  } else  {
+  } else {
     inputWrapper.innerHTML = `
     <select id="select">
     <option value="" selected disabled>👇 Select a restaurant..</option>
@@ -130,18 +128,18 @@ const ShowOptions = (type) => {
     </select>
     `
   }
-  
-  const select = document.getElementById('select');
-  select.addEventListener('change', () => nextQuestion(select.value));
+
+  const select = document.getElementById('select')
+  select.addEventListener('change', () => nextQuestion(select.value))
 }
 
-const HapyChoice = (choice) => {
+const HappyChoice = (choice) => {
   questionNr++
   botReply(`Nice! Are you happy with your choice?`)
 
   inputWrapper.innerHTML = `
-  <button id="restart">NO</button>
-  <button id="confirm">YES</button>
+    <button id="restart">NO</button>
+    <button id="confirm">YES</button>
   `
   document
   .getElementById('restart')
@@ -150,24 +148,21 @@ const HapyChoice = (choice) => {
     return false
   })
   document
-  .getElementById('confirm')
-  .addEventListener('click', () => nextQuestion('Yes!'))
+    .getElementById('confirm')
+    .addEventListener('click', () => nextQuestion('Yes!'))
 }
 
 const thankYou = () => {
-  botReply(`Thank you for using TheGuide! See you soon!`)
+  botReply(`Thank you for using TheGuide! See you soon👋🏼`)
   inputWrapper.innerHTML = ``
 }
-  
+
 // How the bot acts when user clicks on button or responds
 sendBtn.addEventListener('click', () => nextQuestion(input.value))
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter' && input.value) nextQuestion(input.value)
 })
 
-
-
-
 // When website loaded, chatbot asks first question.
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1500);
+setTimeout(greetUser, 1500)
