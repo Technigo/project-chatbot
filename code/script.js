22105b1d853ed50496d523fa206a2647495756bf
Bridget Mailley <bmailley42@gmail.com>
2023-02-08 20:10:55 +0100

    Added craft buttons and console log actions, tidied code

--- code/script.js
+++ code/script.js
@@ -2,11 +2,14 @@
 const chat = document.getElementById('chat');
 const form = document.getElementById('name-form');
 const nameInput = document.getElementById('name-input');
+//const username = nameInput.value; // this won't work here because nameInput does not yet have a value, will only show up empty
+const input = document.getElementById('input-wrapper');
 
 // If you need any global variables that you can use across different functions, declare them here:
 
 
 // Declare your functions after this comment
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -36,8 +39,7 @@ const showMessage = (message, sender) => {
 }
 
 
-
-// Starts here
+  // Starts here - The greetUser function that is called at the end and starts the chat bot running is defined here.
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there, crafter! Who do I have the pleasure of speaking with today?", 'bot');
@@ -47,13 +49,42 @@ const greetUser = () => {
 form.addEventListener('submit', function(event){
   event.preventDefault()
   const username = nameInput.value
-  console.log(username)
   showMessage(username, 'user')
-  setTimeout(() => showFirstResponse(username), 500)
+  setTimeout(() => showFirstResponse(username), 500) //the username in parentheses is passing off data to the next function showFirstResponse
 });
 
-const showFirstResponse = () => {
-  showMessage(`Hi there, ${nameInput.value}! Are you interested in getting quilting, crochet, or cross-stitch ideas?`, 'bot')
+const showFirstResponse = (username) => { //the param username is collecting the username data from previous function to be used in following function
+  showMessage(`Hi there, ${username}! Are you interested in getting quilting, crochet, or cross-stitch ideas?`, 'bot')
+  input.innerHTML = `
+    <button id="quilt-button">Quilting</button>
+    <button id="crochet-button">Crochet</button>
+    <button id="cross-button">Cross-Stitch</button>
+    </div> 
+  `
+  showCraftButtons();
+}
+
+const showCraftButtons = () => { //once the variables and eventListeners are stored in a function they can be called at any point and don't make the code break
+
+  const quiltButton = document.getElementById('quilt-button');
+  const crochetButton = document.getElementById('crochet-button');
+  const crossButton = document.getElementById('cross-button');
+
+  quiltButton.addEventListener('click', function() {
+    //event.preventDefault()
+    console.log("Quilt");
+  });
+
+  crochetButton.addEventListener('click', function() {
+    //event.preventDefault()
+    console.log("Crochet");
+  });
+
+  crossButton.addEventListener('click', function() {
+    //event.preventDefault()
+    console.log("Cross-stitch");
+  });
+
 }
 
 
@@ -67,3 +98,5 @@ const showFirstResponse = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 500);
+
+
