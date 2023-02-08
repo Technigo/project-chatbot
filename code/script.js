884808abe032073503aa6404e534a3463cfc8362
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-02-08 21:27:32 +0100

    app in progress, working with three responses

--- code/script.js
+++ code/script.js
@@ -1,15 +1,17 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
+const chat = document.getElementById("chat");
 
 // If you need any global variables that you can use across different functions, declare them here:
+const nameForm = document.getElementById("name-form");
+const inputWrapper = document.getElementById("input-wrapper");
+const nameInput = document.getElementById("name-input");
+const sendButton = document.querySelector(".send-btn");
+const stew = document.getElementById("#stew");
 
+let usersName, classChoice;
 
-// Declare your functions after this comment
-
-// This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -17,9 +19,8 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
+    `;
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -27,25 +28,78 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+
+  // Makes the chat scroll to the last message
   chat.scrollTop = chat.scrollHeight;
+};
+
+const greetUser = () => {
+  showMessage("Hello there, What's your name?", "bot");
+};
+
+const showClassOptions = () => {
+  inputWrapper.innerHTML += `
+  <select id="class">
+  <option value="Human">Human</option>
+  <option value="elf">Elf</option>
+  <option value="dwarf">Dwarf</option>
+  <option value="orc">Orc</option>
+</select>
+<button id="options-btn" type="send">Chose your class</button>
+  `;
+  const optionButton = document.getElementById("options-btn");
+  const classSelect = document.getElementById("class");
+  optionButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    classChoice = classSelect.value;
+    // console.log(classChoise);
+    classSelect.remove();
+    optionButton.remove();
+    showMessage(classChoice, "user");
+
+    setTimeout(() => {
+      showMessage(`So you are a ${classChoice}, interesting...`, "bot");
+      setTimeout(() => {
+        showMessage(
+          "...I might have a nice adventure for you, there might be a big reward",
+          "bot"
+        );
+        //Add next user option here
+        AdventurePicker();
+      }, 1000);
+    }, 1000);
+  });
+};
+
+const AdventurePicker = () => {
+  inputWrapper.innerHTML += `
+  <button value="save-the-queen">Save the Queen</button>
+  <button value="find-the-arc">Find the lost arc</button>`;
+};
+
+function askNextQuestion() {
+  setTimeout(
+    () => showMessage(` ${nameInput.value} , second question `, "bot"),
+    1000
+  );
+  nameForm.remove();
+  /*  */
+  setTimeout(() => showClassOptions(), 1000);
 }
 
-// Starts here
-const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
+function askThirdQuestion() {
+  showMessage(` ${nameInput.value} , Third question? `, "bot");
 }
 
 // Set up your eventlisteners here
+sendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  usersName = nameInput.value;
+  console.log(usersName);
+  showMessage(usersName, "user");
+  setTimeout(() => askNextQuestion(), 1000);
+});
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 1000);
