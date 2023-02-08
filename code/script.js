84530b78ab8dc4fec5a5eb52da9c8ed1b8ed5a3c
theresBL <tbrannberglendt@gmail.com>
2023-02-08 15:21:26 +0100

    the name input works

--- code/script.js
+++ code/script.js
@@ -1,15 +1,17 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
+const sendBtn = document.getElementById('sendBtn'); // 👈🏼 här kan du göra en "förkortning" till sendBtn
+const nameForm = document.getElementById('nameForm'); // 👈🏼 här kan du göra en "förkortning" till nameForm
+const nameInput =document.getElementById('nameInput')
 
 // If you need any global variables that you can use across different functions, declare them here:
-
-
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
+    console.log ('User is replyng')
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -20,6 +22,7 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log ('Bot asking question')
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/laundry_bot.png" alt="Bot" />
@@ -33,14 +36,34 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight;
 }
 
-// Starts here
+// Chat starting here when bot greets user
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hi there, what´s your name?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage("Hi there, what´s your name?", 'bot')
 }
 
+//User types in Name and a massage is shown with the users name
+const handleNameInput = (event) => {
+  event.preventDefault()
+  console.log('Här ska vi ta hand om namnet')
+  console.log(nameInput.value) // kolla in console om du ser namnet komma igenom
+  // vi måste få namnet fårn nameInput.value. men igen måste vi hitta input genom
+  // document.getElementById('nameInput') så det kan vi sätta upp högst upp, som en "shortcut"
+  showMessage(nameInput.value, 'user') 
+// skicka med input value och att det är user som skickar det nameInput.value är en variable så inga fnuttar där. bara fnuttar när det ska vara vanlig text
+
+  nameInput.value = '' // det här är på slutet, för att tömma inputen.
+
+  // Boten svarar med "Hi 'name', what a lovley day for laundry" + ny fråga  
+}
+
+
 // Set up your eventlisteners here
+sendBtn.addEventListener('submit', handleNameInput);
+// 💡 HÄR VET INTE KODEN VAD sendBtn ÄR. DU MÅSTE GÖRA SOM HÖGST UPP MED chat
+// Jag gjorde det åt dig så kan du se hur det ser ut
+
+nameForm.addEventListener('submit', handleNameInput);
+// Lyssna till hela form submit. Eftersom man kan använda enter på tangentbordet också. Och inte bara själva kanppen
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -48,4 +71,13 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
+  setTimeout(greetUser, 1000);
+
+
+
+  //Enter name and click button
+ 
+  //Storing username for access later
+
+  //What type of laundry will you be washing today?
+  //-Dark? White? ........
