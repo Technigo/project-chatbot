aa5aeda5eca5f8d6afc430bcc54de32e91825d42
Emilia Saberski <emilia@saberski.com>
2023-02-08 10:36:01 +0100

    put in new js

--- code/script.js
+++ code/script.js
@@ -40,9 +40,22 @@ const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there, What's your name?", 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
-}
+}  
 
 // Set up your eventlisteners here
+const handleNameInput = (event) => {
+  event.preventDefault()
+  // Store the value in a variable so we can access it after we 
+	// clear it from the input
+  const name = nameInput.value
+  showMessage(name, 'user')
+  nameInput.value = ''
+
+  // After 1 second, show the next question by invoking the next function.
+	// passing the name into it to have access to the user's name if we want
+	// to use it in the next question from the bot.
+  setTimeout(() => showFoodOptions(name), 1000)
+}
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -50,4 +63,5 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
+setTimeout(greetUser, 1000); 
+
