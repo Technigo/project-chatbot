3eecacd0f9fd811834cf84902fdc406883beea3b
Andreas Axelsson <pererikandreas@gmail.com>
2023-02-09 17:45:10 +0100

    added Sound

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The assignment was to create a chatbot with javascript, css and html.
 
 ## The problem
 
-I approached this task by start watching all attached videos about javascript. Then a coded along with Jennie creating the ground in HTML and CSS and some of the Javascript. Attached in the folder provided in github for this task there was some prewritten java-code. I wrote down with comments what I thought should be done and started to look (google) for ways to do it. I felt that I needed a wholeistic view on how to complete the code so I looked at some of the previus technigo students code and tried out different functions they had been using.
+I approached this task by start watching all attached videos about javascript. Then I coded along with Jennie creating the ground for the HTML and CSS and some of the Javascript. Attached in the folder provided in github for this task there was some prewritten java-code. I wrote down with comments what I thought should be done and started to look (google) for ways to do it. That was hard and felt that I needed a wholeistic view on how to complete the code so I looked at some of the previus technigo students code and tried out different functions they had been using.
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -43,10 +43,22 @@ chat.scrollTop = chat.scrollHeight;
   // Next up we start using the showMessage function that we declared on row 14. 
   // geetUser function makes the bot automaticly ask the first question: "Hej!" "Vad heter du?"
   // Arguments going in to the "machine"/function is msg="Vad heter du?" and sender="bot".
+  // But first I declare the bottrack and the usertrack that start on all messages
+
+const bottrack = new Audio('./assets/chatsoundbot.mp3');
+const playsound = () => {
+      bottrack.play();
+}
+
+const usertrack = new Audio('./assets/chatsounduser.mp3');
+const makesound = () => {
+      usertrack.play();
+}
 
 const greetUser = () => { // Automatic greeting
       showMessage("Hej människa!", "bot"); // First bot-message
       setTimeout(() => showMessage("Vad heter du?", "bot"), 1500); // Opens the second bot-message 1,5 sec after the first.
+      bottrack.play(); // This starts the bottrack(sound)
 };
 
 setTimeout(greetUser, 2000); // Makes the first message from the bot appear in 2 sec after site is loaded
@@ -61,10 +73,12 @@ const handleNameInput = (event) => { // This function stores the event (user wri
 
 const name = nameInput.value // Here we take the nameInput.value and delare it as the same as just "name"
       showMessage(name, "user")
+      usertrack.play(); // This starts the usertrack(sound)
       showMessage(`Hej ${nameInput.value}! Jag heter August Lemmel.`, "bot")
+      setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 500 millisec
       setTimeout(() => showMessage("Har jag ditt förtroende?", "bot"), 1500);
       nameInput.value = '' // Cleares the nam input form. Its placed here after the value is used otherwise it clears it and it cant be used
-      inputArea.innerHTML = 
+      inputArea.innerHTML = // Presents the first Ja/Nej buttons
       `<button id="jaButton" type="submit">Ja</button>
       <button id="nejButton" type="submit">Nej</button>`
 
@@ -72,8 +86,10 @@ const name = nameInput.value // Here we take the nameInput.value and delare it a
 
       document
         .getElementById("jaButton")
-        .addEventListener("click", () => {
+        .addEventListener("click", () => { // The following happens when the button is clicked
           showMessage("Ja", "user");
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 500 millisec
           showMessage("Tack! Jag vill tipsa dej om nått passande från vår shop.", "bot");
           setTimeout(() => showMessage("Vad tycker du värst om?", "bot"), 1000);
           personalizeButtons() // Function starts the personalizedButtons-alternatives 
@@ -83,8 +99,10 @@ const name = nameInput.value // Here we take the nameInput.value and delare it a
 
       document
         .getElementById("nejButton")
-        .addEventListener("click", () => {
+        .addEventListener("click", () => { // The following happens when the button is clicked
           showMessage("Nej", "user")
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 500); // This starts the bottrack(sound) and delays it with 500 millisec
           showMessage('Synd.. Men du kolla in denna! Ha en fin dag!<br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>', "bot")
           jaButton.remove(); // Removes Ja-button
           nejButton.remove(); // Removes Nej-button
@@ -101,9 +119,11 @@ const personalizeButtons = () => {
   // FIRST personalizeButton is "Baristas" and will print out the following
 
       document
-        .getElementById("baristas")
-        .addEventListener("click", () => {
+        .getElementById("baristas") // 1st button
+        .addEventListener("click", () => { // The following happens when the button is clicked
           showMessage("Baristas", "user")
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 1000 millisec
           showMessage(`Fattar helt! Kolla in den här pannan <img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/186885537-origpic-b912cf_750x.jpg?v=1664429478">`, "bot");
           setTimeout(() => showMessage('<a href="https://lemmelkaffe.com/en/collections/tillbehor/products/svaertan">Svärtan</a> heter den. Kan det va något för dej?', "bot"), 1000);
           lastYesOrNo() // Starts the last Ja/nej-answer-button 
@@ -112,9 +132,11 @@ const personalizeButtons = () => {
   // SECOND personalizeButton is "Snabbkaffe" and will print out the following
 
       document
-        .getElementById("snabbkaffe")
-        .addEventListener("click", () => {
+        .getElementById("snabbkaffe") // 2th button
+        .addEventListener("click", () => { // The following happens when the button is clicked
           showMessage("Snabbkaffe", "user")
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 1000 millisec
           showMessage(`Ja huvva! Kolla in denna! <br><p><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/HOST22AKKA_750x.jpg?v=1664298939">`, "bot")
           setTimeout(() => showMessage('Längre från snabbkaffe än <a href="https://lemmelkaffe.com/collections/kaffe/products/akka-eko-krav-400g">Akka Eko/Krav</a> är svårt att komma. Känns det rätt för dej?', "bot"), 2000);
           lastYesOrNo() // Starts the last Ja/nej-answer-button 
@@ -123,9 +145,11 @@ const personalizeButtons = () => {
   // THIRD personalizeButton is "Ugglor" and will print out the following
 
       document
-        .getElementById("ugglor")
-        .addEventListener("click", () => {
-          showMessage("Ugglor", "user")
+        .getElementById("ugglor") // 3rd button
+        .addEventListener("click", () => { // The following happens when the button is clicked
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 500); // This starts the bottrack(sound) and delays it with 500 millisec
+          showMessage("Ugglor", "user") // The user message Ugglor and the bot replyes with an message that ends the conversation
           showMessage(`Word! Värsta sortens!`, "bot")
           setTimeout(() => showMessage('Slipp se dom i dessa! <br><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/185533511-origpic-feb617_750x.jpg?v=1674466597"> ', 'bot'), 2000)
           setTimeout(() => showMessage('Bågarna heter <a href="https://lemmelkaffe.com/collections/tillbehor/products/lucifer">Lucifer</a>! Känns det bra?', 'bot'), 2000)
@@ -139,23 +163,27 @@ const lastYesOrNo = () => {inputArea.innerHTML =
         `<button id="jaButton" type="submit">Ja</button>
         <button id="nejButton" type="submit">Nej</button>`
 
-  //IF the user clicks the jaButton the following message will be shown
+  // IF the user clicks the jaButton the following message will be shown
 
       document
         .getElementById("jaButton")
-        .addEventListener("click", () => {
-          showMessage("Ja", "user");
+        .addEventListener("click", () => { // The following happens when the button is clicked
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 1000 millisec
+          showMessage("Ja", "user"); // The user message Ja and the bot replyes with an message that ends the conversation
           showMessage('Perfekt! Finns i shoppen. Ha en fin dag!', "bot");
           jaButton.remove(); // Removes Ja-button
           nejButton.remove(); // Removes Nej-button
         })
 
-  //IF the user clicks the noButton the following message will be shown
+  // IF the user clicks the noButton the following message will be shown
 
       document
         .getElementById("nejButton")
-        .addEventListener("click", () => {
-          showMessage("Nej", "user")
+        .addEventListener("click", () => { // The following happens when the button is clicked
+          usertrack.play(); // This starts the usertrack(sound)
+          setTimeout(() => bottrack.play(), 500); // This starts the bottrack(sound) and delays it with 500 millisec
+          showMessage("Nej", "user") // The user message Nej and the bot replyes with the video
           showMessage(`Trisst, men här kommer nått som gör alla glada! <br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>`, "bot")
           jaButton.remove(); // Removes Ja-button
           nejButton.remove(); // Removes Nej-button
