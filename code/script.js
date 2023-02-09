7f1a1d38c14581624c643b35d3765c6454db134d
Oscar <oscar.sindemark@gmail.com>
2023-02-08 23:13:44 -0800

    Understand the console logs and hopefully can successfully push this after re-cloning the code

--- code/script.js
+++ code/script.js
@@ -3,13 +3,13 @@ const chat = document.getElementById('chat');
 
 // If you need any global variables that you can use across different functions, declare them here:
 
-
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
+    console.log("hello not world")
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -20,6 +20,7 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log("hello world")
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -48,4 +49,4 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
+setTimeout(greetUser, 500);
