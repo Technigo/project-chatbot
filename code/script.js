a0b8a78159fa31685442043e6e1b14fb3b6501b3
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-02-09 07:01:53 +0100

    Added function for user input

--- code/script.js
+++ code/script.js
@@ -1,7 +1,9 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
 
+
 // If you need any global variables that you can use across different functions, declare them here:
+
 
 
 // Declare your functions after this comment
@@ -18,15 +20,13 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
+    `} else if (sender === 'bot') {
     console.log("Message from bot");
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
-          <p>${message}</p>
+          <p id="message">${message}</p>
         </div>
       </section>
     `
@@ -37,17 +37,25 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage("Greetings stranger! Are you ready to play?", 'bot');
 }
+
+
 
 // Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
+
 setTimeout(greetUser, 1000);
+
+//function for userAnswer
+const userAnswer = () => {
+  let userInput = document.getElementById("user-input").value;
+  showMessage(userInput, "user");
+  //clear text-field
+  document.getElementById('user-input').value=null;
+};
+
+//prevents page from reloading
+document.getElementById("send").addEventListener("click", function(event){
+  event.preventDefault();
+});
