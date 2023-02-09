207b736b645c7dc56f5fdb4c0b32756552b9cad4
Sofia Gerdmar <Fia@MacBook-Air.lan>
2023-02-10 00:08:08 +0100

    Iteration2

--- code/script.js
+++ code/script.js
@@ -20,10 +20,8 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
-    console.log
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -49,4 +47,4 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 20);
+setTimeout(greetUser, 1000);
