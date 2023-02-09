802bb964933fed96f1756b04b20370b3a872bbe5
Andreas Axelsson <pererikandreas@gmail.com>
2023-02-09 13:54:00 +0100

    added structure and tried to fint repeats in the JS

--- code/script.js
+++ code/script.js
@@ -1,17 +1,17 @@
-//---------------------------------------Here is the conection to the HTML-------------------------------------------------------------------//
+//---------------------------------------Here is the conection to the HTML / DOM-------------------------------------------------------------------//
 
-  // Variables that is linked to the DOM / HTML
+  // Variables:
 
 const chat = document.getElementById("chat");
 const inputArea = document.getElementById('input-area')
 const form = document.getElementById("name-form");
 const nameInput= document.getElementById("name-input");
 
-//--------------------------------------------------User and bot chat-function starts----------------------------------------------------------//
+//--------------------------------------------------User and bot chat-function is created----------------------------------------------------------//
 
-  // This function will add a chatbubbels in the main frame depending on who the sender is. If checks if the sender is the same as "user"
+  // This function adds chatbubbels in the main frame depending on who the sender is. It checks IF the sender === "user" ELSE IF it is the "bot"
 
-const showMessage = (msg, sender) => {
+const showMessage = (msg, sender) => { // Here we are creating the chatbubbles. From now it is refered as only -showMessage("msg, sender)-
 if (sender === "user") {chat.innerHTML += 
   `<section class="user-msg">
       <div class="bubble user-bubble">
@@ -30,38 +30,40 @@ else if (sender === "bot") {chat.innerHTML +=
         <p>${msg}</p>
       </div>
   </section>`
-  }
+}
 
   // This code makes the mainchat scroll when there are to many messages
+
 chat.scrollTop = chat.scrollHeight;
 };
 
 //--------------------------------------------------User and bot chat-function ends------------------------------------------------------------//
-//-----------------------------------------------------------Chating starts-------------------------------------------------------------------//
+//----------------------------------------------------Chating Conversation starts--------------------------------------------------------------//
 
-  // Next up we start using the showMessage function that we declared on row 15. 
+  // Next up we start using the showMessage function that we declared on row 14. 
   // geetUser function makes the bot automaticly ask the first question: "Hej!" "Vad heter du?"
   // Arguments going in to the "machine"/function is msg="Vad heter du?" and sender="bot".
 
-const greetUser = () => {
-      showMessage("Hej människa!", "bot");
-      setTimeout(() => showMessage("Vad heter du?", "bot"), 1500);
+const greetUser = () => { // Automatic greeting
+      showMessage("Hej människa!", "bot"); // First bot-message
+      setTimeout(() => showMessage("Vad heter du?", "bot"), 1500); // Opens the second bot-message 1,5 sec after the first.
 };
 
-setTimeout(greetUser, 2000);
+setTimeout(greetUser, 2000); // Makes the first message from the bot appear in 2 sec after site is loaded
 
-  // USER: types in name with help from the handleNameInput conected to the id=name-input in the HTML
-  // Now we will store the name as a value in a variable (nameInput.value) and can acces it with ${nameInput.value} in the messages
+  // USER: types in name wich gets stored handleNameInput conected to the id=name-input in the HTML and the -form.addEventListener("submit", handleNameInput)-
+  // Now it will store the name as a value in a variable (nameInput.value) and you can acces it with ${nameInput.value} in the messages
 
-const handleNameInput = (event) => {
+const handleNameInput = (event) => { // This function stores the event (user writing its name) as a value
       event.preventDefault() // Prevents autosubmittning the form
 
   // BOT: Answers two bubbles with the stored username${nameInput.value} and ends with showing the Ja/Nej-buttons
 
-const name = nameInput.value
+const name = nameInput.value // Here we take the nameInput.value and delare it as the same as just "name"
       showMessage(name, "user")
       showMessage(`Hej ${nameInput.value}! Jag heter August Lemmel.`, "bot")
       setTimeout(() => showMessage("Har jag ditt förtroende?", "bot"), 1500);
+      nameInput.value = '' // Cleares the nam input form. Its placed here after the value is used otherwise it clears it and it cant be used
       inputArea.innerHTML = 
       `<button id="jaButton" type="submit">Ja</button>
       <button id="nejButton" type="submit">Nej</button>`
@@ -69,28 +71,28 @@ const name = nameInput.value
   // IF the user presses the jaButton the following message will be print and the 3 personalizeButtons will be shown.
 
       document
-      .getElementById("jaButton")
-      .addEventListener("click", () => {
-        showMessage("Ja", "user");
-        showMessage("Tack! Jag vill tipsa dej om nått passande från vår shop.", "bot");
-        setTimeout(() => showMessage("Vad tycker du värst om?", "bot"), 1000);
-        personalizeButtons()
-      })
+        .getElementById("jaButton")
+        .addEventListener("click", () => {
+          showMessage("Ja", "user");
+          showMessage("Tack! Jag vill tipsa dej om nått passande från vår shop.", "bot");
+          setTimeout(() => showMessage("Vad tycker du värst om?", "bot"), 1000);
+          personalizeButtons() // Function starts the personalizedButtons-alternatives 
+        })
 
   // IF the user presses the nejButton the following message will be shown and the chat will end. No more questions.
 
       document
-      .getElementById("nejButton")
-      .addEventListener("click", () => {
-        showMessage("Nej", "user")
-        showMessage('Synd.. Men du kolla in denna! Ha en fin dag!<br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>', "bot")
-        jaButton.remove();
-        nejButton.remove(); 
-      }) 
+        .getElementById("nejButton")
+        .addEventListener("click", () => {
+          showMessage("Nej", "user")
+          showMessage('Synd.. Men du kolla in denna! Ha en fin dag!<br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>', "bot")
+          jaButton.remove(); // Removes Ja-button
+          nejButton.remove(); // Removes Nej-button
+        }) 
 
-  // Here is the function and the connection to the 3 personalizing costumer-question alternatives.
+  // Here is the function created and the connection to the 3 personalizing costumer-question alternatives.
 
-const personalizeButtons = () => {      
+const personalizeButtons = () => {  
       inputArea.innerHTML = 
       `<button id="baristas" type="submit">Baristas</button>
        <button id="snabbkaffe" type="submit">Snabbkaffe</button>
@@ -104,7 +106,7 @@ const personalizeButtons = () => {
           showMessage("Baristas", "user")
           showMessage(`Fattar helt! Kolla in den här pannan <img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/186885537-origpic-b912cf_750x.jpg?v=1664429478">`, "bot");
           setTimeout(() => showMessage('<a href="https://lemmelkaffe.com/en/collections/tillbehor/products/svaertan">Svärtan</a> heter den. Kan det va något för dej?', "bot"), 1000);
-          lastYesOrNo()
+          lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
 
   // SECOND personalizeButton is "Snabbkaffe" and will print out the following
@@ -115,7 +117,7 @@ const personalizeButtons = () => {
           showMessage("Snabbkaffe", "user")
           showMessage(`Ja huvva! Kolla in denna! <br><p><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/HOST22AKKA_750x.jpg?v=1664298939">`, "bot")
           setTimeout(() => showMessage('Längre från snabbkaffe än <a href="https://lemmelkaffe.com/collections/kaffe/products/akka-eko-krav-400g">Akka Eko/Krav</a> är svårt att komma. Känns det rätt för dej?', "bot"), 2000);
-          lastYesOrNo()
+          lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
 
   // THIRD personalizeButton is "Ugglor" and will print out the following
@@ -127,7 +129,7 @@ const personalizeButtons = () => {
           showMessage(`Word! Värsta sortens!`, "bot")
           setTimeout(() => showMessage('Slipp se dom i dessa! <br><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/185533511-origpic-feb617_750x.jpg?v=1674466597"> ', 'bot'), 2000)
           setTimeout(() => showMessage('Bågarna heter <a href="https://lemmelkaffe.com/collections/tillbehor/products/lucifer">Lucifer</a>! Känns det bra?', 'bot'), 2000)
-          lastYesOrNo()
+          lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
 }
     
@@ -144,8 +146,8 @@ const lastYesOrNo = () => {inputArea.innerHTML =
         .addEventListener("click", () => {
           showMessage("Ja", "user");
           showMessage('Perfekt! Finns i shoppen. Ha en fin dag!', "bot");
-          jaButton.remove();
-          nejButton.remove();
+          jaButton.remove(); // Removes Ja-button
+          nejButton.remove(); // Removes Nej-button
         })
 
   //IF the user clicks the noButton the following message will be shown
@@ -155,19 +157,14 @@ const lastYesOrNo = () => {inputArea.innerHTML =
         .addEventListener("click", () => {
           showMessage("Nej", "user")
           showMessage(`Trisst, men här kommer nått som gör alla glada! <br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>`, "bot")
-          jaButton.remove();
-          nejButton.remove();
+          jaButton.remove(); // Removes Ja-button
+          nejButton.remove(); // Removes Nej-button
         }) 
 }
 }
 
-//-----------------------------------------------------------Chat Ends Here-------------------------------------------------------------------//
+//-----------------------------------------------------------Chat Conversation Ends Here-------------------------------------------------------------------//
 //-----------------------------------------------------------Eventlisteners-------------------------------------------------------------------//
 
-  // Set up your eventlisteners here
-
 form.addEventListener("submit", handleNameInput);
-
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
 
