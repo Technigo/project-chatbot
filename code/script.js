9cd32184e12a0bb7ba00c62b6b3e8f567e894254
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-02-09 22:42:30 +0100

    added inner html

--- code/script.js
+++ code/script.js
@@ -13,7 +13,7 @@ const botSay = (words) => {
 }
 
 const userSay = (words) => {
-  showMessage (words, "user")
+  showMessage(words, "user")
 }
 
 
@@ -31,7 +31,7 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-  
+
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
     console.log(message);
@@ -45,58 +45,76 @@ const showMessage = (message, sender) => {
     `
   }
 
-    // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-    chat.scrollTop = chat.scrollHeight;
-  }
+  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+  chat.scrollTop = chat.scrollHeight;
+}
 
 
-  // Starts here
-  const greetUser = () => {
-    // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-    botSay("Hello! What species do you believe yourself to be?", 'bot');
-    // Just to check it out, change 'bot' to 'user' here 👆
-  }
-  
-  const progressFromGuess = () => {
-    inputWrapper.innerHTML = "hOWDY";
+// Starts here
+const greetUser = () => {
+  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
+  botSay("Hello! What species do you believe yourself to be?", 'bot');
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
+
+
+const progressFromGuess = () => {
+  inputWrapper.innerHTML =
+    `<button id="btnBig">I'm a UNIT</button>
+     <button id="btnAverage">Come over here</button>
+     <button id="btnSmall">Tiny, yet brave</button>
+    `
   botSay(`Let's find out if ${guess} is true! How smol are you?`, "bot");
-  }
+}
 
-  //inputWrapper.innerHTML =
-  //<button id="btnBig">A Unit</button>
-  //<button id="btnAverage">Regular</button>
-  //<button id="btnSmall">Tiny, yet brave</button>
 
-  // Set up your eventlisteners here
+const progressFromSize = () => {
+  inputWrapper.innerHTML = "hOWDY";
+  console.log("howdy");
+  botSay(`new question`, "bot");
+}
 
-  let guess = "";
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log(event);
-    const userGuess = document.getElementById("nameInput").value;
-    console.log(userGuess);
-    guess = userGuess;
-    showMessage(`I would say ${guess}`, "user"); 
-  })
+// Set up your eventlisteners here
 
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log(event);
-    setTimeout(() => progressFromGuess(), 2000)
-  })
+let guess = "";
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(event);
+  const userGuess = document.getElementById("nameInput").value;
+  console.log(userGuess);
+  guess = userGuess;
+  showMessage(`I would say ${guess}`, "user");
+})
 
-  //button.onclick = progressFromGuess;p
-  ////let delay = 3000;
-  //setTimeout(progressFromGuess, 3000)
-  // When website loaded, chatbot asks first question.
-  // normally we would invoke a function like this:
-  // greeting()
-  // But if we want to add a little delay to it, we can wrap it in a setTimeout:
-  // setTimeout(functionName, timeToWaitInMilliSeconds)
-  // This means the greeting function will be called one second after the website is loaded.
-  setTimeout(greetUser, 1000);
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  nameInput.value = "";
+  console.log(event);
+  setTimeout(() => progressFromGuess(), 1500)
+})
 
- 
+
+document.getElementById("btnBig").addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log("hey");
+  // setTimeout (() => progressFromSize(), 1500)
+})
+
+//button.onclick = progressFromGuess;p
+////let delay = 3000;
+//setTimeout(progressFromGuess, 3000)
+// When website loaded, chatbot asks first question.
+// normally we would invoke a function like this:
+// greeting()
+// But if we want to add a little delay to it, we can wrap it in a setTimeout:
+// setTimeout(functionName, timeToWaitInMilliSeconds)
+// This means the greeting function will be called one second after the website is loaded.
+setTimeout(greetUser, 1000);
+
+
 
 // TO DO
-//Write inner.HTML for botmsgs. Wrap botmsgs in timer, or find other way to make them trigger at the right time. is it possible to put them in an array and referr to them in a trigger function?
+//Write inner.HTML for botmsgs.
+//How to stop the click from working after first click?
+//add eventlisteners to innerhtml doesn't work.
+//copy the structure with questionnumbers from the technigo code
