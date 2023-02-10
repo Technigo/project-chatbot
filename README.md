7af4e612421273489506a79973c1e94c6835aa02
Andreas Axelsson <pererikandreas@gmail.com>
2023-02-10 13:59:11 +0100

    Tried to add age question. Also made more comments in JS file to understand

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ The assignment was to create a chatbot with javascript, css and html.
 
 ## The problem
 
-I approached this task by start watching all attached videos about javascript. Then I coded along with Jennie creating the ground for the HTML and CSS and some of the Javascript. Attached in the folder provided in github for this task there was some prewritten java-code. I wrote down with comments what I thought should be done and started to look (google) for ways to do it. That was hard and felt that I needed a wholeistic view on how to complete the code so I looked at some of the previus technigo students code and tried out different functions they had been using.
+I approached this task by start watching all attached videos about javascript. Then I coded along with Jennie creating the ground for the HTML and CSS and some of the Javascript. Attached in the folder provided in github for this task there was some prewritten java-code. I wrote down with comments what I thought should be done and started to look (google) for ways to do it. That was hard and felt that I needed a wholeistic view on how to complete the code so I looked at some of the previus technigo students code and tried out different ways that they had been using.
+
+I decided to make my chat-bot to my friends coffee-company Lemmelkaffe. They have this backstory about how the animal lemmings collect the beans and go with the golfstream to the articcircle etc etc.. 
+
+Their webpage is on swedish so the conversation with the lemmings are to (sorry). (plan is to have a english version to).
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -3,9 +3,9 @@
   // Variables:
 
 const chat = document.getElementById("chat");
-const inputArea = document.getElementById('input-area')
+const inputArea = document.getElementById("input-area");
 const form = document.getElementById("name-form");
-const nameInput= document.getElementById("name-input");
+const nameInput = document.getElementById("name-input");
 
 //--------------------------------------------------User and bot chat-function is created----------------------------------------------------------//
 
@@ -36,6 +36,19 @@ else if (sender === "bot") {chat.innerHTML +=
 
 chat.scrollTop = chat.scrollHeight;
 };
+//--------------------------------------------------Chatsounds------------------------------------------------------------//
+
+  // Chatsoundfunctions created
+
+const bottrack = new Audio('./assets/chatsoundbot.mp3');
+const playsound = () => {
+      bottrack.play();
+};
+
+const usertrack = new Audio('./assets/chatsounduser.mp3');
+const makesound = () => {
+      usertrack.play();
+};
 
 //--------------------------------------------------User and bot chat-function ends------------------------------------------------------------//
 //----------------------------------------------------Chating Conversation starts--------------------------------------------------------------//
@@ -45,15 +58,7 @@ chat.scrollTop = chat.scrollHeight;
   // Arguments going in to the "machine"/function is msg="Vad heter du?" and sender="bot".
   // But first I declare the bottrack and the usertrack that start on all messages
 
-const bottrack = new Audio('./assets/chatsoundbot.mp3');
-const playsound = () => {
-      bottrack.play();
-}
-
-const usertrack = new Audio('./assets/chatsounduser.mp3');
-const makesound = () => {
-      usertrack.play();
-}
+  //-----FIRST QUESTION BOT
 
 const greetUser = () => { // Automatic greeting
       showMessage("Hej människa!", "bot"); // First bot-message
@@ -64,17 +69,24 @@ const greetUser = () => { // Automatic greeting
 setTimeout(greetUser, 2000); // Makes the first message from the bot appear in 2 sec after site is loaded
 
   // USER: types in name wich gets stored handleNameInput conected to the id=name-input in the HTML and the -form.addEventListener("submit", handleNameInput)-
+
+  //-----FIRST ANSWER USER
+
   // Now it will store the name as a value in a variable (nameInput.value) and you can acces it with ${nameInput.value} in the messages
+
 
 const handleNameInput = (event) => { // This function stores the event (user writing its name) as a value
       event.preventDefault() // Prevents autosubmittning the form
 
+  //-----SECOND QUESTION BOT
+
   // BOT: Answers two bubbles with the stored username${nameInput.value} and ends with showing the Ja/Nej-buttons
 
+
 const name = nameInput.value // Here we take the nameInput.value and delare it as the same as just "name"
-      showMessage(name, "user")
+      showMessage(name, "user") // "User and bot chat-function" created in the beginning of the document
       usertrack.play(); // This starts the usertrack(sound)
-      showMessage(`Hej ${nameInput.value}! Jag heter August Lemmel.`, "bot")
+      showMessage(`Hej ${nameInput.value}! Jag heter August Lemmel.`, "bot") // "User and bot chat-function" created in the beginning of the document
       setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 500 millisec
       setTimeout(() => showMessage("Har jag ditt förtroende?", "bot"), 1500);
       nameInput.value = '' // Cleares the nam input form. Its placed here after the value is used otherwise it clears it and it cant be used
@@ -84,10 +96,12 @@ const name = nameInput.value // Here we take the nameInput.value and delare it a
 
   // IF the user presses the jaButton the following message will be print and the 3 personalizeButtons will be shown.
 
+  //-----SECOND ANSWER USER "JA"
+
       document
         .getElementById("jaButton")
         .addEventListener("click", () => { // The following happens when the button is clicked
-          showMessage("Ja", "user");
+          showMessage("Ja", "user"); // "User and bot chat-function" created in the beginning of the document
           usertrack.play(); // This starts the usertrack(sound)
           setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 500 millisec
           showMessage("Tack! Jag vill tipsa dej om nått passande från vår shop.", "bot");
@@ -97,16 +111,21 @@ const name = nameInput.value // Here we take the nameInput.value and delare it a
 
   // IF the user presses the nejButton the following message will be shown and the chat will end. No more questions.
 
+  //-----SECOND ANSWER USER "NEJ"
+
       document
         .getElementById("nejButton")
         .addEventListener("click", () => { // The following happens when the button is clicked
-          showMessage("Nej", "user")
+          showMessage("Nej", "user") // "User and bot chat-function" created in the beginning of the document
           usertrack.play(); // This starts the usertrack(sound)
           setTimeout(() => bottrack.play(), 500); // This starts the bottrack(sound) and delays it with 500 millisec
           showMessage('Synd.. Men du kolla in denna! Ha en fin dag!<br><p><iframe width="100%" height="80px" src="https://www.youtube.com/embed/OhO08XZASB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>', "bot")
           jaButton.remove(); // Removes Ja-button
           nejButton.remove(); // Removes Nej-button
         }) 
+};
+
+  //-----THIRD QUESTION BOT
 
   // Here is the function created and the connection to the 3 personalizing costumer-question alternatives.
 
@@ -116,12 +135,12 @@ const personalizeButtons = () => {
        <button id="snabbkaffe" type="submit">Snabbkaffe</button>
        <button id="ugglor" type="submit">Ugglor</button>`
 
-  // FIRST personalizeButton is "Baristas" and will print out the following
+  //-----THIRD ANSWER USER 1 "Baristas"
 
       document
         .getElementById("baristas") // 1st button
         .addEventListener("click", () => { // The following happens when the button is clicked
-          showMessage("Baristas", "user")
+          showMessage("Baristas", "user") // "User and bot chat-function" created in the beginning of the document
           usertrack.play(); // This starts the usertrack(sound)
           setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 1000 millisec
           showMessage(`Fattar helt! Kolla in den här pannan <br><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/186885537-origpic-b912cf_750x.jpg?v=1664429478">`, "bot");
@@ -129,12 +148,12 @@ const personalizeButtons = () => {
           lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
 
-  // SECOND personalizeButton is "Snabbkaffe" and will print out the following
+  //-----THIRD ANSWER USER 2"Snabbkaffe"
 
       document
         .getElementById("snabbkaffe") // 2th button
         .addEventListener("click", () => { // The following happens when the button is clicked
-          showMessage("Snabbkaffe", "user")
+          showMessage("Snabbkaffe", "user") // "User and bot chat-function" created in the beginning of the document
           usertrack.play(); // This starts the usertrack(sound)
           setTimeout(() => bottrack.play(), 1000); // This starts the bottrack(sound) and delays it with 1000 millisec
           showMessage(`Ja huvva! Kolla in denna! <br><p><img src="https://cdn.shopify.com/s/files/1/0556/5690/6841/products/HOST22AKKA_750x.jpg?v=1664298939">`, "bot")
@@ -142,7 +161,7 @@ const personalizeButtons = () => {
           lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
 
-  // THIRD personalizeButton is "Ugglor" and will print out the following
+  //-----THIRD ANSWER USER "Snabbkaffe"
 
       document
         .getElementById("ugglor") // 3rd button
@@ -155,15 +174,18 @@ const personalizeButtons = () => {
           setTimeout(() => showMessage('Bågarna heter <a href="https://lemmelkaffe.com/collections/tillbehor/products/lucifer">Lucifer</a>! Känns det bra?', 'bot'), 2000)
           lastYesOrNo() // Starts the last Ja/nej-answer-button 
         })
-}
+};
     
   // Here is the last Ja/Nej-button-alternatives for the user to klick.
 
-const lastYesOrNo = () => {inputArea.innerHTML = 
+    //-----FOURTH QUESTION BOT
+
+const lastYesOrNo = () => {
+      inputArea.innerHTML = 
         `<button id="jaButton" type="submit">Ja</button>
         <button id="nejButton" type="submit">Nej</button>`
 
-  // IF the user clicks the jaButton the following message will be shown
+  //-----FOURTH ANSWER USER JA
 
       document
         .getElementById("jaButton")
@@ -176,7 +198,7 @@ const lastYesOrNo = () => {inputArea.innerHTML =
           nejButton.remove(); // Removes Nej-button
         })
 
-  // IF the user clicks the noButton the following message will be shown
+  //-----FOURTH ANSWER USER NEJ
 
       document
         .getElementById("nejButton")
@@ -188,11 +210,11 @@ const lastYesOrNo = () => {inputArea.innerHTML =
           jaButton.remove(); // Removes Ja-button
           nejButton.remove(); // Removes Nej-button
         }) 
-}
-}
+};
 
 //-----------------------------------------------------------Chat Conversation Ends Here-------------------------------------------------------------------//
-//-----------------------------------------------------------Eventlisteners-------------------------------------------------------------------//
+//-----------------------------------------------------------------Eventlisteners--------------------------------------------------------------------------//
 
-form.addEventListener("submit", handleNameInput);
+  // Here we tell the eventlistener where to conect in the HTML and in what ways the user can interact. Submitt (SkickaButton and name-input).
 
+form.addEventListener("submit", handleNameInput); 
