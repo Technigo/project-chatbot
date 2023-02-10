b86c1e8b7f4b52bfe2f020ff15516a0016a3af09
Johanna Leonsson <johanna@leonsson.se>
2023-02-10 10:33:04 +0100

    Iteration 3

--- code/script.js
+++ code/script.js
@@ -1,8 +1,11 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat');
+const input = document.getElementById('name-input') //trying to figure this out
+const form = document.getElementById('name-form')
+const submit = document.getElementById('submit')
+const inputWrapper = document.getElementById('input-wrapper')
 
 // If you need any global variables that you can use across different functions, declare them here:
-
 
 // Declare your functions after this comment
 
@@ -38,14 +41,25 @@ const showMessage = (message, sender) => {
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hi pretty, what's your name hun?", 'bot');
+  showMessage("Hi pretty, what's your name?", 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
-  
+}
+
+const handleInput = (event) => { // when submiting this will be used
+  event.preventDefault() // prevents chat from refreshing when submitting
+  const message = input.value // input value shall be stored in the const name 
+  showMessage(`${message}`, 'user') // users answer 
+  input.value = ''
+  setTimeout(() => question1(message), 500)
+}
+
+const question1 = (message) => {
+  showMessage(`Nice to meet you ${message}! How are you feeling today?`, 'bot')
 }
 
 // Set up your eventlisteners here
 
-
+form.addEventListener('submit', handleInput)
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -54,3 +68,5 @@ const greetUser = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 500);
+
+
