6751b9a5993dde045dc63aa3058f903e1ea8ae2b
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-02-10 13:13:10 +0100

    Made another zodiacform and put it in inner-html

--- code/script.js
+++ code/script.js
@@ -2,8 +2,13 @@
 const chat = document.getElementById("chat");
 const name = document.getElementById("name-input");
 const sendButton = document.getElementById("send");
+const inputWrapper = document.getElementById("input-wrapper");
 
 // If you need any global variables that you can use across different functions, declare them here:
+
+let questionNumber = 1;
+
+let userName = "";
 
 // Declare your functions after this comment
 
@@ -34,6 +39,7 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight;
 };
 
+//const nextQuestion = (message) => {};
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
@@ -41,20 +47,52 @@ const greetUser = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 };
 
+// Store the value in a variable so I can access it after we
+
 // Initial button click, here I should get the name entered
 sendButton.addEventListener("click", (event) => {
   event.preventDefault();
-
-  // Store the value in a variable so I can access it after we
-  // clear it from the input
-  let userName = name.value;
+  userName = name.value;
   showMessage(`${userName}`, "user");
-
-  // Clears the input field
-  name.value = "";
-  //Here I call the function where I present the dishes to choose from. I will also pass the userName
-  setTimeout(() => showFoodTypes(userName), 1000);
+  name.value = ""; // clear it from the input
+  setTimeout(() => askZodiacSigns(), 1000); //Here I call the function where I present the dishes to choose from. I will also pass the userName
 });
+
+const askZodiacSigns = () => {
+  console.log("this works");
+  showMessage(
+    `Welcome ${userName} happy youre here! What's your zodiac sign?`,
+    "bot"
+  );
+
+  inputWrapper.innerHTML = `
+ <form id="zodiac-form">
+          <label for="zodiac-form">Zodiac</label>
+          <input id="zodiac-input" type="text" />
+          <button class="send-btn" id="send" type="submit">Send</button>
+        </form>
+   `;
+};
+
+//
+//
+//  //declare variable using "input-wrapper" from HTML - this can go at the top of the page
+//  //Then write the code to change the HTML in Javascript
+
+//   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
+
+// sendButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   // Store the value in a variable so I can access it after we
+//   // clear it from the input
+//   let answer = name.value;
+//   showMessage(`${answer}`, "user");
+
+//   // Clears the input field
+//   name.value = "";
+//   //Here I call the function where I present the dishes to choose from. I will also pass the userName
+// });
 
 // Set up your eventlisteners here
 
