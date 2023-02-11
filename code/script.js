45bdd998dc19d0fa091d9e78993e9583d2304092
AjmalZ <ajmal.zahir@hotmail.com>
2023-02-11 19:07:44 +0100

    Added a video background and some styling

--- code/script.js
+++ code/script.js
@@ -3,6 +3,7 @@ const chat = document.getElementById('chat');
 const form = document.getElementById('name-form')
 const inputSection = document.getElementById('input-wrapper');
 
+
 // This is the global variables
 let myBurger = ""
 let myDrink = ""
@@ -139,10 +140,10 @@ const sendMessage = (event) => {
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
     console.log(`User is sending a message. The message is: ${message}`)
-    chat.innerHTML += `
+    chat.innerHTML += 
+    `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
@@ -150,22 +151,21 @@ const showMessage = (message, sender) => {
         <img src="userAvatar.gif" alt="User" />  
       </section>
     `
-    // The else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
     console.log(`Bot is sending a message. The message is: ${message}`)
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="chatbotAvatar.gif" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
+    setTimeout(() => {
+      chat.innerHTML += `
+        <section class="bot-msg">
+          <img src="chatbotAvatar.gif" alt="Bot" />
+          <div class="bubble bot-bubble">
+            <p>${message}</p>
+          </div>
+        </section>
+      `
+      chat.scrollTop = chat.scrollHeight;
+    }, 400);
   }
-  // The scrollTop property sets or returns the number of pixels an element's content is scrolled vertically. It makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
 }
-
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
@@ -180,8 +180,9 @@ form.addEventListener('submit', sendMessage);
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
-// greeting()
+greetUser()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 500);
+//setTimeout(greetUser, 500);
+
