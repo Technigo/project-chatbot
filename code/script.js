93f98f76c29e00e7418e45d6f4f14e624f9b7c08
Hannah Ek <hannahek@Hannahs-MacBook-Pro.local>
2023-02-11 15:29:19 +0100

    iteration 3

--- code/script.js
+++ code/script.js
@@ -1,58 +1,128 @@
-// Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
-
+// All the DOM selectors stored as short variables
+const chat = document.getElementById("chat");
+const formWrapper = document.getElementById('formWrapper');
+const form = document.getElementById('form');
 const messageSound = new Audio ("assets/pop.mp3");
-// If you need any global variables that you can use across different functions, declare them here:
+const timeGreetings = [`Good morning my friend! What's your name?`, `Hello my friend! What's your name?`, `Good evening my friend! What's your name?`, `I am sorry my friend, but we are closed!`];
+
+// Variable for date & time.
+
+const date = new Date();
+console.log(date);
+const time = date.getHours();
+console.log(time);
 
 
-// Declare your functions after this comment
+const greetUser = (timeGreetings, time) => {
+  
+if (time >= 5 && time <= 10) {
+    showMessage(timeGreetings[0], "bot");           //Morning (5 to 10)
+} else if (time > 10 && time <= 18) {
+        showMessage(timeGreetings[1], "bot");       //Hello (11 to 18)
+} else if (time > 18 && time <= 22) {
+    showMessage(timeGreetings[2], "bot");           //Evening (19 to 22)
+} else {
+    showMessage(timeGreetings[3], "bot");           //Late (23 to 4)
+    showMessage("Anyways, since you're up. What's your name?", "bot");
+}
+};
 
-// This function will add a chat bubble in the cor srect place based on who the sender is
-const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
-  if (sender === 'user') {
-    console.log("User is writing")
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
-    console.log("Bot is writing")
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
-  }
+//1 second delay for greeting
+setTimeout(greetUser, 1000, timeGreetings, time);
 
-    //makes sound when sending a message 
+
+//--This is the general way of adding a chat bubble from both the user and the bot. Create more detailed ones for what we want to do later.
+const showMessage = (msg, sender) => {
+
+if (sender === 'user') {
+    chat.innerHTML += `<section class="user-msg">
+    <div class="bubble user-bubble">
+        <p>${msg}</p>
+    </div>
+    <img src="./user.png" alt="chat bot" />
+</section>`;
+} 
+else if (sender === 'bot') {
+    chat.innerHTML += `<section class="bot-msg">
+    <img src="./bot.png" alt="user bot" />
+    <div class="bubble bot-bubble">
+        <p>${msg}</p>
+    </div>
+</section>`;
+}
+
+  
+   //Makes sound when sending a message 
   messageSound.currentTime = 0
   messageSound.play()
+  
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
+};
+    
+//Eventlistener: Starts the function when user clicks submit. 
+form.addEventListener('submit', (event) => {
+  event.preventDefault()   //Stops it from autosaving.
+  
+  const firstName = document.getElementById('firstName').value;
+  console.log(firstName)           
+  
+// Calling the function "showMessage".
+  showMessage(firstName, 'user');    
+
+   //This code slows it with 1 sec.
+  setTimeout(() => recognize(firstName), 1000)  
+  
+})
+
+
+//Question 2
+
+const recognize = (firstName) => {
+  console.log(firstName);
+
+  chat.innerHTML += `<section class="bot-msg">
+    <img src="./bot.png" alt="user bot" />
+    <div class="bubble bot-bubble">
+        <p>Which book would you like ${firstName}?</p>
+    </div>
+</section>`;
+
+        console.log(formWrapper);
+    formWrapper.innerHTML = `<button id="yogaBtn">How to Yoga book</button>
+    <button id="cookBtn">How to cook book</button>
+    <button id="englishBtn">Learn English book</button>`;
+  
+  
+  let yogaBtn = document.getElementById('yogaBtn');
+ yogaBtn.addEventListener('click',() => choiceBook)
+  
+  let cookBtn = document.getElement('cookBtn');
+ cookBtn.addEventListenerById('click',() => choiceBook)
+  
+  let englishBtn = document.getElement('englishBtn');
+   englishBtn.addEventListenerById('click',() => choiceBook)
 }
 
-// Starts here
-const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello you! How can I help you?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
+/*
+   setTimeout(() => choiceBook(firstName), 1000) 
+}*/
 
-// Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
+
+// Question number 3
+
+const choiceBook = (choiceBook) => {
+    showMessage(`Would you like bound or pocket book, ${firstName}?`, 'bot');
+
+  formWrapper.innerHTML =
+        `<button id="boundBtn" type="submit" value="Bound book">Bound Book</button>
+      <button id="pocketBtn" type="submit" value="Pocket book">Pocket Book</button>`;
+     
+    
+  let boundBtn = document.getElementById('boundBtn');
+   boundBtn.addEventListener('click', )
+  
+  let pocketBtn = document.getElementById('pocketBtn');
+   pocketBtn.addEventListener('click', )
+}  
