50d239303317c50a037804fa7454de5084fce033
Yu Miao <yu_miao@yahoo.com>
2023-02-11 22:43:55 +0100

    ifelse

--- code/script.js
+++ code/script.js
@@ -11,7 +11,7 @@ const submitButton = document.getElementById("send-btn");
 
 // Declare your functions after this comment
 const handleNameInput = (event) => {
-
+  
   event.preventDefault();
   // Store the value in a variable so we can access it after we 
   //   clear it from the input
@@ -19,54 +19,75 @@ const handleNameInput = (event) => {
   showMessage(name, 'user');
   nameInput.value = "";
 
+  // creat if...else to practice
+  let botsay="";
+  if(name === "Mr.P"){
+    
+    botsay = `Hi! ${name}! Nice to meet you, Do you have a reservation!`;
+  
+  }else if(name === "Zlatan"){
+
+    botsay = `Hi ${name}! Nice to meet you, Do you play basketball?`;
+   
+  }else{
+
+    botsay = `Hi ${name}!Nice to meet you, do you have a room card?`;
+   
+  };
+  setTimeout(() => showMessage(botsay,"bot"), 1000);
+
   // After 1 second, show the next question by invoking the next function.
   // passing the name into it to have access to the user's name if we want
   // to use it in the next question from the bot.
-  setTimeout(() =>showBookingOptions(name), 1000);
+  setTimeout(() => showBookingOptions(name), 1000);
 
 }
 // Question 2 from bot, and the user will get option to choose.
 
-const showBookingOptions = () =>{
-  showMessage(`Nice to meet you , do you have booking number?`,'bot');
+const showBookingOptions = () => {
   inputwrapper.innerHTML =
-  `<button id="yes" class="yes"> YES </button>
+    `<button id="yes" class="yes"> YES </button>
    <button id="no" class="no"> NO </button>`;
-  document.getElementById("yes").addEventListener("click", yesbutton); 
-   document.getElementById("no").addEventListener("click",nobutton);
+
+    document.getElementById("yes").addEventListener("click", yesbutton);
+    document.getElementById("no").addEventListener("click", nobutton);
 }
+
 // option answer from user
 const yesbutton = (event) => {
   showMessage("Yes, I do!",'user');
-  showMessage("Here is your room card, See you next time!",'bot');
+  setTimeout(() =>showMessage("Here is your room card, See you next time!",'bot'), 1000); 
 }
 const nobutton = (event) => {
   showMessage("No, I don't!",'user');
   setTimeout(() =>idOption(), 1000);  
 }
 
+
 // question 3 from bot.
-const idOption = () =>{
-  showMessage("Please choose your valid identity document!",'bot');
+const idOption = () => {
+  showMessage("Please choose your valid identity document!", 'bot');
   inputwrapper.innerHTML =
-  `
+    `
   <form>
     <select class="drop-menu" name="drop-menu" id="drop-menu">
       <option value="id">Id Number</option>
       <option value="passport">Passport Number</option>
       <option value="others">Others</option>
     </select> 
-    <button class="doc-btn" id="doc-btn" type="submit">submit</button>
+    <button class="doc-btn" id="doc-btn">submit</button>
   </form>
   `;
   const docButton = document.getElementById("doc-btn");
-  docButton.addEventListener("click",docSubmitButton);
+  docButton.addEventListener("click", docSubmitButton);
 }
 
-// answer from uers to choose from drop menu
-const docSubmitButton = (event) =>{
-  showMessage("Thank you for your booking! GoodBye","bot");
-  
+// answer from uers to choose dropdown menu and submit it.
+const docSubmitButton = (event) => {
+ 
+  showMessage("Thank you for your booking! GoodBye!", "bot");
+  event.preventDefault();
+
 }
 
 
@@ -106,9 +127,7 @@ const greetUser = () => {
 }
 
 // Set up your eventlisteners here 
-submitButton.addEventListener("click",handleNameInput);
-
-
+submitButton.addEventListener("click", handleNameInput);
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
