a801ff9f05677a9735c0472653d4b35386e4ae50
khleoth <leothunell@gmail.com>
2023-02-11 08:02:31 +0100

    removed comments, added new

--- code/script.js
+++ code/script.js
@@ -1,133 +1,10 @@
-/* // Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
-
-// If you need any global variables that you can use across different functions, declare them here:
-const nameInput = document.getElementById('name-input')
-const form = document.getElementById('name-form')
-const submit = document.getElementById('submit')
-const main = document.getElementById('main')
-const inputWrapper = document.getElementById('input-wrapper')
-
-// Declare your functions after this comment
-
-// This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
-  if (sender === 'user') {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/th.jfif" alt="th" />  
-      </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
-    console.log('Be gone!')
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/zoltark.jpg" alt="Zoltark" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
-}
-
-
-// Starts here
-const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Greetings child, tell me your name", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
-
-// Set up your eventlisteners here
-const handleNameInput = (event) => {
-  event.preventDefault()
-  // Store the value in a variable so we can access it after we 
-	// clear it from the input
-  const name = nameInput.value
-  console.log(name)
-
-  showMessage(`${name} , oh great Zoltark`, 'user')
-  nameInput.value = ''
-
-  // After 1 second, show the next question by invoking the next function.
-	// passing the name into it to have access to the user's name if we want
-	// to use it in the next question from the bot.
-  setTimeout(() => fortuneYesNo(name), 1000)
-}
-
-const fortuneYesNo = (name) => { 
-  showMessage(`Well met, ${name}! Will you tell me of your deepset desires?`, 'bot') //sends second message from bot
-  
-  
-  inputWrapper.innerHTML = // add Yes No button
-    `<button id="yesBtn" type="submit">Yes</button>
-    <button id="noBtn" type="submit">No</button>`
-
-
-  const yesBtn = document.GetElementById('yesBtn');
-  const noBtn = document.GetElementById('noBtn');
-
-  document
-    .getElementById('yesBtn')
-    .addEventListener('click', () => {
-      showMessage('Yes, tell me of what is yet to come!', 'user')
-      setTimeout(showMessage('So be it, choose wisely', 'bot'), 1000)
-      fortuneOptions('yes')
-      
-    })
-
-  document
-    .getElementById('noBtn')
-    .addEventListener('click', () => {
-      showMessage('No thank you, goodbye good sir!', 'user')
-      fortuneOptions('no')
-    }) 
-  }
-
-  const fortuneOptions = (option) => { 
-      if (option === 'yes') {
-      inputWrapper.innerHTML = 
-      `<select id="select">
-          <option value="" selected disabled>👇 Tell me about...</option>
-          <option value="myFuture">my future</option>
-          <option value="myFamily">the fate of my family</option>
-          <option value="getBig">..I just want to be big</option>
-      </select>`
-        
-        /* setTimeout(() => complimentOptions(name), 1000) */
-    
-      /* } else {
-        setTimeout(showMessage('Very well, until we meet again...', 'bot'), 3000)//sends second message from bot
-        inputWrapper.innerHTML = // add HTML for no 
-        `Here you put your new HTML for no`
-      }  
-  } */
-
-  
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-//setTimeout(greetUser, 800);
-
-//form.addEventListener('submit', handleNameInput)
-
 // All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const inputWrapper = document.getElementById('input-wrapper')
 const input = document.getElementById('input')
 const sendBtn = document.getElementById('send')
 
+// sound files added for future reference
 const bottrack = new Audio('./assets/zoltartrack.mp3')
 const zoltarSound = () => {
       bottrack.play()
@@ -143,6 +20,7 @@ const introSound = () => {
 
 let questionNumber = 1
 
+//Bot/user reply with sounds shortened function
 const botReply = (msg) => {
   showMessage(msg, 'bot')
   zoltarSound()
@@ -177,6 +55,7 @@ const showMessage = (message, sender) => {
 }
 
 
+// function to swap from one question to the next
 const nextQuestion = (message) => {
   console.log('questionNumber', questionNumber)
 
@@ -209,6 +88,7 @@ const greeting = () => {
 const fortuneYesNo = (msg) => {
   questionNumber++
   botReply(`Well met, ${msg}! Will you tell me of your deepset desires?`)
+  // had to insert introsound here due to browsers blocking .play on page load without user interaction
   throatsing.play()
 
   inputWrapper.innerHTML = `
@@ -306,40 +186,6 @@ const thankYou = (amount) => {
     .getElementById("leave")
     .addEventListener("click", () => setTimeout(location.reload(), 3000));
 };
-
-/* const showPrice = () => {
-  questionNumber++
-
-  setTimeout(botReply('I hope my guidance will find you peace of mind. With that said, this is no charity.'), 1000)
-
-  inputWrapper.innerHTML = `
-  <input value="input" type="number"/>
-  <button id="pay" class="send-btn">$ pay up</button>
-  `
-  const money = document.getElementById("input");
-  document.getElementById("pay").addEventListener("click", () => {
-    nextQuestion(money.value);
-  });
-} 
-
-
-
-const thankYou = (amount) => {
-
-if (amount > 20) {
-  botReply(`*Takes his money, closes his eyes and sits in silence*`)
-} else {
-  botReply(`Keep you money, such an inadequate sum is not worthy of my attention`)
-}
-
- 
-  inputWrapper.innerHTML = `
-  <button id="leave">Thank you, oh great Zoltar</button>`
-
-  document
-    .getElementById('leave')
-    .addEventListener('click', () => setTimeout(location.reload(), 3000))
-} */
 
 sendBtn.addEventListener('click', () => nextQuestion(input.value))
 input.addEventListener('keypress', (event) => {
