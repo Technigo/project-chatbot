2b4b405f962a887790d09ed2d5eb1f2eee2ace4e
Emma Holtegaard <emmaholtegaard@gmail.com>
2023-02-11 15:40:07 +0100

    Added a bunch of interactions with different types of inputs(buttons and numbers).

--- code/script.js
+++ code/script.js
@@ -48,7 +48,7 @@ const greetUser = () => {
 
 setTimeout(greetUser, 1000);
 
-//This function listens to input and deals with the name-input
+//This function listens to name-input and deals with the name-input
 sendBtn.addEventListener('click', (event) => {
   event.preventDefault()
   console.log('User has types their name');
@@ -63,7 +63,7 @@ sendBtn.addEventListener('click', (event) => {
 
 //This function asks a new qustion with 3 possible answers.
 const howAreYouFeeling = (name) => {
-  showMessage(`Happy to have you here, ${name}! Are you feeling anxious or stressed today?`, 'bot')
+  showMessage(`Good to have you here, ${name}! Are you feeling anxious or stressed today?`, 'bot')
   console.log('Bot asks about mood')
   inputWrapper.innerHTML = `
       <div class="input-wrapper" id="input-wrapper">
@@ -77,32 +77,167 @@ const howAreYouFeeling = (name) => {
           <button id="calm" class="mood-btn" type="submit">
             No, I'm calm.
           </button>
+        </form>
+      </div>
       `
-  
+  //eventListeners for the 3 buttoms, each with a call to the next function
   document
     .getElementById('little-stressed')
     .addEventListener('click', (event) => {
       event.preventDefault()
       showMessage('Yes, a little.', 'user')
-      setTimeout(sightQuestion, 1500)
+      setTimeout(() => startQuestion(), 1500)
   })
   document
     .getElementById('very-stressed')
     .addEventListener('click', (event) => {
       event.preventDefault()
       showMessage('Yes, very much.', 'user')
-      setTimeout(sightQuestion, 1500)
-    })
+      setTimeout(() => startQuestion(), 1500)
+  })
   document
     .getElementById('calm')
     .addEventListener('click', (event) => {
       event.preventDefault()
       showMessage(`No, I'm calm`, 'user')
-      setTimeout(sightQuestion, 1500)
-    })
+      setTimeout(() => startQuestion(calm), 1500);
+   })
      } 
 
-const sightQuestion = (mood) => {
-  showMessage('testy test', 'bot')
+
+//A function for question that starts the exercise by asking for a colour.
+const startQuestion = (mood) => {
+  if (mood === calm) {
+    showMessage(`Happy to hear that! Let's try some grounding techniques anyways.`, 'bot')
+    setTimeout(() => showMessage("First, choose a colour", "bot"), 1000);
+    inputWrapper.innerHTML =
+    `<div class="input wrapper" id="input-wrapper">
+      <form id="name-form">
+        <button id="green-btn" class="green-btn" type="submit">Green</button>
+        <button id="blue-btn" class="blue-btn" type="submit">Blue</button>
+        <button id="red-btn" class="red-btn" type="submit">Red</button>
+      </form>
+     </div>
+    `
+  } else {
+    showMessage(`Sorry to hear that! Let's try some grounding techniques together.`, 'bot')
+    setTimeout(() => showMessage("First, choose a colour", "bot"), 1000);
+    inputWrapper.innerHTML =
+    `<div class="input wrapper" id="input-wrapper">
+      <form id="name-form">
+        <button id="green-btn" class="green-btn" type="submit">Green</button>
+        <button id="blue-btn" class="blue-btn" type="submit">Blue</button>
+        <button id="red-btn" class="red-btn" type="submit">Red</button>
+      </form>
+     </div>
+    `
+  }
+
+  document
+    .getElementById('green-btn')
+    .addEventListener('click', (event) => {
+      event.preventDefault()
+      showMessage("Green", "user");
+      setTimeout(() => sightQuestion("green"), 1500);
+    })
+
+  document
+    .getElementById('blue-btn')
+    .addEventListener('click', (event) => {
+      event.preventDefault()
+      showMessage("Blue", "user")
+      setTimeout(() => sightQuestion("blue"), 1500);
+    })
+
+  document
+    .getElementById('red-btn')
+    .addEventListener('click', (event) => {
+      event.preventDefault()
+      showMessage("Red", "user")
+      setTimeout(() => sightQuestion("red"), 1500);
+    }) 
 }
 
+//A function for question that activates SIGHT
+const sightQuestion = (color) => {
+  showMessage(`Good choice! Look around you. How many ${color} things can you see?`, "bot");
+  inputWrapper.innerHTML = `      
+    <div class="input-wrapper" id="input-wrapper">
+      <form id="number-form">
+        <input id="number-input" type="number"/>
+      <button id="sight-btn" class="send-btn" type="submit">
+        Send
+      </button>
+    </form>
+  </div>
+  `  
+  const numberInput = document.getElementById("number-input")
+
+  //event listener for number input
+  document
+  .getElementById("sight-btn")
+  .addEventListener('click', (event) => {
+    event.preventDefault()
+    const number = numberInput.value
+    showMessage(`${number}`, "user")
+    numberInput.value = ""
+    setTimeout(() => hearingQuestion(`${number}`, `${color}`), 1500)
+  })
+  }
+  
+//A function for question that activates hearing
+const hearingQuestion = (numberOfSights, color) => {
+  if (numberOfSights == 0) {
+    showMessage(`That's okay. Maybe you can find some sounds instead. Take a moment to listen. How many different sounds do you hear right now?`, "bot")
+  } 
+  else if (numberOfSights > 3) {
+    showMessage(`${numberOfSights}? That's is a lot of ${color} things!`, "bot")
+    setTimeout(() => showMessage("Now, take a moment to listen. How many different sounds do you hear?", "bot"), 900)
+  }
+  else {
+    showMessage(`Good. Now, take a moment to listen. How many different sounds do you hear?`, "bot")
+  }
+  inputWrapper.innerHTML = `
+   <div class="input-wrapper" id="input-wrapper">
+    <form id="number-form">
+      <input id="number-input" type="number"/>
+    <button id="smell-btn" class="send-btn" type="submit">
+     Send
+    </button>
+    </form>
+   </div>
+  `
+
+  //eventlistener that deals with smell input
+  const numberInput = document.getElementById("number-input")
+  document
+  .getElementById("smell-btn")
+  .addEventListener('click', (event) => {
+    event.preventDefault()
+    const number = numberInput.value
+    showMessage(`${number}`, "user")
+    numberInput.value = ""
+    setTimeout(() => smellQuestion(`${number}`), 1500)
+  })
+}
+
+//a function that reacts to sound question, and asks question that actives smell, and leads onto 
+const smellQuestion = (numberOfSounds) => {
+  showMessage(`${numberOfSounds}!!! Test`, "bot")
+  //if there are 0 sounds, say nice and quiet, huh?
+
+  //Otherwise, "awesome, your ears are sharp!"
+
+  //new message: Now, sniff the air. WHat smell do you notice most?
+
+  //New HTML: text input for most noticable smell.
+
+}
+
+
+//A function that reacts to smell question, and calls question that encourages self-care
+//Bot: "Time to take big sniff of that INSERT SMELL". Take 3 deep breaths.
+//Bot: Delayed message: "Before you leave, choose what you need most"
+//Drop down menu: rest, food and water, fun, fresh air.
+
+//Response: Alright! Make "INSERT THINGS" a priority today.
