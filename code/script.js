19333c52bb99e4b77d2454951d683eddbda73045
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-02-12 15:03:08 +0100

    new structure with all questions

--- code/script.js
+++ code/script.js
@@ -1,141 +1,228 @@
-// Variables that point to selected DOM elements
+//variables
 const chat = document.getElementById('chat');
 const sendBtn = document.getElementById('send');
 const inputWrapper = document.getElementById("input-wrapper");
-const nameForm = document.getElementById("name-form");
+const form = document.getElementById("form");
+const body = document.getElementById("body");
 
-
-// If you need any global variables that you can use across different functions, declare them here:
+//lets
+let QuestionNumber = 0;
 let score = 0;
-let questionNumber = 1;
+let input = document.getElementById("input");
 
-
-/* // Declare your functions after this comment
-const userReply = (message) => {
-  showMessage(message, 'user')
-};
-const bot = (message) => {
-  showMessage(message, 'bot')
-}; */
-
-// This function will add a chat bubble in the correct place based on who the sender is
+//Objects
+const botAnswers = {
+    greeting:  "Greetings stranger! What's your name?",
+    wannaPlay: `Nice to meet you ${name}! Do you wanna play a little quiz?`,
+    question1: "Awesome! Let's start with an easy one: What's the name of the Swedish Prime Minister?",
+    question2: "Interesting choice... Next question: What is the population size of Sweden?",
+    question3: "What's the name of the highest mountain in Sweden?",
+    question4: "What's the fourth largest city in Sweden?",
+    question5: "What's the name of the Swedish king?",
+    scoreReport: "Thank you for playing. This is your score...",
+    };
+  
+//message-function
 const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
-  if (sender === 'user') {
-    console.log("Message from user");
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `} else if (sender === 'bot') {
-    console.log("Message from bot");
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p id="message">${message}</p>
-        </div>
-      </section>
-    `
+    if (sender === 'user') {
+      chat.innerHTML += `
+        <section class="user-msg">
+          <div class="bubble user-bubble">
+            <p>${message}</p>
+          </div>
+          <img src="assets/user.png" alt="User" />  
+        </section>
+      `} else if (sender === 'bot') {
+      chat.innerHTML += `
+        <section class="bot-msg">
+          <img src="assets/bot.png" alt="Bot" />
+          <div class="bubble bot-bubble">
+            <p id="message">${message}</p>
+          </div>
+        </section>
+      `
+    }
+    // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+    chat.scrollTop = chat.scrollHeight;
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight;
-}
 
-// Starts here
-const greetUser = () => {
-  questionNumber = 1;
-  showMessage("Greetings stranger! What's your name?", 'bot');
-};
-setTimeout(greetUser, 1000);
+ 
+
+//prevent form from reloading page
 
 
-//user answer: name
-const userAnswer = (event) => {
-  event.preventDefault()
-  // Store the value in a variable so we can access it after we 
-	// clear it from the input
-  const name = userInput.value
-  showMessage(name, 'user')
-  userInput.value = null;
-  setTimeout(() => niceToMeetYou(name), 1000)
-};
 
-//launch userAnswer on submit-button
-sendBtn.addEventListener("click", userAnswer);
+//question flow based on questionNumber
+const questionOrder = (message) => {
+    console.log('questionNumber', questionNumber);
+    if (questionNumber === 0) {
+      let name = input.value
+      showMessage(name, "user");
+     input.value = "";
+      setTimeout(() => niceToMeetYou(name), 1000)
+    } else if (questionNumber === 1) {
+      setTimeout(() => q1(message), 1000)
+    } else if (questionNumber === 2) {
+      setTimeout(() => q2(message), 1000)
+    } else if (questionNumber === 3) {
+      setTimeout(() => q3(message), 1000)
+    } else if (questionNumber === 4){
+      setTimeout(q4, 1000)
+    } else if (questionNumber === 5){
+        setTimeout(q5, 1000)
+    } else if (questionNumber === 6){
+        setTimeout(scoreBoard, 1000)}
+    };
 
 
-const niceToMeetYou = (name) => {
-  //launch new question
-  showMessage(`Nice to meet you ${name}! Do you wanna play a little quiz?`, 'bot');
-  chat.innerHTML +=
+  //write together? with b
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+});
+//starting messsage
+  const greetUser = () => {
+    questionNumber = 0;
+    showMessage( botAnswers.greeting, 'bot');
+  };
+  setTimeout(() => {
+    greetUser()
+  }, 1000);
+ 
+ //b
+ form.addEventListener("submit", questionOrder);
+
+
+ //Nice to meet you!
+ const niceToMeetYou = (name) => {
+    questionNumber ++;
+    showMessage(`Nice to meet you ${name}! Do you wanna play a little quiz game?`, 'bot');
+    setTimeout(() => {
+    chat.innerHTML +=
   `<div class="yesNoWrapper" id="yesNoWrapper">
-    <button id="yesButton" class= "yesNoButton yesButton" onclick = "q1()">Yes</button>
-    <button id="noButton" class= "yesNoButton noButton" onclick = "noPlay()">No</button>
+    <button id="yesButton" class= "yesNoButton yesButton">Yes</button>
+    <button id="noButton" class= "yesNoButton noButton">No</button>
   </div>`;
-  nameForm.innerHTML = "";
-}
-
-//question 1 function (if yes)
-const q1 = () => {
-  showMessage("Ok, let's start with an easy one: What's the name of the Swedish Prime Minister?", 'bot');
-  chat.innerHTML +=
-  `<div class="q1Wrapper" id="q1Wrapper">
-    <button id="olof" class= "q1a1Button q1Button" onclick = "wrongAnswer()">Olof Palme</button>
-    <button id="magdalena" class= "q1a2Button q1Button" onclick = "wrongAnswer()">Magdalena Andersson</button>
-    <button id="ulf" class= "q1a2Button q1Button" onclick = correctAnswer()>Ulf Kristersson</button>
-  </div>`
-}
-
-//if answer is no make bot say "Some other time then. Have a nice day!"
-const noPlay = () => {
-  chat.innerHTML =`
-  <div class = "dogwrap">
-  <p>That's too bad :( </p>
-  <img class="dog" src="sad dog.jpg" alt="sad dog">
-  </div>
-  `;
-  nameForm.innerHTML ="";
+  document.getElementById("yesButton").addEventListener("click", questionOrder);
+  document.getElementById("noButton").addEventListener("click", noPlay); 
+  }, 1000);  
 };
 
-//Make if-statement: If answer is correct, give score and move to next question, else just go to next.
-const wrongAnswer = () => {
- showMessage("Interesting choice... Next question: What is the population of Sweden?", "bot");
- console.log(score);
+//no-click
+const noPlay = () => {
+    showMessage("That's too bad :(", "bot")
+    chat.innerHTML +=`
+    <div class = "dogwrap">
+    <img class="dog" src="sad dog.jpg" alt="sad dog">
+    </div>
+    `
+};
+  
+//First quiz-question
+const q1 = () => {
+    questionNumber ++;
+    showMessage(botAnswers.question1, 'bot');
+    chat.innerHTML +=
+    `<div class="q1Wrapper" id="q1Wrapper">
+      <button id="olof" class= "q1a1Button q1Button">Olof Palme</button>
+      <button id="magdalena" class= "q1a2Button q1Button">Magdalena Andersson</button>
+      <button id="ulf" class= "q1a2Button q1Button">Ulf Kristersson</button>
+    </div>`;
+    document.getElementById("olof").addEventListener("click", questionOrder);
+    document.getElementById("magdalena").addEventListener("click", questionOrder);
+    document.getElementById("ulf").addEventListener("click", () => {
+        score ++;
+        console.log(score);
+        questionOrder()});
+  };
+
+//2nd 
+  const q2 = () => {
+    questionNumber ++;
+    showMessage(botAnswers.question2, "bot");
+    console.log(score);
+    chat.innerHTML +=
+    `<div class="q1Wrapper" id="q1Wrapper">
+    <button id="8,2" class= "q1a1Button q1Button">8,2 mill ion</button>
+    <button id="10,4" class= "q1a2Button q1Button">10,4 million</button>
+    <button id="13,6" class= "q1a2Button q1Button">13,6 million</button>
+    </div>`;
+    document.getElementById("8,2").addEventListener("click", questionOrder);
+    document.getElementById("10,4").addEventListener("click",() => {
+        score ++;
+        console.log(score);
+        questionOrder()});
+    document.getElementById("13,6").addEventListener("click", questionOrder);
+  };
+  
+//3rd
+const q3 = () => {
+    questionNumber ++;
+    showMessage(botAnswers.question3, "bot");
+    console.log(score);
+    chat.innerHTML +=
+    `<div class="q1Wrapper" id="q1Wrapper">
+    <button id="kebne" class= "q1a1Button q1Button">Kebnekaise</button>
+    <button id="åre" class= "q1a2Button q1Button">Åreskutan</button>
+    <button id="hammarby" class= "q1a2Button q1Button">Hammarbybacken</button>
+    </div>`;
+    document.getElementById("åre").addEventListener("click", questionOrder);
+    document.getElementById("hammarby").addEventListener("click", questionOrder);
+    document.getElementById("kebne").addEventListener("click",() => {
+    score ++;
+    console.log(score);
+    questionOrder()});
+  };
+
+  //4th
+  const q4 = () => {
+    questionNumber ++;
+    showMessage(botAnswers.question4, "bot");
+    console.log(score);
+    chat.innerHTML +=
+    `<div class="q1Wrapper" id="q1Wrapper">
+    <button id="linköping" class= "q1a1Button q1Button">Linköping</button>
+    <button id="uppsala" class= "q1a2Button q1Button">Uppsala</button>
+    <button id="örebro" class= "q1a2Button q1Button">Örebro</button>
+    </div>`;
+    document.getElementById("linköping").addEventListener("click", questionOrder);
+    document.getElementById("örebro").addEventListener("click", questionOrder);
+    document.getElementById("uppsala").addEventListener("click",() => {
+    score ++;
+    console.log(score);
+    questionOrder()});
+  };
+
+  //5th
+  const q5 = () => {
+    questionNumber ++;
+    showMessage(botAnswers.question5, "bot");
+    console.log(score);
+    chat.innerHTML +=
+    `<div class="q1Wrapper" id="q1Wrapper">
+    <button id="GustavVasa" class= "q1a1Button q1Button">Gustav Vasa</button>
+    <button id="KarlXII" class= "q1a2Button q1Button">Karl XII</button>
+    <button id="CarlXVIGustaf" class= "q1a2Button q1Button">Carl XVI Gustaf</button>
+    </div>`;
+    document.getElementById("GustavVasa").addEventListener("click", questionOrder);
+    document.getElementById("KarlXII").addEventListener("click", questionOrder);
+    document.getElementById("CarlXVIGustaf").addEventListener("click",() => {
+    score ++;
+    console.log(score);
+    questionOrder()});
+  };
 
 
+//Scoreboard
+const scoreBoard = () => {
+    console.log(`this is your score: ${score}`);
+    showMessage(botAnswers.scoreReport, "bot");
+    chat.innerHTML +=`
+    <div>
+        <h3>Scoreboard</h3>
+        <p>Your score: ${score}</p>
+
+    </div>
+    `
 }
 
-const correctAnswer = () => {
-  score = score + 1;
-  showMessage("Alright! Next question: ", "bot");
-  console.log(score);
-}
 
-
-//Order based on question number
-
-
-/* const questionOrder = (message) => {
-  console.log('questionNumber', questionNumber);
-  if (questionNumber === 1) {
-    userReply(message)
-    input.value = ''
-    setTimeout(() => niceToMeetYou(message), 1000)
-  } else if (questionNumber === 2) {
-    userReply(message)
-    setTimeout(() => an(message), 1000)
-  } else if (questionNumber === 3) {
-    userReply(message)
-    setTimeout(() => showDishSize(message), 1000)
-  } else if (questionNumber === 4) {
-    userReply(message)
-    setTimeout(() => showPrice(message), 1000)
-  } else {
-    userReply(message)
-    setTimeout(thankYou, 1000)
-  }
-} */
