d6fd66613e6100b86d1c31034ac5ed732f459547
AnnikaSonnek <annika.sonnek@gmail.com>
2023-02-12 21:22:45 +0100

    fixed indentations of the code

--- code/script.js
+++ code/script.js
@@ -19,7 +19,6 @@ const handleNameInput = (event) => {
 
   //After 1 second, show the next question by invoking the next function. Passin the name into it to have access to the user's name if we want to use it in the next question from the bot. 
   setTimeout(() => showFoodOptions(name), 1000);
-  
 }
 
 //**FUNCTION** Show Food options
@@ -27,10 +26,10 @@ const showFoodOptions = (name) => {
   showMessage(`Hello ${name}! What would you like to order?`, 'bot');
  //Buttons appear for cake, bun or sandwich option
   inputWrapper.innerHTML = `
-  <button id='cakeBtn' value="cake">Cake</button>
-  <button id='bunBtn' value="bun">Bun</button>
-  <button id='sandwichBtn' value="sandwich">Sandwich</button>
-  `
+    <button id='cakeBtn' value="cake">Cake</button>
+    <button id='bunBtn' value="bun">Bun</button>
+    <button id='sandwichBtn' value="sandwich">Sandwich</button>
+    `
 
   //Listen for clicks on the buttons and trigger the next function, sends user to different function depending on the user's choiche
   document.getElementById('cakeBtn').addEventListener ('click', CakeOrder); 
@@ -47,38 +46,37 @@ const CakeOrder = () => {
 //Select with cake choices below
 inputWrapper.innerHTML = `
 <form id="cake-form">
-    <select id="cake-type">
-        <option value="carrot cake">Carrotcake</option>
-        <option value="cheese cake">Cheesecake</option>
-        <option value="chocolate cake">Chocolate cake</option>
-    </select>
-
-<button id= "cakebtn"> Order cake </button>
+  <select id="cake-type">
+    <option value="carrot cake">Carrotcake</option>
+    <option value="cheese cake">Cheesecake</option>
+    <option value="chocolate cake">Chocolate cake</option>
+  </select>
+  <button id= "cakebtn"> Order cake </button>
 </form>
   `  
-const cakeform = document.getElementById('cake-type'); //creating variable from cake choice
+  const cakeform = document.getElementById('cake-type'); //creating variable from cake choice
 
-//EventListener, sends user to next function. Takes the value of variable but leaves two empty spots in the index so other choices also could be entered in the next function.  
-cakebtn.addEventListener("click", () => { 
-  const cake = cakeform.value; //creating new variable from cakeform variable
-  Delivery(cake, undefined, undefined);
-});
-  }
+  //EventListener, sends user to next function. Takes the value of variable but leaves two empty spots in the index so other choices also could be entered in the next function.  
+  cakebtn.addEventListener("click", () => { 
+    const cake = cakeform.value; //creating new variable from cakeform variable
+    Delivery(cake, undefined, undefined);
+  });
+  
+}
 
 //**FUNCTION** Bun order select (drop down menu)
-  const BunOrder = () => {
-    showMessage(`I want a bun, please!`, 'user'); //message from the user about choice 
-    setTimeout(() => showMessage(`Okay, you want a bun. What type of bun would you like?`, 'bot'), 800); //question from bot
+const BunOrder = () => {
+  showMessage(`I want a bun, please!`, 'user'); //message from the user about choice 
+  setTimeout(() => showMessage(`Okay, you want a bun. What type of bun would you like?`, 'bot'), 800); //question from bot
   //Select with bun choices below
 inputWrapper.innerHTML = `
   <form id="bun-form">
-      <select id="bun-type">
-          <option value="cinnamon bun">Cinnamon bun</option>
-          <option value="cardamom bun">Cardamom bun</option>
-          <option value="vanilla bun">Vanilla bun</option>
-      </select>
-  
-  <button id= "bunBtn"> Order bun </button>
+    <select id="bun-type">
+      <option value="cinnamon bun">Cinnamon bun</option>
+      <option value="cardamom bun">Cardamom bun</option>
+      <option value="vanilla bun">Vanilla bun</option>
+    </select>  
+    <button id= "bunBtn"> Order bun </button>
   </form>
     `  
   const bunform = document.getElementById('bun-type'); //creating variable from bun choice
@@ -88,31 +86,33 @@ inputWrapper.innerHTML = `
     const bun = bunform.value; //creating new variable from bunform variable
     Delivery(undefined, bun, undefined);
   });
-    }
+
+}
+
 //**FUNCTION Sandwich order  select (drop down menu)
-    const SandwichOrder = () => {
-      showMessage(`I want a sandwich, please!`, 'user'); //message from the user about choice 
-      setTimeout(() => showMessage(`Okay, you want sandwich. What would you like on your sandwich? &#129386;`, 'bot'), 800); //question from bot
+const SandwichOrder = () => {
+  showMessage(`I want a sandwich, please!`, 'user'); //message from the user about choice 
+  setTimeout(() => showMessage(`Okay, you want sandwich. What would you like on your sandwich? &#129386;`, 'bot'), 800); //question from bot
     //Select with bun choices below  
-    inputWrapper.innerHTML = `
+  inputWrapper.innerHTML = `
     <form id="sandwich-form">
-        <select id="sandwich-type">
-            <option value="chicken curry sandwich">Chicken curry topping</option>
-            <option value="cheese and ham sandwich">Ham and cheese</option>
-            <option value="cream cheese sandwich">Cream cheese</option>
-        </select>
-    
-    <button id= "sandwichBtn"> Order sandwich </button>
+      <select id="sandwich-type">
+        <option value="chicken curry sandwich">Chicken curry topping</option>
+        <option value="cheese and ham sandwich">Ham and cheese</option>
+        <option value="cream cheese sandwich">Cream cheese</option>  
+      </select>
+      <button id= "sandwichBtn"> Order sandwich </button>
     </form>
       `  
-    const sandwichform = document.getElementById('sandwich-type'); //creating variable from bun choice
-    
-    //EventListener, sends user to next function. Takes the value of variable but leaves two empty spots in the index so other choices also could be entered in the next function. 
-    sandwichBtn.addEventListener("click", () => { 
-      const sandwich = sandwichform.value; //creating new variable from bunform variable
-      Delivery(undefined, undefined, sandwich);
-    });
-      }
+  const sandwichform = document.getElementById('sandwich-type'); //creating variable from bun choice
+
+  //EventListener, sends user to next function. Takes the value of variable but leaves two empty spots in the index so other choices also could be entered in the next function. 
+  sandwichBtn.addEventListener("click", () => { 
+    const sandwich = sandwichform.value; //creating new variable from bunform variable
+    Delivery(undefined, undefined, sandwich);
+  });
+      
+}
 
 
 
@@ -140,7 +140,6 @@ const Delivery = (cake, bun, sandwich) => {
     <form id = "delivery-adress">
         <input type="text" input id="adress" placeholder="Please enter adress here" name="adress" required>
       </form>
-
       <button id= "adressBtn"> Submit </button>
       `  
   const userAdress = document.getElementById('adress'); //creating variable form input from user
@@ -152,7 +151,6 @@ const Delivery = (cake, bun, sandwich) => {
     if (adress === '' || null || 0){
       showMessage("You have to enter an adress! Try again.", 'bot');
     }
-
     else {
     GoodBye(cake, bun, sandwich, adress);
     }
@@ -195,9 +193,9 @@ const showMessage = (message, sender) => {
       </section>
     `
     //Above, we get ahold of chat and add som innerHTML using += (add something to it). The show message function will be re-used several times. The innerHTML section will be added to the chat-section in the HTML-dokument. 
-    
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
+  } 
+  else if (sender === 'bot') {
     
     chat.innerHTML += `
       <section class="bot-msg">
@@ -220,6 +218,7 @@ const greetUser = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
   showMessage("");
 }
+
 setTimeout(greetUser, 1000);
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
