9b0d92c0c702e8efc85dcdb9a82e641eed0dd939
augustaquilon <august.aquilon@gmail.com>
2023-08-09 13:40:56 +0200

    Revert "Iterations 1, 2, 3 and 4 CodeBuddy"
    This reverts commit 79220ae6cb7eb73e6520564e418ccaa926e1ad0e.

--- code/script.js
+++ code/script.js
@@ -2,36 +2,62 @@
 const chat = document.getElementById('chat')
 const inputWrapper = document.getElementById("input-wrapper")
 const nameInput = document.getElementById("input")
-
-
-// If you need any global variables that you can use across different functions, declare them here:
+const greetBtn = document.getElementById("greet-btn")
+const ignoreBtn = document.getElementById("ignore-btn")
+const petBtn = document.getElementById("pet-btn")
+const pullawayBtn = document.getElementById("pullaway-btn")
+const petmoreBtn = document.getElementById("petmore-btn")
+const pictureBtn = document.getElementById("picture-btn")
+const forfeitBtn = document.getElementById("forfeit-btn")
+const moreloveBtn = document.getElementById("morelove-btn")
+const meowlong = document.getElementById("meowlong")
+const meowshort = document.getElementById("meowshort")
+const purrlong = document.getElementById("purrlong")
+const purrbreath = document.getElementById("purrbreath")
 
 
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
+const showMessage = (message, sender, displayRandomImage) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+    if (displayRandomImage) {
+      const randomImageUrl = getRandomImageUrl();
+      chat.innerHTML += `
+        <section class="user-msg">
+          <div class="bubble user-bubble">
+            <img
+              src="${randomImageUrl}"
+              class="catpic"
+              alt="Cat" />
+          </div>
+          <img src="assets/user.png" alt="User" class="avatar" />  
+        </section>
+      `;
+    } else {
+      console.log("User");
+      chat.innerHTML += `
+        <section class="user-msg">
+          <div class="bubble user-bubble">
+            <p>${message}</p>
+          </div>
+          <img src="assets/user.png" alt="User" class="avatar" />  
+        </section>
+      `;
+    }
   } else if (sender === 'bot') {
+    console.log("Bot");
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/cat.png" alt="Bot" class="avatar" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
+
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
 }
@@ -39,77 +65,237 @@ const showMessage = (message, sender) => {
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, how can I assist you today?", 'bot');
+  showMessage("Meow?", 'bot');
+  // Add an event listener to play the sound when the message starts playing
+  meowshort.addEventListener("play", () => {
+    setTimeout(() => {
+      showMessage("<p style='font-style: italic;'>Oh snap! I seem to have woken Bob up from a nap.</p>", 'user');
+    }, 3000)
+  });
+  // Play the sound
+  meowshort.play();
   setTimeout(() => {
-    firstBtn.removeAttribute("hidden");
-    secondBtn.removeAttribute("hidden");
-    thirdBtn.removeAttribute("hidden");
-  }, 1000)
+    // show buttons
+    greetBtn.removeAttribute('hidden');
+    ignoreBtn.removeAttribute('hidden');
+  }, 4000)
+  
   // Just to check it out, change 'bot' to 'user' here 👆
+}
+
+const greetBob = () => {
+  // hide buttons after user has made choice
+  greetBtn.setAttribute('hidden', true);
+  ignoreBtn.setAttribute('hidden', true);
+  setTimeout(() => {
+    showMessage("Hello there little fella👋", 'user');
+  }, 1000);
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>sniff</p>", 'bot');
+  }, 3000);
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>sniff sniff</p>", 'bot');
+  }, 4000);
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>...</p>", 'user');
+  }, 5000);
+  setTimeout(() => {
+    showMessage("You smell...", 'bot');
+  }, 6000);
+  setTimeout(() => {
+    showMessage("...like friend😻", 'bot');
+  }, 8000);
+  setTimeout(() => {
+    showMessage("Gimme love?", 'bot');
+  }, 9000);
+  setTimeout(() => {
+    petBtn.removeAttribute('hidden');
+    pullawayBtn.removeAttribute('hidden');
+    chat.scrollTop = chat.scrollHeight;
+  }, 10000);
+}
+
+const showReloadBtn = () => {
+  chat.innerHTML += `
+  <button type="button" onclick="window.location.reload()">Try again?</button>`;
+}
+
+const ignoreBob = () => {
+  // Hide the buttons after the user has made their choice
+  greetBtn.setAttribute('hidden', true);
+  ignoreBtn.setAttribute('hidden', true);
+  petBtn.setAttribute('hidden', true);
+  pullawayBtn.setAttribute('hidden', true);
+  showMessage("You know what? I don't even like cats.", 'user');
+  setTimeout(() => {
+    showMessage("Bob feels sad now. 😿 He curls up into a ball and goes back to sleep.", 'bot');
+  }, 1000);
+  setTimeout(() => {
+    showReloadBtn();
+    chat.scrollTop = chat.scrollHeight;
+  }, 3000);
+}
+
+const petBob = () => {
+  // Hide the buttons after the user has made their choice
+  petBtn.setAttribute('hidden', true);
+  pullawayBtn.setAttribute('hidden', true);
+  purrlong.addEventListener("play", () => {
+    setTimeout(() => {
+      showMessage("<p style='font-style: italic;'>purrr...</p>", 'bot');
+    }, 1000);
+  });
+  purrlong.play();
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>What a cute kitty🥹</p>", 'user');
+  }, 7000);
+  setTimeout(() => {
+    petmoreBtn.removeAttribute('hidden');
+    pullawayBtn.removeAttribute('hidden');
+    chat.scrollTop = chat.scrollHeight;
+  }, 9000);
+}
+
+const moreLove = () => {
+  petmoreBtn.setAttribute('hidden', true);
+  pullawayBtn.setAttribute('hidden', true);
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>purrrpurrpurrr...</p>", 'bot');
+  purrbreath.addEventListener("ended", () => {
+      setTimeout(() => {
+        showMessage("<p style='font-style: italic;'>licks hand</p>", 'bot');
+        setTimeout(() => {
+          showMessage("<p style='font-style: italic;'>OMG🥹</p>", 'user');
+          setTimeout(() => {
+            showMessage("I think we have a bond", 'user');
+            setTimeout(() => {
+              showMessage("I must snap picture", 'user');
+              setTimeout(() => {
+                showMessage("<p style='font-style: italic;'>pulls out phone</p>", 'user');
+                setTimeout(() => {
+                  showMessage("Meeeooooow! Why u stop?", 'bot');
+                  meowlong.addEventListener("ended", () => {
+                    setTimeout(() => {
+                      showMessage("<p style='font-style: italic;'>looks angry</p>", 'bot');
+                      setTimeout(() => {
+                        showMessage("<p style='font-style: italic;'>Hmm, he doesn't seem to like when I stop...</p>", 'user');
+                        setTimeout(() => {
+                          pictureBtn.removeAttribute('hidden');
+                          forfeitBtn.removeAttribute('hidden');
+                          chat.scrollTop = chat.scrollHeight;
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                });
+                meowlong.play();
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  });
+
+  purrbreath.play();
+}
+
+const imageUrls = [
+  "assets/catpics/cat1.png",
+  "assets/catpics/cat2.png",
+  "assets/catpics/cat3.png",
+  "assets/catpics/cat4.png",
+  "assets/catpics/cat5.png",
+  "assets/catpics/cat6.png",
+  "assets/catpics/cat7.png",
+  "assets/catpics/cat8.png",
+  "assets/catpics/cat9.png"
+]
+
+// get random image
+const getRandomImageUrl = () => {
+  const randomIndex = Math.floor(Math.random() * imageUrls.length);
+  return imageUrls[randomIndex];
+};
+
+
+const takePic = () => {
+  pictureBtn.setAttribute('hidden', true);
+  forfeitBtn.setAttribute('hidden', true);
+  showMessage("C'mon, just a quick one", 'user');
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>takes pic📸</p>", 'user');
+  }, 2000);
+  setTimeout(() => {
+    showMessage("Tadaaa",'user', true);
+  }, 3000);
+  setTimeout(() => {
+    showMessage("That's a...pretty cat",'user');
+  }, 4000);
+  // two choices appear, more love or ignore
+  setTimeout(() => {
+    showMessage("...", 'bot');
+    moreloveBtn.removeAttribute('hidden');
+    ignoreBtn.removeAttribute('hidden');
+    chat.scrollTop = chat.scrollHeight;
+  }, 6000);
+
+}
+
+const tooMuchLove = () => {
+  moreloveBtn.setAttribute('hidden', true);
+  ignoreBtn.setAttribute('hidden', true);
+  showMessage("<p style='font-style: italic;'>bites hand</p>", 'bot');
+  setTimeout(() => {
+    showMessage("Aooooow! F*ck", 'user');
+  }, 1000);
+  setTimeout(() => {
+    showMessage("You prick", 'user');
+  }, 2000);
+  setTimeout(() => {
+    showMessage("I thought we were friends", 'user');
+  }, 3000);
+  setTimeout(() => {
+    showMessage("<p style='font-style: italic;'>pulls away hand</p>", 'user');
+  }, 4000);
+  setTimeout(() => {
+    showMessage("Bob has had enough of humans for another day. 😼 He licks his paw, curls up into a ball and goes back to sleep.", 'bot');
+  }, 5000);
+  setTimeout(() => {
+    showReloadBtn();
+    chat.scrollTop = chat.scrollHeight;
+  }, 6000);
+}
+
+const noPic = () => {
+  pictureBtn.setAttribute('hidden', true);
+  forfeitBtn.setAttribute('hidden', true);
+  showMessage("Okay then, I give up, no picture...", 'user');
+  setTimeout(() => {
+    showMessage("Sorry to have annoyed you little kittle", 'user');
+  }, 1000);
+  setTimeout(() => {
+    showMessage("...", 'bot');
+  }, 2000)
+  setTimeout(() => {
+    moreloveBtn.removeAttribute('hidden');
+    ignoreBtn.removeAttribute('hidden');
+    chat.scrollTop = chat.scrollHeight;
+  }, 4000);
 }
 
 // Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000);
-
-// Add this after the greetUser function
-document.getElementById("userinput-form").addEventListener("submit", (event)=> {
-  event.preventDefault();
-  handleUserInput(event);
-});
+greetBtn.addEventListener("click", greetBob);
+ignoreBtn.addEventListener("click", ignoreBob);
+petBtn.addEventListener("click", petBob);
+pullawayBtn.addEventListener("click", ignoreBob);
+petmoreBtn.addEventListener("click", moreLove);
+pictureBtn.addEventListener("click", takePic);
+forfeitBtn.addEventListener("click", noPic);
+moreloveBtn.addEventListener("click", tooMuchLove);
 
 
-// Define the function to handle user input
-const handleUserInput = (event) => {
-  event.preventDefault(); // Prevent form submission
-  const userInputSelect = document.getElementById("user-input");
-  const selectedOption = userInputSelect.value;
 
-  if (selectedOption !== "") {
-    showMessage(selectedOption, 'user'); // Display user-selected option in the chat
-    processUserInput(selectedOption);
-    userInputSelect.value = ""; // Reset the dropdown selection
-  }
-}
 
-const processUserInput = (selectedOption) => {
-  // Process the selected option and provide bot responses accordingly
-  switch (selectedOption) {
-    case "option1":
-      showMessage("You want help debugging.", 'bot');
-      // Add specific response for Option 1
-      break;
-    case "option2":
-      showMessage("You want help with Git.", 'bot');
-      // Add specific response for Option 2
-      break;
-    case "option3":
-      showMessage("You want help with time management.", 'bot');
-      // Add specific response for Option 3
-      break;
-    case "option4":
-      showMessage("You want help with time management.", 'bot');
-      // Add specific response for Option 3
-      break;  
-    case "option5":
-      showMessage("You want help with time management.", 'bot');
-      // Add specific response for Option 3
-      break;
-    case "option6":
-      showMessage("You want help with time management.", 'bot');
-      // Add specific response for Option 3
-      break;
-    case "option7":
-      showMessage("You want help with time management.", 'bot');
-      // Add specific response for Option 3
-      break;
-    default:
-      showMessage("I'm sorry, I didn't recognize your selection. Please try again.", 'bot');
-  }
-}
+setTimeout(greetUser, 2000)
