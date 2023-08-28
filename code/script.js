401c7aef3cf7a300df074c9a94d5b1cf52cf616d
Isa Robertini <robertiniisa@gmail.com>
2023-08-28 11:38:16 +0200

    first greeting

--- code/script.js
+++ code/script.js
@@ -1,6 +1,6 @@
-// Variables that point to selected DOM elements //tryout
+// Variables that point to selected DOM elements 
 const chat = document.getElementById('chat')
-
+const nameInput = document.getElementById('name-input')
 // If you need any global variables that you can use across different functions, declare them here:
 
 
@@ -17,7 +17,7 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    `;
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
     chat.innerHTML += `
@@ -27,21 +27,43 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
-}
+};
 
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot')
+  showMessage(`Hello there, What's your name?`, 'bot');
   // Just to check it out, change 'bot' to 'user' here 👆
-}
+};
 
+
+const handleNameInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  const name = nameInput.value;
+  showMessage(`${name}`, 'user');
+
+  setTimeout(() => {
+    showMessage(`Hello ${name} nice to meet you!`, 'bot');
+    nameInput.value = "";
+
+    // After 1 second, show the next question by invoking the next function.
+    // passing the name into it to have access to the user's name if we want
+    // to use it in the next question from the bot.
+    setTimeout(() => showFoodOptions(name), 2000);
+  }, 1000);
+};
 // Set up your eventlisteners here
-
+nameInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    handleNameInput(event);
+  }
+});
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
 // greeting()
