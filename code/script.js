4b7938a7fa8987dda874e47617d03372509a594b
Isa Robertini <robertiniisa@gmail.com>
2023-08-29 11:15:55 +0200

    maneged to take away buttons

--- code/script.js
+++ code/script.js
@@ -1,17 +1,12 @@
 // Variables that point to selected DOM elements 
-const chat = document.getElementById('chat')
-const inputWrapper = document.getElementById('input-wrapper')
-const nameInput = document.getElementById('name-input')
-const choiceBtn1 = document.getElementById('choiceBtn1')
-const choiceBtn2 = document.getElementById('choiceBtn2')
-// If you need any global variables that you can use across different functions, declare them here:
-
-
-// Declare your functions after this comment
+const chat = document.getElementById('chat');
+const inputWrapper = document.getElementById('input-wrapper');
+const nameInput = document.getElementById('name-input');
+const choiceBtn1 = document.getElementById('choiceBtn1');
+const choiceBtn2 = document.getElementById('choiceBtn2');
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
     chat.innerHTML += `
       <section class="user-msg">
@@ -21,7 +16,6 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `;
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
@@ -32,59 +26,74 @@ const showMessage = (message, sender) => {
       </section>
     `;
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight;
 };
 
-// Starts here
+
+// Function greets the user, asking for name.
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage(`Hello there, What's your name?`, 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
 };
 
-
+// Function storing the name of the user.
 const handleNameInput = (event) => {
   event.preventDefault();
-  // Store the value in a variable so we can access it after we
-  // clear it from the input
   const name = nameInput.value;
   showMessage(`${name}`, 'user');
 
+  // Function displaying the name of the user in a ShowMessage.
   setTimeout(() => {
     showMessage(`Hello ${name} nice to meet you!`, 'bot');
     nameInput.value = "";
 
-    // After 1 second, show the next question by invoking the next function.
+    // If Btn1 is pressed, dysplay:
+    const handlechoiceBtn1 = (event, name) => {
+      event.preventDefault();
+      showMessage(`mmm, yellow snacks!`, 'bot');
+
+      setTimeout(() => {
+        showMessage(`So, I guess you like yellow snacks, huh?`, 'bot');
+      }, 2000);
+    };
+
+    // else if Btn2 is pressed, dysplay:
+    const handlechoiceBtn2 = (event, name) => {
+      event.preventDefault();
+      showMessage(`mmm, blue snacks!`, 'bot');
+
+      setTimeout(() => {
+        showMessage(`So, I guess you like blue snacks, huh?`, 'bot');
+      }, 2000);
+    };
+
+
     setTimeout(() => {
       showMessage(`So, ${name} what would you like to order today?`, 'bot');
       choiceBtn1.style.display = 'block';
       choiceBtn2.style.display = 'block';
       inputWrapper.style.display = 'none';
 
-      choiceBtn1.onclick = () => {
-        showMessage(`mmm, yellow snacks!`, 'bot');
+      choiceBtn1.onclick = (event) => {
+        handlechoiceBtn1(event, name);
+        choiceBtn1.style.display = 'none';
+        choiceBtn2.style.display = 'none';
+        inputWrapper.style.display = 'block';
       };
-      choiceBtn2.onclick = () => {
-        showMessage(`mmm, blue snacks!`, 'bot');
+      choiceBtn2.onclick = (event) => {
+        handlechoiceBtn2(event, name);
+        choiceBtn1.style.display = 'none';
+        choiceBtn2.style.display = 'none';
+        inputWrapper.style.display = 'block';
       };
-
-      setTimeout(() => showFoodOptions(name), 1000);
-    }, 1500)
+    }, 1500);
   }, 1000);
 };
-// Set up your eventlisteners here
+// Event listener, when user press enter.
 nameInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     handleNameInput(event);
-
   }
 });
 
 // When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+setTimeout(greetUser, 1000);
