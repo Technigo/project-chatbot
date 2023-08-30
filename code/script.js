a56dbc3f61faaea657c29575e31630362a45f08e
Idah Collin <idah_collin@hotmail.com>
2023-08-30 18:23:35 +0200

    Step 3 Local function for name input and new question

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@
 const chat = document.getElementById('chat')
 const nameForm = document.getElementById('name-form')
 const nameInput = document.getElementById('name-input')
-
+const sendBtn = document.getElementsByClassName('send-btn')
 // If you need any global variables that you can use across different functions, declare them here:
 
 
@@ -23,7 +23,7 @@ function showMessage(message, sender) {
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
 
-    console.log("Current message:", message);
+    //console.log("Current message:", message);
 
     chat.innerHTML += `
       <section class="bot-msg">
@@ -34,18 +34,45 @@ function showMessage(message, sender) {
       </section>
     `
   }
+
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
+
 // Starts here
+//Conversation starts here. First greet from bot 
+
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hello there happy developer, what's your name?", 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
 // Set up your eventlisteners here
+//When name is input by user. Local function.
+const handleNameInput = event => {
+  event.preventDefault();
+  userName = nameInput.value;
+  nameInput.value = "";
+  showMessage(`${userName}`, "user");
+  setTimeout(reply, 1500);
+}
+
+nameForm.addEventListener("submit", handleNameInput);
+
+function reply() {
+  showMessage(`I think you need a riddle to relax your mind ${userName}. <br>Type: <br>1 for yes <br>2 for no`, 'bot');
+}
+// Store the value in a variable so we can access it after we
+// clear it from the input
+
+
+// After 1 second, show the next question by invoking the next function.
+// passing the name into it to have access to the user's name if we want
+// to use it in the next question from the bot.
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
