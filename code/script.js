1d62a99a45ba4ed179b3ff9ddf6bb99aef0609c0
Minadarabi <minaadabirii@gmail.com>
2023-08-30 06:50:00 -0700

    added some styles and java scripts code

--- code/script.js
+++ code/script.js
@@ -1,5 +1,21 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const btn = document.getElementById("btn");
+const nameInput = document.getElementById("name-input");
+const btnsEl = document.getElementById("buttons");
+const nameFormEl = document.getElementById("name-form");
+const pizzaBtn = document.getElementById("pizzabtn");
+const pastaBtn = document.getElementById("pastabtn");
+const saladBtn = document.getElementById("saladbtn");
+const pizzaMenue = document.getElementById("pizza-menue");
+const pastaMenue = document.getElementById("pasta-menue");
+const saladMenue = document.getElementById("salad-menue");
+
+btnsEl.style.display = "none";
+nameFormEl.style.display = "flex";
+pizzaMenue.style.display = "none";
+pastaMenue.style.display = "none";
+saladMenue.style.display = "none";
 
 // If you need any global variables that you can use across different functions, declare them here:
 
@@ -40,12 +56,50 @@ const greetUser = () => {
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
+
 // Set up your eventlisteners here
+btn.addEventListener("click" , (e) => {
+  e.preventDefault();
+
+  message = nameInput.value;
+  showMessage(`${message}` , 'user');
+  clearInput();
+  setTimeout(() => {
+    showMessage(`Nice to meet you ${message}.what type of food would you like to order?` , 'bot')
+    btnsEl.style.display = "flex";
+    nameFormEl.style.display = "none";
+  },1000);
+
+});
+
+const clearInput = () =>{
+  nameInput.value = "";
+}
 
 // When website loaded, chatbot asks first question.
+
 // normally we would invoke a function like this:
 // greeting()
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
+pizzaBtn.addEventListener("click" , () =>{
+  showMessage(`Pizza` , 'user');
+  btnsEl.style.display = "none";
+  setTimeout(()=>{
+    showMessage(`oh so you are in the mood for Pizza? Great choice! please select something from menue `, 'bot')
+
+  },1000)
+  
+});
+pastaBtn.addEventListener("click" , () =>{
+  showMessage(`Pasta` , 'user');
+  btnsEl.style.display = "none";
+  
+});
+saladBtn.addEventListener("click" , () =>{
+  showMessage(`Salad` , 'user');
+  btnsEl.style.display = "none";
+})
+
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 1000)
