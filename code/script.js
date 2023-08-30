1f8bc8bb753f092878221acfee0ffa8c740fb768
Your Name <you@example.com>
2023-08-30 11:26:01 +0200

    The bot now asks for name, user type name and then the bot says hi and ask if you want to learn som danish

--- code/script.js
+++ code/script.js
@@ -1,15 +1,18 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const nameForm = document.getElementById('name-form');
+const nameInput = document.getElementById('name-input');
 
 // If you need any global variables that you can use across different functions, declare them here:
 
 
-// Declare your functions after this comment
+// MY FUNCTIONS
 
-// This function will add a chat bubble in the correct place based on who the sender is
+// 1. This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
+    console.log(`The user said something!`)
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -18,8 +21,9 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+    // 2. the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log(`The bot said something!`)
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -29,18 +33,35 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  // 3. This thing makes the chat scroll to the last message when there are too many to be shown in the chat box
+  chat.scrollTop = chat.scrollHeight;
 }
 
-// Starts here
+// THE CONVERSATION LOOP
+
+// here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage("Hello there, What's your name?", 'bot');
+}
+
+// CHANGE This function saves the text entered in the nameInput variable as a new variable (name) and invokes the showMessage function to render it in the chat. After that, it clears the input field.
+const handleNameInput = (event) => { 
+  event.preventDefault()
+  const name = nameInput.value
+  showMessage(name, 'user')
+  nameInput.value = ''
+  // Change this. After 1 second, it renders the next question by invoking the next function. We're taking the 'name' variable with us to the next function.
+  setTimeout(() => intro(name), 1000)
+}
+
+//Change this.This function displays a bot message in response to the handleNameInput function above and invokes the next question/function.
+const intro = (name) => {
+  showMessage (`Hi ${name}! Do you want to learn some Danish?`, 'bot');
+  setTimeout(showYesNoBtn, 1000)
 }
 
 // Set up your eventlisteners here
+nameForm.addEventListener('submit', handleNameInput);
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
