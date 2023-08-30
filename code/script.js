753827eba95924595d28053c61960c50239748e1
Isa Robertini <robertiniisa@gmail.com>
2023-08-30 10:27:30 +0200

    yes and no btn

--- code/script.js
+++ code/script.js
@@ -4,6 +4,8 @@ const inputWrapper = document.getElementById('input-wrapper');
 const nameInput = document.getElementById('name-input');
 const choiceBtn1 = document.getElementById('choiceBtn1');
 const choiceBtn2 = document.getElementById('choiceBtn2');
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -69,22 +71,46 @@ const handleNameInput = (event) => {
       // Functions answer if Btn1:
       const handlechoiceBtn1 = (event, name) => {
         event.preventDefault();
-        showMessage(`mmm, yellow snacks!`, 'bot');
+        showMessage(`yellow snacks!`, 'user');
 
         setTimeout(() => {
           showMessage(`So ${name}, I guess you like yellow snacks, huh?`, 'bot');
+          yesBtn.style.display = 'block';
+          noBtn.style.display = 'block';
+          inputWrapper.style.display = 'none';
+
         }, 2000);
       };
 
       // Functions answer if Btn2:
       const handlechoiceBtn2 = (event, name) => {
         event.preventDefault();
-        showMessage(`mmm, blue snacks!`, 'bot');
+        showMessage(`Blue snacks!`, 'user');
 
         setTimeout(() => {
           showMessage(`So ${name}, I guess you like blue snacks, huh?`, 'bot');
+          yesBtn.style.display = 'block';
+          noBtn.style.display = 'block';
+          inputWrapper.style.display = 'none';
         }, 2000);
       };
+      // Functions hides Btn3, Btn4 after clicking it..
+      yesBtn.onclick = (event) => {
+        handlechoiceBtn1(event, name);
+        yesBtn.style.display = 'none';
+        noBtn.style.display = 'none';
+        inputWrapper.style.display = 'block';
+        showMessage(`Yes!`, 'user');
+      };
+      noBtn.onclick = (event) => {
+        handlechoiceBtn1(event, name);
+        yesBtn.style.display = 'none';
+        noBtn.style.display = 'none';
+        inputWrapper.style.display = 'block';
+        showMessage(`No!`, 'user');
+      };
+
+
     }, 1500);
   }, 1000);
 };
