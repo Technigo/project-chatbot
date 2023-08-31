c39e177bcedaf0f936ffa0fe58e953ee983babbe
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-08-31 14:13:13 +0200

    -m final

--- code/script.js
+++ code/script.js
@@ -141,25 +141,30 @@ function handleSpecificAge(choice) {
 }
 
 function confirmOrder(foodChoice, sizeChoice) {
-  showMessage(`Here's your order, ${userName}:<br>You want ${foodChoice}<br>Portion size ${sizeChoice}`, 'bot');
+  showMessage(`Here's your order summary, ${userName}:<br>Food: ${foodChoice}<br>Size: ${sizeChoice}`, 'bot');
 
   inputWrapper.innerHTML = `
-    <button class="send-btn" id="confirm">I'm ready to order</button>
+    <button class="send-btn" id="confirm">Yes, I'm ready to order</button>
     <button class="send-btn" id="cancel">No, I changed my mind</button>
   `;
 
-    confirmButton.addEventListener('click', () => {
-      showMessage('Your order has been confirmed. Thank you for choosing us!', 'bot');
-      inputWrapper.innerHTML = ''; 
-    });
+  const confirmButton = document.getElementById('confirm');
+  const cancelButton = document.getElementById('cancel');
+
+  confirmButton.addEventListener('click', () => {
+    showMessage('Your order has been confirmed. Thank you for choosing us!', 'bot');
+    inputWrapper.innerHTML = ''; 
+  });
+
+  cancelButton.addEventListener('click', () => {
+    showMessage('Your order has been canceled. Feel free to order again later.', 'bot');
+    inputWrapper.innerHTML = ''; 
+  });
+}
+
+
+
   
-    cancelButton.addEventListener('click', () => {
-      showMessage('Your order has been canceled. Feel free to order again later.', 'bot');
-      inputWrapper.innerHTML = ''; 
-    });
-  }
-
-
   setTimeout(greetUser, 500)
 
 
