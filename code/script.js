3bbf2a59cdd162933af716a8f681a26af41586d7
isaackamran <isaackamran@hotmail.com>
2023-08-31 18:19:16 +0200

    added console.log

--- code/script.js
+++ code/script.js
@@ -20,6 +20,7 @@ const showMessage = (message, sender) => {
     `
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
+    console.log("Hiya!")
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/pizzaman.png" alt="Bot" />
@@ -42,10 +43,5 @@ const greetUser = () => {
 
 // Set up your eventlisteners here
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
+
 setTimeout(greetUser, 1000)
