90f3a20fb2beae651581d274c7333391ab7efed2
veronica astrom <v.aastrom@gmail.com>
2023-08-31 11:54:00 +0200

    added the first user input

--- code/script.js
+++ code/script.js
@@ -1,17 +1,22 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const inputField = document.getElementById('name-input');
+const sendBtn = document.getElementById('send-btn');
 
-// If you need any global variables that you can use across different functions, declare them here:
+// FUNCTIONS are declared below
 
-
-// Declare your functions after this comment
+// Function to clear the input field
+// Since the input field needs to be cleared after every input I created a function for this.
+const clearInputField = () => {
+  inputField.value = "";
+};
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
+   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
-    console.log("Sender: ", sender);
-    console.log("Message: ", message);
+    console.log('Message is:', message);
+    console.log('Sender is:', sender);
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -20,10 +25,10 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+      // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
-    console.log("Sender: ", sender);
-    console.log("Message: ", message);
+    console.log('Message is:', message);
+    console.log('Sender is:', sender);
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -37,14 +42,37 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-// Starts here
+// Function to greet the user
+// The function showMessage is called with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Hi, what's your name?", 'bot')
- 
 }
 
-// Set up your eventlisteners here
+// Function for the name input from the user
+const handleNameInput = (event) => {
+  event.preventDefault();
+
+  // the dot . means that we're accessing an attribute/method of the variable left of the dot.
+  // Here we are accessing the value that exists in the input element with the id "name-input". 
+  const name = inputField.value;
+
+  // If the function that is being called was defined with parameters, like showMessage = (message, sender), they need to be added when said function is called/used.
+  // the showMessage function is called and passes on the arguments name (which is the value of the input field) and the sender (which depends on the if statement).
+  showMessage(name, "user");
+
+  //Function that clears the input field.
+  clearInputField();
+
+  // After 1 second, show the next question by invoking the next function.
+  // passing the name into it to have access to the user's name if we want to use it in the next question from the bot.
+  // To pass parameter values to a function, that is itself passed as a parameter to another function, use an "anonymous function" () => {}
+  // Meaning, when a function is a parameter and needs a parameter itself, you need to use an anonymous function
+  setTimeout(() => handleFoodOptions(name), 1000);
+};
+
+// EVENTLISTENERS
+
+sendBtn.addEventListener('click', handleNameInput);
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -52,4 +80,4 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 500)
+setTimeout(greetUser, 100)
