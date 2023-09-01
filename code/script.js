7a965a766604fdd5e677438f27290394929b55d9
Anna Robertsson <robertsson_anna@hotmail.com>
2023-09-01 02:28:33 +0200

    added footer and updated styling

--- code/script.js
+++ code/script.js
@@ -37,7 +37,7 @@ const showMessage = (message, sender) => {
 // Starts here
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hey there Traveller! Now it's time to go on a virtual trip to a foreign country and explore it! What can I call you? Please enter your name so we can get to know each other!", 'bot')
+  showMessage("Hey there Traveller! Join me on a virtual trip to a foreign country and let's explore it! Please enter your name so I know what to call my new travel buddy!", 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆
 }
 
@@ -45,104 +45,112 @@ const handleNameInput = (event) => {
   event.preventDefault();
   const name = nameInput.value;
   if (name === '') {
-    showMessage('You must enter a valid name, please try again' , 'bot')
+    showMessage('You must enter a valid name, please try again', 'bot')
   } else {
     showMessage(name, 'user'); //User stod med ""
-  username = nameInput.value;
-  nameInput.value = ''; //Stod med ""
+    username = nameInput.value;
+    nameInput.value = ''; //Stod med ""
 
-  setTimeout(() => travelOption(nameInput.value), 1500);
-  inputWrapper.innerHTML =''
+    setTimeout(() => travelOption(nameInput.value), 1500);
+    inputWrapper.innerHTML = ''
   }
 
-  
+
 }
 
 // Bot greets user
 const travelOption = () => {
-  showMessage(`Hi ${username}, so nice to have you onboard on this trip! Which of the lovely countries do you want to visit?` ,'bot') 
+  showMessage(`Hi ${username}, so nice to have you onboard on this trip! Which of the lovely countries do you want to visit? 🧭`, 'bot')
   inputWrapper.innerHTML = `
-  <button id='cuba'>Cuba</button>
-  <button id='italy'>Italy</button>
-  <button id='netherlands'>The Netherlands</button>
+  <button id='cuba'>Cuba ⛱️</button>
+  <button id='italy'>Italy 🍕</button>
+  <button id='netherlands'>The Netherlands 🌷</button>
   `
-    document.getElementById('cuba')
+  document.getElementById('cuba')
     .addEventListener('click', () => {
-    showMessage('Cuba' , 'user')
-    setTimeout(() => activityQuestion('Cuba'), 1000)})
-  
-    document.getElementById('italy')
+      showMessage('I want to go to Cuba!', 'user')
+      setTimeout(() => activityQuestion('Cuba'), 1000)
+    })
+
+  document.getElementById('italy')
     .addEventListener('click', () => {
-    showMessage('🍕 Italy' , 'user')
-    setTimeout(() => activityQuestion('Italy'), 1000)})
-  
-    document.getElementById('netherlands')
+      showMessage('I want to go to Italy!', 'user')
+      setTimeout(() => activityQuestion('Italy'), 1000)
+    })
+
+  document.getElementById('netherlands')
     .addEventListener('click', () => {
-      showMessage('The Netherlands' , 'user')      
+      showMessage('I want to go to The Netherlands!', 'user')
       setTimeout(() => activityQuestion('The Netherlands'), 1000)
     })
 }
 
 //Här klistrar jag in en switch att testa med
-  let activityChoice = ""
-  let activity = ""
-  let selectedActivity = ""
-  //let selectedTravelOption = ""
-  
-  const activityQuestion = (selectedTravelOption) => {
-    showMessage(`Wow, ${selectedTravelOption}, what an amazing choice! What activity would you like to participate in?` , 'bot')
+let activityChoice = ""
+let activity = ""
+let selectedActivity = ""
+//let selectedTravelOption = ""
 
-    console.log(`activityQuestion executed ${selectedTravelOption}`);
-    
+const activityQuestion = (selectedTravelOption) => {
+  showMessage(`Wow, ${selectedTravelOption}, what an amazing choice! What activity would you like to participate in?`, 'bot')
 
-    switch (selectedTravelOption) {
-    case "Cuba": 
-      
+  console.log(`activityQuestion executed ${selectedTravelOption}`);
+
+
+  switch (selectedTravelOption) {
+    case "Cuba":
+
       activity = ["Cigar activity", "Veteran car activity", "Salsa activity"]
 
       inputWrapper.innerHTML = `
       <select id="activityChoiceSelect">
-        <option value="" selected disabled>Choose an activity</option>
+        <option value="" selected disabled>Choose one of our amazing activities!</option>
         <option value="1">Cigar activity</option>
         <option value="2">Veteran car activity</option>
         <option value="3">Salsa activity</option>
       </select>
       `
 
-      //LÄGG TILL PÅ DE ANDRA CASEN + CSS-BILDER
+      //Makes the background-image change when chosing Cuba by activating body.cuba in the CSS
       document.getElementById("body").className = "cuba"
 
       console.log('inputWrapper.innerHTML changed (cuba)');
 
       break
-    case "Italy": 
-      
+    case "Italy":
+
       activity = ["Make pasta activity", "Wineyard activity", "tango activity"]
 
       inputWrapper.innerHTML = `
       <select id="activityChoiceSelect">
-        <option value="" selected disabled>Choose an activity</option>
+        <option value="" selected disabled>Choose one of our amazing activities!</option>
         <option value="1">Make pasta activity</option>
         <option value="2">Wineyard activity</option>
         <option value="3">tango activity</option>
       </select>
       `
 
+      //Makes the background-image change when chosing Italy by activating body.italy in the CSS
+      document.getElementById("body").className = "italy"
+
       console.log('inputWrapper.innerHTML changed (italy)');
 
       break
-    case "The Netherlands": 
-      
+    case "The Netherlands":
+
       activity = ["tulips activity", "Windmill activity", "Stream activity"]
 
       inputWrapper.innerHTML = `
       <select id="activityChoiceSelect">
-        <option value="" selected disabled>Choose an activity</option>
+        <option value="" selected disabled>Choose one of our amazing activities!</option>
         <option value="1">Tulips activity</option>
         <option value="2">Windmill activity</option>
         <option value="3">Stream activity</option>
       </select>
       `
+
+      //Makes the background-image change when chosing The Netherlands by activating body.netherlands in the CSS
+      document.getElementById("body").className = "netherlands"
 
       console.log('inputWrapper.innerHTML changed (netherlands)');
 
@@ -151,54 +159,56 @@ const travelOption = () => {
   }
 
   console.log('set activitychoice listener');
-  
-  const activityChoiceSelect=document.getElementById('activityChoiceSelect')
+
+  const activityChoiceSelect = document.getElementById('activityChoiceSelect')
 
   activityChoiceSelect.addEventListener('change', () => {
     const activityChoice = parseInt(activityChoiceSelect.value)
     console.log(activityChoice)
-    selectedActivity = activity[activityChoice - 1] 
+    selectedActivity = activity[activityChoice - 1]
     console.log(selectedActivity)
-        
-    
-    showMessage(selectedActivity , 'user')
 
-    inputWrapper.innerHTML =''
+
+    showMessage(selectedActivity, 'user')
+
+    inputWrapper.innerHTML = ''
     setTimeout(() => confirmationActivity(), 1000)
   })
 }
 
 
 const confirmationActivity = () => {
-  showMessage (`${username}, are you happy with your choice?`, 'bot')
+  showMessage(`Perfect, ${username}! Are you happy with your choice?`, 'bot')
   inputWrapper.innerHTML = `
   <button id='yesButton'>Yes</button>
   <button id='noButton'>No</button>
   `
   document.getElementById('yesButton')
     .addEventListener('click', () => {
-    showMessage('Yes' , 'user')
-    setTimeout(() => happyAnswer('Yes'), 1000)})
-  
-    document.getElementById('noButton')
+      showMessage('Yes', 'user')
+      setTimeout(() => happyAnswer('Yes'), 1000)
+    })
+
+  document.getElementById('noButton')
     .addEventListener('click', () => {
-    showMessage('No' , 'user')
-    setTimeout(() => happyAnswer('No'), 1000)})
+      showMessage('No', 'user')
+      setTimeout(() => happyAnswer('No'), 1000)
+    })
 }
 
 const happyAnswer = (happyAnswer) => {
   if (happyAnswer === 'Yes') {
-    showMessage ('Awesome! We will contact you for further information.' ,'bot')
+    showMessage('Awesome! We will contact you for further information.', 'bot')
   } else {
-    showMessage ('So sorry to hear that, the booking will be cancelled. Hope to see you again for a new booking in the future.' ,'bot')
+    showMessage('So sorry to hear that, the booking will be cancelled. Hope to see you again for a new booking in the future.', 'bot')
   }
-  inputWrapper.innerHTML = ''
+  inputWrapper.innerHTML = '' //Makes the yes and no button to disappear so the bot conversation ends
   //TA BORT OM INTE NÅGON NY FRÅGA  setTimeout(() => confirmationQuestion(), 500)
 }
 
 
 // Set up your eventlisteners here
-  nameForm.addEventListener('submit', handleNameInput)
+nameForm.addEventListener('submit', handleNameInput)
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
