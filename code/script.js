ac103122bb37df1f4ebf078a9a75078bb5b4fafa
BeckieMorton <beckam11@gmail.com>
2023-09-01 14:59:04 +1000

    question 3 complete with 3 buttons

--- code/script.js
+++ code/script.js
@@ -1,11 +1,13 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+
 
 // If you need any global variables that you can use across different functions, declare them here:
-
 let userAnswered = false;
 let usersName = "Jane Doe";
 let questionCount = 0;
+let continentSelected = "Africa";
+
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
@@ -40,53 +42,55 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
   return (userAnswered);
 }
-
 // Starts here - these are the bot's questions
 const greetUser = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot')
+  showMessage("Welcome to Mayhem Mystery Flights &#9992", 'bot');
+  showMessage("To get us started, what is your name?", 'bot');
+  console.log(`question count is:`, questionCount)
 }
+
 
 //ask user if they are interested in a flight, yes/no answer
 const botQuestion2 = (name) => {
   console.log("botQuestion2 function is entered")
-  showMessage(`Welcome, ${usersName}. Are you interested in booking a mystery flight with us today? Type YES or NO`, 'bot');
+  showMessage(`Welcome, ${usersName}. Are you interested in booking a mystery flight with us today?`, 'bot');
+  showMessage(`Type YES or NO`, 'bot');
 }
 
+//ask user which 3 continents they are interested in
 const botQuestion3 = (yesOrNo) => {
   console.log("we have entered question 3 function")
-  console.log(usersName)
+
   if (usersName === "yes") {
-    showMessage(`Fantastic! &#128578 We have a few questions for you that will help us choose the best destination for your flight.`, 'bot')
+    showMessage(`Fantastic! &#128578 We have a few questions that will help us choose the best destination for your flight.`, 'bot')
     showMessage(`Where would you rather go?`, 'bot');
-    //remove the text input bar
-    const removeInputVar = document.querySelector("#name-input");
-    removeInputVar.remove();
-    //add a dropdown select bar
-    const addDropdownQ3 = document.createElement("select") //create a dynamic element with a class name to link in CSS
-    addDropdownQ3.classname = "q3-dropdown-select"
-    const parent = document.querySelector("#input-wrapper")
-    parent.innerHTML = `
-    <select name="q3-dropdown-select" id="q3-dropdown-select" required>
-    <option selected="selected">&#128071 Select a Continent here</option>
-    <option value="africa">Africa</option>
-    <option value="asia">Asia</option>
-    <option value="europe">Europe</option>
-  </select>
+
+    questionCount = 2
+
+    //create 3 buttons - africa, asia, europe
+    const q3Buttons = document.getElementById("input-wrapper")
+    q3Buttons.innerHTML = `
+   <button id="africa" value="africa">Africa</button>
+   <button id="asia" value="asia">Asia</button>
+   <button id="europe" value="europe">Europe</button>
     `
-    console.log(addDropdownQ3)
-    return addDropdownQ3;
-    //the 3 buttons for location options goes here
-
-    //   form.innerHTML += `
-    //   <section class="bot-msg">
-    //     <img src="assets/bot.png" alt="Bot" />
-    //     <div class="bubble bot-bubble">
-    //       <p>${message}</p>
-    //     </div>
-    //   </section>
-    // `
-
+    q3Buttons.addEventListener("click", function (whichButton) {
+      if (whichButton.target.value == "africa") {
+        continentSelected = "Africa"
+        //call next question
+      } else if (whichButton.target.value == "asia") {
+        continentSelected = "Asia"
+        //call next question
+      } else if (whichButton.target.value == "europe") {
+        continentSelected = "Europe"
+        //call next question
+      }
+      // Store the Continent choice so we can access it after we clear it from the input
+      console.log("continent selected is:", continentSelected);
+      showMessage(`${continentSelected}`, 'user');
+      setTimeout(botQuestion4, 1000);
+    })
   } else if (usersName === "no") {
     showMessage(`We hope you visit again soon, Good bye`, 'bot');
   } else {
@@ -94,10 +98,30 @@ const botQuestion3 = (yesOrNo) => {
     showMessage(`Please reload the page and try chatting with us again`, 'bot')
   }
 }
+const botQuestion4 = (continent) => {
+  showMessage(`${continentSelected} is a great choice! 	&#128512`, 'bot');
+  showMessage(`There are so many amazing countries to visit in ${continentSelected} , so let's narrow it down...`, 'bot');
+  showMessage(`What area in ${continentSelected} most interests you?`, 'bot');
+
+}
+
+//
+//
+//
 // Set up your eventlisteners here
 
-//listen for when form is submitted (button or 'enter pressed)
+// const buttonTest = document.getElementById("send-btn");
+// const formTest = document.getElementById("name-form");
+// const SelectDivTest = document.getElementById("q3-dropdown-selec");
+
+// formTest.addEventListener("submit", function (e) {
+//   e.preventDefault(); // Prevents the form from actually submitting and refreshing the page
+//   alert("Form was submitted!");
+// });
+
+//listen for when form is submitted(button or 'enter pressed)
 logSubmit = (event) => {
+  console.log(`Question count inside logsubmit:`, questionCount)
   event.preventDefault();
   // Store the value in a variable so we can access it after we
   // clear it from the input
@@ -106,14 +130,18 @@ logSubmit = (event) => {
   usersName = nameSubmitted.value;
   showMessage(`${usersName}`, 'user');
   nameSubmitted.value = "";
-  questionCount = 1 + questionCount
-  console.log(`Question count:`, questionCount)
-  if (questionCount == 1) {
+  if (questionCount == 0) {
+    questionCount = 1;
     setTimeout(botQuestion2, 1000); //I had problems with where to put this, got help from a code snippet in our Slack :-)
-  } else if (questionCount == 2) {
+  } else if (questionCount == 1) {
+    questionCount = 2;
     setTimeout(botQuestion3, 1000);
+  } else if (questionCount == 2) {
+    questionCount = 3;
+    setTimeout(botQuestion4, 1000);
   }
 }
+
 //when form is submitted
 const form = document.getElementById("name-form");
 form.addEventListener("submit", logSubmit);
