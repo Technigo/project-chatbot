9efe43f9a69db397fc965bee0aabb58c138f9c4e
colognia <frauke.schmidt@protonmail.com>
2023-08-31 20:08:09 -0500

    Add fkt and variables for last query, add calculation
    Add new fkt, Variables and Listeners to new user query.
    Correct mistakes in calculation for section 3.
    Add a goodbye routine for user.

--- code/script.js
+++ code/script.js
@@ -7,12 +7,23 @@ const chat = document.getElementById('chat');
 // 1a. service offers
 let serviceChoice = "nada";
 let FeeDomain = 0;
+// per year
 
 // 1b. domain offers
 let domain = "";
+let FeeHosting = 0;
+// per month
 
 // 2. plan
 let plan = "";
+let sum = 0;
+
+// 3. invoice
+let discount = 0;
+
+// 4. offer a developer
+let developerOption = "";
+
 
 // -----------------------------------------------------
 // functions
@@ -58,10 +69,10 @@ const serviceOffers = () => {
   document.getElementById("input-wrapper").innerHTML = `
   <div id="services-wrapper" style="display: flex">
     <button id="domain" value="domain">Domain<br>registration:<br>15 credits / year</button>
-    <button id="hosting" value="hosting">Hosting:<br>5 - 55 credits / month</button>
+    <button id="hosting" value="hosting">Hosting:<br>5  credits / month</button>
     <button id="domainAndHosting" value="domainAndHosting">Registration<br>and Hosting:<br>15% discount</button>
   </div>`;
-  // additional div for targeting content
+  // additional div for targeting content and serving as flex container, input-wrapper needs to ramain
   // do not nest ""
 
   const servicesWrapper = document.getElementById("services-wrapper");
@@ -77,9 +88,16 @@ const serviceOffers = () => {
       // calling next question - 1b
     } else if (event.target.value == "hosting") {
       serviceChoice = "Hosting only";
-      console.log("ende")
+      // console.log("end");
+      FeeHosting = 5;
+      FeeHosting = FeeHosting * 12;
+      setTimeout(planSelection, 1000);
     } else {
       serviceChoice = "Domain and Hosting";
+      FeeHosting = 5;
+      FeeHosting = FeeHosting * 12;
+      FeeDomain = 15;
+      discount = 0.15;
       setTimeout(domainOffers, 1000);
       // without timeout confirmation too late
       // calling next question - 1b
@@ -90,6 +108,7 @@ const serviceOffers = () => {
     // console.log("end of services");
   });
 }
+
 const selectionConfirmation = () => showMessage(`I pick: <br>${serviceChoice}`, "user");
 const domainConfirmation = () => showMessage(`I go for ${domain}-Domain.`, "user")
 // end for service offers
@@ -104,9 +123,8 @@ const domainOffers = () => {
   // add input field
   document.getElementById("input-wrapper").innerHTML = `
   <form id="domain-form">
-  <label>Domain
+  <label for="domain-input">Domain</label>
   <input id="domain-input" type="text" />
-  </label>
   <button class="send-btn" id="send-button-domainchoice">
   Send
   </button>
@@ -173,9 +191,8 @@ const planSelection = () => {
   // add input field
   document.getElementById("input-wrapper").innerHTML = `
   <form id="plan-form">
-  <label>Domain
+  <label for="plan-input">Plan</label>
   <input id="plan-input" type="text" />
-  </label>
   <button class="send-btn" id="send-button-plannchoice">
   Send
   </button>
@@ -216,9 +233,74 @@ const planValidation = () => {
   }
 }
 
-const planConfirmation = () => showMessage(`I pick: <br>${plan}`, "user");
+const planConfirmation = () => {
+  showMessage(`I pick: <br>${plan}`, "user");
+  calculation();
+}
+
+//3. calculation of price
+const calculation = () => {
+  let planDuration = document.getElementById("plan-input").value;
+  console.log(planDuration);
+  sum = ((FeeDomain + FeeHosting) * planDuration);
+  sum = sum * (1 - discount);
+  // console.log(sum);
+  setTimeout(invoiceMessage, 1000);
+}
+
+const invoiceMessage = () => {
+  showMessage(`Your final annual price is:<br>${sum} credits per year.`, "bot");
+  setTimeout(offerDeveloper, 1000);
+}
+// 4. offer help thru developer
+const offerDeveloper = () => {
+  showMessage(`
+  Do you need some help creating your website?<br>
+  We have commited junior developers at hand.
+  `, "bot");
+
+  // create 2 buttons
+  document.getElementById("input-wrapper").innerHTML = `
+<div id="assistance-wrapper" style="display: flex">
+  <button id="yes" value="yes">YES</button>
+  <button id="no" value="no">No</button>
+</div>`;
+  // additional div for targeting content and serving as flex container, input-wrapper needs to ramain
+  // do not nest ""
+
+  const assistanceWrapper = document.getElementById("assistance-wrapper");
+
+  assistanceWrapper.addEventListener("click", function (event) {
+    // do not target predefined input-wrapper for every query! target needs to be different, therefore add additional div in costum html
+    // console.log(event.target.value); -> visible after click
+    if (event.target.value == "yes") {
+      developerOption = "yes";
+      setTimeout(goodByeMessage, 1000);
+    } else {
+      developerOption = "no";
+      setTimeout(goodByeMessage, 1000);
+    }
+    developerConfirmation();
+  });
+}
+
+const developerConfirmation = () => {
+  showMessage(`I guess: <br>
+${developerOption}`, "user")
+};
+
+const goodByeMessage = () => {
+  if (developerOption == "yes") {
+    showMessage(`Great! We get in touch with you soon to connect you! Good bye!`, "bot");
+  } else {
+    showMessage(`Ìf you need help later, just+ get back to us. Good bye!`, "bot");
+  }
+  document.getElementById("assistance-wrapper").style.display = "none";
+  // let vanish buttons
+}
+
 // ---------------------------------------
-// main code starts here
+// INITIAL CODE STARTS HERE //
 
 // 0. greeting user
 setTimeout(greetUser, 1000)
