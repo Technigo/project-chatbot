fb97b577bd635ac3185827495f275675934ad86d
Idah Collin <idah_collin@hotmail.com>
2023-09-01 20:57:43 +0200

    Added comments in the js

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 Project during web. developer bootcamp. The project was to building a functional chatbot interface using JavaScript.
 
-
 ## The problem
 
-Problems were solved by having sessions with my team, Stack, class material and chatGPT. Had particulary one problem with the bot repeating one of the questions. Eventually this was solved by adding a global variable. Lots of time was put in to solve the issue which took time from developing the bot further. Next step would be to add more questions, try out a dropdown list, improve the design and play around with sound effects.
+Problems were solved by having sessions with my team, Stack, class material and chatGPT. Had particulary one problem with the bot repeating one of the questions. Eventually this was solved by adding a global variable. Lots of time was put in to solve the issue which took time from developing the bot further. Next step would be to add more questions, try out a dropdown list, add more setTimeouts and make the code more DRY and clean. 
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -6,7 +6,7 @@ const nameInput = document.getElementById('name-input')
 // Global variable to prevent the chat-bubbles to start over
 let run = true;
 
-// This function will add a chat bubble in the correct place based on who the sender is
+// This function adds a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
   if (sender === 'user') {
@@ -21,7 +21,6 @@ const showMessage = (message, sender) => {
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
   } else if (sender === 'bot') {
 
-    //console.log("Current message:", message);
 
     chat.innerHTML += `
       <section class="bot-msg">
@@ -33,24 +32,21 @@ const showMessage = (message, sender) => {
     `
   }
 
-  // Makes the chat scroll to the last message when there are too many to be shown in the chat box
+  // Makes the chat scroll to the last message in the beginning of the chat
   chat.scrollTop = chat.scrollHeight
 }
 
 
-// Starts here
 //Conversation starts here. First greet from bot 
 
 const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
   showMessage("Bonjour developer student, what's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
 }
 
 //Global setTimeout for the Greeting
 setTimeout(greetUser, 800)
 
-// Set up your eventlisteners here
+
 //When name is input by user.
 const handleNameInput = event => {
   event.preventDefault();
@@ -65,30 +61,28 @@ const handleNameInput = event => {
 
 nameForm.addEventListener('submit', handleNameInput);
 
-
+//Riddles start here
 const reply = () => {
   showMessage(`I think you need a riddle to relax your mind ${userName}. Choose a riddle by clicking on one of the apples below:`, 'bot');
-
+  //Buttons for the ridddles
   nameForm.innerHTML = `
       <button class="options" id='oneBtn'>🍏</button>
       <button class="options" id='twoBtn'>🍎</button>
     `
-
   document.getElementById('oneBtn')
     .addEventListener('click', () => riddleOption('one'))
-
-
 
   document.getElementById('twoBtn')
     .addEventListener('click', () => riddleOption('two'))
 }
 
-//Different options of riddles that occurs depending on apple-choice
+//Different options of riddles that occurs depending on apple-choice. Conditional statements.
 const riddleOption = option => {
+  //Riddle choice: green apple
   if (option === "one") {
     showMessage('🍏', 'user')
     showMessage('🍏: I am used for styling, look and feel. Colors and layout make it real. In files separate, my rules align. Junior dev, what am I, in web design?', 'bot')
-
+    //Buttons for green apple riddle
     nameForm.innerHTML = `
     <button class="options" id='htmlBtn'>HTML</button>
     <button class="options" id='cssBtn'>CSS</button>
@@ -100,10 +94,11 @@ const riddleOption = option => {
     document.getElementById('cssBtn')
       .addEventListener('click', () => greenCorrectAnswer('greenCss'))
   }
+  //Riddle choice: red apple
   else if (option === "two") {
     showMessage('🍎', 'user')
     showMessage('🍎: I bring interactivity, make things fun. With functions and events, the job has begun. In the browser realm, where actions play smart. Junior dev can you name this part?', 'bot')
-
+    //Buttons for red apple riddle
     nameForm.innerHTML = `
       <button class="options" id='jsBtn'>JavaScript</button>
       <button class="options" id='htmlBtn'>HTML</button>
@@ -117,11 +112,13 @@ const riddleOption = option => {
   }
 }
 
-//Depending on the answer of the riddles, one of these functions will start:
+//Depending on the answer of the riddles, one of these four functions will start:
+//Wrong answer green apple riddle
 const greenWrongAnswer = () => {
   showMessage('HTML', 'user')
   showMessage('Almost! Correct answer is CSS. Thanks for playing with me! Good luck on your studies, you are awesome!', 'bot')
   showMessage('<img src="https://cdn.pixabay.com/photo/2020/05/02/09/59/pup-5120625_1280.jpg" alt="Happy dog" style="width: 225px; height: auto;" />', 'bot');
+  //Added to prevent the reply function to start over.
   run = false;
   //Added to prevent the buttons to appear again.
   const htmlBtn = document.getElementById('htmlBtn');
@@ -133,11 +130,12 @@ const greenWrongAnswer = () => {
     cssBtn.remove();
   }
 }
-
+//Correct answer green apple riddle
 const greenCorrectAnswer = () => {
   showMessage('CSS', 'user')
   showMessage('CSS is correct! Thanks for playing with me! Good luck on your studies, you are awesome!', 'bot')
   showMessage('<img src="https://cdn.pixabay.com/photo/2020/05/02/09/59/pup-5120625_1280.jpg" alt="Happy dog" style="width: 225px; height: auto;" />', 'bot');
+  //Added to prevent the reply function to start over.
   run = false;
   //Added to prevent the buttons to appear again.
   const htmlBtn = document.getElementById('htmlBtn');
@@ -149,11 +147,12 @@ const greenCorrectAnswer = () => {
     cssBtn.remove();
   }
 }
-
+//Correct answer red apple riddle
 const redCorrectAnswer = () => {
   showMessage('JavaScript', 'user')
-  showMessage('Good job! JavaScript is the correct answer! Good luck on your studies, you are awesome!', 'bot')
+  showMessage('Good job! JavaScript is the correct answer! Thanks for playing with me! Good luck on your studies, you are awesome!', 'bot')
   showMessage('<img src="https://cdn.pixabay.com/photo/2020/05/02/09/59/pup-5120625_1280.jpg" alt="Happy dog" style="width: 225px; height: auto;" />', 'bot');
+  //Added to prevent the reply function to start over.
   run = false;
   //Added to prevent the buttons to appear again.
   const jsBtn = document.getElementById('jsBtn');
@@ -165,11 +164,12 @@ const redCorrectAnswer = () => {
     htmlBtn.remove();
   }
 }
-
+//Wrong answer red apple riddle
 const redWrongAnswer = () => {
   showMessage('HTML', 'user')
-  showMessage('Naaah, correct answer is JavaScript. Good luck on your studies, you are awesome!', 'bot')
+  showMessage('Naaah, correct answer is JavaScript. Thanks for playing with me! Good luck on your studies, you are awesome!', 'bot')
   showMessage('<img src="https://cdn.pixabay.com/photo/2020/05/02/09/59/pup-5120625_1280.jpg" alt="Happy dog" style="width: 225px; height: auto;" />', 'bot');
+  //Added to prevent the reply function to start over.
   run = false;
   //Added to prevent the buttons to appear again.
   const jsBtn = document.getElementById('jsBtn');
