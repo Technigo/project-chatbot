7c2feda80935ac7d2c5aa3d3653906130df8fde5
Emmy Jansson <emmybengtsson@live.se>
2023-09-01 17:36:55 +0200

    added the menu buttons

--- code/script.js
+++ code/script.js
@@ -1,8 +1,9 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat')
 const form = document.getElementById('name-form');
-const submitBtn = document.getElementById('submit-btn');
+const sendBtn = document.getElementById('send-btn');
 const inputField = document.getElementById('name-input');
+const inputWrapper = document.getElementById('input-wrapper');
 
 // If you need any global variables that you can use across different functions, declare them here:
 let userAnswer = "";
@@ -38,10 +39,8 @@ const showMessage = (message, sender) => {
 
 // Starts here
 const greeting = (name) => {
-  showMessage("Welcome to the Icecream dream! What's your name?", 'bot')
+  showMessage("Welcome to the Ice Cream dream! What's your name?", 'bot')
 }
-
-
   //Here you need to invoke the next function
 const handleNameInput = event => {
   event.preventDefault();
@@ -51,25 +50,75 @@ const handleNameInput = event => {
     setTimeout(reply, 300);
 }
 
+//The users answer
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const userMessage = inputField.value;
   showMessage(userMessage, 'user');
   inputField.value = '';
-  setTimeout(icecream, 300);
+  setTimeout(iceCreamMenu, 300);
 })
 
-const icecream = userName => {
-  showMessage(`Nice to meet you, ${userName}! What would you like to order today?`, 'bot');
- setTimeout(showMessage, 300);
+//First menu choises
+const iceCreamMenu = userName => {
+  showMessage(`Nice to meet you! What would you like to order today?`, 'bot');
+  inputWrapper.innerHTML = `
+  <button class="send-btn" type="IceCream">Ice Cream</button>
+  <button class="send-btn" type="SoftCream">Soft Cream</button>
+  <button class="send-btn" type="Milkshake">Milkshake</button>
+  `
 }
 
-// Set up your eventlisteners here
+//The submenu with different flavors
+const initialButtons = inputWrapper.querySelectorAll(".send-btn");
+  initialButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const iceCreamChoice = button.getAttribute("type");
+      handleButtonClick(iceCreamChoice);
+    });
+  });
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
+const handleButtonClick = (iceCreamChoice) => {
+  if (iceCreamMenu === "IceCream") {
+  showMessage(`Great choise! Which flavor would you like to have?`, 'bot');
+  inputWrapper.innerHTML = `
+  <button class="send-btn" type="Chocolate">Chocolate</button>
+  <button class="send-btn" type="Passion fruit">Passion fruit</button>
+  <button class="send-btn" type="Lemon">Lemon</button>
+  `
+  } 
+  
+  else if (iceCreamMenu === "SoftCream") {
+    showMessage(`Great choise! Which flavor would you like to have?`, 'bot');
+  inputWrapper.innerHTML = `
+  <button class="send-btn" type="Vanilla">Vanilla</button>
+  <button class="send-btn" type="Chocolate">Chocolate</button>
+  <button class="send-btn" type="Mix">Mix of vanilla and chocolate</button>
+  `
+  }
+  
+  else if (iceCreamMenu === "Milkshake") {
+    showMessage(`Great choise! Which flavor would you like to have?`, 'bot');
+  inputWrapper.innerHTML = `
+  <button class="send-btn" type="Strawberry">Strawberry</button>
+  <button class="send-btn" type="Peach">Peach</button>
+  <button class="send-btn" type="Mocha">Mocha</button>
+  `
+  }
+}
+
+//The users order confirmation
+//const orderConfirmation = () => {
+ // showMessage(`Great! Your delicious ${iceCreamMenu} will be prepared for you! That will be 20 SEK. Are you sure you want to order this?`, 'bot');
+ // inputWrapper.innerHTML = `
+ // <button class="send-btn" type="Yes">Yes, please</button>
+ // <button class="send-btn" type="No">No thank you</button>
+ // `
+//}
+
+//The bots answer depending on the answer from the order confirmation
+
+//showMessage(`Thank you for your order! You will get a notice when your order is done!`, 'bot')
+//showMessage(`No worries! Welcome back another time!`, 'bot')
+
 setTimeout(greeting, 300)
