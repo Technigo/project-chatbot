5f99fb5d721c3ca1fc9ce12e9a3112218c33d356
Breanna Sand <breanna.sand8@gmail.com>
2023-09-02 13:54:17 +0200

    user input appears on user bubble

--- code/script.js
+++ code/script.js
@@ -1,10 +1,38 @@
 // Variables that point to selected DOM elements
 const chat = document.getElementById('chat')
+const wrapper = document.getElementById('input-wrapper')
+const nameSubmit = document.getElementById('name-form')
+const nameInput = document.getElementById('name-input')
 
 // If you need any global variables that you can use across different functions, declare them here:
-
+let userName = "";
 
 // Declare your functions after this comment
+// Starts here
+const greetUser = () => {
+  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
+  showMessage(`Why hello there, may i ask you your name?`, 'bot')
+  //handleNameInput();
+  // Just to check it out, change 'bot' to 'user' here 👆
+}
+const handleNameInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  userName = nameInput.value;
+  showMessage(`${userName}`, 'user');
+  nameInput.value = "";
+
+  // After 1 second, show the next question by invoking the next function.
+  // passing the name into it to have access to the user's name if we want
+  // to use it in the next question from the bot.
+  setTimeout(reply(userName), 1000);
+};
+
+const reply = (userName) => {
+  showMessage(`What are you up to today, ${userName}? Care to have some company?`, 'bot');
+}
+
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -35,14 +63,11 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
-// Starts here
-const greetUser = () => {
-  // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Why hello there, may i ask you your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆
-}
+
 
 // Set up your eventlisteners here
+nameSubmit.addEventListener('submit', handleNameInput);
+
 
 // When website loaded, chatbot asks first question.
 // normally we would invoke a function like this:
@@ -50,4 +75,4 @@ const greetUser = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 4000)
+setTimeout(greetUser, 1000)
