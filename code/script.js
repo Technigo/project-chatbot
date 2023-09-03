9195397502c44c44b9cc7d8dd216f046175284ac
“mikaeloling” <mikael.oling@gmail.com>
2023-09-03 15:49:22 +0300

    Cleaned up HTML

--- code/script.js
+++ code/script.js
@@ -49,3 +49,55 @@ const greetUser = () => {
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 1000)
+
+//nameInput.addEventListener('input', handleNameInput);
+
+
+const handleNameInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  const name = nameInput.value;
+  showMessage(name, "user");
+  nameInput.value = "";
+
+};
+
+
+  // After 1 second, show the next question by invoking the next function.
+  // passing the name into it to have access to the user's name if we want
+  // to use it in the next question from the bot.
+ // setTimeout(() => showFoodOptions(name), 1000);
+
+
+
+/*In this code line 74:
+We define a foodOptions object to map food categories (Pizza, Sushi, Pasta) to arrays of options.
+We check if the user's input matches one of the keys in the foodOptions object.
+If it does, we retrieve the corresponding array of food options and display them.
+If the input doesn't match any of the keys, we display a message indicating that the food category is not recognized.*/
+
+/*function showFoodOptions(name) {
+  // Define a mapping of food categories to options
+  const foodOptions = {
+    Pizza: ["Margerita", "Napoletana", "Opera"],
+    Sushi: ["California", "Philadelphia", "Sashimi"],
+    Pasta: ["Bolognese", "Mac n cheese", "Alfredo"]
+  };
+
+  // Check if the user's choice is in the mapping
+  if (name in foodOptions) {
+    const userChoice = foodOptions[name];
+    // Display the user's food options
+    showMessage(`Here are some ${name} options: ${userChoice.join(", ")}`, "bot");
+  } else {
+    // Handle the case where the user's choice is not recognized
+    showMessage("I'm sorry, I don't have options for that food category.", "bot");
+  }
+}
+
+
+
+
+
+
