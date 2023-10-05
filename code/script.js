2f73f1c0983af1c8630aa9a4f95889402f0970c3
Linnea Johansson <Linneajoh@gmail.com>
2023-10-05 15:14:10 +0200

    added not valid message

--- code/script.js
+++ code/script.js
@@ -30,12 +30,20 @@ const showMessage = (message, sender) => {
 const greetUser = () => {
   showMessage("Hello and welcome to Linneas Restaurant, What's your name?", 'bot')  
 }
+
+
 const handleNameInput = event => {
   event.preventDefault();
   userName = nameInput.value;
   nameInput.value = "";
-  showMessage( `${userName}`, `user`);
-  setTimeout(reply, 1000)
+  
+  if (userName.trim() === "") {
+    showMessage("That's not a valid answer. Please enter your name.", 'bot'); 
+       
+  } else {
+    showMessage(`${userName}`, `user`);
+    setTimeout(reply, 1000);
+  }
 }
 
 nameForm.addEventListener (`submit`, handleNameInput);
@@ -52,9 +60,20 @@ document.getElementById('button1').addEventListener('click', () => {
   document.getElementById('dropdown-div').style.display = 'block';
 });
 
-document.getElementById('button2').addEventListener('click', () => {
-  showMessage('Our customer service is open between 13:55 and 14:00 on thursdays, please get back to us within opening hours', 'bot');
+document.getElementById('submit-food').addEventListener('click', () => {
+  const selectedOption = document.getElementById('food-options').value;
+  const thankYouMessage = `Thank you for ordering from us ${userName}! You are welcome to collect your food in 30 minutes. If you have any questions, please contact customer service.`;
+  showMessage(thankYouMessage, 'bot');
+  document.getElementById('thank-you-message').style.display = 'block';
+  document.getElementById('button1').style.display = 'none';
+  document.getElementById('dropdown-div').style.display = 'none';
+  document.getElementById('food-options').value = ''; 
+  document.getElementById('button-div').style.display = 'block';
 });
 
+document.getElementById('button2').addEventListener('click', () => {
+  showMessage('Our customer service is open between 13:55 and 14:00 on thursdays, please get back to us within opening hours', 'bot');
+  document.getElementById('button2').style.display = 'none';
+});
   
 setTimeout(greetUser, 1000)
