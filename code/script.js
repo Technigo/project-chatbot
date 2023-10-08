52bebad8500f764461d4a6a74555ebdf2ed076f4
Carolina Luna <kroluna@gmail.com>
2023-10-08 19:52:03 +0200

    Added an alert if the user types a name with numbers, aks for the user to write a name

--- code/script.js
+++ code/script.js
@@ -1,12 +1,11 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
-const inputWrapper = document.getElementById('input-wrapper');
-
+const chat = document.getElementById("chat");
+const inputWrapper = document.getElementById("input-wrapper");
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -16,7 +15,7 @@ const showMessage = (message, sender) => {
       </section>
     `;
     // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
-  } else if (sender === 'bot') {
+  } else if (sender === "bot") {
     console.log("Welcome to our cakeshop!");
     chat.innerHTML += `
       <section class="bot-msg">
@@ -29,115 +28,122 @@ const showMessage = (message, sender) => {
   }
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
-}
-
+};
 
 // Starts here
 const greetUser = () => {
-  console.log("Hi user!")
-  showMessage("Hello there, What's your name?", 'bot')
-}
+  console.log("Hi user!");
+  showMessage("Hello there, What's your name?", "bot");
+};
 
 // Set up your eventlisteners here
 
-document.getElementById('name-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+document
+  .getElementById("name-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  const name = event.target[0].value;
+    const name = event.target[0].value;
 
-  showMessage(name, 'user');
-  showMessage(`Nice to meet you, ${name}! What kind of cake would you like to order?`, 'bot');
+    if (/\d/.test(name)) {
+      // This regex checks if the string contains any digits.
+      showMessage(
+        "That's not a valid answer. Please provide a name without numbers.",
+        "bot"
+      );
+    } else {
+      showMessage(name, "user");
+      showMessage(
+        `Nice to meet you, ${name}! What kind of cake would you like to order?`,
+        "bot"
+      );
+      //This invokes the next step
+      updateInput("cake");
+    }
 
-  event.target[0].value = "";
-//This invokes the next step
-  updateInput('cake');
-});
+    event.target[0].value = "";
+  });
 
 const showCakeOptions = (userName) => {
-  console.log("Cake options")
+  console.log("Cake options");
   // Send a bot message to the chat
-  showMessage(userName, 'user');
+  showMessage(userName, "user");
   showMessage();
 
   // Update the input section with the cake options
-  updateInput('cake');
-}
+  updateInput("cake");
+};
 
 const updateInput = (step) => {
-  const inputWrapper = document.getElementById('input-wrapper');
-  let htmlContent = '';
+  const inputWrapper = document.getElementById("input-wrapper");
+  let htmlContent = "";
 
   switch (step) {
-      case 'cake':
-          htmlContent = `
+    case "cake":
+      htmlContent = `
               <div id="cake-buttons">
                   <button onclick="handleCakeChoice('Cheesecake')">Cheesecake</button>
                   <button onclick="handleCakeChoice('Carrot Cake')">Carrot Cake</button>
                   <button onclick="handleCakeChoice('Strawberry Cake')">Strawberry Cake</button>
               </div>
           `;
-          break;
+      break;
 
-      case 'people':
-            htmlContent = `
+    case "people":
+      htmlContent = `
                 <form id="people-form" onsubmit="handlePeopleCount(event)">
                   <input type="number" name="peopleCount" placeholder="Enter number of people" required>
                   <button type="submit">Submit</button>
                 </form>
               `;
-              break;
-        
-            case 'confirmOrder':
-              htmlContent = `
+      break;
+
+    case "confirmOrder":
+      htmlContent = `
                 <div id="confirm-buttons">
                   <button onclick="handleOrderConfirmation('yes')">Yes</button>
                   <button onclick="handleOrderConfirmation('no')">No</button>
                 </div>
               `;
-              break;
-
+      break;
   }
 
   inputWrapper.innerHTML = htmlContent;
-}
+};
 
 const handleCakeChoice = (cake) => {
   // Display the user's cake choice in the chat
-  showMessage(cake, 'user');
+  showMessage(cake, "user");
   // Continue the conversation based on the user's cake choice
-  showMessage(`Great choice! How many people will be eating?`, 'bot');
+  showMessage(`Great choice! How many people will be eating?`, "bot");
   // Proceed to ask about the number of people
-  updateInput('people');
-
- }
-
+  updateInput("people");
+};
 
 const handlePeopleCount = (event) => {
   event.preventDefault();
-  
-  const peopleCount = event.target[0].value;
-  
-  showMessage(peopleCount, 'user');
-  showMessage(`Thank you! That will be for ${peopleCount} people. Would you like to confirm your order?`, 'bot');
-  
-  updateInput('confirmOrder');
 
-}
+  const peopleCount = event.target[0].value;
+
+  showMessage(peopleCount, "user");
+  showMessage(
+    `Thank you! That will be for ${peopleCount} people. Would you like to confirm your order?`,
+    "bot"
+  );
+
+  updateInput("confirmOrder");
+};
 
 const handleOrderConfirmation = (confirmation) => {
-  if (confirmation === 'yes') {
-    showMessage('Yes', 'user');
-    showMessage('Thank you for your order! We are processing it now 🎂', 'bot');
-    inputWrapper.innerHTML = '';
+  if (confirmation === "yes") {
+    showMessage("Yes", "user");
+    showMessage("Thank you for your order! We are processing it now 🎂", "bot");
+    inputWrapper.innerHTML = "";
   } else {
-    showMessage('No', 'user');
-    showMessage('No worries! Let us know if you change your mind.', 'bot');
-    inputWrapper.innerHTML = '';
+    showMessage("No", "user");
+    showMessage("No worries! Let us know if you change your mind.", "bot");
+    inputWrapper.innerHTML = "";
   }
-
-
-}
-
+};
 
 setTimeout(greetUser, 1000);
-
