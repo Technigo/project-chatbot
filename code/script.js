f320fc8e3eab9ae266e6cb5f8fe3cdbe057ee0c1
Malin <malinlundhe@hotmail.com>
2023-12-10 15:30:58 +0100

    added eventlistener for user interaction

--- code/script.js
+++ code/script.js
@@ -6,7 +6,7 @@ const userPreferences = {
 };
 
 //Function to show messages
-const showMessage = (message, sender) => {
+const showMessage = (message, sender, options) => {
   const chat = document.getElementById('chat');
   const userInput = document.getElementById('user-input');
 
@@ -24,6 +24,7 @@ const showMessage = (message, sender) => {
       <img src="assets/bot.png" alt="Bot" />
       <div class="bubble bot-bubble">
         <p>${message}</p>
+        ${options ? options.map(option => `<button class="option-btn" onclick="${option.handler}">${option.label}</button>`).join('') : ''}
       </div>
     </section>
   `;
@@ -63,9 +64,25 @@ const provideCoffeeRecommendation = (preference, strength, extras) => {
     recommendation = 'You might enjoy a sweetened espresso!';
   } else if (preference === 'cold' && strength === 'Medium' && extras === 'Milk') {
     recommendation = 'How about trying an iced latte?';
+  } else if (preference === 'hot' && strength === 'Medium' && extras === 'No thanks') {
+    recommendation = 'A classic black coffee might be your choice!';
+  } else if (preference === 'cold' && strength === 'Wake me up' && extras === 'Milk') {
+    recommendation = 'Try a strong iced latte for an energy boost!';
+  } else if (preference === 'hot' && strength === 'Caffeinefree' && extras === 'Sweetness') {
+    recommendation = 'Consider a decaffeinated sweetened coffee!';
+  } else if (preference === 'cold' && strength === 'Medium' && extras === 'No thanks') {
+    recommendation = 'How about a refreshing black iced coffee?';
+  } else if (preference === 'hot' && strength === 'Wake me up' && extras === 'Milk') {
+    recommendation = 'A creamy and strong hot latte could be perfect!';
+  } else if (preference === 'cold' && strength === 'Caffeinefree' && extras === 'Sweetness') {
+    recommendation = 'Explore a decaffeinated sweetened iced coffee!';
+  } else if (preference === 'hot' && strength === 'Medium' && extras === 'Milk') {
+    recommendation = 'A classic hot latte with milk might suit your taste!';
+  } else if (preference === 'cold' && strength === 'Wake me up' && extras === 'Sweetness') {
+    recommendation = 'How about a sweetened iced coffee for a refreshing kick?';
   } else {
     recommendation = 'We have many options! Feel free to explore our menu.';
-}
+  }
 
   showMessage(recommendation, 'bot');
 
@@ -78,16 +95,30 @@ const startCoffeeChatbot () => {
 
 //Function to ask the second question
 const showSecondQuestion = () => {
-  showMessage('Do you prefer your coffee hot or cold?', 'bot');
+  const options = [
+    { label: 'Hot', handler: 'handleCoffeePreference("hot")' },
+    { label: 'Cold', handler: 'handleCoffeePreference("cold")' }
+  ];
+  showMessage('Do you prefer your coffee hot or cold?', 'bot', options);
 };
 
 //Function to ask the third question
 const showThirdQuestion = () => {
+  const options = [
+    { label: 'Wake me up', handler: 'handleCoffeeStrength("wake me up")' },
+    { label: 'Medium', handler: 'handleCoffeeStrength("medium")' },
+    { label: 'Caffeinefree', handler: 'handleCoffeeStrength("caffeinefree")' }
+  ];
   showMessage('How many "horsepowers" do you need in your coffee?', 'bot');
 };
 
 //Function to ask the fourth question
 const showFourthQuestion = () => {
+  const options = [
+    { label: 'Sweet', handler: 'handleCoffeeExtras("sweet")' },
+    { label: 'Milk', handler: 'handleCoffeeExtras("milk")' },
+    { label: 'No thanks', handler: 'handleCoffeeExtras("no thanks")' }
+  ];
   showMessage('Add some extra?', 'bot');
 };
 
