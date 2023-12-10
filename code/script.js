b6a4d17569ebef5ae2c032474496c74cd2a27bce
Malin <malinlundhe@hotmail.com>
2023-12-10 15:09:15 +0100

    add coffeerecommendations

--- code/script.js
+++ code/script.js
@@ -5,7 +5,73 @@ const userPreferences = {
   extras:'',
 };
 
-//Function to first question
+//Function to show messages
+const showMessage = (message, sender) => {
+  const chat = document.getElementById('chat');
+  const userInput = document.getElementById('user-input');
+
+  if (sender === 'user') {
+    chat.innerHTML += `
+    <section class="user-msg">
+      <div class="bubble user-bubble">
+        <p>${message}</p>
+      </div>
+    </section>
+  `;
+  } else if (sender === 'bot') {
+    chat.innerHTML += `
+    <section class="bot-msg">
+      <img src="assets/bot.png" alt="Bot" />
+      <div class="bubble bot-bubble">
+        <p>${message}</p>
+      </div>
+    </section>
+  `;
+}
+
+// Clear user input after showing the message
+userInput.value = '';
+};
+
+//Function to handle user's coffee preference
+const handleCoffeePreference = (preference) => {
+  showMessage(preference, 'user');
+  userPreferences.coffee = preference;
+  setTimeout(() => showThirdQuestion(), 1000);
+};
+
+//Function to handle user's coffee strength preference
+const handleCoffeeStrength = (strength) => {
+  showMessage(strength, 'user');
+  userPreferences.strength = strength;
+  setTimeout(() => showFourthQuestion(), 1000);
+};
+
+//Function to handle user's coffee extras preference
+const handleCoffeeExtras = (extras) =>  {
+  showMessage(extras, 'user');
+  userPreferences.extras = extras;
+  //Provide recommendations based on user's answer
+  provideCoffeeRecommendation(userPreferences.coffee, userPreferences.strength, userPreferences.extras);
+};
+
+//Function to provide recommendation
+const provideCoffeeRecommendation = (preference, strength, extras) => {
+  let recommendation = '';
+
+  if (preference === 'hot' && strength === 'Wake me up' && extras === 'Sweetness') {
+    recommendation = 'You might enjoy a sweetened espresso!';
+  } else if (preference === 'cold' && strength === 'Medium' && extras === 'Milk') {
+    recommendation = 'How about trying an iced latte?';
+  } else {
+    recommendation = 'We have many options! Feel free to explore our menu.';
+}
+
+  showMessage(recommendation, 'bot');
+
+};
+
+//Function to ask first question
 const startCoffeeChatbot () => {
   showMessage('Do you want help to order a coffee?', 'bot');
 };
