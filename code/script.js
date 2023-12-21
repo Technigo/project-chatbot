8de57e71aaee263d8b7bdf7080f8fe9ee69b220c
elbines <141184135+elbines@users.noreply.github.com>
2023-12-21 15:54:49 +0100

    changes after feedback

--- code/script.js
+++ code/script.js
@@ -1,290 +1,232 @@
 console.log('script.js is running');
-// Variables that point to selected DOM elements
+
+// Select important elements from the HTML and assign them to variables for later use
 const chat = document.getElementById('chat');
 const nameForm = document.getElementById('name-form');
-//const emailForm = document.getElementById('email-form');
 const nameInput = document.getElementById('name-input');
-const sendBtn = document.getElementById('send-btn');//
 const inputWrapper = document.getElementById('input-wrapper');
-//const chatToggle = document.getElementById('js-chat-toggle');
-//const chatContainer = document.getElementById('js-chat-container')
-
 const chatToggle = document.getElementById('js-chat-toggle');
 const chatContainer = document.getElementById('js-chat-container');
 
-// If you need any global variables that you can use across different functions, declare them here:
-
-//let userEmail = '';
+// Declare global variables to store user information
 let userName = '';
-const choice = '';
-const yourPrice = '';
-let answer = '';
 
-// Declare your functions after this comment
-
+// Funct. to display messages in the chat window
 const showMessage = (message, sender) => {
-
-  if (sender === 'user') {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
+  let messageSection = `
+      <section class='${sender}-msg'>
+        <img src='assets/${sender}1.png' alt='${sender}' />
+        <div class='bubble ${sender}-bubble'>
           <p>${message}</p>
         </div>
-        <img src="assets/user1.png" alt="User" />  
-      </section>
-    `
-  }
-  else if (sender === 'bot') {
-    //console.log(sender);
-    //console.log(message);
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/bot1.png" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>`
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+      </section>`;
 
+  chat.innerHTML += messageSection;
+  chat.scrollTop = chat.scrollHeight;
+};
 
+// Capitalize the first letter of a string
 const capitalizeFirstLetter = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
-//Changes first letter in name to capital letter
 
+// Send a series of greetings from the bot when the page loads
 const greetUser = () => {
-  showMessage(`Hello there!`, 'bot')
-  showMessage(`I'm Elbine, a chatbot.`, 'bot')
-  showMessage(`What's your name?`, 'bot')
-}
+  showMessage(`Hello there!`, 'bot');
+  showMessage(`I'm Elbine, a chatbot.`, 'bot');
+  showMessage(`What's your name?`, 'bot');
+};
 
-const userNameInput = event => {
+// Handle the event when the user submits their name
+const userNameInput = (event) => {
   event.preventDefault();
-  let userName = nameInput.value.trim()
-  //trim(): to remove any leading or trailing spaces from the user's input to ensure consistent capitalization.
+  userName = nameInput.value.trim();
 
-  if (userName !== "") {
+  if (userName) {
     userName = capitalizeFirstLetter(userName);
-
-    nameForm.remove(); //removes imputfield after writing the name
-
-    nameInput.value = "";
-    showMessage(` ${userName}`, "user");
-    setTimeout(() => reply(userName), 700);
-    setTimeout(() => askForChoise(), 1800);
+    nameForm.remove();
+    showMessage(userName, 'user');
+    setTimeout(() => welcomeUser(userName), 500);
+    setTimeout(() => askForChoice(), 1000);
   }
 };
 
-function reply(userName) {
-  showMessage(`Welcome to 'Elbines-Plants' ${userName}!`, 'bot')
+function welcomeUser(userName) {
+  showMessage(`Welcome to 'Elbines-Plants' ${userName}!`, 'bot');
 }
 
-const askForChoise = (YesNo) => {
-  showMessage(`We can help you by selecting some options for you based on your preferences. <br><br> Would you like to continue?`, 'bot')
+const askForChoice = () => {
+  showMessage(`Would you like to continue?`, 'bot');
+  inputWrapper.innerHTML = `
+    <button class='send-btn' id='Yes'>YES</button> 
+    <button class='send-btn' id='No'>NO</button>`;
 
-  inputWrapper.innerHTML =
-    `
-    <button class="send-btn" id="Yes">YES</button> 
-    <button class="send-btn" id="No">NO</button>
-    `;
+  //Event listeners to the Yes and No buttons
+  document.querySelectorAll('.send-btn').forEach(button => {
+    button.addEventListener('click', handleContinueChoice);
+  });
+  chat.scrollTop = chat.scrollHeight;
+};
 
-  //Adding eventlisteners to buttons
-  const choiseButtons = inputWrapper.querySelectorAll('.send-btn');
-  choiseButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      const choicIsMade = event.target.id
-      //console.log(choicIsMade)
-      showMessage(`${choicIsMade}`, 'user')
-      handleYesNoChoice(choicIsMade); //DID NOT WORK WITHOUT THIS
-    })
-  })
-  chat.scrollTop = chat.scrollHeight
-}
-
-
-function handleYesNoChoice(choice) {
-  inputWrapper.innerHTML = ''
-
+// Handles the user's choice of Yes or No
+function handleContinueChoice(event) {
+  inputWrapper.innerHTML = '';
+  const choice = event.target.id;
 
   if (choice === 'Yes') {
-    showMessage(`Please select a catagory from below:`, 'bot')
-    const plantCategory = ['Green Plants 🌱 ', 'Flowers 🌻 ', 'Plant Supplies 🧰 ']
-    showOptionsPlantCat(plantCategory)
+    showMessage(`Please select a category:`, 'bot');
+    showOptionsPlantCat(['Green Plants 🌱 ', 'Flowers 🌻 ', 'Plant Supplies 🧰 ']);
   } else if (choice === 'No') {
-    showMessage(`Ok, I understand. Feel free to browse our webpage. If you need help be sure to ask!`, 'bot')
+    showMessage(`Feel free to browse our webpage.`, 'bot');
   }
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight;
 }
 
+// Displays buttons for different plant categories
 function showOptionsPlantCat(options) {
   inputWrapper.innerHTML = '';
-
   options.forEach(option => {
     const button = document.createElement('button');
     button.className = 'send-btn';
     button.textContent = option;
-    button.addEventListener('click', () => {
-      showMessage(`${option}`, 'user');
-      setTimeout(() => askPriceRange(option), 700)
-      //So I can use selected option in the next function
-    });
+    //Event listeners to each category button
+    button.addEventListener('click', () => selectCategory(option));
     inputWrapper.appendChild(button);
   });
 }
 
+// Handle user's selection of a plant category
+function selectCategory(option) {
+  showMessage(option, 'user');
+  setTimeout(() => askPriceRange(option), 500);
+}
+
+// Ask user for preferred price range
 function askPriceRange(option) {
-  showMessage(`What is your price range for<br> ${option}?`, 'bot')
-
-
-  //Drop down menu 
+  showMessage(`What is your price range for ${option}?`, 'bot');
   inputWrapper.innerHTML = `
-  <select id="price-select">
-  <option value="">Please choose an option:</option>
-  <option value="PriceRange1">From 100 - 300,- </option>
-  <option value="PriceRange2">From 300 - 500,- </option>
-  <option value="PriceRange3">From 500 - 1000,- </option>
-  <option value="PriceRange4">1000,- and up </option>
-  </select>
-  `;
+    <select id='price-select'>
+      <option value=''>Please choose an option:</option>
+      <option value='PriceRange1'>From 100 - 300,- </option>
+      <option value='PriceRange2'>From 300 - 500,- </option>
+      <option value='PriceRange3'>From 500 - 1000,- </option>
+      <option value='PriceRange4'>1000,- and up </option>
+    </select>`;
 
-  select = document.getElementById("price-select")
-
-  const priceSelected = document.getElementById('price-select')
-
-    .addEventListener('change', (event) => {
-      //Storing the value of the event target by redeclaring global variabel
-      selectedPriRan = event.target.value
-
-      select.remove() //Removing the buttons to make the input look clean.
-      setTimeout(() => confirmPriceRange(priceSelected), 700)
-    })
+  // Event listener to the price range selection
+  document.getElementById('price-select').addEventListener('change', confirmPriceRange);
 }
 
-const confirmPriceRange = (priceRange) => {
-  if (selectedPriRan === 'PriceRange1') {
-    selectedPriRan = "100 - 300kr"
-    showMessage(`My price range is ${selectedPriRan}`, 'user')
-    setTimeout(() => showMessage(`Great! I will select some great options (in the price range ${selectedPriRan}) for you. I hope you love them as much as I do!`, 'bot'), 500)
-  }
-  else if
-    (selectedPriRan === 'PriceRange2') {
-    selectedPriRan = "300 - 500kr"
-    showMessage(`My price range is  ${selectedPriRan}`, 'user')
-    setTimeout(() => showMessage(`How fun! I will select some great options (in the price range ${selectedPriRan}) for you. I hope you love them as much as I do!`, 'bot'), 500)
-  }
-  else if
-    (selectedPriRan === 'PriceRange3') {
-    selectedPriRan = "500 - 1000kr"
-    showMessage(`My price range is  ${selectedPriRan}`, 'user')
-    setTimeout(() => showMessage(`How exciting! I will select some great options (in the price range ${selectedPriRan}) for you. I hope you love them as much as I do!`, 'bot'), 500)
-  }
-  else if
-    (selectedPriRan === 'PriceRange4') {
-    selectedPriRan = "1000kr and up"
-    showMessage(`My price range is ${selectedPriRan}`, 'user')
-    setTimeout(() => showMessage(`Wow, now we're talking! I will select some amazing options (in the price range ${selectedPriRan}) for you. I just know you'll love them!`, 'bot'), 500)
+// Confirm the selected price range
+const confirmPriceRange = (event) => {
+  let selectedPriRan = event.target.value;
+  let priceRangeText = '';
+
+  switch (selectedPriRan) {
+    case 'PriceRange1':
+      priceRangeText = '100 - 300kr';
+      break;
+    case 'PriceRange2':
+      priceRangeText = '300 - 500kr';
+      break;
+    case 'PriceRange3':
+      priceRangeText = '500 - 1000kr';
+      break;
+    case 'PriceRange4':
+      priceRangeText = '1000kr and up';
+      break;
+    default:
+      priceRangeText = 'Unknown range';
   }
 
-  setTimeout(() => deliveryStepBtn(), 1300)
-}
+  showMessage(`My price range is ${priceRangeText}`, 'user');
+  setTimeout(() => showMessage(`Options for ${priceRangeText} will be selected.`, 'bot'), 500);
+  setTimeout(() => deliveryStepBtn(), 1300);
+};
 
-
-// how tehy want it deliviered
+// Asks user for their delivery preference
 const deliveryStepBtn = () => {
-  showMessage(`If you were to place an order, would you prefer pick-up or delivery?`, 'bot');
+  showMessage(`Do you prefer pick-up or delivery?`, 'bot');
   inputWrapper.innerHTML = `
-    <button class="send-btn" id="pick-up">PICK UP</button> 
-    <button class="send-btn" id="delivery">DELIVERY</button>
-  `;
-  chat.scrollTop = chat.scrollHeight;
+    <button class='send-btn' id='pick-up'>PICK UP</button> 
+    <button class='send-btn' id='delivery'>DELIVERY</button>`;
 
-  const confirmBtndelivery = inputWrapper.querySelectorAll('.send-btn');
-  confirmBtndelivery.forEach(button => {
-    button.addEventListener('click', (event) => {
-      let deliveryChoice = event.target.id;
-      inputWrapper.innerHTML = ''; // clean up buttons
-
-      if (deliveryChoice === 'pick-up') {
-        showMessage(`I want to pick up myself.`, 'user');
-        showMessage(`Great! Orders can be picked up at our store.`, `bot`);
-      } else if (deliveryChoice === 'delivery') {
-        showMessage(`I would like a delivery.`, 'user');
-        showMessage(`Perfect! We offer next-day delivery.`, `bot`);
-      }
-      setTimeout(emailYesNo, 1000);
-    });
+  //Event listeners to the delivery options
+  document.querySelectorAll('.send-btn').forEach(button => {
+    button.addEventListener('click', handleDeliveryChoice);
   });
 };
 
-// Ask if they want an email
+// Handle the user's delivery choice
+function handleDeliveryChoice(event) {
+  let deliveryChoice = event.target.id;
+  showMessage(deliveryChoice === 'pick-up' ? `I want to pick up myself.` : `I would like a delivery.`, 'user');
+  showMessage(deliveryChoice === 'pick-up' ? `Sure! Orders can be picked up at our store.` : `Sure! We offer next-day delivery.`, `bot`);
+  setTimeout(emailYesNo, 500);
+}
+
+// Asks the user if they want to receive an email
 const emailYesNo = () => {
-  showMessage(`Would you like us to send you an e-mail with options based on your preferences?<br><br>Please enter Yes or No`, "bot");
-  document.getElementById("input-wrapper").innerHTML = `
-    <form id="yes-no-form">
-      <label for="yes-no-input"></label>
-      <input id="yes-no-input" type="text" placeholder="Type here...." autofocus="autofocus" />
-      <button class="send-btn" type="submit">Send</button>
-    </form>`;
-  document.getElementById("yes-no-form").addEventListener("submit", emailSubmit);
-  chat.scrollTop = chat.scrollHeight;
+  showMessage(`Do you want us to send you an email with options based on your preferences?`, 'bot');
+  showMessage(`Please type Yes or No.`, 'bot');
+  inputWrapper.innerHTML = `
+    <form id='yes-no-form'>
+      <input id='yes-no-input' type='text' placeholder='Type here...' autoFocus>`;
+
+  //Event listener for email consent form submission
+  document.getElementById('yes-no-form').addEventListener('submit', handleEmailConsent);
 };
 
-function emailSubmit(event) {
+// Handle the submission of the Yes/No question about receiving an email
+function handleEmailConsent(event) {
   event.preventDefault();
-  const userInput = document.getElementById("yes-no-input").value.trim().toLowerCase();
+  const userInput = document.getElementById('yes-no-input').value.trim().toLowerCase();
   showMessage(userInput, 'user');
-  if (userInput === "yes") {
+  if (userInput === 'yes') {
     enterEmailForPreferences();
-  } else if (userInput === "no") {
-    setTimeout(() => showMessage("No problem, feel free to browse our site!", "bot"), 700);
+  } else if (userInput === 'no') {
+    setTimeout(() => showMessage('No problem, feel free to browse our site!', 'bot'), 500);
   } else {
-    setTimeout(() => showMessage("Invalid answer, please enter Yes or No.", "bot"), 700);
+    setTimeout(() => showMessage('Invalid answer, please enter Yes or No.', 'bot'), 500);
   }
 };
 
-
-// Ask for email
+// Asks user to enter their email
 const enterEmailForPreferences = () => {
-  showMessage(`Please enter your e-mail`, "bot");
-  document.getElementById("input-wrapper").innerHTML = `
-    <form id="preferences-email-form">
-      <label for="preferences-email-input"></label>
-      <input id="preferences-email-input" type="text" placeholder="Enter your email..." required autofocus="autofocus" />
-      <button class="send-btn" type="submit">Send</button>
-    </form>`;
-  document.getElementById("preferences-email-form").addEventListener("submit", submitPreferencesEmail);
-  chat.scrollTop = chat.scrollHeight;
+  showMessage(`Please enter your e-mail`, 'bot');
+  inputWrapper.innerHTML = `
+    <form id='preferences-email-form'>
+      <input id='preferences-email-input' type='email' placeholder='Enter your email...' required autoFocus>`;
+
+  //Event listener for preferences email form submission
+  document.getElementById('preferences-email-form').addEventListener('submit', submitPreferencesEmail);
 };
 
+// Handle the submission of user's email
 function submitPreferencesEmail(event) {
   event.preventDefault();
-  const email = document.getElementById("preferences-email-input").value;
+  const email = document.getElementById('preferences-email-input').value;
   showMessage(email, 'user');
   if (validateEmail(email)) {
-    setTimeout(() => showMessage(`Thank you! You will soon receive an email from us at ${email}.`, "bot"), 700);
-    // where can I save and later use and email like this?
+    setTimeout(() => showMessage(`Thank you! You will soon receive an email at ${email}.`, 'bot'), 500);
   } else {
-    setTimeout(() => showMessage("Please enter a valid email address.", "bot"), 700);
+    setTimeout(() => showMessage('Please enter a valid email address.', 'bot'), 500);
   }
 };
 
-// Validate that email is typed
+// Validate the entered email format, so only text that looks like emails are approved
 function validateEmail(email) {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(String(email).toLowerCase());
 };
 
+// Event listener for submitting the user's name
+nameForm.addEventListener('submit', userNameInput);
 
-
-// Set up your eventlisteners here ??
-
-nameForm.addEventListener('submit', userNameInput)
-
-chatToggle.addEventListener('click', function () {
+// Event listener for toggling the chat interface
+chatToggle.addEventListener('click', () => {
   chatContainer.classList.toggle('open');
 });
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 700)
+
+// Calls the greetUser function shortly after the page loads
+setTimeout(greetUser, 500);
