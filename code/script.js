2ff7c37c2749c84541636ded39ac7ed82f196fe4
SofieFerrari <sofiestrahl@gmail.com>
2024-02-13 20:51:45 +0100

    started working on age/portion size. Tried onchange.

--- code/script.js
+++ code/script.js
@@ -1,8 +1,8 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat');
-const sendButton = document.getElementById('send-button');
-const nameInput = document.getElementById('name-input');
-const foodCategory =  document.getElementById("food-category");
+const chat = document.getElementById("chat");
+const sendButton = document.getElementById("send-button");
+const nameInput = document.getElementById("name-input");
+const foodCategory = document.getElementById("food-category");
 const nameWrapper = document.getElementById("input-wrapper");
 
 const pizzaButton = document.getElementById("pizza-button");
@@ -13,13 +13,15 @@ const subPizza = document.getElementById("sub-pizza");
 const subSalad = document.getElementById("sub-salad");
 const subKebab = document.getElementById("sub-kebab");
 
+const subSelect = document.getElementsByClassName("sub-select");
+
 // Functions goes here 👇
 
 // A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // The if statement checks if the sender is the user and if that's the case it inserts
   // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -27,10 +29,10 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    `;
     // The else if statement checks if the sender is the bot and if that's the case it inserts
     // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -38,45 +40,56 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
 
   // This little thing makes the chat scroll to the last message when there are too many to
   // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
-
+  chat.scrollTop = chat.scrollHeight;
+};
 
 // A function to start the conversation
 const greetUser = () => {
   // Here we call the function showMessage, that we declared earlier with the argument:
   // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'bot')
+  showMessage("Hello there, what's your name?", "bot");
   // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
+  console.log(greetUser);
+};
 
 const foodOptions = (name) => {
   foodCategory.style.display = "flex";
   nameWrapper.style.display = "none";
   showMessage(
     `
-  Hi ${name}! What would you like to order? `, "bot"
+  Hi ${name}! What would you like to order? `,
+    "bot"
   );
 };
 
 //Skapa stor CS för alla val ex if pizza....alt, els if sallad osv
 const subOptions = (category) => {
-  showMessage(`You chose ${category}`, "bot");
   foodCategory.style.display = "none";
+  console.log(showMessage);
 
-  if (category == "pizza"){
+  if (category == "pizza") {
     subPizza.style.display = "flex";
-  } else if (category == "salad"){
+    showMessage("Pizza", "user");
+  } else if (category == "salad") {
     subSalad.style.display = "flex";
-  } else { subKebab.style.display = "flex"};
-}
+    showMessage("Salad", "user");
+  } else {
+    subKebab.style.display = "flex";
+    showMessage("Kebab", "user");
+  }
 
-//Store name for future use. 
+  showMessage(
+    `You chose ${category}. What kind of ${category} would you like?`,
+    "bot"
+  );
+};
+
+//Store name for future use.
 //Clear name input
 const handleNameInput = (event) => {
   event.preventDefault();
@@ -85,35 +98,38 @@ const handleNameInput = (event) => {
   showMessage(name, "user");
   nameInput.value = "";
   foodOptions(name);
-}
- 
+};
 
+/*
+const hungerCheck = () => {
+  showMessage ("How hungry are you?", "bot");
+  
+}
+*/
 
 // Eventlisteners goes here 👇
 sendButton.onclick = handleNameInput;
-//pizzaButton.addEventListener("click", pizzaOptions("pizza")); 
-// kebabButton.onclick = pizzaOptions("kebab"); 
+//pizzaButton.addEventListener("click", pizzaOptions("pizza"));
+// kebabButton.onclick = pizzaOptions("kebab");
 
-pizzaButton.addEventListener('click', 
-    function() {
-        subOptions('pizza');
-    }
-);
-saladButton.addEventListener('click', 
-function() {
-    subOptions('salad');
-}
-);
-kebabButton.addEventListener('click', 
-    function() {
-        subOptions('kebab');
-    }
-);
+pizzaButton.addEventListener("click", function () {
+  subOptions("pizza");
+});
+saladButton.addEventListener("click", function () {
+  subOptions("salad");
+});
+kebabButton.addEventListener("click", function () {
+  subOptions("kebab");
+});
+
+
+//OnChange skulle kunna funka för att kolla vilken option på Select.
+subSelect.onchange(showMessage(`I chose ${subSelect.value}`, "user"));
 
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
 // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
 // and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// 1.) the function we want to delay, and 2.) the delay in milliseconds
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+setTimeout(greetUser, 1000);
