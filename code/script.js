431405b06f8b4d129f576d0b2f1bbb61146ecb03
SofieFerrari <sofiestrahl@gmail.com>
2024-02-13 10:01:38 +0100

    created functions for name and food, created variabled for DOM and created eventListeners

--- code/script.js
+++ code/script.js
@@ -1,5 +1,13 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
+const chat = document.getElementById('chat');
+const sendButton = document.getElementById('send-button');
+const nameInput = document.getElementById('name-input');
+const foodCategory =  document.getElementById("food-category");
+const nameWrapper = document.getElementById("input-wrapper");
+
+const pizzaButton = document.getElementById("pizza-button");
+const saladButton = document.getElementById("salad-button");
+const kebabButton = document.getElementById("kebab-button");
 
 // Functions goes here 👇
 
@@ -34,15 +42,48 @@ const showMessage = (message, sender) => {
   chat.scrollTop = chat.scrollHeight
 }
 
+
 // A function to start the conversation
 const greetUser = () => {
   // Here we call the function showMessage, that we declared earlier with the argument:
   // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'user')
+  showMessage("Hello there, what's your name?", 'bot')
   // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
 }
 
+const foodOptions = (name) => {
+  foodCategory.style.display = "flex";
+  nameWrapper.style.display = "none";
+  showMessage(
+    `
+  Hi ${name}! What would you like to order? `, "bot"
+  );
+};
+
+//Skapa stor CS för alla val ex if pizza....alt, els if sallad osv
+const pizzaOptions = (category) => {
+  showMessage(`You chose ${category}`, "bot");
+
+}
+
+//Store name for future use. 
+//Clear name input
+const handleNameInput = (event) => {
+  event.preventDefault();
+  const name = nameInput.value;
+  console.log(name);
+  showMessage(name, "user");
+  nameInput.value = "";
+  foodOptions(name);
+}
+ 
+
+
 // Eventlisteners goes here 👇
+sendButton.onclick = handleNameInput;
+pizzaButton.onclick = pizzaOptions("pizza"); 
+kebabButton.onclick = pizzaOptions("kebab"); 
+
 
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
