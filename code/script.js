bae1a0948d5c07ac607032a9dcf11081ac4e1f21
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-02-14 11:25:50 +0100

    added name age gender

--- code/script.js
+++ code/script.js
@@ -1,14 +1,15 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
-
+const chat = document.getElementById("chat");
+const input = document.getElementById("name-input");
+const form = document.getElementById("name-form");
 // Functions goes here 👇
 
 // A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
   // The if statement checks if the sender is the user and if that's the case it inserts
   // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
-    console.log("Message handled by user:", message)
+  if (sender === "user") {
+    console.log("Message handled by user:", message);
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -16,11 +17,11 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    `;
     // The else if statement checks if the sender is the bot and if that's the case it inserts
     // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
-    console.log("Message handled by bot:", message)
+  } else if (sender === "bot") {
+    console.log("Message handled by bot:", message);
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -28,28 +29,74 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
 
   // This little thing makes the chat scroll to the last message when there are too many to
   // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+  chat.scrollTop = chat.scrollHeight;
+};
 
 // A function to start the conversation
 const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what can i help you with?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
+  showMessage("Hello there, what´s your name?", "bot");
+};
 
 // Eventlisteners goes here 👇
+const handleNameInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  const name = input.value;
+  showMessage(name, "user");
+  input.value = "";
+  showMessage(`Welcome ${name}`, "bot");
+  //tar bort så att inte boten frågar igen om namn
+  event.target.removeEventListener("submit", handleNameInput);
+  setTimeout(askAge, 1000);
+};
+//att den frågar om namnet o går vidare till nästa
+function askName() {
+  form.addEventListener("submit", handleNameInput);
+}
+// event.target.removeEventListener("submit", handleNameInput);
+// After 1 second, show the next question by invoking the next function.
+// passing the name into it to have access to the user's name if we want
+// to use it in the next question from the bot.
+// const askNextQuestion = () => {
+// };
+const handleAgeInput = (event) => {
+  event.preventDefault();
+  const age = input.value;
+  showMessage(`${age}`, "user");
+  input.value = "";
+  setTimeout(askGender, 1000);
+};
+function askAge() {
+  showMessage(`how old are you?`, "bot");
+  form.addEventListener("submit", handleAgeInput);
+}
+
+const handleGenderInput = (event) => {
+  event.preventDefault();
+  const gender = input.value;
+  showMessage(`${gender}`, "user");
+  input.value = "";
+};
+function askGender() {
+  showMessage(`what's your gender?`, "bot");
+  form.addEventListener("submit", handleGenderInput);
+}
+
+// Händelselyssnare för inmatningsformuläret för ålder
 
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
 // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
 // and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// 1.) the function we want to delay, and 2.) the delay in milliseconds
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 500)
+setTimeout(2000);
+setTimeout(greetUser, 600);
+
+askName();
