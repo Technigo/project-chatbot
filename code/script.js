5f2f4c825112d984bae01fb2af70248f428b7466
Your El1an3 <eliane_w@yahoo.com>
2024-02-14 21:30:41 +0100

    version_1: name input

--- code/script.js
+++ code/script.js
@@ -1,53 +1,75 @@
-// DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
-
-// Functions goes here 👇
+// Variables that point to the selected DOM elements
+const chat = document.getElementById(`chat`);
+const nameInput = document.getElementById(`name-input`);
+const inputWrapper = document.getElementById(`input-wrapper`);
 
 // A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  //the if statement checks if the sender is user and if that's the case outputs the message
+  if (sender === "user") {
+    console.log(sender);
+    console.log(message);
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user-crab.jpg" alt="User" />  
       </section>
-    `
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+    `;
+  } else if (sender === "bot") {
+    //the else if statement checks if the sender is bot and if that's the case outputs the message
+    console.log(message);
+    console.log(sender);
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/bot-fish.jpg" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
-
-  // This little thing makes the chat scroll to the last message when there are too many to
-  // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+  // makes the chat scroll to the last message when there are too many
+  chat.scrollTop = chat.scrollHeight;
+};
 
 // A function to start the conversation
 const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
+  showMessage(
+    `Hello. I'm Finnly. Nice to meet you. Wanna find out what kind of sea creature you are?`,
+    `bot`
+  );
+  showMessage(
+    `Great! Let's get started. But first, let me get your name.`,
+    `bot`
+  );
+};
 
-// Eventlisteners goes here 👇
+//user types in name
+const nameOutput = () => {
+  const name = nameInput.value;
+  showMessage(`Hi, my name is ${name}`, `user`);
+  username = name;
+  nameInput.value = ``; //Clearing the input field
+  inputWrapper.innerHTML = ``;
+};
+nameOutput(nameInput);
+
+//bot welcomes the user and asks first question, 2 options of buttons to chose from
+const creatureNature = (a, b) => {
+  showMessage(
+    `Hi ${username}. Now let's check out some of your personality traits to find out what sea creature suits you. Are you mor of an introvert or an extrovert?`,
+    "bot"
+  );
+};
+
+// Eventlisteners
 
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
 // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
 // and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// 1.) the function we want to delay, and 2.) the delay in milliseconds
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+setTimeout(greetUser, 1000);
