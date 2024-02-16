d1d305ba90c891ab7b5383335ae0a0c1c9ee1598
izzibizz <contact@izabellind.com>
2024-02-16 10:52:53 -0800

    update confetti

--- code/script.js
+++ code/script.js
@@ -215,6 +215,37 @@ const handleThirdChoice = (subChoice) => {
     })
 }
 
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+};
+
+function shoot() {
+  confetti({
+    ...defaults,
+    particleCount: 30,
+    scalar: 1.2,
+    shapes: ["circle", "square"],
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 20,
+    scalar: 2,
+    shapes: ["emoji"],
+    shapeOptions: {
+      emoji: {
+        value: ["🦄", "🌈"],
+      },
+    },
+  });
+}
+
+
 //Test
  
 
@@ -234,3 +265,4 @@ const handleThirdChoice = (subChoice) => {
 
 
 /// setTimeout -> Nått event i slutet som firar att ordern är lagd
+
