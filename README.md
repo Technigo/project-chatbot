86de319ee5d9c2146401a6b7f3b5b929e44d6c5d
izzibizz <contact@izabellind.com>
2024-02-16 12:23:21 -0800

    final changes, added readme

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-# Project Name
+# Valentine's Chatbot
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+This weeks project was to make a chatbot with focus on DOM and functions.The user was supposed to place an order, the bot respond to the user input. We worked in theams of 2, this project was made by Alma Hellström and Izabel Lind Färnstrand. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+We solved the problem by creating functions handling the different types of user input. The functions were triggered by event listeners listening for clicks on the order buttons. We planned the program by discussing how we wanted the order to flow. If we had more time, we would have refactored the code to make it cleaner, lighter and easier to follow, as it got quite long and heavy with plenty of if statements.
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -1,20 +1,15 @@
-// DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
-const submitButton = document.getElementById('send-btn')
-const nameInput = document.getElementById('name-input')
-const form = document.getElementById('form')
-const inputWrapper = document.getElementById('input-wrapper')
+// DOM selectors 👇
+const chat = document.getElementById("chat")
+const submitButton = document.getElementById("send-btn")
+const nameInput = document.getElementById("name-input")
+const form = document.getElementById("form")
+const inputWrapper = document.getElementById("input-wrapper")
 const sound = document.getElementById("click-sound")
 
-
-
-// Functions goes here 👇
-
-// A function that will add a chat bubble in the correct place based on who the sender is
+// Function that will add a chat bubble
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  // User bubble
+  if (sender === "user") {
     sound.play()
     chat.innerHTML += `
       <section class="user-msg">
@@ -24,9 +19,8 @@ const showMessage = (message, sender) => {
         <img src="assets/user2.png" alt="User" />  
       </section>
     `
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+    // Bot bubble
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot2.png" alt="Bot" />
@@ -37,53 +31,41 @@ const showMessage = (message, sender) => {
     `
   }
 
-
-  // This little thing makes the chat scroll to the last message when there are too many to
-  // be shown in the chat box
+  // Scroll down to see the last message
   chat.scrollTop = chat.scrollHeight
 }
 
 // A function to start the conversation
 const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage(`Hello there my name is Fikabot, what's your name?`, 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
+  showMessage(`Hello there my name is Fikabot, what's your name?`, "bot")
 }
 
-// Eventlisteners goes here 👇
-
-
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
-// This means the greeting function will be called one second after the website is loaded.
+//Call for first greeting, with a short delay
 setTimeout(greetUser, 1000)
 
-
-
+//Function that handle the name input
 const handleNameInput = (event) => {
   event.preventDefault()
   // Store the value in a variable so we can access it after we
-  // clear it from the input
   const name = nameInput.value
-  showMessage(`I'm ${name}`, 'user')
+  if (name !== "") {
+    showMessage(`I'm ${name}`, "user")
+    setTimeout(() => firstChoice(name), 1000)
+  } else {
+    showMessage(`I'm ${name}`, "user")
+    showMessage(`Please type your real name.`, "bot")
+    setTimeout(() => greetUser(), 1000)
+  }
+  // clear value from the input
   nameInput.value = ""
-  setTimeout(() => firstChoice(name), 1000);
-
-  // After 1 second, show the next question by invoking the next function.
-  // passing the name into it to have access to the user's name if we want
-  // to use it in the next question from the bot.
-
-
 }
 
+//Call for handleNameInput func. to activate 1st responce
 submitButton.onclick = handleNameInput
 
 let choice
 
+// Add buttons for choice and handle user choice
 const firstChoice = (name) => {
   showMessage(`So ${name} what would you like to order?`, "bot")
   form.innerHTML = `
@@ -92,116 +74,133 @@ const firstChoice = (name) => {
   <button class="order-btn" id="bread" type="submit">Bread</button>
   <button class="order-btn" id="pastry" type="submit">Pastry</button></div>`
 
-  document.getElementById("coffee").addEventListener("click", (handleFirstChoice) => {
-    handleFirstChoice.preventDefault()
-    choice = "coffee"
-    showMessage("I would like a coffee, please!", "user")
-    setTimeout(() => handleSecondChoice(choice), 1000)
-    
-  })
-  document.getElementById("bread").addEventListener("click", (handleFirstChoice) => {
-    handleFirstChoice.preventDefault()
-    choice = "bread"
-    showMessage("I would like some bread, please!", "user")
-    setTimeout(() => handleSecondChoice(choice), 1000)
-   
-  })
-  document.getElementById("pastry").addEventListener("click", (handleFirstChoice) => {
-    handleFirstChoice.preventDefault()
-    choice = "pastry"
-    showMessage("I would like a pastry, please!", "user")
-    setTimeout(() => handleSecondChoice(choice), 1000)
-    
-  })
+  document
+    .getElementById("coffee")
+    .addEventListener("click", (handleFirstChoice) => {
+      handleFirstChoice.preventDefault()
+      choice = "coffee"
+      showMessage("I would like a coffee, please!", "user")
+      setTimeout(() => handleSecondChoice(choice), 1000)
+    })
+  document
+    .getElementById("bread")
+    .addEventListener("click", (handleFirstChoice) => {
+      handleFirstChoice.preventDefault()
+      choice = "bread"
+      showMessage("I would like some bread, please!", "user")
+      setTimeout(() => handleSecondChoice(choice), 1000)
+    })
+  document
+    .getElementById("pastry")
+    .addEventListener("click", (handleFirstChoice) => {
+      handleFirstChoice.preventDefault()
+      choice = "pastry"
+      showMessage("I would like a pastry, please!", "user")
+      setTimeout(() => handleSecondChoice(choice), 1000)
+    })
 }
 let subChoice
 
+//Function handling the subchoice of the order
 const handleSecondChoice = (choice) => {
-  showMessage(`Great choice, ${choice} coming up, what kind of ${choice} would you like?`, "bot")
+  showMessage(
+    `Great choice, ${choice} coming up, what kind of ${choice} would you like?`,
+    "bot"
+  )
   if (choice === "coffee") {
     form.innerHTML = `<div class="sub-categories">
     <button class="send-btn" id="filter" type="submit">Filter Coffee</button>
     <button class="send-btn" id="espresso" type="submit">Espresso</button>
     <button class="send-btn" id="flat" type="submit">Flat White</button></div>`
-    
-    document.getElementById("filter").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "filter coffee"
-      showMessage("I would like a filter coffee, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
-    document.getElementById("espresso").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "espresso"
-      showMessage("I would like an espresso, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-    
-    })
-    document.getElementById("flat").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "flat white"
-      showMessage("I would like a flat white, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-    
-    })
+
+    document
+      .getElementById("filter")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "filter coffee"
+        showMessage("I would like a filter coffee, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("espresso")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "espresso"
+        showMessage("I would like an espresso, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("flat")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "flat white"
+        showMessage("I would like a flat white, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
   } else if (choice === "bread") {
     form.innerHTML = `<div class="sub-categories">
     <button class="send-btn" id="baguette" type="submit">Baguette</button>
     <button class="send-btn" id="sourdough" type="submit">Sourdough</button>
     <button class="send-btn" id="rye" type="submit">Rye Bread</button></div>`
-  
-    document.getElementById("baguette").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "baguette"
-      showMessage("I would like a baguette, s'il vous plaît!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
-    document.getElementById("sourdough").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "sourdough bread"
-      showMessage("I would like sourdough bread, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
-    document.getElementById("rye").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "rye bread"
-      showMessage("I would like a rye bread, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
+
+    document
+      .getElementById("baguette")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "baguette"
+        showMessage("I would like a baguette, s'il vous plaît!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("sourdough")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "sourdough bread"
+        showMessage("I would like sourdough bread, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("rye")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "rye bread"
+        showMessage("I would like a rye bread, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
   } else if (choice === "pastry") {
     form.innerHTML = `<div class="sub-categories">
     <button class="send-btn" id="semla" type="submit">Semla</button>
     <button class="send-btn" id="cinnamon" type="submit">Cinnamon Bun</button>
     <button class="send-btn" id="brownie" type="submit">Brownie</button></div></form>`
-    
-    document.getElementById("semla").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "semla"
-      showMessage("I would like a semla, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
-    document.getElementById("cinnamon").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "cinnamon bun"
-      showMessage("I would like cinnamon bun, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-     
-    })
-    document.getElementById("brownie").addEventListener("click", (handleSecondChoice) => {
-      handleSecondChoice.preventDefault()
-      subChoice = "brownie"
-      showMessage("I would like a brownie, please!", "user")
-      setTimeout(() => handleThirdChoice(subChoice), 1000)
-      
-    })
-  }}
 
-  //Options for amount
+    document
+      .getElementById("semla")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "semla"
+        showMessage("I would like a semla, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("cinnamon")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "cinnamon bun"
+        showMessage("I would like cinnamon bun, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+    document
+      .getElementById("brownie")
+      .addEventListener("click", (handleSecondChoice) => {
+        handleSecondChoice.preventDefault()
+        subChoice = "brownie"
+        showMessage("I would like a brownie, please!", "user")
+        setTimeout(() => handleThirdChoice(subChoice), 1000)
+      })
+  }
+}
+
+//Function handling the amount input from user
 const handleThirdChoice = (subChoice) => {
   showMessage(`How many ${subChoice}s would you like?`, "bot")
   form.innerHTML = `<input id="amount-input" type="text" />
@@ -212,28 +211,63 @@ const handleThirdChoice = (subChoice) => {
     const amount = amountInput.value
     showMessage(`${amount} ${subChoice}s, please!`, `user`)
     amountInput.value = ""
-    })
+    setTimeout(() => handleFinalConfirmation(amount), 1000)
+  })
 }
 
-
-
-//Test
- 
-
-     //showMessage(`You have ordered ${event.target.value} ${subChoice}s`,`bot`)
-    //showMessage(`Thank you`, `user`)
-
-  //härifrån buggar det (kod från 15/2)
-  /*
-  const numberInput = document.getElementById("number-input")
- document.getElementById("amount-btn").addEventListener("click", () => {
-  handleThirdChoice.preventDefault()
-  const orderAmount = numberInput.value
-  numberInput.value = ""
-  showMessage(`I would like to order ${orderAmount} ${subChoice}s`, 'user')
+//Function giving the final response from bot depending on amount input from user
+const handleFinalConfirmation = (amount) => {
+  if (amount >= 1 && amount <= 10) {
+    showMessage(
+      `Thank you for your order! It will be ready in 10 minutes! ❤️`,
+      "bot"
+    )
+    setTimeout(shoot, 100)
+    form.remove()
+  } else if (amount > 10) {
+    showMessage(
+      `That's a lot! We take larger orders by phone, please call 000 123 456 ❤️`,
+      "bot"
+    )
+    form.remove()
+  } else if (amount === "0") {
+    showMessage(`Are you sure you chose the right amount?`, "bot")
+    setTimeout(() => handleThirdChoice(subChoice), 1000)
+  } else {
+    showMessage(`I didn't catch that, please try again! ❤️`, "bot")
+    setTimeout(() => handleThirdChoice(subChoice), 1000)
   }
-)}*/
+}
 
+//confetti function
+//setTimeout(shoot, 100);
 
-/// setTimeout -> Nått event i slutet som firar att ordern är lagd
+const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+}
 
+function shoot() {
+  confetti({
+    ...defaults,
+    particleCount: 30,
+    scalar: 1.2,
+    shapes: ["circle", "square"],
+    colors: ["#fa5a4e", "#e41304", "#a01b11", "#f7d6d3", "#ff6aa8"],
+  })
+
+  confetti({
+    ...defaults,
+    particleCount: 20,
+    scalar: 2,
+    shapes: ["emoji"],
+    shapeOptions: {
+      emoji: {
+        value: ["🥰", "❤️", "💗"],
+      },
+    },
+  })
+}
