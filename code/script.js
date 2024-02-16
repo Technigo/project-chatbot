67cec7f71eca55354d902cf9dd95a948347d87f8
fridaforser <fridaforser@gmail.com>
2024-02-16 13:06:10 +0100

    final changes

--- code/script.js
+++ code/script.js
@@ -1,23 +1,26 @@
-// DOM selectors goes here 
-const chat = document.getElementById('chat')
-const nameInput = document.getElementById('name-input')
-const button = document.getElementById('button')
-const nameForm = document.getElementById("name-form");
-const inputWrapper = document.getElementById("input-wrapper");
+//DOM selectors: 
+const chat = document.getElementById("chat")
+const nameForm = document.getElementById("name-form")
+const nameInput = document.getElementById("name-input")
+const button = document.getElementById("button")
 
-//Variables
-//Type of math-problem
-let typeChoice = ""
+const inputWrapper = document.getElementById("input-wrapper")
+
+//Variables:
+let userName = ""
+let mathType = ""
 //Numbers in the math-problems
 let a = ""
 let b = ""
 let c = ""
+let subtractOrder = ""
+let correctAnswer = ""
 
-// Functions goes here 
-// A function that will add a chat bubble in the correct place based on who the sender is
+//Functions: 
+//Adds a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // Checks if the sender is the user and adds posted message from the user
-  if (sender === 'user') {
+  //Checks if the sender is the user and adds posted message from the user
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -26,8 +29,8 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // Checks if the sender is the bot and adds posted message from the bot
-  } else if (sender === 'bot') {
+    //Checks if the sender is the bot and adds posted message from the bot
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -37,274 +40,223 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-  // Makes the chat scroll to the last message when there are many 
+  //Makes the chat scroll to the last message when there are many 
   chat.scrollTop = chat.scrollHeight
 }
-
+//Clears the chat window
+const clearWindow = () => {
+  chat.innerHTML = `
+    <section class="bot-msg">
+      <img src="assets/bot.png" id="endBot" alt="Bot" />
+    </section>
+  `
+}
 // Start the conversation
 const greetUser = () => {
-    showMessage("Hey, I'm Mathew, the mathbot. Welcome to my wonderful world of math. What's your name?", 'bot')
-nameForm.addEventListener("submit", userNameInput);
+  showMessage("Hey, I'm Mathew, the mathbot. Welcome to my wonderful world of math. What's your name?", "bot")
+  nameForm.addEventListener("submit", handleNameInput)
 }
-
-// Eventlisteners goes here 👇
-
- //Gives back name from input
-
-let userName = ""
-const userNameInput = (input) => {
-input.preventDefault(); 
-userName = nameInput.value;
-showMessage(`${userName}`, "user");
-nameInput.value = ""; 
-setTimeout(() => startQuestion(), 1000);
-};
-
-//Bot asks if you want to start
-const startQuestion = () => {
+//Stores the user name
+const handleNameInput = (input) => {
+  input.preventDefault()
+  userName = nameInput.value
+  showMessage(`${userName}`, "user")
+  nameInput.value = ""
+  setTimeout(() => askStartQuestion(), 1000)
+}
+//Asks if you want to start
+const askStartQuestion = () => {
   if (userName == ""){
-  showMessage(`I need a name to be able to talk to you. Let's try again - What is your name?`, "bot");
-  }
-
-  else {
-  showMessage(`Nice to meet you, ${userName}. Are you ready to solve some math problems?`, "bot");
-  //Buttons for answering "yes" or "no"
+    showMessage(`I need a name to be able to talk to you. Let's try again - What is your name?`, "bot")
+  } else {
+    showMessage(`Nice to meet you, ${userName}. Are you ready to solve some math problems?`, "bot")
+    //Buttons for answering "yes" or "no"
     inputWrapper.innerHTML = `
-    <button id="yes" type="submit" class="chat-btn">Yes</button>
-    <button id="no" type="submit" class="chat-btn">No</button>`
-    document.getElementById("yes").addEventListener("click", yesChoice);  
-    document.getElementById("no").addEventListener("click", noChoice);
+      <button id="yes" class="chat-btn" type="submit">Yes</button>
+      <button id="no" class="chat-btn" type="submit">No</button>
+    `
+    document.getElementById("yes").addEventListener("click", handleYes) 
+    document.getElementById("no").addEventListener("click", handleNo)
   }
 }
 //If you answer "no"
-const noChoice = () => {
+const handleNo = () => {
   showMessage(`No`, "user")
   showMessage(`Sorry to hear that, ${userName}. You're welcome back anytime!`, "bot")
   inputWrapper.innerHTML = ``
-  setTimeout(() => clearWindow(), 4000);
-};
-
-const clearWindow = () => {
-chat.innerHTML = `
-      <section class="bot-msg">
-          <img src="assets/bot.png" id="endBot" alt="Bot" />
-      </section>
-    `
+  setTimeout(() => clearWindow(), 4000)
 }
-
 //If you answer "yes"
-const yesChoice = () => {
+const handleYes = () => {
   showMessage(`Yes`, "user")
-  setTimeout(() => choiceQuestion(), 1000);
-};
+  setTimeout(() => askMathType(), 1000)
+}
 //Ask what type of question you want
-const choiceQuestion = () => {
-  showMessage(`What type of math problem do you want to solve?`, "bot");
-    inputWrapper.innerHTML = `
+const askMathType = () => {
+  showMessage(`What type of math problem do you want to solve?`, "bot")
+  inputWrapper.innerHTML = `
     <button id="add" type="submit" class="choice-btn">Addition</button>
     <button id="subtract" type="submit" class="choice-btn">Subtraction</button>
     <button id="multiply" type="submit" class="choice-btn">Multiplication</button>
-    <button id="divide" type="submit" class="choice-btn">Division</button>`
-    document.getElementById("add").addEventListener("click", additionChoice);  
-    document.getElementById("subtract").addEventListener("click", subtractionChoice);
-    document.getElementById("multiply").addEventListener("click", multiplicationChoice);  
-    document.getElementById("divide").addEventListener("click", divisionChoice);
-    
+    <button id="divide" type="submit" class="choice-btn">Division</button>
+  `
+  document.getElementById("add").addEventListener("click", handleAddition)  
+  document.getElementById("subtract").addEventListener("click", handleSubtraction)
+  document.getElementById("multiply").addEventListener("click", handleMultiplication)  
+  document.getElementById("divide").addEventListener("click", handleDivision)   
 }
 //If you choose addition
-const additionChoice = () => {
-  typeChoice = "addition"
-  showMessage(`Addition`, "user")
-  setTimeout(() => additionRandom(), 1000);
+const handleAddition = () => {
+  mathType = "addition"
+  showMessage (`Addition`, "user")
+  setTimeout(() => askAdditionQuestion(), 1000)
 }
 //If you choose subtraction
-const subtractionChoice = () => {
-  typeChoice = "subtraction"
-  showMessage(`Subtraction`, "user")
-  setTimeout(() => subtractionRandom(), 1000);
+const handleSubtraction = () => {
+  mathType = "subtraction"
+  showMessage (`Subtraction`, "user")
+  setTimeout(() => askSubtractionQuestion(), 1000)
 }
 //If you choose multiplication
-const multiplicationChoice = () => {
-  typeChoice = "multiplication"
+const handleMultiplication = () => {
+  mathType = "multiplication"
   showMessage(`Multiplication`, "user")
-  setTimeout(() => multiplicationRandom(), 1000);
+  setTimeout(() => askMultiplicationQuestion(), 1000)
 }
 //If you choose division
-const divisionChoice = () => {
-  typeChoice = "division"
+const handleDivision = () => {
+  mathType = "division"
   showMessage(`Division`, "user")
-  setTimeout(() => divisionRandom(), 1000);
+  setTimeout(() => askDivisionQuestion(), 1000)
 }
-
 //Prints a mathproblem with addition
-const additionRandom = () => {
-  a = Math.floor (Math.random() * 100) + 1;
-  b = Math.floor(Math.random() * 100) + 1;
+const askAdditionQuestion = () => {
+  a = Math.floor (Math.random() * 100) + 1
+  b = Math.floor(Math.random() * 100) + 1
   showMessage (`What is ${a}+${b}?`, "bot")
   inputWrapper.innerHTML = `
-      <form id="answerForm"> 
-        <label>Answer</label>
-        <input id="answer" type="text" />  
-        <button id="button" class="send-btn" type="submit"> 
-           Send
-        </button>
-      </form>
-    `
-  document.getElementById("answerForm").addEventListener("submit", answerInput);
+    <form id="answerForm"> 
+      <label>Answer</label>
+      <input id="answer" type="text" />  
+      <button id="button" class="send-btn" type="submit"> 
+          Send
+      </button>
+    </form>
+  `
+  document.getElementById("answerForm").addEventListener("submit", handleAnswer)
 }
 //Prints a mathproblem with subtraction
-let subtractOrder = ""
-const subtractionRandom = () => {
-  a = Math.floor (Math.random() * 10) + 1;
-  b = Math.floor(Math.random() * 100) + 1;
-  if (a >= b){
-  showMessage (`What is ${a}-${b}?`, "bot")
-  subtractOrder = "a-b"
+const askSubtractionQuestion = () => {
+  a = Math.floor (Math.random() * 100) + 1
+  b = Math.floor(Math.random() * 100) + 1
+  if (a >= b) {
+    showMessage (`What is ${a}-${b}?`, "bot")
+    subtractOrder = "a-b"
   } else {
-  showMessage (`What is ${b}-${a}?`, "bot")
-  subtractOrder = "b-a"
+    showMessage (`What is ${b}-${a}?`, "bot")
+    subtractOrder = "b-a"
   }
   inputWrapper.innerHTML = `
-      <form id="answerForm"> 
-        <label>Answer</label>
-        <input id="answer" type="text" />  
-        <button id="button" class="send-btn" type="submit"> 
-           Send
-        </button>
-      </form>
-    `
-  document.getElementById("answerForm").addEventListener("submit", answerInput);
+    <form id="answerForm"> 
+      <label>Answer</label>
+      <input id="answer" type="text" />  
+      <button id="button" class="send-btn" type="submit"> 
+          Send
+      </button>
+    </form>
+  `
+  document.getElementById("answerForm").addEventListener("submit", handleAnswer)
 }
 //Prints a mathproblem with multiplication
-const multiplicationRandom = () => {
-  a = Math.floor (Math.random() * 10) + 1;
-  b = Math.floor(Math.random() * 10) + 1;
+const askMultiplicationQuestion = () => {
+  a = Math.floor (Math.random() * 11)
+  b = Math.floor(Math.random() * 11)
   showMessage (`What is ${a}*${b}?`, "bot")
-   inputWrapper.innerHTML = `
-      <form id="answerForm"> 
-        <label>Answer</label>
-        <input id="answer" type="text" />  
-        <button id="button" class="send-btn" type="submit"> 
-           Send
-        </button>
-      </form>
-    `
-  document.getElementById("answerForm").addEventListener("submit", answerInput);
+  inputWrapper.innerHTML = `
+    <form id="answerForm"> 
+      <label>Answer</label>
+      <input id="answer" type="text" />  
+      <button id="button" class="send-btn" type="submit"> 
+          Send
+      </button>
+    </form>
+  `
+  document.getElementById("answerForm").addEventListener("submit", handleAnswer)
 }
 //Prints a mathproblem with division
-const divisionRandom = () => {
-  a = Math.floor (Math.random() * 10) + 1;
-  b = Math.floor(Math.random() * 10) + 1;
+const askDivisionQuestion = () => {
+  a = Math.floor (Math.random() * 10) + 1
+  b = Math.floor(Math.random() * 11)
   c = a*b
   showMessage (`What is ${c}/${a}?`, "bot")
-   inputWrapper.innerHTML = `
-      <form id="answerForm"> 
-        <label>Answer</label>
-        <input id="answer" type="text" />  
-        <button id="button" class="send-btn" type="submit"> 
-           Send
-        </button>
-      </form>
-    `
-  document.getElementById("answerForm").addEventListener("submit", answerInput);
+  inputWrapper.innerHTML = `
+    <form id="answerForm"> 
+      <label>Answer</label>
+      <input id="answer" type="text" />  
+      <button id="button" class="send-btn" type="submit"> 
+          Send
+      </button>
+    </form>
+  `
+  document.getElementById("answerForm").addEventListener("submit", handleAnswer)
 }
-
-//Handles the answer
-let answerInput = (addInput) => {
-addInput.preventDefault(); 
-showMessage(`${answer.value}`, "user");
-setTimeout(() => checkAnswer(), 1000);
-};
-
-
-let correctAnswer =""
-
+//Handle the answer
+let handleAnswer = (addInput) => {
+  addInput.preventDefault()
+  showMessage(`${answer.value}`, "user")
+  setTimeout(() => checkAnswer(), 1000)
+}
 //Checks the answer and replies "correct" or "wrong"
 const checkAnswer = () => {
-  //ADDITION
-  if (typeChoice === "addition"){
-    //Calculate the correct answer
+  if (mathType === "addition"){
     correctAnswer = a+b
-    //Checks if the answer is correct
     if (answer.value == correctAnswer){
       showMessage (`That's correct, good job! 🥳`, "bot")
-    }
-    else {
+    } else {
       showMessage (`Nope, that's incorrect. The answer is ${correctAnswer}.`, "bot")
     }
-    //Frågar om du vill köra igen
-    setTimeout(() => askNewProblem(), 2000);
-  }
-
-
-  //SUBTRACION
-    else if (typeChoice === "subtraction"){
-      ////Calculates the correct answerKollar vilket tal som kommer först och ger rätt svar
-      if (subtractOrder === "a-b"){
-        correctAnswer = a-b
-      } else if (subtractOrder === "b-a"){
-        correctAnswer = b-a
-      }
-      //Kollar om svaret är rätt
-      if (answer.value == correctAnswer){
+    setTimeout(() => askNewProblem(), 1000)
+  } else if (mathType === "subtraction") {
+    if (subtractOrder === "a-b") {
+      correctAnswer = a-b
+    } else if (subtractOrder === "b-a") {
+      correctAnswer = b-a
+    }
+    if (answer.value == correctAnswer) {
       showMessage (`That's correct, good job! 🥳`, "bot")
-      } else {
+    } else {
       showMessage (`Nope, that's incorrect. The answer is ${correctAnswer}.`, "bot")
-      }
-      //Frågar om du vill köra igen
-      setTimeout(() => askNewProblem(), 2000);
-      
-    
     }
-  
-    
-
-  //MULTIPLICATION
-    else if (typeChoice === "multiplication"){
-      //Calculates the correct answer
-      correctAnswer = a*b
-      //Kollar om svaret är rätt
-      if (answer.value == correctAnswer){
-      showMessage (`That's correct, good job! 🥳`, "bot")
-      } else{
-      showMessage (`Nope, that's incorrect. The answer is ${correctAnswer}.`, "bot")
-      }
-    //Frågar om du vill köra igen
-      setTimeout(() => askNewProblem(), 2000);
-
+    setTimeout(() => askNewProblem(), 1000)
+  } else if (mathType === "multiplication"){
+    correctAnswer = a*b
+    if (answer.value == correctAnswer){
+    showMessage (`That's correct, good job! 🥳`, "bot")
+    } else {
+    showMessage (`Nope, that's incorrect. The answer is ${correctAnswer}.`, "bot")
     }
-
-  //DIVISION
-    else if (typeChoice === "division"){
-    ///Calculates the correct answer
-      correctAnswer = c/a
-    //Kollar om svaret är rätt
-      if (answer.value == correctAnswer){
+    setTimeout(() => askNewProblem(), 1000)
+  } else if (mathType === "division"){
+    correctAnswer = c/a
+    if (answer.value == correctAnswer){
       showMessage (`That's correct, good job! 🥳`, "bot")
-      } else{
+    } else {
       showMessage (`Nope, that's incorrect. The answer is ${correctAnswer}.`, "bot")
-      }
-  //Frågar om du vill köra igen
-    setTimeout(() => askNewProblem(), 2000);
+    }
+    setTimeout(() => askNewProblem(), 1000)
   }
 }
  
 const askNewProblem = () => {
-
-showMessage (`Do you want to solve another math problem?`, "bot")
-    inputWrapper.innerHTML = `
+  showMessage (`Do you want to solve another math problem?`, "bot")
+  inputWrapper.innerHTML = `
     <button id="yes" type="submit" class="chat-btn">Yes</button>
-    <button id="no" type="submit" class="chat-btn">No</button>`
-    document.getElementById("yes").addEventListener("click", yesChoice);  
-    document.getElementById("no").addEventListener("click", noChoice);
-    
-   } 
+    <button id="no" type="submit" class="chat-btn">No</button>
+  `
+  document.getElementById("yes").addEventListener("click", handleYes)  
+  document.getElementById("no").addEventListener("click", handleNo)
+} 
 
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
-// This means the greeting function will be called one second after the website is loaded.
+// Calls for the greeting function one second after the website is loaded.
 setTimeout(greetUser, 1000)
 
