8e00452295d5a945a5a70821a0150170138491bb
Lina Roos <linaroos@MBPsomtlhorLina.home>
2024-02-17 23:37:07 +0100

    added more interaction

--- code/script.js
+++ code/script.js
@@ -6,6 +6,7 @@ const submit = document.getElementById('submit')
 const main = document.getElementById('main')
 const inputWrapper = document.getElementById('input-wrapper')
 
+// Separates bot and user
 const showMessage = (message, sender) => {
   if (sender === 'user') {
     chat.innerHTML += `
@@ -26,10 +27,10 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
-
+// Scroll function
   chat.scrollTop = chat.scrollHeight
 }
-
+// Greeting user
 const greetUser = () => {
   showMessage("Welcome to Pizzeria Penguin, what's your name?", 'bot')
 }
@@ -46,38 +47,57 @@ const handleNameInput = (event) => {
 };
 
 nameForm.addEventListener('submit', handleNameInput); 
-
+// User choice of pizza
 const showFoodOptions = (name) => {
-  showMessage(`Hi ${name}, what type of pizza would you like?`, 'bot');
- 
+  showMessage(`Hi ${name}, what type of pizza would you like to order?`, 'bot');
+
  inputWrapper.innerHTML = `
  <button id="margaritaBtn">Margarita</button>
  <button id="pepperoniBtn">Pepperoni</button>
- <button id="hawaiiBtn">Vegetarian</button>
+ <button id="hawaiiBtn">Hawaii</button>
 `;
 document.getElementById('margaritaBtn').addEventListener('click', () => selectPizza('Margarita'));
 document.getElementById('pepperoniBtn').addEventListener('click', () => selectPizza('Pepperoni'));
 document.getElementById('hawaiiBtn').addEventListener('click', () => selectPizza('Hawaii'));
 };
-
-  const selectPizza = (pizzaType) => {
+const selectPizza = (pizzaType) => {
   showMessage(`I have chosen a ${pizzaType} pizza`, 'user');
   showMessage(`${pizzaType}, excellent choice!`, 'bot');
   setTimeout(() => askForDrink(), 1000);
   };
-  const askForDrink = () => {
+// User choice of drink
+const askForDrink = () => {
   showMessage("Please select a drink:", 'bot');
   inputWrapper.innerHTML = `
-   <select id="drink-select">
-     <option value="water">Water</option>
-     <option value="soda">Soda</option>
-     <option value="juice">Juice</option>
-   </select>
-   <button id="drinkBtn">Select</button>
+    <button id="cokeBtn">Coke</button>
+    <button id="fantaBtn">Fanta</button>
   `;
-  
-  document.getElementById('drinkBtn').addEventListener('click', () => selectDrink());
-  };  
+  document.getElementById('cokeBtn').addEventListener('click', () => selectDrink('Coke'));
+  document.getElementById('fantaBtn').addEventListener('click', () => selectDrink('Fanta'));
+};
+
+const selectDrink = (drinkType) => {
+  showMessage(`I have chosen ${drinkType}`, 'user');
+  showMessage(`${drinkType}, perfect choice!`, 'bot');
+  setTimeout(askForAge, 1000); // Anropa askForAge här
+};
+
+// Age of user
+const askForAge = () => {
+  showMessage("Are you an adult or a child?", 'bot');
+  inputWrapper.innerHTML = `
+    <button id="childBtn">👧🏽</button>
+    <button id="adultBtn">🧔🏻‍♂️</button>
+  `;
+  document.getElementById('childBtn').addEventListener('click', () => selectAge('Child'));
+  document.getElementById('adultBtn').addEventListener('click', () => selectAge('Adult'));
+};
+
+const selectAge = (ageType) => {
+  showMessage(`I am a ${ageType}`, 'user');
+  showMessage(`${ageType}, great!`, 'bot');
+
+};
   
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
