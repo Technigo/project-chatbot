57eb32b8427c3e23eaaa73768a18ca8142623d1f
unknown <linda.frischknecht@bluewin.ch>
2024-02-17 18:08:06 +0100

    added last question and fixed my if..else statement

--- code/script.js
+++ code/script.js
@@ -76,36 +76,40 @@ const handleNameInput = (event) => {
 
 submitButton.addEventListener("click", handleNameInput)
 
+let weatherType
 let coldWeather
 let warmWeather
 let hotWeather
+let weatherTypeText =""
 
 const askWeatherType = () => {
   showMessage(`Hi and welcome ${nameUser} to your personal Travel Advisor, what type of weather do you like?`, 'bot')
   inputWrapper.innerHTML = `
-    <button id="cold-weather" type="button">Cold Weather</button>
-    <button id="warm-weather" type="button">Warm Weather</button>
-    <button id="hot-weather" type="button">Hot Weather</button>`
+    <div id="weather-type">
+      <button id="cold-weather" type="button">Cold Weather</button>
+      <button id="warm-weather" type="button">Warm Weather</button>
+      <button id="hot-weather" type="button">Hot Weather</button>
+    </div>`
+  weatherType = document.getElementById("weather-type")
   coldWeather = document.getElementById("cold-weather")
   warmWeather = document.getElementById("warm-weather")
   hotWeather = document.getElementById("hot-weather")
+  weatherTypeText = weatherType.value
   coldWeather.addEventListener("click", () => handleWeatherInput(`Cold Weather`))
   warmWeather.addEventListener("click", () => handleWeatherInput(`Warm Weather`))
   hotWeather.addEventListener("click", () => handleWeatherInput(`Hot Weather`))
 }
 
-let weatherTypeText
-
 const handleWeatherInput = (weatherType) => {
   showMessage(weatherType, 'user')
-  weatherTypeText = weatherType.value
+  weatherTypeText = weatherType
   setTimeout(() => askTripLength(weatherType), 1000)
 }
 
 console.log(askWeatherType)
-//console.log(weatherType)
+console.log(weatherTypeText)
 
-let tripLengthText
+let tripLengthText =""
 
 const askTripLength = (weatherType) => {
   showMessage(`${weatherType} is a great choice! How long do you want your holiday to last?`, 'bot')
@@ -133,36 +137,175 @@ const handleTripInput = (tripLengthText) => {
 
 console.log(handleTripInput)
 
+let groupType
 let soloGroupType
 let partnerGroupType
 let familyGroupType
 let groupGroupType
+let groupTypeText =""
 
 const askGroupType = (tripLengthText) => {
   showMessage(`Your trip will last ${tripLengthText}. What kind of Group will you be travelling with? I will travel...`, 'bot')
   inputWrapper.innerHTML = `
-    <button id="solo" type="button">alone.</button>
-    <button id="partner" type="button">with a partner.</button>
-    <button id="family" type="button">with family.</button>
-    <button id="group" type="button">with a group.</button>`
+    <div id="group-type">
+      <button id="solo" type="button">alone.</button>
+      <button id="partner" type="button">with a partner.</button>
+      <button id="family" type="button">with family.</button>
+      <button id="group" type="button">with a group.</button>
+    </div>`
+  groupType = document.getElementById("group-type")
   soloGroupType = document.getElementById("solo")
   partnerGroupType = document.getElementById("partner")
   familyGroupType = document.getElementById("family")
   groupGroupType = document.getElementById("group")
+  groupTypeText = groupType.value
   soloGroupType.addEventListener("click", () => handleGroupInput(`alone.`))
   partnerGroupType.addEventListener("click", () => handleGroupInput(`with a partner.`))
   familyGroupType.addEventListener("click", () => handleGroupInput(`with family.`))
   groupGroupType.addEventListener("click", () => handleGroupInput(`with a group.`))
 }
 
-let groupType
-
 const handleGroupInput = (groupType) => {
   showMessage(groupType, 'user')
+  groupTypeText = groupType
   setTimeout(() => askPermission(groupType), 1000)
 }
 
-console.log(askGroupType)
+
+const holidayDestination = () => {
+  if (weatherTypeText === "Cold Weather") {
+    if (tripLengthText === "three to four Days") {
+      if (groupTypeText === "alone.") {
+        return ("Denmark")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Finland")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Norway")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Netherlands")
+      }
+    }
+    else if (tripLengthText === "one to two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Faroe Islands")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Iceland")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Ireland")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Estonia")
+      }
+    }
+    else if (tripLengthText === "more than two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Canada")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Alaska")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Canada")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Canada")
+      }
+    }
+  }
+  else if (weatherTypeText === "Warm Weather") {
+    if (tripLengthText === "three to four Days") {
+      if (groupTypeText === "alone.") {
+        return ("Austria")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("France")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Croatia")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Croatia")
+      }
+    }
+    else if (tripLengthText === "one to two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Portugal")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Italy")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Greece")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Spain")
+      }
+    }
+    else if (tripLengthText === "more than two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Greece")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Madeira")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("USA")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("USA")
+      }
+    }
+  }
+  else if (weatherTypeText === "Hot Weather") {
+    if (tripLengthText === "three to four Days") {
+      if (groupTypeText === "alone.") {
+        return ("Türkiye")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Türkiye")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Andorra")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Monaco")
+      }
+    }
+    else if (tripLengthText === "one to two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Morocco")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("United Arab Emirates")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Egypt")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("United Arab Emirates")
+      }
+    }
+    else if (tripLengthText === "more than two Weeks") {
+      if (groupTypeText === "alone.") {
+        return ("Thailand")
+      }
+      else if (groupTypeText === "with a partner.") {
+        return ("Seychelles")
+      }
+      else if (groupTypeText === "with family.") {
+        return ("Maldives")
+      }
+      else if (groupTypeText === "with a group.") {
+        return ("Puerto Rico")
+      }
+    }
+  }
+}
 
 let yesPermission
 let noPermission
@@ -170,22 +313,27 @@ let startOverPermission
 
 const askPermission = () => {
   showMessage(`All right, ${nameUser}! According to your preferences:
-    ${weatherTypeText}
+    ${weatherTypeText}.
     A trip lenght of ${tripLengthText}.
-    And the fact that you are travelling ${groupType}.
-    We will book a trip to XYZ for you.
+    And the fact that you are travelling ${groupTypeText}
+    We will book a trip to ${holidayDestination()} for you.
     Do you want us to book this trip?
-  `)
+  `, 'bot')
   inputWrapper.innerHTML = `
     <button id="yes" type="button">Yes!</button>
     <button id="no" type="button">No.</button>
     <button id="again" type="button">Start over.</button>`
-    yesPermission = document.getElementById("yes")
-    noPermission = document.getElementById("no")
-    startOverPermission = document.getElementById("again")
-    yesPermission.addEventListener("click", () => handlePermissionInput(`Yes!`))
-    noPermission.addEventListener("click", () => handlePermissionInput(`No.`))
-    startOverPermission.addEventListener("click", () => handlePermissionInput(`Start over.`))
+  yesPermission = document.getElementById("yes")
+  noPermission = document.getElementById("no")
+  startOverPermission = document.getElementById("again")
+  yesPermission.addEventListener("click", () => handlePermissionInput(`Yes!`))
+  noPermission.addEventListener("click", () => handlePermissionInput(`No.`))
+  startOverPermission.addEventListener("click", () => handlePermissionInput(`Start over.`))
+  console.log(nameUser)
+  console.log(weatherTypeText)
+  console.log(tripLengthText)
+  console.log(groupTypeText)
+  console.log(holidayDestination())
 }
 
 console.log(askPermission)
