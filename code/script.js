168c2ad2fb4beaa5caa795bae9bed4611f138610
Your El1an3 <eliane_w@yahoo.com>
2024-02-17 17:54:24 +0100

    final version

--- code/script.js
+++ code/script.js
@@ -6,7 +6,7 @@ const sendButton = document.querySelector(`.send-btn`);
 const nameForm = document.getElementById(`name-form`);
 
 let username = "";
-let answerArray = []; // oder über 3 Variablen lösen
+let answerArray = []; // can also use three variables instead
 
 // A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
@@ -42,10 +42,14 @@ const showMessage = (message, sender) => {
 // A function to start the conversation
 const greetUser = () => {
   showMessage(
-    `Hello. I'm Finnly. Nice to meet you. Wanna find out what kind of sea creature you are?`,
+    `Hello there 👋. I'm Finnly. Nice to meet you. Do you want to find out what kind of sea creature you are?`,
     `bot`
   );
-  showMessage(`Let's get started. But first, let me get your name.`, `bot`);
+  showMessage(
+    `Let's get started.
+  But first, let me know your name.`,
+    `bot`
+  );
 };
 
 //invoke the first function to get the chatbot to ask the first question
@@ -57,7 +61,8 @@ const saveUsername = (event) => {
   //store the value in a variable so we can acces it later
   username = nameInput.value;
   showMessage(`Hi, I'm ${username}!`, `user`);
-  chooseIntroExtro();
+  setTimeout(chooseIntroExtro, 2000);
+  //chooseIntroExtro();
 };
 
 // Event listener
@@ -66,10 +71,11 @@ nameForm.addEventListener("submit", saveUsername);
 //bot asks first question, 2 options (introvert vs extrovert) to chose from
 const chooseIntroExtro = () => {
   showMessage(
-    `Nice to meet you ${username}. Let's find out wat sea creature is mostly like you.`,
-    "bot"
+    `Nice to meet you ${username}.
+    Let's find out which marine animal is most like you.`,
+    `bot`
   );
-  showMessage(`Are you more of an introvert or an extrovert?`, "bot");
+  showMessage(`Are you more introverted or extroverted?`, `bot`);
 
   //create new form with buttons for first question (intro/extro)
   inputWrapper.innerHTML = `
@@ -83,24 +89,28 @@ const chooseIntroExtro = () => {
   const extrovertButton = document.getElementById(`extro-btn`);
   //event listeners for buttons
   introvertButton.addEventListener("click", () =>
-    handleSelection1("introvert")
+    handleSelectionIntroExtro("introvert")
   );
   extrovertButton.addEventListener("click", () =>
-    handleSelection1("extrovert")
+    handleSelectionIntroExtro("extrovert")
   );
 };
 
 //function to display selection
-const handleSelection1 = (answer1) => {
-  showMessage(`I'm more of an ${answer1}`, "user");
+const handleSelectionIntroExtro = (answer1) => {
+  showMessage(`I'm more of an ${answer1}`, `user`);
   answerArray[0] = answer1;
   //handle option selection (e.g. move to the next question)
-  chooseDayNight();
+  setTimeout(chooseDayNight, 2000);
 };
 
 //bot asks second question, 2 options (day vs night) to chose from
 const chooseDayNight = () => {
-  showMessage(`Amazing! Now, are you a night owl or a early bird?`, "bot");
+  showMessage(
+    `Amazing!
+  Are you an early bird 🐓 or a night owl 🦉?`,
+    `bot`
+  );
 
   //create new form with buttons for second question (day/night)
   inputWrapper.innerHTML = `
@@ -114,25 +124,28 @@ const chooseDayNight = () => {
   const nightButton = document.getElementById(`night-btn`);
 
   //event listeners for buttons
-  dayButton.addEventListener("click", () => handleSelection2("days"));
-  nightButton.addEventListener("click", () => handleSelection2("nights"));
+  dayButton.addEventListener("click", () => handleSelectionDayNight("days"));
+  nightButton.addEventListener("click", () =>
+    handleSelectionDayNight("nights")
+  );
 };
 
 //function to display selection
-const handleSelection2 = (answer2) => {
+const handleSelectionDayNight = (answer2) => {
   showMessage(`I prefer ${answer2}`, "user");
   answerArray[1] = answer2;
   //handle option selection (e.g. move to the next question)
-  chooseOutIn();
+  setTimeout(chooseOutIn, 2000);
 };
 
 //bot asks third question, 2 options (Stand out vs Blend in) to chose from
 const chooseOutIn = () => {
   showMessage(
-    `Interesting! I have one last question for you, before I assign you a sea creature.`,
-    "bot"
+    `Interesting!
+    I have one last question for you before I assign you a marine animal.`,
+    `bot`
   );
-  showMessage(`Do you rather stand out or blend in?`, "bot");
+  showMessage(`Would you rather stand out or blend in?`, `bot`);
 
   //create new form with buttons for third question (intro/extro)
   inputWrapper.innerHTML = `
@@ -146,15 +159,15 @@ const chooseOutIn = () => {
   const inButton = document.getElementById(`in-btn`);
 
   //event listeners for buttons
-  outButton.addEventListener("click", () => handleSelection3("stand out"));
-  inButton.addEventListener("click", () => handleSelection3("blend in"));
+  outButton.addEventListener("click", () => handleSelectionOutIn("stand out"));
+  inButton.addEventListener("click", () => handleSelectionOutIn("blend in"));
 };
 
-const handleSelection3 = (answer3) => {
-  showMessage(`I ${answer3}`, "user");
+const handleSelectionOutIn = (answer3) => {
+  showMessage(`I ${answer3}`, `user`);
   answerArray[2] = answer3;
   console.log(answerArray);
-  defineSeaCreature();
+  setTimeout(defineSeaCreature, 2000);
 };
 
 //function to define the sea creature based on users selections
@@ -191,7 +204,9 @@ const defineSeaCreature = () => {
               response = "Ghostpipe Fish";
               break;
           }
+          break;
       }
+      break;
     case "extrovert":
       switch (defineDayNight) {
         case "days":
@@ -203,6 +218,7 @@ const defineSeaCreature = () => {
               response = "Dolphin";
               break;
           }
+          break;
         case "nights":
           switch (defineOutIn) {
             case "stand out":
@@ -212,10 +228,53 @@ const defineSeaCreature = () => {
               response = "Manta Ray";
               break;
           }
+          break;
       }
+      break;
     default:
       break;
   }
   //response
-  showMessage(`Turns out you are most likely a ${response}`, "bot");
+  showMessage(`You're most likely a ${response}.`, `bot`);
+
+  setTimeout(exitChat, 2000);
+};
+
+//chatbot solution added
+// Function to exit the chat
+const exitChat = () => {
+  showMessage(
+    `Click YES if you're happy with your result, or NO if you want to start over.`,
+    "bot"
+  );
+  console.log(showMessage);
+
+  // Create buttons for user feedback
+  inputWrapper.innerHTML = `
+  <div id="options">
+    <button id="yes-btn">Yes</button>
+    <button id="no-btn">No</button>
+  </div>`;
+
+  // Get buttons
+  const yesButton = document.getElementById(`yes-btn`);
+  const noButton = document.getElementById(`no-btn`);
+
+  // Event listeners for user feedback
+  yesButton.addEventListener("click", () => handleFeedback("yes"));
+  noButton.addEventListener("click", () => handleFeedback("no"));
+};
+
+// Function to handle user feedback
+const handleFeedback = (feedback) => {
+  if (feedback === "yes") {
+    showMessage(`Bye👋`, "bot");
+    return;
+  } else {
+    showMessage(`Ok, try again 🔝`, "bot");
+    setTimeout(function () {
+      location.reload();
+    }, 4000);
+    console.log(setTimeout);
+  }
 };
