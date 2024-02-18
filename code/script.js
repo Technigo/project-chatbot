e5fa4b59d40333bed41c7b17a65f5330f8ae474d
loren <lorenzo.dastoli@gmail.com>
2024-02-18 22:25:34 +0100

    added the entire code

--- code/script.js
+++ code/script.js
@@ -1,33 +1,216 @@
-// DOM selectors (variables that point to selected DOM elements) goes here 👇
+// All the DOM selectors stored as short variables
 const chat = document.getElementById('chat')
 const sendButton = document.getElementById('send-btn')
 const nameInput = document.getElementById('name-input')
 const form = document.getElementById('name-form')
 
-// Global variables
-let currentQuestionNumber = 1
-let saveEvent = ""
 
-// Functions goes here 👇
-// A function that will add a chat bubble in the correct place based on who the sender is
+// Global variables, if you need any, declared here
+
+let currentQuestionNumber = 1
+let saveEvent = ""  // Saves a value outside the function
+
+// Starts here
+const greeting = () => {
+  showMessage(`Hello there, My name is JetSetBot.  What's your name?`, 'bot')
+}
+setTimeout(greeting, 500)
+
+
+// Set up your eventlisteners here
+sendButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  currentQuestion();
+});
+
+// Functions declared here
+// DECIDES WHICH QUESTION WE ARE HANDLING
+const currentQuestion = () => {
+  if (currentQuestionNumber === 1) {
+    handleNameInput();
+  }
+  else if (currentQuestionNumber === 2) {
+    classQuestion();
+  }
+  else if (currentQuestionNumber === 3) {
+    timeQuestion();
+  }
+  else if (currentQuestionNumber === 4) {
+    confirmQuestion();
+  }
+  else if (currentQuestionNumber === 5) {
+    askForEmail();
+  }
+  else if (currentQuestionNumber === 6) {
+    sayThankYou();
+  }
+  else {
+    console.log('doesntWork')
+  }
+  // Adds a number att current question
+  currentQuestionNumber++
+}
+
+const handleNameInput = () => {
+  const name = nameInput.value;
+  showMessage(name, 'user')
+  nameInput.value = '';
+  setTimeout(() => showCityOptions(name), 500)
+
+  // Function that changes layout to three buttons
+  const showCityOptions = () => {
+    showMessage(`Hi ${name} , Where do you want to go? Have a look at our available destinations.`, 'bot')
+    form.innerHTML =
+      `<div class="btn-wrapper">
+      <button id="london-btn" name="London">LONDON</button>
+      <button id="paris-btn" name="Paris">PARIS</button>
+      <button id="rome-btn" name="Rome" >ROME</button>
+      </div>`
+
+    const londonBth = document.getElementById('london-btn')
+    londonBth.addEventListener('click', () => {
+      const chosenDestination = londonBth.name;
+      saveEvent = londonBth.name;
+      showMessage(chosenDestination, 'user')
+      currentQuestion()
+      setTimeout(() => showMessage(`You have chosen ${chosenDestination}. Are you flying in business or economy?`, 'bot'), 500)
+    })
+
+    const parisBth = document.getElementById('paris-btn')
+    parisBth.addEventListener('click', () => {
+      const chosenDestination = parisBth.name;
+      saveEvent = parisBth.name;
+
+      showMessage(chosenDestination, 'user')
+      currentQuestion()
+      setTimeout(() => showMessage(`You have chosen ${chosenDestination}. Are you flying in business or economy?`, 'bot'), 500)
+    })
+    const romeBth = document.getElementById('rome-btn')
+    romeBth.addEventListener('click', () => {
+      const chosenDestination = romeBth.name;
+      saveEvent = romeBth.name;
+      console.log("This is", romeBth.name)
+      showMessage(chosenDestination, 'user')
+      currentQuestion()
+      setTimeout(() => showMessage(`You have chosen ${chosenDestination}. Are you flying in business or economy?`, 'bot'), 500)
+    })
+  }
+};
+
+const classQuestion = () => {
+  // Function that changes layout to 2 buttons
+  form.innerHTML =
+    `<div class="btn-wrapper">
+    <button id="economy-btn" class="size-btn" name="economy">ECONOMY</button>
+    <button id="business-btn" class="size-btn" name="business">BUSINESS</button>
+    </div>`
+  console.log(form.innerHTML)
+
+  const economyBtn = document.getElementById('economy-btn')
+  economyBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenClass = economyBtn.name;
+    showMessage(chosenClass, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`OK, Economy class it is. Do your prefer a morning or evening flight?`, 'bot'), 500)
+  })
+
+  const businessBtn = document.getElementById('business-btn')
+  businessBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenClass = businessBtn.name;
+    showMessage(chosenClass, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`OK, Business class it is. Do you prefer a morning or evening flight?`, 'bot'), 500)
+  })
+}
+
+const timeQuestion = () => {
+  // Function that changes layout to 2 buttons
+  form.innerHTML =
+    `<div class="btn-wrapper">
+     <button id="morning-btn" class="morningtime-btn" name="morning">MORNING</button>
+     <button id="evening-btn" class="eveningtime-btn" name="evening">EVENING</button>
+    </div>`
+  console.log(form.innerHTML)
+
+  const morningBtn = document.getElementById('morning-btn')
+  morningBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenTime = morningBtn.name;
+    showMessage(chosenTime, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`You have chosen an early flight. Thank you for your selection. Do you want to proceed?`, 'bot'), 500)
+  })
+
+  const eveningBtn = document.getElementById('evening-btn')
+  eveningBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenTime = eveningBtn.name;
+    showMessage(chosenTime, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`You have chosen an evening flight. Thank you for your selection. Do you want to proceed?`, 'bot'), 500)
+  })
+}
+
+const confirmQuestion = () => {
+  // Function that changes layout to 2 buttons
+  form.innerHTML =
+    `<div class="btn-wrapper">
+     <button id="confirm-btn" class="size-btn" name="confirm">CONFIRM</button>
+     <button id="decline-btn" class="size-btn" name="decline">DECLINE</button>
+    </div>`
+  console.log(form.innerHTML)
+
+  const confirmBtn = document.getElementById('confirm-btn')
+  confirmBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenConfirmation = confirmBtn.name;
+    showMessage(chosenConfirmation, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`Please enter your email address to receive a quote`, 'bot'), 500)
+  })
+  const declineBtn = document.getElementById('decline-btn')
+  declineBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const chosenConfirmation = declineBtn.name;
+    showMessage(chosenConfirmation, 'user')
+    currentQuestion()
+    setTimeout(() => showMessage(`You have declined your trip. If you change your mind you know where to find me!`, 'bot'), 500)
+  })
+}
+const askForEmail = () => {
+  form.innerHTML = `
+    <div class="email-input">
+      <label for="email">Please enter your email:</label>
+      <input type="email" id="email" name="email">
+      <button id="email-submit">Submit</button>
+    </div>
+  `;
+  const emailSubmitBtn = document.getElementById('email-submit');
+  emailSubmitBtn.addEventListener('click', () => {
+    const userEmail = document.getElementById('email').value;
+    setTimeout(() => sayThankYou("Thank you for using our service. We've sent more information to your email address.", 'bot'),2000);
+  });
+};
+
+
+// This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
   if (sender === 'user') {
     chat.innerHTML += `
       <section class="user-msg">
+
         <div class="bubble user-bubble">
           <p>${message}</p>
         </div>
-        <img src="assets/user.png" alt="User" />  
+        <img src="assets/user2.png" alt="User" />  
       </section>
     `
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
-        <img src="assets/bot.png" alt="Bot" />
+        <img src="assets/bot1.png" alt="Bot" />
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
@@ -35,30 +218,14 @@ const showMessage = (message, sender) => {
     `
   }
 
-
-  // This little thing makes the chat scroll to the last message when there are too many to
-  // be shown in the chat box
+  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight
 }
 
-// A function to start the conversation
-const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
-
-// Eventlisteners goes here 👇
-sendButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  currentQuestion();
-});
-
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// When website loaded, chatbot asks first question.
+// normally we would invoke a function like this:
+// greeting()
+// But if we want to add a little delay to it, we can wrap it in a setTimeout:
+// setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+
