665a36e517a7d5416b909dfbf33e253214bcd15d
unknown <linda.frischknecht@bluewin.ch>
2024-02-18 12:04:44 +0100

    cleaned up my js code and added the readme file, deleted the pictures i don't use

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # Project Name
 
-Replace this readme with your own information about your project.
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+We had an assignment to make a bot who is asking the user some questions. There should be different input types.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started by testing the code who was already in the script.js file. Than I started adding the questions with the help of functions. In the end I tried to brush up my code by putting all the Dom selectors and global variables to the top and therefore make the code more readable.
 
 ## View it live
 
-Have you deployed your project somewhere? Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+How it looks like:
--- code/script.js
+++ code/script.js
@@ -1,17 +1,19 @@
-// DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
+// DOM selectors goes here 👇
+const chat = document.getElementById("chat")
 const nameInput = document.getElementById("name-input")
 const submitButton = document.getElementById("submit-button")
 const inputWrapper = document.getElementById("input-wrapper")
 
+// Global Variables go here 👇
+let nameUser =""
+let weatherTypeText =""
+let tripLengthText =""
+let groupTypeText =""
+let permissionTypeText = ""
 
 // Functions goes here 👇
-
-// A function that will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  if (sender === "user") {
     console.log(`${message}`)
     chat.innerHTML += `
       <section class="user-msg">
@@ -19,14 +21,8 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
         <img src="assets/user02.png" alt="User" />  
-      </section>
-    `
-
-    //+= means add to it only = would exchange everything. Here we only always want to add things
-
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+      </section>`
+  } else if (sender === "bot") {
     console.log(`${message}`)
     chat.innerHTML += `
       <section class="bot-msg">
@@ -34,92 +30,63 @@ const showMessage = (message, sender) => {
         <div class="bubble bot-bubble">
           <p>${message}</p>
         </div>
-      </section>
-    `
+      </section>`
   }
-
+  //+= means add to it only = would exchange everything.
   // This little thing makes the chat scroll to the last message when there are too many to
   // be shown in the chat box
   chat.scrollTop = chat.scrollHeight
-  console.log(`${message}`)
 }
 
-// A function to start the conversation
 const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
+  showMessage("Hello there, what's your name?", "bot")
 }
-
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
-// This means the greeting function will be called one second after the website is loaded.
 setTimeout(greetUser, 1500)
-
-// Eventlisteners goes here 👇
-
-let nameUser =""
 
 const handleNameInput = (event) => {
   event.preventDefault()
   nameUser = nameInput.value.trim()
   if (nameUser !== "") {
-    showMessage(nameUser, 'user')
+    showMessage(nameUser, "user")
   }
   else {
-    showMessage(`Please enter your name!`, 'bot')
+    showMessage("Please enter your name!", "bot")
     handleNameInput()
   }
   nameInput.value = ""
   setTimeout(() => askWeatherType(), 1000)
-  console.log(nameUser)
 }
-
 //Remember put the setTimeout into the function before the one that should be called afterwards. 
 
-submitButton.addEventListener("click", handleNameInput)
-
-let weatherType
-let coldWeather
-let warmWeather
-let hotWeather
-let weatherTypeText =""
-
 const askWeatherType = () => {
-  showMessage(`Hi and welcome ${nameUser} to your personal Travel Advisor, what type of weather do you like?`, 'bot')
+  showMessage(`Hi and welcome ${nameUser} to your personal Travel Advisor, what type of weather do you like?`, "bot")
   inputWrapper.innerHTML = `
     <div id="weather-type">
       <button id="cold-weather" type="button">Cold Weather</button>
       <button id="warm-weather" type="button">Warm Weather</button>
       <button id="hot-weather" type="button">Hot Weather</button>
     </div>`
-  weatherType = document.getElementById("weather-type")
-  coldWeather = document.getElementById("cold-weather")
-  warmWeather = document.getElementById("warm-weather")
-  hotWeather = document.getElementById("hot-weather")
+    
+  const weatherType = document.getElementById("weather-type")
+  const coldWeather = document.getElementById("cold-weather")
+  const warmWeather = document.getElementById("warm-weather")
+  const hotWeather = document.getElementById("hot-weather")
+
   weatherTypeText = weatherType.value
-  coldWeather.addEventListener("click", () => handleWeatherInput(`Cold Weather`))
-  warmWeather.addEventListener("click", () => handleWeatherInput(`Warm Weather`))
-  hotWeather.addEventListener("click", () => handleWeatherInput(`Hot Weather`))
+
+  coldWeather.addEventListener("click", () => handleWeatherInput("Cold Weather"))
+  warmWeather.addEventListener("click", () => handleWeatherInput("Warm Weather"))
+  hotWeather.addEventListener("click", () => handleWeatherInput("Hot Weather"))
 }
 
 const handleWeatherInput = (weatherType) => {
-  showMessage(weatherType, 'user')
+  showMessage(weatherType, "user")
   weatherTypeText = weatherType
   setTimeout(() => askTripLength(weatherType), 1000)
 }
 
-console.log(askWeatherType)
-console.log(weatherTypeText)
-
-let tripLengthText =""
-
 const askTripLength = (weatherType) => {
-  showMessage(`${weatherType} is a great choice! How long do you want your holiday to last?`, 'bot')
+  showMessage(`${weatherType} is a great choice! How long do you want your holiday to last?`, "bot")
   inputWrapper.innerHTML = `
     <select id="trip-length">
       <option value="" disabled selected hidden>Choose from the following options:</option>
@@ -127,32 +94,22 @@ const askTripLength = (weatherType) => {
       <option value="one to two Weeks">One to two Weeks</option>
       <option value="more than two Weeks">More than two Weeks</option>
     </select>`
+
   const tripLength = document.getElementById("trip-length")
+
   tripLength.addEventListener("change", () => {
     tripLengthText = tripLength.value
     handleTripInput(tripLengthText)
   })
-  console.log(tripLength)
 }
 
-console.log(askTripLength)
-
 const handleTripInput = (tripLengthText) => {
-  showMessage(tripLengthText, 'user')
+  showMessage(tripLengthText, "user")
   setTimeout(() => askGroupType(tripLengthText), 1000)
 }
 
-console.log(handleTripInput)
-
-let groupType
-let soloGroupType
-let partnerGroupType
-let familyGroupType
-let groupGroupType
-let groupTypeText =""
-
 const askGroupType = (tripLengthText) => {
-  showMessage(`Your trip will last ${tripLengthText}. What kind of Group will you be travelling with? I will travel...`, 'bot')
+  showMessage(`Your trip will last ${tripLengthText}. What kind of Group will you be travelling with? I will travel...`, "bot")
   inputWrapper.innerHTML = `
     <div id="group-type">
       <button id="solo" type="button">alone.</button>
@@ -160,24 +117,26 @@ const askGroupType = (tripLengthText) => {
       <button id="family" type="button">with family.</button>
       <button id="group" type="button">with a group.</button>
     </div>`
-  groupType = document.getElementById("group-type")
-  soloGroupType = document.getElementById("solo")
-  partnerGroupType = document.getElementById("partner")
-  familyGroupType = document.getElementById("family")
-  groupGroupType = document.getElementById("group")
+
+  const groupType = document.getElementById("group-type")
+  const soloGroupType = document.getElementById("solo")
+  const partnerGroupType = document.getElementById("partner")
+  const familyGroupType = document.getElementById("family")
+  const groupGroupType = document.getElementById("group")
+
   groupTypeText = groupType.value
-  soloGroupType.addEventListener("click", () => handleGroupInput(`alone.`))
-  partnerGroupType.addEventListener("click", () => handleGroupInput(`with a partner.`))
-  familyGroupType.addEventListener("click", () => handleGroupInput(`with family.`))
-  groupGroupType.addEventListener("click", () => handleGroupInput(`with a group.`))
+
+  soloGroupType.addEventListener("click", () => handleGroupInput("alone."))
+  partnerGroupType.addEventListener("click", () => handleGroupInput("with a partner."))
+  familyGroupType.addEventListener("click", () => handleGroupInput("with family."))
+  groupGroupType.addEventListener("click", () => handleGroupInput("with a group."))
 }
 
 const handleGroupInput = (groupType) => {
-  showMessage(groupType, 'user')
+  showMessage(groupType, "user")
   groupTypeText = groupType
   setTimeout(() => askPermission(groupType), 1000)
 }
-
 
 const holidayDestination = () => {
   if (weatherTypeText === "Cold Weather") {
@@ -314,12 +273,6 @@ const holidayDestination = () => {
   }
 }
 
-let yesPermission
-let noPermission
-let startOverPermission
-let permissionType
-let permissionTypeText = ""
-
 const askPermission = () => {
   showMessage(`All right, ${nameUser}! According to your preferences:
     ${weatherTypeText}.
@@ -327,35 +280,29 @@ const askPermission = () => {
     And the fact that you are travelling ${groupTypeText}
     We will book a trip to ${holidayDestination()} for you.
     Do you want us to book this trip?
-  `, 'bot')
+  `, "bot")
+
   inputWrapper.innerHTML = `
     <div id="permission-type">
       <button id="yes" type="button">Yes!</button>
       <button id="no" type="button">No.</button>
       <button id="again" type="button">Start over.</button>
     </div>`
-  yesPermission = document.getElementById("yes")
-  noPermission = document.getElementById("no")
-  startOverPermission = document.getElementById("again")
-  permissionType = document.getElementById("permission-type")
-  permissionTypeText = permissionType.value
-  yesPermission.addEventListener("click", () => handlePermissionInput(`Yes!`))
-  noPermission.addEventListener("click", () => handlePermissionInput(`No.`))
-  startOverPermission.addEventListener("click", () => handlePermissionInput(`Start over.`))
 
-  console.log(nameUser)
-  console.log(weatherTypeText)
-  console.log(tripLengthText)
-  console.log(groupTypeText)
-  console.log(holidayDestination())
+  const yesPermission = document.getElementById("yes")
+  const noPermission = document.getElementById("no")
+  const startOverPermission = document.getElementById("again")
+  const permissionType = document.getElementById("permission-type")
+
+  permissionTypeText = permissionType.value
+
+  yesPermission.addEventListener("click", () => handlePermissionInput("Yes!"))
+  noPermission.addEventListener("click", () => handlePermissionInput("No."))
+  startOverPermission.addEventListener("click", () => handlePermissionInput("Start over."))
 }
 
-console.log(askPermission)
-
-
-
 const handlePermissionInput = (permissionType) => {
-  showMessage(permissionType, 'user')
+  showMessage(permissionType, "user")
   permissionTypeText = permissionType
   setTimeout(() => confirmChoice(), 1000)
 }
@@ -363,19 +310,17 @@ const handlePermissionInput = (permissionType) => {
 const confirmChoice = () => {
   inputWrapper.innerHTML =""
   if (permissionTypeText === "Yes!") {
-    showMessage(`Thank you for your booking. We are looking forward to seeing you again soon, ${nameUser}!`, 'bot')
+    showMessage(`Thank you for your booking. We are looking forward to seeing you again soon, ${nameUser}!`, "bot")
   }
   else if (permissionTypeText === "No.") {
-    showMessage(`Sadly we can't book your trip this way. Please come back for your next holiday, ${nameUser}`, 'bot')
+    showMessage(`Sadly we can't book your trip this way. Please come back for your next holiday, ${nameUser}`, "bot")
   }
   else if (permissionTypeText === "Start over.") {
-    showMessage(`Your planning will start from the beginning, ${nameUser}`, 'bot')
+    showMessage(`Your planning will start from the beginning, ${nameUser}`, "bot")
     setTimeout(() => askWeatherType(), 1500) 
   }
-  console.log(permissionTypeText) 
-  }
-
-
-//If we create new HTML elements in JS we can't declare them at the top of the file. Do this when working next time to fix the null error! we need to add them inside the function
-
+}
 //Name Function as verbs so it explains what is does
+
+// Eventlisteners goes here 👇
+submitButton.addEventListener("click", handleNameInput)
