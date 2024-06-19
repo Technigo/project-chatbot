2a9097256107b36d6dad02971a8eaf6e7a756e26
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-06-19 22:18:50 +0200

    added step 4 & 5

--- code/script.js
+++ code/script.js
@@ -1,27 +1,18 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
+const chat = document.getElementById("chat")
 const nameForm = document.getElementById("name-form")
-const nameFormChildren = nameForm.querySelectorAll("*"); // all children i formuläret
-let nameInput = document.getElementById('name-input')
-const output = document.getElementById('output')
-const selectbutton = document.getElementById('selectbutton')
+const nameFormChildren = nameForm.querySelectorAll("*") // all children i formuläret
+let nameInput = document.getElementById("name-input")
+const output = document.getElementById("output")
+const selectbutton = document.getElementById("selectbutton")
+const inputWrapper = document.getElementById("input-wrapper")
 
-//OK, so first I selected hard coded buttons from the HTML. Later Pernilla showed how to 
-//get them dynamic by implementing them in the JS file. 
-
-//const buttonJavaScript = document.getElementById('javascript')
-//const buttonPython = document.getElementById('python')
-//const buttonJava = document.getElementById('java')
-
-
-
-
-// Functions goes here 👇 
+// Functions goes here 👇
 // A function that will add a chat bubble in the correct place based on who the sender is
 function showMessage(message, sender) {
   // The if statement checks if the sender is the user and if that's the case it inserts
   // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -32,7 +23,7 @@ function showMessage(message, sender) {
 
     // The else if statement checks if the sender is the bot and if that's the case it inserts
     // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -51,83 +42,144 @@ function showMessage(message, sender) {
 const greetUser = () => {
   // Here we call the function showMessage, that we declared earlier with the argument:
   // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage ("Hello there, what's your name?", 'bot')
-  } 
-
+  showMessage("Hello there, what's your name?", "bot")
+}
 
 //type name in name-input
 
 const handleNameInput = (event) => {
   event.preventDefault()
   const username = nameInput.value
-  showMessage(`Hi, I'm ${username}`, 'user')
-  let input = nameInput.value = ""
- 
-//get "Hello 'name' in return 
-// Set a timer
 
- // callback func ()
- setTimeout(() => {
-  showMessage(
-    `Welcome to week 2, ${username}, nice to meet you. 
+  showMessage(`Hi, I'm ${username}`, "user")
+  let input = (nameInput.value = "")
+
+  //get "Hello 'name' in return
+  // Set a timer
+
+  // callback func ()
+  setTimeout(() => {
+    showMessage(
+      `Welcome to week 2, ${username}, nice to meet you. 
     Please pick a programming language that'll make your brain melt 
     and drip out through your nose`,
-    'bot'
-  );
-}, 1000);
- setTimeout(clickButton, 1000)
- }
-
-
-
+      "bot"
+    )
+  }, 1000)
+  setTimeout(clickButton, 1000)
+}
 
 //when a button is clicked, a message shows accordingly
 const clickButton = () => {
-//console.log('Test')
+  nameFormChildren.forEach((child) => {
+    child.style.display = "none"
+  })
 
-nameFormChildren.forEach((child) => {
-  // Här körs loopen. Där har du tillgång till child, inte utanför loopen. (Pernilla)
-  child.style.display = "none";
-});
-
-
-// Skapa element (Pernillas scope)
-  nameForm.innerHTML += `
+  // User choice of lagnuage
+  nameForm.innerHTML = `
   <button id="javaScript" type="submit" class="choice-btn">JavaScript</button>
   <button id="python" type="submit" class="choice-btn">Python</button>
   <button id="java" type="submit" class="choice-btn">Java</button>
-  `;
+  `
 
-  document.getElementById("javaScript").addEventListener('click', nextFunction);
-  document.getElementById("python").addEventListener('click', nextFunction);
-  document.getElementById("java").addEventListener('click', nextFunction);
+  document.getElementById("javaScript").addEventListener("click", nextFunction)
+  document.getElementById("python").addEventListener("click", nextFunction)
+  document.getElementById("java").addEventListener("click", nextFunction)
 }
-//Here, I want to create a return message for each button id. 
-//Following is my attempt to code this with conditionals:
-
-//if (click === buttonJavaScript){
-//return (showMessage(''))
-//} else if (click === buttonPython){
-//return (showMessage("Ouff.. You're really brave!"))
-//} else if (click === buttonJava){
-//return (showMessage('You got it, tiger.'))}
-
 
 const nextFunction = (e) => {
-  e.preventDefault();
-    showMessage("Attaboy, you got another thing coming!", 'bot'); 
+  e.preventDefault()
+  showMessage("Attaboy, you got another thing coming!", "bot")
+  setTimeout(() => askForLevel(), 1000)
+}
+//ask for the users level of knowledge
+const askForLevel = () => {
+  showMessage("Please let me know your experience in that language", "bot")
+  inputWrapper.innerHTML = `
+  <select id="level-select">
+  <option value="">Select your level</option>
+  <option value="Beginner">Beginner</option>
+  <option value="Intermediate">Intermediate</option>
+  <option value="Advanced">Advanced</option>
+  </select>
+  <button id="levelBtn">Select</button>
+  `
+  document.getElementById("levelBtn").addEventListener("click", selectLevel)
+}
+const selectLevel = () => {
+  const knowledgeLevel = document.getElementById("level-select").value
+
+  if (knowledgeLevel) {
+    showMessage(`You selected: ${knowledgeLevel}`, "user")
+
+    setTimeout(() => {
+      showMessage(
+        `Thank you for letting me know your level is ${knowledgeLevel}. We'll ensure you get a challenge!`,
+        "bot"
+      )
+      setTimeout(() => {
+        askForAge()
+      }, 1500)
+    }, 1000)
+
+    inputWrapper.innerHTML = ""
+  } else {
+    showMessage("You didn't select a level of knowledge", "bot")
+  }
 }
 
-//HERE AS A FINAL MESSAGE:
-//Something like: "prepare for a bumpy ride, friend! Bye bye"
+//Ask the user for their age
+const askForAge = () => {
+  showMessage("Are you younger or older than 20?", "bot")
+  inputWrapper.innerHTML = `
+    <button id="youngerBtn">younger</button>
+    <button id="olderBtn">older</button>
+  `
+  document
+    .getElementById("youngerBtn")
+    .addEventListener("click", () => selectAge("Young"))
+  document
+    .getElementById("olderBtn")
+    .addEventListener("click", () => selectAge("Old"))
+}
+const selectAge = (ageType) => {
+  showMessage(`${ageType}`, "user")
+  const totalPrice = ageType === "Old" ? "2200€" : "1700€"
+  showMessage(
+    `Thank you, the total price for your education is ${totalPrice}. Would you like to proceed?`,
+    "bot"
+  )
+  confirmationOptions()
+}
 
+const confirmationOptions = () => {
+  inputWrapper.innerHTML = `
+    <button id="yesBtn">Yes</button>
+    <button id="noBtn">No</button>
+  `
+  document
+    .getElementById("yesBtn")
+    .addEventListener("click", () => confirmOrder(true))
+  document
+    .getElementById("noBtn")
+    .addEventListener("click", () => confirmOrder(false))
+}
 
-//Pernillas element, saved for another time:
-/* <input type="radio" name="test" id="clickButton" value="clickButton"/><label>Hej</label> */
+const confirmOrder = (isConfirmed) => {
+  if (isConfirmed) {
+    showMessage("Yes", "user")
+    showMessage("Prepare for a bumpy ride, friend!", "bot")
+  } else {
+    showMessage("No", "user")
+    showMessage(
+      "No problem, you are always welcome back to take on a challenge!",
+      "bot"
+    )
+  }
+  inputWrapper.innerHTML = ""
+}
 
-
-
-// Eventlisteners goes here 
+// Eventlisteners goes here
 nameForm.addEventListener("submit", handleNameInput)
 
 //selectbutton.addEventListener("click", clickButton)
@@ -136,8 +188,10 @@ nameForm.addEventListener("submit", handleNameInput)
 // the website is loaded. Normally we invoke functions like this: greeting()
 // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
 // and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// 1.) the function we want to delay, and 2.) the delay in milliseconds
 // This means the greeting function will be called one second after the website is loaded.
 
 setTimeout(greetUser, 1000)
 //setTimeout(showMessage, 2000)
+
+//showMessage("Prepare for a bumpy ride, friend! Bye bye", "bot")
