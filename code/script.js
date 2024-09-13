41878d8e1bb263b7e690e5e5e28316c47b37cb9d
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-09-13 14:14:30 +0200

    Update - Step 3,4 & 5

--- code/script.js
+++ code/script.js
@@ -1,14 +1,47 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
+document.addEventListener("DOMContentLoaded", () => {
 
-// Functions goes here 👇
+  // The form element
+  const form = document.getElementById("name-form");
+  // The input where the name is typed
+  const nameInput = document.getElementById("name-input");
+  // The chat container where messages are shown
+  const chat = document.getElementById("chat");
+  let inputWrapper = document.getElementById("inputWrapper");
 
-// A function that will add a chat bubble in the correct place based on who the sender is
-const showMessage = (message, sender) => {
-  // The if statement checks if the sender is the user and if that's the case it inserts
-  // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
-    chat.innerHTML += `
+  // Functions goes here 👇
+  const handleNameInput = (event) => {
+    event.preventDefault();
+    // Store the value in a variable so we can access it after we
+    // clear it from the input
+    const name = nameInput.value;
+    showMessage(name, "user");
+    nameInput.value = "";
+
+    // After 1 second, show the next question by invoking the next function.
+    // passing the name into it to have access to the user's name if we want
+    // to use it in the next question from the bot.
+    setTimeout(() => showFoodOptions(name), 1000);
+  };
+
+  const showFoodOptions = (name) => {
+    if ((name === "Pizza") || (name === "pizza")) {
+      showMessage("You've ordered a Daily Surprise Pizza! It'll be ready for pick up in 15 minutes, pay upon arrival.<br> Adult size = 150SEK Child size = 100SEK 😋", "bot");
+    } else if ((name === "Pasta") || (name === "pasta")) {
+      showMessage("You've ordered a Daily Surprise Pasta! It'll be ready for pick up in 15 minutes, pay upon arrival.<br> Adult size = 130SEK Child size = 80SEK 😋", "bot");
+    } else if ((name === "Salad") || (name === "salad")) {
+      showMessage("You've ordered a Daily Surprise Salad! It'll be ready for pick up in 15 minutes, pay upon arrival.<br>Adult size = 120SEK Child size = 70SEK 😋", "bot");
+    } else {
+      showMessage(`Hello ${name}, what type of food would you like today?\nPlease write your option:\nPizza, \nPasta\n or Salad 🍽️`, "bot");
+    }
+  }
+
+  // A function that will add a chat bubble in the correct place based on who the sender is
+  const showMessage = (message, sender) => {
+    // The if statement checks if the sender is the user and if that's the case it inserts
+    // an HTML section inside the chat with the posted message from the user
+    if (sender === 'user') {
+      chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
           <p>${message}</p>
@@ -16,12 +49,12 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // The else if statement checks if the sender is the bot and if that's the case it inserts
-    // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
-    console.log(`Welcome to the chat`);
+      // The else if statement checks if the sender is the bot and if that's the case it inserts
+      // an HTML section inside the chat with the posted message from the bot
+    } else if (sender === 'bot') {
+      console.log(`Welcome to the chat`);
 
-    chat.innerHTML += `
+      chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
         <div class="bubble bot-bubble">
@@ -29,27 +62,27 @@ const showMessage = (message, sender) => {
         </div>
       </section>
     `
+    }
+
+    // This little thing makes the chat scroll to the last message when there are too many to
+    // be shown in the chat box
+    chat.scrollTop = chat.scrollHeight
   }
 
-  // This little thing makes the chat scroll to the last message when there are too many to
-  // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+  // A function to start the conversation
+  const greetUser = () => {
+    // Here we call the function showMessage, that we declared earlier with the argument:
+    showMessage("Hello there, welcome to Elina's Pizzeria! What's your name?", "bot")
+  }
 
-// A function to start the conversation
-const greetUser = () => {
-  // Here we call the function showMessage, that we declared earlier with the argument:
-  // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, welcome to Elina's Pizzeria! What's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
+  // Eventlisteners goes here 
+  form.addEventListener("submit", handleNameInput);
 
-// Eventlisteners goes here 👇
-
-// Here we invoke the first function to get the chatbot to ask the first question when
-// the website is loaded. Normally we invoke functions like this: greeting()
-// To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
-// and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+  // Here we invoke the first function to get the chatbot to ask the first question when
+  // the website is loaded. Normally we invoke functions like this: greeting()
+  // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
+  // and pass along two arguments:
+  // 1.) the function we want to delay, and 2.) the delay in milliseconds 
+  // This means the greeting function will be called one second after the website is loaded.
+  setTimeout(greetUser, 1000)
+});
