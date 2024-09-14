371fb227a487ff567f38a333bc940aaab9417caf
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-09-14 19:53:11 +0200

    - added event listener for types of food buttons

--- code/script.js
+++ code/script.js
@@ -28,11 +28,86 @@ const showMessage = (message, sender) => {
 
 //3. Order Food
 
-const orderFood = () => {
-  showMessage(`Great! What would you like to eat?`, "bot")
+const orderFood = (username) => {
+  showMessage(`Great, ${username}! What would you like to eat?`, "bot")
 
+  // Food type choices
+  form.innerHTML += `
+ <div class="foodtype-container" id="foodtype-container">
+        <button class="foodtype-button" id="fish-button" type="button">🐟 Fish</button>
+        <button class="foodtype-button" id="shellfish-button" type="button">🦀 Shellfish</button>
+        <button class="foodtype-button" id="mollusks-button" type="button">🦪 Mollusks</button>
+        <button class="foodtype-button" id="soup-button" type="button">🥣 Soups and Stews</button>
+      </div>
+`
 
+  const fishButton = document.getElementById("fish-button")
+
+  const mollusksButton = document.getElementById("mollusks-button")
+  const soupButton = document.getElementById("soup-button")
+  const foodtype = document.getElementById("foodtype-container")
+  const shellfishContainer = document.getElementById("shellfish-container")
+
+  fishButton.addEventListener("click", () => {
+    showMessage("You like to order fish. Please choose a dish!", "bot")
+    foodtype.style.display = "none"
+
+    // Dish choices for food type fish
+    form.innerHTML += `
+    <div class="dish-container" id="dish-container">
+           <button class="dish-button" id="salmon-button" type="button">Grilled Salmon</button>
+           <button class="dish-button" id="fish-chips-button" type="button">Fish and Chips</button>
+         </div>
+   `
+    //  Event listener für dishes
+
+    const salmonButton = document.getElementById("salmon-button")
+    const dishContainer = document.getElementById("dish-container")
+    const fishChipsButton = document.getElementById("fish-chips-button")
+
+    salmonButton.addEventListener("click", () => {
+      showMessage("Grilled Salmon! Great choice! Thank you for your order!", "bot")
+      dishContainer.style.display = "none"
+    })
+
+    fishChipsButton.addEventListener("click", () => {
+      showMessage("Fish and Chips! Great choice! Thank you for your order!", "bot")
+      dishContainer.style.display = "none"
+    })
+  })
 }
+
+
+// Dish choices for food type shellfish
+// const shellfishButton = document.getElementById("shellfisch-button")
+
+// shellfishButton.addEventListener("click", () => {
+//   showMessage("You like to order shellfish. Please choose a dish!", "bot")
+//   foodtype.style.display = "none"
+
+//   form.innerHTML += `
+//  <div class="dish-container" id="dish-container">
+//         <button class="dish-button" id="shrimp-button" type="button">Shrimp</button>
+//         <button class="dish-button" id="lobster-button" type="button">Lobster</button>
+//       </div>
+// `
+// })
+
+//  Event listener für dishes
+
+// const shrimpButton = document.getElementById("shrimp-button")
+// const lobsterButton = document.getElementById("lobster-button")
+// shrimpButton.addEventListener("click", () => {
+//   showMessage("Shrimp! Great choice! Thank you for your order!", "bot")
+//   dishContainer.style.display = "none"
+// })
+
+// lobsterButton.addEventListener("click", () => {
+//   showMessage("Lobster! Great choice! Thank you for your oder!", "bot")
+//   dishContainer.style.display = "none"
+// })
+
+
 
 
 //2. Asking for the users name and storing the name
@@ -44,24 +119,24 @@ const askName = () => {
   <input class="input" id="name-input"></input>
       <button class="input-button" id="send-button" type="submit">ok</button>
   `
+
   const inputButton = document.getElementById("send-button")
+  const input = document.getElementById('name-input')
+
 
   inputButton.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("toll")
 
-    const input = document.getElementById('name-input')
     const username = input.value
-
 
     showMessage(username, "user")
     input.style.display = "none"
     inputButton.style.display = "none"
-    orderFood()
+    orderFood(username)
   }
   )
 }
-
 
 
 // 1. Greeting and displaying the button
